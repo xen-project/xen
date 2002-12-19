f45220f281121be7d0cb69e170a428ae74622113
lynx@idefix.cl.cam.ac.uk <lynx@idefix.cl.cam.ac.uk>
2002-12-19 11:55:43 +0000

    bitkeeper revision 1.7.3.32 (3e01b3bfwncL6UEh6B6GraNyfKW6Sg)
    debug

--- xenolinux-2.4.16-sparse/arch/xeno/drivers/dom0/dom0_memory.c
+++ xenolinux-2.4.16-sparse/arch/xeno/drivers/dom0/dom0_memory.c
@@ -179,7 +179,8 @@ unsigned long direct_mmap(unsigned long phys_addr, unsigned long size,
     }
 
     /* add node on the list of directly mapped areas */ 
-    dmmap = (direct_mmap_node_t *)kmalloc(GFP_KERNEL, sizeof(direct_mmap_node_t));
+    //dmmap = (direct_mmap_node_t *)kmalloc(GFP_KERNEL, sizeof(direct_mmap_node_t));
+    dmmap = (direct_mmap_node_t *)kmalloc(GFP_KERNEL, 128);
     dmmap->addr = addr;
     list_add(&dmmap->list, &current->mm->context.direct_list);
 
