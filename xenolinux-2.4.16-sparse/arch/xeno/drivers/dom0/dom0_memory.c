57c4047e75ebba9002347473397713bfa386cd11
lynx@idefix.cl.cam.ac.uk <lynx@idefix.cl.cam.ac.uk>
2002-12-26 11:19:15 +0000

    bitkeeper revision 1.7.3.47 (3e0ae5b3dvzaVNlhYDw9iW4oIXXB3A)
    debug

--- xenolinux-2.4.16-sparse/arch/xeno/drivers/dom0/dom0_memory.c
+++ xenolinux-2.4.16-sparse/arch/xeno/drivers/dom0/dom0_memory.c
@@ -62,7 +62,7 @@ static inline void direct_remappte_range(pte_t * pte, unsigned long address, uns
 		pte_t oldpage;
 		oldpage = ptep_get_and_clear(pte);
         
-		//printk(KERN_ALERT "bd240 debug: %lx - %lx\n", pte, phys_addr);
+		printk(KERN_ALERT "bd240 debug: %lx - %lx\n", pte, phys_addr);
 
  		direct_set_pte(pte, direct_mk_pte_phys(phys_addr, prot));
 
