781032d9b13cdaf47f5cea84ad4fabf757198e16
kaf24@scramble.cl.cam.ac.uk <kaf24@scramble.cl.cam.ac.uk>
2003-07-05 14:52:49 +0000

    bitkeeper revision 1.259.2.2 (3f06e641O43LXATNfuodJ2gD211aLg)
    get_unmapped_area.c, dom0_core.c, Rules.mk, Makefile:
      Some small cleanups, particularly to the dom0 /proc code. Pending some big changes there.
    .del-vifinit~c5aa7eadc09606f3:
      Delete: tools/domain_builder/vifinit
    .del-newdom~ae685a05562d2af:
      Delete: tools/domain_builder/newdom
    .del-mynewdom~841547bf5108dbd4:
      Delete: tools/domain_builder/mynewdom
    .del-mem_defs.h~e1a458f2404d3c0c:
      Delete: tools/domain_builder/mem_defs.h
    .del-hypervisor_defs.h~8cb4ad0e3005c361:
      Delete: tools/domain_builder/hypervisor_defs.h
    .del-dom_kill.c~5e62eec71982bbcf:
      Delete: tools/domain_builder/dom_kill.c
    .del-dom_builder.c~36f2cc835770e7:
      Delete: tools/domain_builder/dom_builder.c
    .del-dom0_ops.h~a070a0ff28a546bd:
      Delete: tools/domain_builder/dom0_ops.h
    .del-dom0_defs.h~9b53986c1f64d2d:
      Delete: tools/domain_builder/dom0_defs.h
    .del-README~eb5dc409431f293e:
      Delete: tools/domain_builder/README
    .del-Makefile~a40ae6069befd9b6:
      Delete: tools/domain_builder/Makefile

--- xenolinux-2.4.21-sparse/arch/xeno/mm/get_unmapped_area.c
+++ xenolinux-2.4.21-sparse/arch/xeno/mm/get_unmapped_area.c
@@ -14,60 +14,11 @@
 #include <asm/uaccess.h>
 #include <asm/pgalloc.h>
 
-/*
-static int direct_mapped(unsigned long addr)
-{
-    direct_mmap_node_t * node;
-    struct list_head * curr;
-    struct list_head * direct_list = &current->mm->context.direct_list;
-
-    curr = direct_list->next;
-    while(curr != direct_list){
-        node = list_entry(curr, direct_mmap_node_t, list);
-        if(node->addr == addr)
-            break;
-        curr = curr->next;
-    } 
-
-    if(curr == direct_list)
-        return 0;
-
-    return 1;
-}
-*/
-/*
-unsigned long arch_get_unmapped_area(struct file *filp, unsigned long addr, unsigned long len, unsigned long pgoff, unsigned long flags)
-{
-	struct vm_area_struct *vma;
-
-	if (len > TASK_SIZE)
-		return -ENOMEM;
-
-	if (addr) {
-		addr = PAGE_ALIGN(addr);
-		vma = find_vma(current->mm, addr);
-		if (TASK_SIZE - len >= addr &&
-		    (!vma || addr + len <= vma->vm_start))
-			return addr;
-	}
-	addr = PAGE_ALIGN(TASK_UNMAPPED_BASE);
-
-	for (vma = find_vma(current->mm, addr); ; vma = vma->vm_next) {
-		if (TASK_SIZE - len < addr)
-			return -ENOMEM;
-        
-		if ((!vma || addr + len <= vma->vm_start) && !direct_mapped(addr))
-			return addr;
-		
-        addr = vma->vm_end;
-	}
-}
-*/
 struct list_head *find_direct(struct list_head *list, unsigned long addr)
 {
-	struct list_head * curr;
-	struct list_head * direct_list = &current->mm->context.direct_list;
-	direct_mmap_node_t * node;
+    struct list_head * curr;
+    struct list_head * direct_list = &current->mm->context.direct_list;
+    direct_mmap_node_t * node;
 
     for ( curr = direct_list->next; curr != direct_list; curr = curr->next )
     {
@@ -78,15 +29,18 @@ struct list_head *find_direct(struct list_head *list, unsigned long addr)
     return curr;
 }
 
-unsigned long arch_get_unmapped_area(struct file *filp, unsigned long
-addr, unsigned long len, unsigned long pgoff, unsigned long flags)
+unsigned long arch_get_unmapped_area(struct file *filp, 
+                                     unsigned long addr, 
+                                     unsigned long len, 
+                                     unsigned long pgoff, 
+                                     unsigned long flags)
 {
     struct vm_area_struct *vma;
     direct_mmap_node_t * node;
     struct list_head * curr;
     struct list_head * direct_list = &current->mm->context.direct_list;
 
-    if (len > TASK_SIZE)
+    if ( len > TASK_SIZE )
         return -ENOMEM;
 
     if ( addr )
@@ -113,7 +67,8 @@ addr, unsigned long len, unsigned long pgoff, unsigned long flags)
     {
         if ( TASK_SIZE - len < addr ) return -ENOMEM;
 
-        if ( vma && ((curr == direct_list) || (vma->vm_start < node->vm_start)))
+        if ( vma && ((curr == direct_list) || 
+                     (vma->vm_start < node->vm_start)) )
         {
             /* Do we fit before VMA node? */
             if ( addr + len <= vma->vm_start ) return addr;
