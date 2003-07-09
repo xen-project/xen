ba505e496d5eae13c902996d34426c656b9a5330
sos22@labyrinth.cl.cam.ac.uk <sos22@labyrinth.cl.cam.ac.uk>
2003-07-09 08:28:43 +0000

    bitkeeper revision 1.303.2.1 (3f0bd23bjmByDrErI4CVnaylQxBxhA)
    Whitespace cleanup.

--- tools/internal/xi_phys_probe.c
+++ tools/internal/xi_phys_probe.c
@@ -10,43 +10,41 @@
 
 int main(int argc, char *argv[])
 {
-  physdisk_probebuf_t buf;
-  int fd;
-  int x;
-  char *strbuf;
+    physdisk_probebuf_t buf;
+    int fd;
+    int x;
+    char *strbuf;
 
-  if (argc != 2) {
-    fprintf(stderr, "Usage: xi_phys_probe <domain_nr>\n");
-    return 1;
-  }
-
-  asprintf(&strbuf, "/proc/xeno/dom%s/phd", argv[1]);
-  fd = open(strbuf, O_RDONLY);
-  if (fd < 0) {
-    fprintf(stderr, "Can\'t open %s: %s.\n", strbuf, strerror(errno));
-    return 1;
-  }
-  free(strbuf);
-
-  memset(&buf, 0, sizeof(buf));
-  buf.n_aces = PHYSDISK_MAX_ACES_PER_REQUEST;
-  do {
-    buf.n_aces = PHYSDISK_MAX_ACES_PER_REQUEST;
-    read(fd, &buf, sizeof(buf));
-    if (!buf.n_aces)
-      break;
-
-    for (x = 0; x < buf.n_aces; x++) {
-      char read = ( buf.entries[x].mode & 1 ? 'r' : ' ' );
-      char write = ( buf.entries[x].mode & 2 ? 'w' : ' ' );
-      printf("%x %x %lx %lx %c%c\n", buf.entries[x].device,
-	     buf.entries[x].partition,
-	     buf.entries[x].start_sect,
-	     buf.entries[x].n_sectors,
-	     read,
-	     write);
+    if (argc != 2) {
+	fprintf(stderr, "Usage: xi_phys_probe <domain_nr>\n");
+	return 1;
     }
-    buf.start_ind += buf.n_aces;
-  } while (buf.n_aces == PHYSDISK_MAX_ACES_PER_REQUEST);
-  return 0;
+
+    asprintf(&strbuf, "/proc/xeno/dom%s/phd", argv[1]);
+    fd = open(strbuf, O_RDONLY);
+    if (fd < 0) {
+	fprintf(stderr, "Can\'t open %s: %s.\n", strbuf, strerror(errno));
+	return 1;
+    }
+    free(strbuf);
+
+    memset(&buf, 0, sizeof(buf));
+    buf.n_aces = PHYSDISK_MAX_ACES_PER_REQUEST;
+    do {
+	buf.n_aces = PHYSDISK_MAX_ACES_PER_REQUEST;
+	read(fd, &buf, sizeof(buf));
+	if (!buf.n_aces)
+	    break;
+
+	for (x = 0; x < buf.n_aces; x++) {
+	    char read = (buf.entries[x].mode & 1 ? 'r' : ' ');
+	    char write = (buf.entries[x].mode & 2 ? 'w' : ' ');
+	    printf("%x %x %lx %lx %c%c\n", buf.entries[x].device,
+		   buf.entries[x].partition,
+		   buf.entries[x].start_sect,
+		   buf.entries[x].n_sectors, read, write);
+	}
+	buf.start_ind += buf.n_aces;
+    } while (buf.n_aces == PHYSDISK_MAX_ACES_PER_REQUEST);
+    return 0;
 }
