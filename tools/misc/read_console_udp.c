405bd4170ebcb054da8aa6cb9ca8549c8a9faa31
iap10@labyrinth.cl.cam.ac.uk <iap10@labyrinth.cl.cam.ac.uk>
2003-07-17 09:13:56 +0000

    bitkeeper revision 1.359 (3f1668d45SYPQy-7cLNFlIKxyY1X8w)
    add mkdevnodes and read_console_udp to tools/misc

---
+++ tools/misc/read_console_udp.c
@@ -0,0 +1,53 @@
+/******************************************************************************
+ * Test program for reading console lines from DOM0 port 666.
+ */
+
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    unsigned char buf[208], abuf[32];
+    struct sockaddr_in addr, from;
+    int fromlen = sizeof(from);
+    int len, fd = socket(PF_INET, SOCK_DGRAM, 0);
+    
+    if ( fd < 0 )
+    {
+        fprintf(stderr, "could not open datagram socket\n");
+        return -1;
+    }
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_addr.s_addr = htonl(0xa9fe0100); /* 169.254.1.0 */
+    addr.sin_port = htons(666);
+    addr.sin_family = AF_INET;
+    if ( bind(fd, (struct sockaddr *)&addr, sizeof(addr)) < 0 )
+    {
+        fprintf(stderr, "could not bind to local address and port\n");
+        return -1;
+    }
+
+    while ( (len = recvfrom(fd, buf, sizeof(buf), 0, 
+                            (struct sockaddr *)&from, &fromlen)) 
+            >= 0 )
+    {
+        printf("%d-byte message from %s:%d --\n", len,
+               inet_ntop(AF_INET, &from.sin_addr, abuf, sizeof(abuf)),
+               ntohs(from.sin_port));
+
+        /* For sanity, clean up the string's tail. */
+        if ( buf[len-1] != '\n' ) { buf[len] = '\n'; len++; }
+        buf[len] = '\0';
+
+        printf("%s", buf);
+
+        fromlen = sizeof(from);
+    }
+
+    return 0;
+}
