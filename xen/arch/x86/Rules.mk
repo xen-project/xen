40828c657dd0c9e654d143393b4ca81b6d23ed76
kaf24@scramble.cl.cam.ac.uk <kaf24@scramble.cl.cam.ac.uk>
2004-07-07 17:49:47 +0000

    bitkeeper revision 1.1041.18.1 (40ec37bbEfVooGtkbbrgCf3ZWEhJEg)
    More x86_64 stuff. Now links, but a bunch of stuff is stubbed out.
    Will it run? :-)

--- xen/arch/x86/Rules.mk
+++ xen/arch/x86/Rules.mk
@@ -5,17 +5,16 @@ CC := gcc
 LD := ld
 
 CFLAGS  := -nostdinc -fno-builtin -fno-common -fno-strict-aliasing -O3
-CFLAGS  += -iwithprefix include -Wall -Werror -fomit-frame-pointer
+CFLAGS  += -iwithprefix include -Wall -Werror -fomit-frame-pointer -pipe
 CFLAGS  += -I$(BASEDIR)/include -Wno-pointer-arith -Wredundant-decls
 
-LDFLAGS := -T $(TARGET_SUBARCH)/xen.lds -N 
-
 ifeq ($(TARGET_SUBARCH),x86_32)
-CFLAGS += -m32 -march=i686
-LDARCHFLAGS := --oformat elf32-i386 
+CFLAGS  += -m32 -march=i686
+LDFLAGS := --oformat elf32-i386 
 endif
 
 ifeq ($(TARGET_SUBARCH),x86_64)
-CFLAGS += -m64
-LDARCHFLAGS := --oformat elf64-x86-64
+CFLAGS  += -m64 -mno-red-zone -fpic -fno-reorder-blocks
+CFLAGS  += -fno-asynchronous-unwind-tables
+LDFLAGS := --oformat elf64-x86-64
 endif
