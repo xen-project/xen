6ee6dbde9af835efb040a72146de3b60dbc87c08
cl349@freefall.cl.cam.ac.uk <cl349@freefall.cl.cam.ac.uk>
2004-07-29 13:04:03 +0000

    bitkeeper revision 1.1108.38.1 (4108f5c3GAt0sOF9Rw25ssmCIA9QCg)
    support creating domains from Linux 2.6

---
+++ linux-2.6.7-xen-sparse/include/asm-xen/xen_proc.h
@@ -0,0 +1,13 @@
+
+#ifndef __ASM_XEN_PROC_H__
+#define __ASM_XEN_PROC_H__
+
+#include <linux/config.h>
+#include <linux/proc_fs.h>
+
+extern struct proc_dir_entry *create_xen_proc_entry(
+    const char *name, mode_t mode);
+extern void remove_xen_proc_entry(
+    const char *name);
+
+#endif /* __ASM_XEN_PROC_H__ */
