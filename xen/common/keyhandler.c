8143f51248d2fd8e27afaf49bcee3aea65136f94
maf46@burn.cl.cam.ac.uk <maf46@burn.cl.cam.ac.uk>
2005-02-18 13:32:50 +0000

    bitkeeper revision 1.1205.1.3 (4215ee82GVkS6-XQcYeidECo27K1Qw)
    Added a keybinding for dumping the debugtrace_printf's

--- xen/common/keyhandler.c
+++ xen/common/keyhandler.c
@@ -155,6 +155,13 @@ void do_debug_key(unsigned char key, struct xen_regs *regs)
                              bit. */
 }
 
+#ifndef NDEBUG
+void debugtrace_key(unsigned char key)
+{
+    debugtrace_dump();
+}
+#endif
+
 void initialize_keytable(void)
 {
     open_softirq(KEYPRESS_SOFTIRQ, keypress_softirq);
@@ -176,7 +183,9 @@ void initialize_keytable(void)
 
 #ifndef NDEBUG
     register_keyhandler(
-        'o', audit_domains_key,  "audit domains >0 EXPERIMENTAL"); 
+        'o', audit_domains_key,  "audit domains >0 EXPERIMENTAL");
+    register_keyhandler(
+        'T', debugtrace_key, "dump debugtrace");
 #endif
 
 #ifdef PERF_COUNTERS
