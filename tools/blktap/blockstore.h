f322bdd2673336057219cb9e07d76c09edddc3f6
akw27@arcadians.cl.cam.ac.uk <akw27@arcadians.cl.cam.ac.uk>
2005-03-03 21:13:06 +0000

    bitkeeper revision 1.1236.1.54 (42277de2WK7_Nt8AUEblj6keTcJhBw)
    Fix short int defines to match other fixes to tools tree.
    
    Signed-off-by: andrew.warfield@cl.cam.ac.uk

--- tools/blktap/blockstore.h
+++ tools/blktap/blockstore.h
@@ -9,21 +9,7 @@
 #ifndef __BLOCKSTORE_H__
 #define __BLOCKSTORE_H__
 
-#ifndef __SHORT_INT_TYPES__
-#define __SHORT_INT_TYPES__
-
-#include <stdint.h>
-
-typedef uint8_t            u8;
-typedef uint16_t           u16;
-typedef uint32_t           u32;
-typedef uint64_t           u64;
-typedef int8_t             s8;
-typedef int16_t            s16;
-typedef int32_t            s32;
-typedef int64_t            s64;
-                           
-#endif /*  __SHORT_INT_TYPES__ */
+#include <xc.h>
 
 #define BLOCK_SIZE  4096
 #define BLOCK_SHIFT   12
