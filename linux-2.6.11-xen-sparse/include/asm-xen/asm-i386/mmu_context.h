1cf78bc5f894a0ca7689d88d0277e77dfe70e30d
kaf24@firebug.cl.cam.ac.uk <kaf24@firebug.cl.cam.ac.uk>
2005-04-01 13:57:39 +0000

    bitkeeper revision 1.1236.52.1 (424d53535i5qoHsSZWrLD8tQJZG12w)
    Fix FS/GS saving on Linux 2.6.
    Signed-off-by: Keir Fraser <keir@xensource.com>

--- linux-2.6.11-xen-sparse/include/asm-xen/asm-i386/mmu_context.h
+++ linux-2.6.11-xen-sparse/include/asm-xen/asm-i386/mmu_context.h
@@ -16,11 +16,29 @@ void destroy_context(struct mm_struct *mm);
 
 static inline void enter_lazy_tlb(struct mm_struct *mm, struct task_struct *tsk)
 {
-#ifdef CONFIG_SMP
+#if 0 /* XEN */
 	unsigned cpu = smp_processor_id();
 	if (per_cpu(cpu_tlbstate, cpu).state == TLBSTATE_OK)
 		per_cpu(cpu_tlbstate, cpu).state = TLBSTATE_LAZY;
 #endif
+}
+
+#define prepare_arch_switch(rq,next)	__prepare_arch_switch()
+#define finish_arch_switch(rq, next)	spin_unlock_irq(&(rq)->lock)
+#define task_running(rq, p)		((rq)->curr == (p))
+
+static inline void __prepare_arch_switch(void)
+{
+	/*
+	 * Save away %fs and %gs. No need to save %es and %ds, as those
+	 * are always kernel segments while inside the kernel. Must
+	 * happen before reload of cr3/ldt (i.e., not in __switch_to).
+	 */
+	__asm__ __volatile__ ( "movl %%fs,%0 ; movl %%gs,%1"
+		: "=m" (*(int *)&current->thread.fs),
+		  "=m" (*(int *)&current->thread.gs));
+	__asm__ __volatile__ ( "movl %0,%%fs ; movl %0,%%gs"
+		: : "r" (0) );
 }
 
 static inline void switch_mm(struct mm_struct *prev,
@@ -32,7 +50,7 @@ static inline void switch_mm(struct mm_struct *prev,
 	if (likely(prev != next)) {
 		/* stop flush ipis for the previous mm */
 		cpu_clear(cpu, prev->cpu_vm_mask);
-#ifdef CONFIG_SMP
+#if 0 /* XEN */
 		per_cpu(cpu_tlbstate, cpu).state = TLBSTATE_OK;
 		per_cpu(cpu_tlbstate, cpu).active_mm = next;
 #endif
@@ -47,7 +65,7 @@ static inline void switch_mm(struct mm_struct *prev,
 		if (unlikely(prev->context.ldt != next->context.ldt))
 			load_LDT_nolock(&next->context, cpu);
 	}
-#ifdef CONFIG_SMP
+#if 0 /* XEN */
 	else {
 		per_cpu(cpu_tlbstate, cpu).state = TLBSTATE_OK;
 		BUG_ON(per_cpu(cpu_tlbstate, cpu).active_mm != next);
