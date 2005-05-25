6a7439fcbc72d4a874a96f40e72802c097fb753f
kaf24@firebug.cl.cam.ac.uk <kaf24@firebug.cl.cam.ac.uk>
2005-05-25 17:39:23 +0000

    bitkeeper revision 1.1556 (4294b84bRLO0dqxUhCbkQfM8qt1UoA)
    NR_IRQS should be a bit bigger, in anticipation of PCI-MSI support.
    Signed-off-by: Keir Fraser <keir@xensource.com>

--- xen/include/asm-x86/mach-default/irq_vectors_limits.h
+++ xen/include/asm-x86/mach-default/irq_vectors_limits.h
@@ -1,7 +1,8 @@
 #ifndef _ASM_IRQ_VECTORS_LIMITS_H
 #define _ASM_IRQ_VECTORS_LIMITS_H
 
-#define NR_IRQS        224
+/* Limited by number of trap vectors. */
+#define NR_IRQS        FIRST_SYSTEM_VECTOR
 #define NR_IRQ_VECTORS NR_IRQS
 
 #endif /* _ASM_IRQ_VECTORS_LIMITS_H */
