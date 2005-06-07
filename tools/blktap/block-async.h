bb33743d43018a9c2c0bfa0c2b694b7faf673633
akw27@arcadians.cl.cam.ac.uk <akw27@arcadians.cl.cam.ac.uk>
2005-06-07 15:26:07 +0000

    bitkeeper revision 1.1686.1.1 (42a5bc8fhkR_9WfuD9N-je5TT27yDw)
    Parallax code cleanups.
    
    Signed-off-by: andrew.warfield@cl.cam.ac.uk

--- tools/blktap/block-async.h
+++ tools/blktap/block-async.h
@@ -12,29 +12,29 @@
 
 struct io_ret
 {
-	enum {IO_ADDR_T, IO_BLOCK_T, IO_INT_T} type;
-	union {
-		u64   a;
-		char *b;
-		int   i;
-	} u;
+    enum {IO_ADDR_T, IO_BLOCK_T, IO_INT_T} type;
+    union {
+        u64   a;
+        char *b;
+        int   i;
+    } u;
 };
 
 typedef void (*io_cb_t)(struct io_ret r, void *param);
 
 /* per-vdi lock structures to make sure requests run in a safe order. */
 struct radix_wait {
-	enum {RLOCK, WLOCK} type;
-	io_cb_t  cb;
-	void    *param;
-	struct radix_wait *next;
+    enum {RLOCK, WLOCK} type;
+    io_cb_t  cb;
+    void    *param;
+    struct radix_wait *next;
 };
 
 struct radix_lock {
-	pthread_mutex_t lock;
-	int                    lines[1024];
-	struct radix_wait     *waiters[1024];
-	enum {ANY, READ, STOP} state[1024];
+    pthread_mutex_t lock;
+    int                    lines[1024];
+    struct radix_wait     *waiters[1024];
+    enum {ANY, READ, STOP} state[1024];
 };
 void radix_lock_init(struct radix_lock *r);
 
@@ -49,20 +49,20 @@ void init_block_async(void);
 
 static inline u64 IO_ADDR(struct io_ret r)
 {
-	assert(r.type == IO_ADDR_T);
-	return r.u.a;
+    assert(r.type == IO_ADDR_T);
+    return r.u.a;
 }
 
 static inline char *IO_BLOCK(struct io_ret r)
 {
-	assert(r.type == IO_BLOCK_T);
-	return r.u.b;
+    assert(r.type == IO_BLOCK_T);
+    return r.u.b;
 }
 
 static inline int IO_INT(struct io_ret r)
 {
-	assert(r.type == IO_INT_T);
-	return r.u.i;
+    assert(r.type == IO_INT_T);
+    return r.u.i;
 }
 
 
