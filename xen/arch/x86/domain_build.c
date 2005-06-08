bf3d595a7cec3867897466b940e74352d0486714
kaf24@firebug.cl.cam.ac.uk <kaf24@firebug.cl.cam.ac.uk>
2005-06-08 09:19:22 +0000

    bitkeeper revision 1.1691.1.6 (42a6b81aGnsCnSz81D0LDCSasR3Jyw)
    Export xen_section info from generic Elf parser. Use this to detect
    PAE mismatch between Xen and dom0 image.
    Signed-off-by: Keir Fraser <keir@xensource.com>

--- xen/arch/x86/domain_build.c
+++ xen/arch/x86/domain_build.c
@@ -74,7 +74,7 @@ int construct_dom0(struct domain *d,
                    unsigned long _initrd_start, unsigned long initrd_len,
                    char *cmdline)
 {
-    int i, rc;
+    int i, rc, dom0_pae, xen_pae;
     unsigned long pfn, mfn;
     unsigned long nr_pages;
     unsigned long nr_pt_pages;
@@ -122,7 +122,8 @@ int construct_dom0(struct domain *d,
     unsigned long mpt_alloc;
 
     extern void physdev_init_dom0(struct domain *);
-    extern void translate_l2pgtable(struct domain *d, l1_pgentry_t *p2m, unsigned long l2mfn);
+    extern void translate_l2pgtable(
+        struct domain *d, l1_pgentry_t *p2m, unsigned long l2mfn);
 
     /* Sanity! */
     if ( d->domain_id != 0 ) 
@@ -149,6 +150,21 @@ int construct_dom0(struct domain *d,
 
     if ( (rc = parseelfimage(&dsi)) != 0 )
         return rc;
+
+    if ( dsi.xen_section_string == NULL )
+    {
+        printk("Not a Xen-ELF image: '__xen_guest' section not found.\n");
+        return -EINVAL;
+    }
+
+    dom0_pae = !!strstr(dsi.xen_section_string, "PAE=yes");
+    xen_pae  = (CONFIG_PAGING_LEVELS == 3);
+    if ( dom0_pae != xen_pae )
+    {
+        printk("PAE mode mismatch between Xen and DOM0 (xen=%s, dom0=%s)\n",
+               xen_pae ? "yes" : "no", dom0_pae ? "yes" : "no");
+        return -EINVAL;
+    }
 
     /* Align load address to 4MB boundary. */
     dsi.v_start &= ~((1UL<<22)-1);
