119c5f91624b6bd91db9715e550ca81b1f136317
djm@kirby.fc.hp.com <djm@kirby.fc.hp.com>
2005-06-17 15:34:00 +0000

    bitkeeper revision 1.1713.2.3 (42b2ed68UZ_qqJ3Kcbs6rXI3ICn3SA)
    Add ia64 version of evtchn_notify
    
    Signed-off-by: Matt Chapman <matthewc@hp.com>

--- xen/include/public/arch-ia64.h
+++ xen/include/public/arch-ia64.h
@@ -182,11 +182,11 @@ typedef struct {
 	unsigned long krs[8];	// kernel registers
 	unsigned long pkrs[8];	// protection key registers
 	unsigned long tmp[8];	// temp registers (e.g. for hyperprivops)
+	int evtchn_vector;
 //} PACKED arch_vcpu_info_t;
 } arch_vcpu_info_t;		// DON'T PACK 
 
 typedef struct {
-	int evtchn_vector;
 	int domain_controller_evtchn;
 	unsigned int flags;
 //} PACKED arch_shared_info_t;
@@ -200,12 +200,22 @@ typedef struct vcpu_guest_context {
 
 #endif /* !__ASSEMBLY__ */
 
-#define	XEN_HYPER_RFI			1
-#define	XEN_HYPER_RSM_DT		2
-#define	XEN_HYPER_SSM_DT		3
-#define	XEN_HYPER_COVER			4
-#define	XEN_HYPER_ITC_D			5
-#define	XEN_HYPER_ITC_I			6
-#define	XEN_HYPER_SSM_I			7
+#define	XEN_HYPER_RFI			0x1
+#define	XEN_HYPER_RSM_DT		0x2
+#define	XEN_HYPER_SSM_DT		0x3
+#define	XEN_HYPER_COVER			0x4
+#define	XEN_HYPER_ITC_D			0x5
+#define	XEN_HYPER_ITC_I			0x6
+#define	XEN_HYPER_SSM_I			0x7
+#define	XEN_HYPER_GET_IVR		0x8
+#define	XEN_HYPER_GET_TPR		0x9
+#define	XEN_HYPER_SET_TPR		0xa
+#define	XEN_HYPER_EOI			0xb
+#define	XEN_HYPER_SET_ITM		0xc
+#define	XEN_HYPER_THASH			0xd
+#define	XEN_HYPER_PTC_GA		0xe
+#define	XEN_HYPER_ITR_D			0xf
+#define	XEN_HYPER_GET_RR		0x10
+#define	XEN_HYPER_SET_RR		0x11
 
 #endif /* __HYPERVISOR_IF_IA64_H__ */
