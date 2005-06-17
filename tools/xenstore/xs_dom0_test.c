6e1ff5f262f5c951de00817f6c261fddda6a2113
cl349@firebug.cl.cam.ac.uk <cl349@firebug.cl.cam.ac.uk>
2005-06-17 09:35:26 +0000

    bitkeeper revision 1.1713.3.3 (42b2995e93GNUwPpV6yP21Fl3iIRRQ)
    Makefile:
      Add xs_dom0_test program to test IOCTL_PRIVCMD_INITDOMAIN_STORE.
    xs_dom0_test.c:
      new file
    From: Rusty Russell <rusty@rustcorp.com.au>
    Signed-off-by: Christian Limpach <Christian.Limpach@cl.cam.ac.uk>

---
+++ tools/xenstore/xs_dom0_test.c
@@ -0,0 +1,43 @@
+/* Test introduction of domain 0 */
+#include <linux/ioctl.h>
+#include <sys/ioctl.h>
+#include "xs.h"
+#include "utils.h"
+#include <xc.h>
+#include <xen/linux/privcmd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+int main()
+{
+	int h, local = 0, kernel = 0;
+	long err;
+	void *page;
+
+	h = xc_interface_open();
+	if (h < 0)
+		barf_perror("Failed to open xc");
+
+	if (xc_evtchn_bind_interdomain(h, DOMID_SELF, 0, &local, &kernel) != 0)
+		barf_perror("Failed to bind interdomain");
+
+	printf("Got ports %i & %i\n", local, kernel);
+
+	err = ioctl(h, IOCTL_PRIVCMD_INITDOMAIN_STORE, kernel);
+	if (err < 0)
+		barf_perror("Failed to initialize store");
+	printf("Got mfn %li\n", err);
+
+	page = xc_map_foreign_range(h, 0, getpagesize(), PROT_READ|PROT_WRITE,
+				    err);
+	if (!page)
+		barf_perror("Failed to map page %li", err);
+	printf("Mapped page at %p\n", page);
+	printf("Page says %s\n", (char *)page);
+	munmap(page, getpagesize());
+	printf("unmapped\n");
+	
+	return 0;
+}
+	
