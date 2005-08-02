ecf4de11628b2295a7e9e6e3e4647863db88dc55
kaf24@firebug.cl.cam.ac.uk <kaf24@firebug.cl.cam.ac.uk>
2005-08-02 12:18:47 +0000

    This is the first patch to move the console code out of Xend and into a separate daemon.  In this patch I've included the daemon and a front-end.  I've also have the console code removed from Xend locally but I wanted to clarify a few things first before submitting that.
    Here's how it works:
    
    1) Daemon sits on control channel (via xcs) waiting for console messages
    2) Buffers all console data received
    3) Publishes the location of a tty that can be used for reading in 
    /console/<domid>/tty
    4) Reads a value from the store to determine what the limit of console 
    data should be (/console/<domid>/limit) to prevent DoS attacks
    
    Using it is quite simple, just run consoled and use the xc_console 
    program to connect to the console.  It uses syslog for logging.
    
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

---
+++ tools/consoled/main.c
@@ -0,0 +1,93 @@
+/*\
+ *  Copyright (C) International Business Machines  Corp., 2005
+ *  Author(s): Anthony Liguori <aliguori@us.ibm.com>
+ *
+ *  Xen Console Daemon
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; under version 2 of the License.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+\*/
+
+#include <getopt.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#include "xc.h"
+#include "xen/io/domain_controller.h"
+#include "xcs_proto.h"
+
+#include "utils.h"
+#include "io.h"
+
+int main(int argc, char **argv)
+{
+	const char *sopts = "hVvi";
+	struct option lopts[] = {
+		{ "help", 0, 0, 'h' },
+		{ "version", 0, 0, 'V' },
+		{ "verbose", 0, 0, 'v' },
+		{ "interactive", 0, 0, 'i' },
+		{ 0 },
+	};
+	bool is_interactive = false;
+	int ch;
+	int syslog_option = LOG_CONS;
+	int syslog_mask = LOG_WARNING;
+	int opt_ind = 0;
+
+	while ((ch = getopt_long(argc, argv, sopts, lopts, &opt_ind)) != -1) {
+		switch (ch) {
+		case 'h':
+			//usage(argv[0]);
+			exit(0);
+		case 'V':
+			//version(argv[0]);
+			exit(0);
+		case 'v':
+			syslog_option |= LOG_PERROR;
+			syslog_mask = LOG_DEBUG;
+			break;
+		case 'i':
+			is_interactive = true;
+			break;
+		case '?':
+			fprintf(stderr,
+				"Try `%s --help' for more information\n",
+				argv[0]);
+			exit(EINVAL);
+		}
+	}
+
+	if (geteuid() != 0) {
+		fprintf(stderr, "%s requires root to run.\n", argv[0]);
+		exit(EPERM);
+	}
+
+	openlog("consoled", syslog_option, LOG_DAEMON);
+	setlogmask(syslog_mask);
+
+	if (!is_interactive) {
+		daemonize("/var/run/consoled.pid");
+	}
+
+	xen_setup();
+
+	handle_io();
+
+	closelog();
+
+	return 0;
+}
