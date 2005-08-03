c5a8bdb1df091ec8a440f75b4b45a7a6f8128092
adsharma@xuni-t01.sc.intel.com <adsharma@xuni-t01.sc.intel.com>
2005-08-02 16:25:11 -0800

    Move copy+patched files to linux-xen directory.
    Signed-off-by: Arun Sharma <arun.sharma@intel.com>

--- xen/arch/ia64/Rules.mk
+++ xen/arch/ia64/Rules.mk
@@ -6,8 +6,11 @@ ifneq ($(COMPILE_ARCH),$(TARGET_ARCH))
 CROSS_COMPILE ?= /usr/local/sp_env/v2.2.5/i686/bin/ia64-unknown-linux-
 endif
 AFLAGS  += -D__ASSEMBLY__
-CPPFLAGS  += -I$(BASEDIR)/include -I$(BASEDIR)/include/asm-ia64 \
-             -I$(BASEDIR)/include/asm-ia64/linux -I$(BASEDIR)/arch/ia64/linux
+CPPFLAGS  += -I$(BASEDIR)/include -I$(BASEDIR)/include/asm-ia64 	\
+             -I$(BASEDIR)/include/asm-ia64/linux 			\
+	     -I$(BASEDIR)/include/asm-ia64/linux-xen 			\
+             -I$(BASEDIR)/arch/ia64/linux -I$(BASEDIR)/arch/ia64/linux-xen
+
 CFLAGS  := -nostdinc -fno-builtin -fno-common -fno-strict-aliasing
 #CFLAGS  += -O3		# -O3 over-inlines making debugging tough!
 CFLAGS  += -O2		# but no optimization causes compile errors!
@@ -15,7 +18,9 @@ CFLAGS  += -O2		# but no optimization causes compile errors!
 CFLAGS  += -iwithprefix include -Wall
 CFLAGS  += -fomit-frame-pointer -I$(BASEDIR)/include -D__KERNEL__
 CFLAGS  += -I$(BASEDIR)/include/asm-ia64 -I$(BASEDIR)/include/asm-ia64/linux \
-           -I$(BASEDIR)/arch/ia64/linux -I$(BASEDIR)/arch/ia64
+           -I$(BASEDIR)/include/asm-ia64/linux 				\
+           -I$(BASEDIR)/include/asm-ia64/linux-xen 			\
+           -I$(BASEDIR)/arch/ia64/linux -I$(BASEDIR)/arch/ia64/linux-xen
 CFLAGS  += -Wno-pointer-arith -Wredundant-decls
 CFLAGS  += -DIA64 -DXEN -DLINUX_2_6
 CFLAGS	+= -ffixed-r13 -mfixed-range=f12-f15,f32-f127
