09d42caafdd5b3a05b4ac36ba84f0dabbbaba801
kaf24@firebug.cl.cam.ac.uk <kaf24@firebug.cl.cam.ac.uk>
2005-08-09 13:17:37 +0000

    Add missing LGPL license notice to python sources that didn't have them.
    Signed-off-by: Mike Wray <mike.wray@hp.com>

--- tools/python/xen/xm/main.py
+++ tools/python/xen/xm/main.py
@@ -5,9 +5,18 @@
 #     Sean Dague <sean at dague dot net>
 #     Mike Wray <mike dot wray at hp dot com>
 #
-# This software may be used and distributed according to the terms
-# of the GNU General Public License v2.  Full details on license
-# terms and conditions are included with this distribution
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 """Grand unified management application for Xen.
 """
