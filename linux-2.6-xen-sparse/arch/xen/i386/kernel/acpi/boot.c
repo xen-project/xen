ec401bab1bb58cc64e29daf0e8c1bda0a07eabbc
kaf24@firebug.cl.cam.ac.uk <kaf24@firebug.cl.cam.ac.uk>
2005-08-31 08:43:57 +0000

    Add back missing part of XenLinux acpi/boot.c. This fixes bug #76 (IRQ sharing fails under load). Signed-off-by: Natasha Jarymowycz <natasha@us.ibm.com>

--- linux-2.6-xen-sparse/arch/xen/i386/kernel/acpi/boot.c
+++ linux-2.6-xen-sparse/arch/xen/i386/kernel/acpi/boot.c
@@ -469,6 +469,18 @@ unsigned int acpi_register_gsi(u32 gsi, int edge_level, int active_high_low)
 	unsigned int irq;
 	unsigned int plat_gsi = gsi;
 
+#ifdef CONFIG_PCI
+	/*
+	 * Make sure all (legacy) PCI IRQs are set as level-triggered.
+	 */
+	if (acpi_irq_model == ACPI_IRQ_MODEL_PIC) {
+		extern void eisa_set_level_irq(unsigned int irq);
+
+		if (edge_level == ACPI_LEVEL_SENSITIVE)
+				eisa_set_level_irq(gsi);
+	}
+#endif
+
 #ifdef CONFIG_X86_IO_APIC
 	if (acpi_irq_model == ACPI_IRQ_MODEL_IOAPIC) {
 		plat_gsi = mp_register_gsi(gsi, edge_level, active_high_low);
