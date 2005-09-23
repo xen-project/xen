58fff2a00bfdb74a29f16e29acac3f1089060c70
djm@kirby.fc.hp.com <djm@kirby.fc.hp.com>
2005-09-22 18:05:29 -0600

    First step toward building drivers/xen for ia64 out of sparse

--- linux-2.6-xen-sparse/include/asm-xen/asm-ia64/hypervisor.h
+++ linux-2.6-xen-sparse/include/asm-xen/asm-ia64/hypervisor.h
@@ -45,4 +45,9 @@ void force_evtchn_callback(void);
 
 #include <asm/hypercall.h>
 
+// for drivers/xen/privcmd/privcmd.c
+#define direct_remap_pfn_range(a,b,c,d,e,f) remap_pfn_range(a,b,c,d,e)
+#define	pfn_to_mfn(x)	(x)
+#define machine_to_phys_mapping 0
+
 #endif /* __HYPERVISOR_H__ */
