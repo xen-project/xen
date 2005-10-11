ed7f93c25bb3d01852c2282fa6ff6a39cbd83835
emellor@ewan <emellor@ewan>
2005-10-11 14:57:28 +0100

    Remove bang path.
    Signed-off-by: Ewan Mellor <ewan@xensource.com>

--- tools/python/xen/xend/server/SrvServer.py
+++ tools/python/xen/xend/server/SrvServer.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 #============================================================================
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of version 2.1 of the GNU Lesser General Public
