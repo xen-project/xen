a442cd731d342fbfd989b20eeb3ae78855554673
kaf24@firebug.cl.cam.ac.uk <kaf24@firebug.cl.cam.ac.uk>
2005-10-12 16:15:02 +0100

    Fix 64-bit compile warnings in firmware. Signed-off-by: Keir Fraser <keir@xensource.com>

--- tools/firmware/vmxassist/gen.c
+++ tools/firmware/vmxassist/gen.c
@@ -27,26 +27,26 @@ main(void)
 {
 	printf("/* MACHINE GENERATED; DO NOT EDIT */\n");
 	printf("#define VMX_ASSIST_CTX_GS_SEL	0x%x\n",
-		offsetof(struct vmx_assist_context, gs_sel));
+		(unsigned int)offsetof(struct vmx_assist_context, gs_sel));
 	printf("#define VMX_ASSIST_CTX_FS_SEL	0x%x\n",
-		offsetof(struct vmx_assist_context, fs_sel));
+		(unsigned int)offsetof(struct vmx_assist_context, fs_sel));
 	printf("#define VMX_ASSIST_CTX_DS_SEL	0x%x\n",
-		offsetof(struct vmx_assist_context, ds_sel));
+		(unsigned int)offsetof(struct vmx_assist_context, ds_sel));
 	printf("#define VMX_ASSIST_CTX_ES_SEL	0x%x\n",
-		offsetof(struct vmx_assist_context, es_sel));
+		(unsigned int)offsetof(struct vmx_assist_context, es_sel));
 	printf("#define VMX_ASSIST_CTX_SS_SEL	0x%x\n",
-		offsetof(struct vmx_assist_context, ss_sel));
+		(unsigned int)offsetof(struct vmx_assist_context, ss_sel));
 	printf("#define VMX_ASSIST_CTX_ESP	0x%x\n",
-		offsetof(struct vmx_assist_context, esp));
+		(unsigned int)offsetof(struct vmx_assist_context, esp));
 	printf("#define VMX_ASSIST_CTX_EFLAGS	0x%x\n",
-		offsetof(struct vmx_assist_context, eflags));
+		(unsigned int)offsetof(struct vmx_assist_context, eflags));
 	printf("#define VMX_ASSIST_CTX_CS_SEL	0x%x\n",
-		offsetof(struct vmx_assist_context, cs_sel));
+		(unsigned int)offsetof(struct vmx_assist_context, cs_sel));
 	printf("#define VMX_ASSIST_CTX_EIP	0x%x\n",
-		offsetof(struct vmx_assist_context, eip));
+		(unsigned int)offsetof(struct vmx_assist_context, eip));
 
 	printf("#define VMX_ASSIST_CTX_CR0	0x%x\n",
-		offsetof(struct vmx_assist_context, cr0));
+		(unsigned int)offsetof(struct vmx_assist_context, cr0));
 
 	return 0;
 }
