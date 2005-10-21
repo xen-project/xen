7ebcb094f9f19f8230ac0d72c4b9c53d55ebb51b
kaf24@firebug.cl.cam.ac.uk <kaf24@firebug.cl.cam.ac.uk>
2005-10-21 18:19:38 +0100

    Tweak to pygrub build config detection. Signed-off-by: Jeremy Katz <katzj@redhat.com>

--- tools/pygrub/setup.py
+++ tools/pygrub/setup.py
@@ -13,7 +13,7 @@ if os.path.exists("/usr/include/ext2fs/ext2_fs.h"):
     cc = new_compiler()
     cc.add_library("ext2fs")
     try:
-        if cc.has_function("ext2fs_open2"):
+        if hasattr(cc, "has_function") and cc.has_function("ext2fs_open2"):
             ext2defines.append( ("HAVE_EXT2FS_OPEN2", None) )
         else:
             sys.stderr.write("WARNING: older version of e2fsprogs installed, not building full\n")
