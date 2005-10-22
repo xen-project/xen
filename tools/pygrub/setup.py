44d95809c515f3207960665b9ba1b640168446f7
kaf24@firebug.cl.cam.ac.uk <kaf24@firebug.cl.cam.ac.uk>
2005-10-22 07:35:36 +0100

    Two patches were applied to pygrub's setup.py to work around python2.2 limitations and only one is needed. Revert one of them.
    Signed-Off-By: Muli Ben-Yehuda <mulix@mulix.org>

--- tools/pygrub/setup.py
+++ tools/pygrub/setup.py
@@ -12,14 +12,11 @@ if os.path.exists("/usr/include/ext2fs/ext2_fs.h"):
     ext2defines = []
     cc = new_compiler()
     cc.add_library("ext2fs")
-    try:
-        if hasattr(cc, "has_function") and cc.has_function("ext2fs_open2"):
-            ext2defines.append( ("HAVE_EXT2FS_OPEN2", None) )
-        else:
-            sys.stderr.write("WARNING: older version of e2fsprogs installed, not building full\n")
-            sys.stderr.write("         disk support for ext2.\n")
-    except AttributeError:
-        pass
+    if hasattr(cc, "has_function") and cc.has_function("ext2fs_open2"):
+        ext2defines.append( ("HAVE_EXT2FS_OPEN2", None) )
+    else:
+        sys.stderr.write("WARNING: older version of e2fsprogs installed, not building full\n")
+        sys.stderr.write("         disk support for ext2.\n")
         
     ext2 = Extension("grub.fsys.ext2._pyext2",
                      extra_compile_args = extra_compile_args,
