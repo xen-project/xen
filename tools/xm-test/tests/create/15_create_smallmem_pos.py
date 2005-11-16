cf01e4ff3b92f5adc21d29af644bbebc2936e846
dan@guaranine.beaverton.ibm.com <dan@guaranine.beaverton.ibm.com>
2005-11-16 20:33:12 +0100

    Test for creating a domain with too little memory.

---
+++ tools/xm-test/tests/create/15_create_smallmem_pos.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+# Copyright (C) International Business Machines Corp., 2005
+# Author: Dan Smith <danms@us.ibm.com>
+
+from XmTestLib import *
+
+MEM = 16
+
+domain = XmTestDomain(extraOpts={"memory":"%i" % MEM})
+
+try:
+    domain.start()
+except DomainError, e:
+    FAIL("Unable to start a domain with %i MB" % MEM)
+
+try:
+    console = XmConsole(domain.getName())
+    console.sendInput("input")
+    console.runCmd("ls")
+except ConsoleError, e:
+    if e.reason == RUNAWAY:
+        FAIL("Bug #380: Starting a console with %i MB crashed the console daemon" % MEM)
+    else:
+        FAIL("Starting a console with %i MB failed: domain dies immediately!" % MEM)
+
+domain.destroy()
