119496c00d39110b3597957a01ae69957295dc4f
kaf24@firebug.cl.cam.ac.uk <kaf24@firebug.cl.cam.ac.uk>
2005-11-16 11:29:52 +0100

    Trivial coding style fix.
    Signed-off-by: Vincent Hanquez <vincent@xensource.com>

--- linux-2.6-xen-sparse/drivers/xen/xenbus/xenbus_probe.c
+++ linux-2.6-xen-sparse/drivers/xen/xenbus/xenbus_probe.c
@@ -854,7 +854,7 @@ int register_xenstore_notifier(struct notifier_block *nb)
 {
 	int ret = 0;
 
-        if(xenstored_ready > 0) 
+	if (xenstored_ready > 0) 
 		ret = nb->notifier_call(nb, 0, NULL);
 	else 
 		notifier_chain_register(&xenstore_chain, nb);
