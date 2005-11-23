d29e39f1f4778bc80b528254b436c91efd510dae
root@localhost.localdomain <root@localhost.localdomain>
2005-11-23 12:32:44 +0100

    xm-test: add tests for network-attach command
    Signed-off-by: Murillo Fernandes Bernardes <mfb@br.ibm.com>

---
+++ tools/xm-test/tests/network-attach/04_network_attach_baddomain_neg.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# Copyright (C) International Business Machines Corp., 2005
+# Author: Murillo F. Bernardes <mfb@br.ibm.com>
+
+from XmTestLib import *
+
+status, output = traceCommand("xm network-attach NOT-EXIST")
+
+eyecatcher = "Error"
+where = output.find(eyecatcher)
+if status == 0:
+	FAIL("xm block-attach returned bad status, expected non 0, status is: %i" % status )
+elif where == -1:
+	FAIL("xm block-attach returned bad output, expected Error, output is: %s" % output )
+	
+
