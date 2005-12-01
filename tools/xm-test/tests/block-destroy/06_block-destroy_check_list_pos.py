fff236db8d613bbb575aed86c42940187eab55ce
dan@localhost.localdomain <dan@localhost.localdomain>
2005-12-01 11:15:31 +0000

    Add test for the last unresolved bit of bug #416.  Check that "xm list --long" reflects removed devices.

---
+++ tools/xm-test/tests/block-destroy/06_block-destroy_check_list_pos.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+# Copyright (C) International Business Machines Corp., 2005
+# Author: Dan Smith <danms@us.ibm.com>
+
+from XmTestLib import *
+
+import time
+import re
+
+def checkBlockList(domain):
+    s, o = traceCommand("xm block-list %s" % domain.getName())
+    if s != 0:
+        FAIL("block-list failed")
+    if re.search("769", o):
+        return True
+    else:
+        return False
+
+def checkXmLongList(domain):
+    s, o = traceCommand("xm list --long %s" % domain.getName())
+    if s != 0:
+        FAIL("xm list --long <dom> failed")
+    if re.search("hda1", o):
+        return True
+    else:
+        return False
+
+domain = XmTestDomain()
+
+try:
+    domain.start()
+except DomainError,e:
+    FAIL(str(e))
+
+s, o = traceCommand("xm block-attach %s phy:/dev/ram0 hda1 w" % domain.getName())
+if s != 0:
+    FAIL("block-attach failed")
+
+if not checkBlockList(domain):
+    FAIL("block-list does not show that hda1 was attached")
+
+if not checkXmLongList(domain):
+    FAIL("xm long list does not show that hda1 was attached")
+
+s, o = traceCommand("xm block-detach %s hda1" % domain.getName())
+if s != 0:
+    FAIL("block-detach failed")
+
+time.sleep(2)
+
+if checkBlockList(domain):
+    FAIL("block-list does not show that hda1 was removed")
+
+if checkXmLongList(domain):
+    FAIL("xm long list does not show that hda1 was removed")
+
+
