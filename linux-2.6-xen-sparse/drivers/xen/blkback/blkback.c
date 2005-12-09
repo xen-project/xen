92fb778f0377f2f4636e00f486e4d4231c15cb72
kaf24@firebug.cl.cam.ac.uk <kaf24@firebug.cl.cam.ac.uk>
2005-12-09 09:29:52 +0100

    Fix blkback to build on ia64.
    Signed-off-by: Dan Magenheimer <dan.magenheimer@hp.com>

--- linux-2.6-xen-sparse/drivers/xen/blkback/blkback.c
+++ linux-2.6-xen-sparse/drivers/xen/blkback/blkback.c
@@ -412,7 +412,7 @@ static void dispatch_rw_block_io(blkif_t *blkif,
 
 		pending_handle(pending_req, i) = map[i].handle;
 #ifdef __ia64__
-		pending_vaddrs[vaddr_pagenr(req, seg)] =
+		pending_vaddrs[vaddr_pagenr(pending_req, i)] =
 			= gnttab_map_vaddr(map[i]);
 #else
 		set_phys_to_machine(__pa(vaddr(
