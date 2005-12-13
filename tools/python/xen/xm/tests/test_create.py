a2529a2204c33391451efe15ad18075197a54ca0
emellor@leeni.uk.xensource.com <emellor@leeni.uk.xensource.com>
2005-12-13 18:08:17 +0000

    Added unit test for VMX configuration files.
    Signed-off-by: Ewan Mellor <ewan@xensource.com>

--- tools/python/xen/xm/tests/test_create.py
+++ tools/python/xen/xm/tests/test_create.py
@@ -141,5 +141,55 @@ cpu_weight = 0.75
                  })
             
 
+    def testVMXConfigFile(self):
+        (fd, fname) = tempfile.mkstemp()
+        try:
+            os.write(fd,
+                     '''
+kernel = "/usr/lib/xen/boot/vmxloader"
+builder='vmx'
+memory = 128
+name = "ExampleVMXDomain"
+vcpus=1
+vif = [ 'type=ioemu, bridge=xenbr0' ]
+disk = [ 'file:/var/images/min-el3-i386.img,ioemu:hda,w' ]
+device_model = '/usr/lib/xen/bin/qemu-dm'
+sdl=0
+vnc=1
+vncviewer=1
+ne2000=0
+                     ''')
+        finally:
+            os.close(fd)
+
+        self.t('-f %s display=fakedisplay' % fname,
+               { 'kernel'      : '/usr/lib/xen/boot/vmxloader',
+                 'builder'     : 'vmx',
+                 'memory'      : 128,
+                 'name'        : 'ExampleVMXDomain',
+                 'vcpus'       : 1,
+                 'nics'        : -1,
+                 'vif'         : ['type=ioemu, bridge=xenbr0'],
+                 'disk'        : [['file:/var/images/min-el3-i386.img',
+                                   'ioemu:hda', 'w', None]],
+                 'device_model': '/usr/lib/xen/bin/qemu-dm',
+
+                 'extra'       : ('VNC_VIEWER=%s:%d ' %
+                                  (xen.xm.create.get_host_addr(),
+                                   xen.xm.create.VNC_BASE_PORT + 1)),
+                 'vnc'         : 1,
+                 'vncviewer'   : 1,
+
+                 'xm_file'     : fname,
+                 'defconfig'   : fname,
+                 'display'     : 'fakedisplay',
+
+                 'boot'        : 'c',
+                 'dhcp'        : 'off',
+                 'interface'   : 'eth0',
+                 'path'        : '.:/etc/xen',
+               })
+
+
 def test_suite():
     return unittest.makeSuite(test_create)
