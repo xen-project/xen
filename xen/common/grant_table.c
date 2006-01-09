e962e03d114d0a5f6ad02cca268ab0c63e35c8f1
emellor@leeni.uk.xensource.com <emellor@leeni.uk.xensource.com>
2006-01-09 11:17:55 +0000

    Fix the error recovery at the undo_out label in __gnttab_map_grant_ref to not unlock the spinlock twice (which causes a BUG).
    Signed-off-by: Ewan Mellor <ewan@xensource.com>

--- xen/common/grant_table.c
+++ xen/common/grant_table.c
@@ -312,8 +312,6 @@ __gnttab_map_grant_ref(
     if ( !act->pin )
         clear_bit(_GTF_reading, &sha->flags);
 
-    spin_unlock(&rd->grant_table->lock);
-
  unlock_out:
     spin_unlock(&rd->grant_table->lock);
     (void)__put_user(rc, &uop->status);
