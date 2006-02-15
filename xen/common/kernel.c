71808be4de72228b25e641563e0db7eae606057f
cl349@firebug.cl.cam.ac.uk <cl349@firebug.cl.cam.ac.uk>
2006-02-15 09:44:11 +0000

    Set feature flags for shadow translate mode guests.
    Signed-off-by: Christian Limpach <Christian.Limpach@cl.cam.ac.uk>

--- xen/common/kernel.c
+++ xen/common/kernel.c
@@ -11,6 +11,7 @@
 #include <xen/compile.h>
 #include <xen/sched.h>
 #include <asm/current.h>
+#include <asm/shadow.h>
 #include <public/nmi.h>
 #include <public/version.h>
 
@@ -189,6 +190,10 @@ long do_xen_version(int cmd, void *arg)
         {
         case 0:
             fi.submap = 0;
+            if (shadow_mode_translate(current->domain))
+                fi.submap |= 1 << XENFEAT_writable_page_tables |
+                    1 << XENFEAT_writable_descriptor_tables |
+                    1 << XENFEAT_auto_translated_physmap;
             break;
         default:
             return -EINVAL;
