0552b3845c6e3fe6afdb005a3f52e80cd6851bd2
stekloff@localhost.localdomain <stekloff@localhost.localdomain>
2006-03-10 00:49:54 +0100

    Adding hping TCP and UDP tests. They hping a number of packets to local interface, dom0, and from domU to domU. HVM support included.
    Signed-off-by: Jim Dykman <dykman@us.ibm.com>
    Signed-off-by: Daniel Stekloff <dsteklof@us.ibm.com>

---
+++ tools/xm-test/tests/network/03_network_local_tcp_pos.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+
+# Copyright (C) International Business Machines Corp., 2006
+# Author:  <dykman@us.ibm.com>
+
+# TCP tests on local interfaces.
+#  - creates a single guest domain
+#  - sets up a single NIC
+#  - conducts hping tcp tests to the local loopback and IP address
+
+# hping2 127.0.0.1 -c 1 -d $size
+# hping2 $local_IP -c 1 -d $size
+#   where $size = 1, 48, 64, 512, 1440, 1448, 1500, 1505,
+#                 4096, 4192, 32767, 65507, 65508
+
+
+trysizes = [ 1, 48, 64, 512, 1440, 1448, 1500, 1505, 4096, 4192, 
+              32767, 65495 ]
+
+
+from XmTestLib import *
+rc = 0
+
+Net = XmNetwork()
+
+try:
+    # read an IP address from the config
+    ip   = Net.ip("dom1", "eth0")
+    mask = Net.mask("dom1", "eth0")
+except NetworkError, e:
+    FAIL(str(e))
+
+# Fire up a guest domain w/1 nic
+if ENABLE_HVM_SUPPORT:
+    brg = "xenbr0"
+    config = {"vif" : ['type=ioemu, bridge=%s' % brg]}
+else:
+    brg = None
+    config = {"vif" : ['ip=%s' % ip]}
+
+domain = XmTestDomain(extraConfig=config)
+try:
+    domain.start()
+except DomainError, e:
+    if verbose:
+        print "Failed to create test domain because:"
+        print e.extra
+    FAIL(str(e))
+
+
+# Attach a console
+try:
+    console = XmConsole(domain.getName(), historySaveCmds=True)
+except ConsoleError, e:
+    FAIL(str(e))
+
+try:
+    # Activate the console
+    console.sendInput("bhs")
+
+    # Bring up the "lo" interface.
+    console.runCmd("ifconfig lo 127.0.0.1")
+
+    console.runCmd("ifconfig eth0 inet "+ip+" netmask "+mask+" up")
+
+    # First do loopback 
+    lofails=""
+    for size in trysizes:
+        out = console.runCmd("hping2 127.0.0.1 -E /dev/urandom -q -c 20 " 
+              + "--fast -d " + str(size))
+        if out["return"]:
+            lofails += " " + str(size)
+
+    # Next comes eth0
+    eth0fails=""
+    for size in trysizes:
+        out = console.runCmd("hping2 " + ip + " -E /dev/urandom -q -c 20 "
+              + "--fast -d "+ str(size))
+        if out["return"]:
+            eth0fails += " " + str(size) 
+except ConsoleError, e:
+        FAIL(str(e))
+except NetworkError, e:
+        FAIL(str(e))
+
+
+# Tally up failures
+failures=""
+if len(lofails):
+        failures += "TCP hping2 over loopback failed for size" + lofails + ". "
+if len(eth0fails):
+        failures += "TCP hping2 over eth0 failed for size" + eth0fails + "."
+if len(failures):
+    FAIL(failures)
+
---
+++ tools/xm-test/tests/network/12_network_domU_tcp_pos.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+
+# Copyright (C) International Business Machines Corp., 2006
+# Author:  <dykman@us.ibm.com>
+
+# TCP tests to domU interface
+#  - creates two guest domains
+#  - sets up a single NIC on each on same subnet 
+#  - conducts tcp tests to the domU IP address.
+
+# hping2 $domU_IP -c 1 -d $size 
+#   where $size = 1, 48, 64, 512, 1440, 1500, 1505, 
+#                 4096, 4192, 32767, 65507, 65508
+
+pingsizes = [ 1, 48, 64, 512, 1440, 1500, 1505, 4096, 4192, 16384, 24567, 
+              32767, 65495 ]
+
+from XmTestLib import *
+
+def netDomain(ip):
+    if ENABLE_HVM_SUPPORT:
+        config = {"vif" : ['type=ioemu']}
+    else:
+        config = {"vif"  : ["ip=%s" % ip]}
+
+    dom = XmTestDomain(extraConfig=config)
+    try:
+        dom.start()
+    except DomainError, e:
+        if verbose:
+            print "Failed to create test domain because:"
+            print e.extra
+        FAIL(str(e))
+    try:
+        # Attach a console
+        console = XmConsole(dom.getName(), historySaveCmds=True)
+        # Activate the console
+        console.sendInput("bhs")
+    except ConsoleError, e:
+        FAIL(str(e))
+    return console
+    
+rc = 0
+
+Net = XmNetwork()
+
+try:
+    # pick an IP address 
+    ip1   = Net.ip("dom1", "eth2")
+    mask1 = Net.mask("dom1", "eth2")
+except NetworkError, e:
+    FAIL(str(e))
+
+try:
+    # pick another IP address 
+    ip2   = Net.ip("dom2", "eth2")
+    mask2 = Net.mask("dom2", "eth2")
+except NetworkError, e:
+    FAIL(str(e))
+
+# Fire up a pair of guest domains w/1 nic each
+src_console = netDomain(ip1)
+dst_console = netDomain(ip2)
+
+try:
+    src_console.runCmd("ifconfig eth0 inet "+ip1+" netmask "+mask1+" up")
+    dst_console.runCmd("ifconfig eth0 inet "+ip2+" netmask "+mask2+" up")
+
+    # Ping the victim over eth0
+    fails=""
+    for size in pingsizes:
+        out = src_console.runCmd("hping2 " + ip2 + " -E /dev/urandom -q -c 20 "
+              + "--fast -d " + str(size))
+        if out["return"]:
+            fails += " " + str(size) 
+            print out["output"]
+except ConsoleError, e:
+    FAIL(str(e))
+
+if len(fails):
+    FAIL("TCP hping2 failed for size" + fails + ".")
+
