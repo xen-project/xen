1e0419bfeebed828f4ce9c7569010a6cf050f10c
kaf24@firebug.cl.cam.ac.uk <kaf24@firebug.cl.cam.ac.uk>
2006-03-12 19:37:38 +0100

    No need to pin/unpin intermediate pagetables in x86/64 xenlinux.
    Signed-off-by: Keir Fraser <keir@xensource.com>

--- linux-2.6-xen-sparse/include/asm-i386/mach-xen/asm/hypervisor.h
+++ linux-2.6-xen-sparse/include/asm-i386/mach-xen/asm/hypervisor.h
@@ -79,12 +79,6 @@ void xen_l3_entry_update(pud_t *ptr, pud_t val); /* x86_64/PAE */
 void xen_l4_entry_update(pgd_t *ptr, pgd_t val); /* x86_64 only */
 void xen_pgd_pin(unsigned long ptr);
 void xen_pgd_unpin(unsigned long ptr);
-void xen_pud_pin(unsigned long ptr); /* x86_64 only */
-void xen_pud_unpin(unsigned long ptr); /* x86_64 only */
-void xen_pmd_pin(unsigned long ptr); /* x86_64 only */
-void xen_pmd_unpin(unsigned long ptr); /* x86_64 only */
-void xen_pte_pin(unsigned long ptr);
-void xen_pte_unpin(unsigned long ptr);
 
 void xen_set_ldt(unsigned long ptr, unsigned long bytes);
 void xen_machphys_update(unsigned long mfn, unsigned long pfn);
