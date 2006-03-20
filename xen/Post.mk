2a2926808c0db2dbecb12c8fe533bcbe79e5710b
kaf24@firebug.cl.cam.ac.uk <kaf24@firebug.cl.cam.ac.uk>
2006-03-20 18:36:35 +0100

    Some more build-system finessing:  1. subdirs-y -> subdir-y (match Linux name, and also obj-y     is singular so this makes sense).  2. subdirs can be declared with or without trailing slash  3. As with Linux, subdirs can be declared in the obj-y list     but they must be distinguished by a trailing slash
    Signed-off-by: Keir Fraser <keir@xensource.com>

--- xen/Post.mk
+++ xen/Post.mk
@@ -1,7 +1,16 @@
+# Ensure each subdirectory has exactly one trailing slash.
+subdir-n := $(patsubst %,%/,$(patsubst %/,%,$(subdir-n)))
+subdir-y := $(patsubst %,%/,$(patsubst %/,%,$(subdir-y)))
 
-subdirs-all := $(subdirs-y) $(subdirs-n)
+# Add explicitly declared subdirectories to the object list.
+obj-y += $(patsubst %,%/built_in.o,$(subdir-y))
 
-obj-y += $(patsubst %,%/built_in.o,$(subdirs-y))
+# Add implicitly declared subdirectories (in the object list) to the
+# subdirectory list, and rewrite the object-list entry.
+subdir-y += $(filter %/,$(obj-y))
+obj-y    := $(patsubst %/,%/built-in.o,$(obj-y))
+
+subdir-all := $(subdir-y) $(subdir-n)
 
 built_in.o: $(obj-y)
 	$(LD) $(LDFLAGS) -r -o $@ $^
@@ -12,7 +21,7 @@ FORCE:
 %/built_in.o: FORCE
 	$(MAKE) -C $*
 
-clean:: $(addprefix _clean_, $(subdirs-all)) FORCE
+clean:: $(addprefix _clean_, $(subdir-all)) FORCE
 	rm -f *.o *~ core
 _clean_%/: FORCE
 	$(MAKE) -C $* clean
