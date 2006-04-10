5dfbb5affbd3e7ba8fd8493da4325d33915465d8
kaf24@firebug.cl.cam.ac.uk <kaf24@firebug.cl.cam.ac.uk>
2006-04-10 13:24:58 +0100

    'make mkpatches' diffs against pristine tree rather than ref tree, so it includes extra patches under patches/ subdirectory.
    Signed-off-by: Keir Fraser <keir@xensource.com>

--- buildconfigs/Rules.mk
+++ buildconfigs/Rules.mk
@@ -124,14 +124,14 @@ linux-2.6-xen.patch: ref-linux-$(LINUX_VER)/.valid-ref
 	rm -rf tmp-$@
 	cp -al $(<D) tmp-$@
 	( cd linux-2.6-xen-sparse && ./mkbuildtree ../tmp-$@ )	
-	diff -Nurp $(<D) tmp-$@ > $@ || true
+	diff -Nurp $(patsubst ref%,pristine%,$(<D)) tmp-$@ > $@ || true
 	rm -rf tmp-$@
 
 %-xen.patch: ref-%/.valid-ref
 	rm -rf tmp-$@
 	cp -al $(<D) tmp-$@
 	( cd $*-xen-sparse && ./mkbuildtree ../tmp-$@ )	
-	diff -Nurp $(<D) tmp-$@ > $@ || true
+	diff -Nurp $(patsubst ref%,pristine%,$(<D)) tmp-$@ > $@ || true
 	rm -rf tmp-$@
 
 %-mrproper: %-mrproper-extra
