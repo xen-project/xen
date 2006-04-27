02842d8a2b844d2d0b1421af7b2448ee8dd37bdf
kaf24@firebug.cl.cam.ac.uk <kaf24@firebug.cl.cam.ac.uk>
2006-04-27 14:03:22 +0100

    Bump Xen interface version number to 0x00030201 for new structural guest handles. Signed-off-by: Keir Fraser <keir@xensource.com>

--- xen/include/public/xen-compat.h
+++ xen/include/public/xen-compat.h
@@ -9,7 +9,7 @@
 #ifndef __XEN_PUBLIC_XEN_COMPAT_H__
 #define __XEN_PUBLIC_XEN_COMPAT_H__
 
-#define __XEN_LATEST_INTERFACE_VERSION__ 0x00030101
+#define __XEN_LATEST_INTERFACE_VERSION__ 0x00030201
 
 #if defined(__XEN__)
 /* Xen is built with matching headers and implements the latest interface. */
@@ -23,9 +23,17 @@
 #error "These header files do not support the requested interface version."
 #endif
 
+/* New sched_op hypercall introduced in 0x00030101. */
 #if __XEN_INTERFACE_VERSION__ < 0x00030101
 #undef __HYPERVISOR_sched_op
 #define __HYPERVISOR_sched_op __HYPERVISOR_sched_op_compat
+#endif
+
+/* Structural guest handles introduced in 0x00030201. */
+#if __XEN_INTERFACE_VERSION__ < 0x00030201
+#undef __DEFINE_XEN_GUEST_HANDLE
+#define __DEFINE_XEN_GUEST_HANDLE(name, type) \
+    typedef type * __guest_handle_ ## name
 #endif
 
 #endif /* __XEN_PUBLIC_XEN_COMPAT_H__ */
