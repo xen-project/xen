e0d136944face7cafb0f2e6ab5758cec17269933
kfraser@localhost.localdomain <kfraser@localhost.localdomain>
2006-07-05 10:32:33 +0100

    Update xen caps to indicate that HVM IA32 PAE guest on PAE host is available. Signed-off-by: Xin Li <xin.b.li@intel.com>

--- xen/arch/x86/setup.c
+++ xen/arch/x86/setup.c
@@ -624,7 +624,7 @@ void arch_get_xen_caps(xen_capabilities_info_t info)
     if ( hvm_enabled )
     {
         p += sprintf(p, "hvm-%d.%d-x86_32 ", XEN_VERSION, XEN_SUBVERSION);
-        //p += sprintf(p, "hvm-%d.%d-x86_32p ", XEN_VERSION, XEN_SUBVERSION);
+        p += sprintf(p, "hvm-%d.%d-x86_32p ", XEN_VERSION, XEN_SUBVERSION);
     }
 
 #elif defined(CONFIG_X86_64)
