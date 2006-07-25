250887c826b082fd060ab611350210df718fd21b
awilliam@xenbuild.aw <awilliam@xenbuild.aw>
2006-07-25 09:51:50 -0600

    [IA64] remove unnecessary panic_domain() declarations
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>

--- xen/arch/ia64/xen/vcpu.c
+++ xen/arch/ia64/xen/vcpu.c
@@ -30,8 +30,6 @@ extern void getfpreg (unsigned long regnum, struct ia64_fpreg *fpval, struct pt_
 
 extern void setfpreg (unsigned long regnum, struct ia64_fpreg *fpval, struct pt_regs *regs);
 
-extern void panic_domain(struct pt_regs *, const char *, ...);
-
 typedef	union {
 	struct ia64_psr ia64_psr;
 	unsigned long i64;
--- xen/include/asm-ia64/vmx.h
+++ xen/include/asm-ia64/vmx.h
@@ -37,7 +37,6 @@ extern void vmx_load_state(struct vcpu *v);
 extern void vmx_setup_platform(struct domain *d);
 extern void vmx_wait_io(void);
 extern void vmx_io_assist(struct vcpu *v);
-extern void panic_domain(struct pt_regs *regs, const char *fmt, ...);
 extern int ia64_hypercall (struct pt_regs *regs);
 extern void vmx_save_state(struct vcpu *v);
 extern void vmx_load_state(struct vcpu *v);
