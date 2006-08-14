318ac791f9f91fa99ea92bee051e144874bdcb00
kfraser@localhost.localdomain <kfraser@localhost.localdomain>
2006-08-14 17:28:25 +0100

    Add utilities needed for SMBIOS generation to hvmloader. Signed-off-by: Andrew D. Ball <aball@us.ibm.com>

--- tools/firmware/hvmloader/hvmloader.c
+++ tools/firmware/hvmloader/hvmloader.c
@@ -116,15 +116,6 @@ check_amd(void)
 }
 
 static void
-cpuid(uint32_t idx, uint32_t *eax, uint32_t *ebx, uint32_t *ecx, uint32_t *edx)
-{
-	__asm__ __volatile__(
-		"cpuid"
-		: "=a" (*eax), "=b" (*ebx), "=c" (*ecx), "=d" (*edx)
-		: "0" (idx) );
-}
-
-static void
 wrmsr(uint32_t idx, uint64_t v)
 {
 	__asm__ __volatile__(
--- tools/firmware/hvmloader/util.h
+++ tools/firmware/hvmloader/util.h
@@ -8,9 +8,21 @@ void outb(uint16_t addr, uint8_t val);
 /* I/O input */
 uint8_t inb(uint16_t addr);
 
+/* Do cpuid instruction, with operation 'idx' */
+void cpuid(uint32_t idx, uint32_t *eax, uint32_t *ebx,
+           uint32_t *ecx, uint32_t *edx);
+
+/* Return number of vcpus. */
+int get_vcpu_nr(void);
+
 /* String and memory functions */
 int strcmp(const char *cs, const char *ct);
+char *strcpy(char *dest, const char *src);
+char *strncpy(char *dest, const char *src, unsigned n);
+unsigned strlen(const char *s);
+int memcmp(const void *s1, const void *s2, unsigned n);
 void *memcpy(void *dest, const void *src, unsigned n);
+void *memset(void *s, int c, unsigned n);
 char *itoa(char *a, unsigned int i);
 
 /* Debug output */
