429cd011b1788fd9697fe68f5c73a94e7ac6d84e
Jimi Xenidis <jimix@watson.ibm.com>
2006-09-01 13:14:53 -0400

    [POWERPC][XEN] Make many of the timbase routine static inline
    Signed-off-by: Jimi Xenidis <jimix@watson.ibm.com>
    Signed-off-by: Hollis Blanchard <hollisb@us.ibm.com>

--- xen/include/asm-powerpc/time.h
+++ xen/include/asm-powerpc/time.h
@@ -13,15 +13,19 @@
  * along with this program; if not, write to the Free Software
  * Foundation, 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (C) IBM Corp. 2005
+ * Copyright (C) IBM Corp. 2005, 2006
  *
  * Authors: Hollis Blanchard <hollisb@us.ibm.com>
+ *          Hollis Blanchard <jimix@watson.ibm.com>
  */
 
 #ifndef _ASM_TIME_H_
 #define _ASM_TIME_H_
 
 #include <xen/types.h>
+#include <xen/time.h>
+#include <xen/percpu.h>
+#include <asm/processor.h>
 
 extern unsigned int timebase_freq;
 #define CLOCK_TICK_RATE timebase_freq
@@ -29,7 +33,26 @@ extern unsigned int timebase_freq;
 #define watchdog_disable() ((void)0)
 #define watchdog_enable()  ((void)0)
 
-extern u64 get_timebase(void);
+static inline u64 get_timebase(void)
+{
+    u64 s;
+
+#ifdef __PPC64__
+    s = mftb();
+#else
+    do {
+        unsigned up;
+        unsigned lo;
+        unsigned up2;
+
+        up = mftbu();
+        lo = mftbl();
+        up2 = mftbu();
+    } while (up1 != up2);
+    s = ((ulong)up << 32) | lo;
+#endif
+    return s;
+}
 
 typedef u64 cycles_t;
 static inline cycles_t get_cycles(void)
@@ -39,4 +62,15 @@ static inline cycles_t get_cycles(void)
     return c;
 }
 
+#define __nano(s) ((s) * 1000000000ULL)
+
+static inline u64 ns_to_tb(u64 ns)
+{
+    return (ns * timebase_freq) / __nano(1);
+}
+
+static inline u64 tb_to_ns(u64 tb)
+{
+    return tb * (__nano(1) / timebase_freq);
+}
 #endif
