3714ab433f4dd19572b7ce3ff431b6be3420f2d7
Ewan Mellor <ewan@xensource.com>
2006-09-15 23:54:04 +0100

    Revert change to vtpm_utils related to skipping the test when the tpm is not found -- we need to skip the test on non-TPM platforms, of course.
    Signed-off-by: Ewan Mellor <ewan@xensource.com>

--- tools/xm-test/tests/vtpm/vtpm_utils.py
+++ tools/xm-test/tests/vtpm/vtpm_utils.py
@@ -8,6 +8,9 @@ from XmTestLib import *
 if ENABLE_HVM_SUPPORT:
     SKIP("vtpm tests not supported for HVM domains")
 
+if not os.path.exists("/dev/tpm0"):
+    SKIP("This machine has no hardware TPM; cannot run this test")
+
 status, output = traceCommand("ps aux | grep vtpm_manager | grep -v grep")
 if output == "":
     FAIL("virtual TPM manager must be started to run this test")
