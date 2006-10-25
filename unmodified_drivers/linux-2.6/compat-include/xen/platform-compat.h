6b8b36040d478d336d3c93e87dec37594cc64a79
Ian Campbell <ian.campbell@xensource.com>
2006-10-25 13:58:30 +0100

    PV-to-HVM: Add compatibility definitions of various #defines when they are not present in the kernel we are building against.
    Signed-off-by: Ian Campbell <ian.campbell@xensource.com>
    Signed-off-by: K. Y. Srinivasan <ksrinivasan@novell.com>
    Signed-off-by: Tsunehisa Doi <Doi.Tsunehisa@jp.fujitsu.com>

--- unmodified_drivers/linux-2.6/compat-include/xen/platform-compat.h
+++ unmodified_drivers/linux-2.6/compat-include/xen/platform-compat.h
@@ -3,4 +3,26 @@
 
 #include <linux/version.h>
 
+#include <linux/spinlock.h>
+
+#if defined(__LINUX_COMPILER_H) && !defined(__always_inline)
+#define __always_inline inline
+#endif
+
+#if defined(__LINUX_SPINLOCK_H) && !defined(DEFINE_SPINLOCK)
+#define DEFINE_SPINLOCK(x) spinlock_t x = SPIN_LOCK_UNLOCKED
+#endif
+
+#if defined(_LINUX_INIT_H) && !defined(__init)
+#define __init
+#endif
+
+#if defined(__LINUX_CACHE_H) && !defined(__read_mostly)
+#define __read_mostly
+#endif
+
+#if defined(_LINUX_SKBUFF_H) && !defined(NET_IP_ALIGN)
+#define NET_IP_ALIGN 0
+#endif
+
 #endif
