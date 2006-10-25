f3bebe5fecaa3bcee1f21fe528c0de85725d7715
Ian Campbell <ian.campbell@xensource.com>
2006-10-25 13:58:30 +0100

    PV-on-HVM: Define system_state as 1 (SYSTEM_RUNNING) on kernels prior to 2.6.7. These kernels did not export the system_state variable to modules.
    Signed-off-by: Ian Campbell <ian.campbell@xensource.com>

--- unmodified_drivers/linux-2.6/platform-pci/platform-compat.c
+++ unmodified_drivers/linux-2.6/platform-pci/platform-compat.c
@@ -1,4 +1,11 @@
 #include <linux/config.h>
 #include <linux/version.h>
 
+#include <linux/module.h>
+
 #include <xen/platform-compat.h>
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,7)
+static int system_state = 1;
+EXPORT_SYMBOL(system_state);
+#endif
