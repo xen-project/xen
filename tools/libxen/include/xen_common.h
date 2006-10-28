98fce5e002975de28d5ecbd7ee76ec142cbbf2a2
Ewan Mellor <ewan@xensource.com>
2006-10-28 11:20:47 +0100

    Tidy.
    Signed-off-by: Ewan Mellor <ewan@xensource.com>

--- tools/libxen/include/xen_common.h
+++ tools/libxen/include/xen_common.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2006 XenSource, Inc.
+ * Copyright (c) 2006 XenSource, Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
-*/
+ */
 
 #ifndef XEN_COMMON_H
 #define XEN_COMMON_H
