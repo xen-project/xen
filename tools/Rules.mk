2f760bd64d513f774358017404b34920226be6e0
kfraser@localhost.localdomain <kfraser@localhost.localdomain>
2006-12-15 11:53:45 +0000

    [TOOLS] USe -mno-direct-seg-refs when building on i386. Signed-off-by: Keir Fraser <keir@xensource.com>

--- tools/Rules.mk
+++ tools/Rules.mk
@@ -19,6 +19,10 @@ CFLAGS  += $(shell getconf LFS_CFLAGS)
 CFLAGS  += -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE
 LDFLAGS += $(shell getconf LFS_LDFLAGS)
 
+# 32-bit x86 does not perform well with -ve segment accesses on Xen.
+CFLAGS-$(CONFIG_X86_32) += $(call cc-option,$(CC),-mno-tls-direct-seg-refs)
+CFLAGS += $(CFLAGS-y)
+
 %.opic: %.c
 	$(CC) $(CPPFLAGS) -DPIC $(CFLAGS) -fPIC -c -o $@ $<
 
