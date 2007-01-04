7c6b36118d37cfe5d50bcabc32b6d28d99aadc11
Christian Limpach <Christian.Limpach@xensource.com>
2007-01-04 13:05:09 +0000

    [linux] Remove some gratuitous differences with plain 2.6.16.33.
    Signed-off-by: Christian Limpach <Christian.Limpach@xensource.com>

--- linux-2.6-xen-sparse/net/core/dev.c
+++ linux-2.6-xen-sparse/net/core/dev.c
@@ -1248,14 +1248,13 @@ static int dev_gso_segment(struct sk_buff *skb)
 	/* Verifying header integrity only. */
 	if (!segs)
 		return 0;
-
+  
 	if (unlikely(IS_ERR(segs)))
 		return PTR_ERR(segs);
 
 	skb->next = segs;
 	DEV_GSO_CB(skb)->destructor = skb->destructor;
 	skb->destructor = dev_gso_skb_destructor;
-
 	return 0;
 }
 
