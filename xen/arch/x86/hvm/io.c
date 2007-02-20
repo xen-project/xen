3482c7ac5ecfa6b9a86a8cf34719f1e1c5b2ebc4
Tim Deegan <Tim.Deegan@xensource.com>
2007-02-20 20:02:49 +0000

    [HVM] Fix MMIO LODS emulation Signed-off-by: Tim Deegan <Tim.Deegan@xensource.com>

--- xen/arch/x86/hvm/io.c
+++ xen/arch/x86/hvm/io.c
@@ -533,6 +533,7 @@ static void hvm_mmio_assist(struct cpu_user_regs *regs, ioreq_t *p,
         break;
 
     case INSTR_LODS:
+        set_reg_value(size, 0, 0, regs, p->data);
         sign = p->df ? -1 : 1;
         regs->esi += sign * p->count * p->size;
         if (mmio_opp->flags & REPZ)
