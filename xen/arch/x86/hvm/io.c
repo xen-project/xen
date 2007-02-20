901eef1bb131604e320558f92bc3dd9d45210096
Tim Deegan <Tim.Deegan@xensource.com>
2007-02-20 20:49:44 +0000

    [HVM] Fix MMIO AND emulation which was falling though into AND... Signed-off-by: Tim Deegan <Tim.Deegan@xensource.com>

--- xen/arch/x86/hvm/io.c
+++ xen/arch/x86/hvm/io.c
@@ -554,6 +554,7 @@ static void hvm_mmio_assist(struct cpu_user_regs *regs, ioreq_t *p,
             diff = (unsigned long) p->data & value;
             set_reg_value(size, index, 0, regs, diff);
         }
+        break;
 
     case INSTR_ADD:
         if (src & REGISTER) {
