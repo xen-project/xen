4af35bad437f20acacf65b18cb8d0eff4d5de089
Tim Deegan <Tim.Deegan@xensource.com>
2007-02-22 12:21:29 +0000

    [XEN] don't emulate stack operations on shadowed pages Signed-off-by: Tim Deegan <Tim.Deegan@xensource.com>

--- xen/arch/x86/mm/shadow/multi.c
+++ xen/arch/x86/mm/shadow/multi.c
@@ -2902,7 +2902,23 @@ static int sh_page_fault(struct vcpu *v,
 
     if ( is_hvm_domain(d) )
         hvm_store_cpu_guest_regs(v, regs, NULL);
-    SHADOW_PRINTK("emulate: eip=%#lx\n", (unsigned long)regs->eip);
+    SHADOW_PRINTK("emulate: eip=%#lx esp=%#lx\n", 
+                  (unsigned long)regs->eip, (unsigned long)regs->esp);
+
+    /* Check whether this looks like a stack operation. */
+    if ( (va & PAGE_MASK) == (regs->esp & PAGE_MASK) )
+    {
+        /* Forcibly unshadow and return.  It's important to do this before
+         * we emulate: if the faulting stack operation was the guest handling
+         * an interrupt, then 
+         * (a) the instruction at %eip is irrelevant; and
+         * (b) we might inject some other fault and mask the real one */
+        gdprintk(XENLOG_DEBUG, "guest stack is on a shadowed frame: "
+                 "%%esp=%#lx, cr2=%#lx, mfn=%#lx\n", 
+                 (unsigned long)regs->esp, va, mfn_x(gmfn));
+        sh_remove_shadows(v, gmfn, 0 /* thorough */, 1 /* must succeed */);
+        goto done;
+    }
 
     emul_ops = shadow_init_emulation(&emul_ctxt, regs);
 
