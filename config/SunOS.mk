9de4f0c67def5ab75c4f5a9a4428b719e149e8b2
kfraser@localhost.localdomain <kfraser@localhost.localdomain>
2007-03-07 10:49:20 +0000

    tools: Clean up use of 'install' command.
    - convert raw "install" command to use $(INSTALL)
    - convert some $(INSTALL) to $(INSTALL_DATA) as appropriate
    - modify the specific $(INSTALL) definitions to use -p.
    
    Original patch by Ben Thomas <ben@virtualiron.com>
    Signed-off-by: Keir Fraser <keir@xensource.com>

--- config/SunOS.mk
+++ config/SunOS.mk
@@ -14,9 +14,9 @@ MSGFMT     = gmsgfmt
 SHELL      = bash
 
 INSTALL      = ginstall
-INSTALL_DIR  = $(INSTALL) -d -m0755
-INSTALL_DATA = $(INSTALL) -m0644
-INSTALL_PROG = $(INSTALL) -m0755
+INSTALL_DIR  = $(INSTALL) -d -m0755 -p
+INSTALL_DATA = $(INSTALL) -m0644 -p
+INSTALL_PROG = $(INSTALL) -m0755 -p
 
 LIB64DIR = lib/amd64
 
