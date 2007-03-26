0f9a40c194beb01f5a2566b5fc002a02e076e6d7
Tim Deegan <Tim.Deegan@xensource.com>
2007-03-26 09:13:03 +0000

    [HVM] Intercept ACPI pm-timer registers Bring the PM1a_STS and PM1a_EN registers into Xen and use them to deliver SCI to the guest before it sees the MSB of the pm-timer change. Also correct some of the semantics of the registers. Signed-off-by: Tim Deegan <Tim.Deegan@xensource.com>

--- xen/include/public/hvm/save.h
+++ xen/include/public/hvm/save.h
@@ -392,7 +392,9 @@ DECLARE_HVM_SAVE_TYPE(HPET, 12, struct hvm_hw_hpet);
  */
 
 struct hvm_hw_pmtimer {
-    uint32_t timer;
+    uint32_t tmr_val;   /* PM_TMR_BLK.TMR_VAL: 24bit free-running counter */
+    uint16_t pm1a_sts;  /* PM1a_EVT_BLK.PM1a_STS: status register */
+    uint16_t pm1a_en;   /* PM1a_EVT_BLK.PM1a_EN: enable register */
 };
 
 DECLARE_HVM_SAVE_TYPE(PMTIMER, 13, struct hvm_hw_pmtimer);
