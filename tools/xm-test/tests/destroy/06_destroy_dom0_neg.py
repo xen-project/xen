54a101a7dd2700b7c9a5a1b08aa3072d8f46396d
Tom Wilkie <tom.wilkie@gmail.com>
2007-03-26 16:18:16 +0100

    Make 06_destroy_dom0_neg pass with xm via XenAPI.
    Make check case insensitive.
    
    signed-off-by: Tom Wilkie <tom.wilkie@gmail.com>

--- tools/xm-test/tests/destroy/06_destroy_dom0_neg.py
+++ tools/xm-test/tests/destroy/06_destroy_dom0_neg.py
@@ -10,5 +10,5 @@ from XmTestLib import *
 status, output = traceCommand("xm destroy 0")
 if status == 0:
     FAIL("xm destroy returned bad status, expected non 0, status is: %i" % status)
-elif not re.search("Error", output):
+elif not re.search("Error", output, re.I):
     FAIL("xm destroy returned bad output, expected Error:, output is: %s" % output)
