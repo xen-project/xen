091eb6c418c8b06a24adeed3b44dd13674426af2
Steven Hand <steven@xensource.com>
2007-07-03 15:49:16 +0100

    Remove over-zealous assertion.
    Signed-off-by: Steven Hand <steven@xensource.com>

--- xen/include/asm-x86/regs.h
+++ xen/include/asm-x86/regs.h
@@ -36,11 +36,6 @@ enum EFLAGS {
     unsigned long diff = (char *)guest_cpu_user_regs() - (char *)(r);         \
     /* Frame pointer must point into current CPU stack. */                    \
     ASSERT(diff < STACK_SIZE);                                                \
-    /* If a guest frame, it must be have guest privs (unless HVM guest).   */ \
-    /* We permit CS==0 which can come from an uninitialised trap entry. */    \
-    ASSERT((diff != 0) || vm86_mode(r) ||                                     \
-           ((r->cs&3) >= GUEST_KERNEL_RPL(current->domain)) ||                \
-           (r->cs == 0) || is_hvm_vcpu(current));                             \
     /* If not a guest frame, it must be a hypervisor frame. */                \
     ASSERT((diff == 0) || (!vm86_mode(r) && (r->cs == __HYPERVISOR_CS)));     \
     /* Return TRUE if it's a guest frame. */                                  \
