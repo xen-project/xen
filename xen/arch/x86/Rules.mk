43aa5ed8c07cf9772cea872cde6adb6c4eef6095
Keir Fraser <keir@xensource.com>
2007-07-07 10:31:15 +0100

    Specify -fno-strict-aliasing in root build config file.
    Fixes correctness issues with xenstored and gcc-4.2.
    
    Original patch by Charles Coffing <ccoffing@novell.com>
    
    Signed-off-by: Keir Fraser <keir@xensource.com>

--- xen/arch/x86/Rules.mk
+++ xen/arch/x86/Rules.mk
@@ -17,7 +17,7 @@ ifneq ($(XEN_OS),SunOS)
 CFLAGS += -nostdinc
 endif
 
-CFLAGS += -fno-builtin -fno-common -fno-strict-aliasing
+CFLAGS += -fno-builtin -fno-common
 CFLAGS += -iwithprefix include -Werror -Wno-pointer-arith -pipe
 CFLAGS += -I$(BASEDIR)/include 
 CFLAGS += -I$(BASEDIR)/include/asm-x86/mach-generic
