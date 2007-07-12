b28538178f7e2edcd25eeb9a9c5ba873cc517d05
kfraser@localhost.localdomain <kfraser@localhost.localdomain>
2007-07-12 16:03:41 +0100

    Fix PV-on-HVM driver build. Signed-off-by: Keir Fraser <keir@xensource.com>

--- unmodified_drivers/linux-2.6/compat-include/xen/platform-compat.h
+++ unmodified_drivers/linux-2.6/compat-include/xen/platform-compat.h
@@ -107,4 +107,13 @@ extern char *kasprintf(gfp_t gfp, const char *fmt, ...)
 #define __supported_pte_mask ((maddr_t)0)
 #endif
 
+#if defined(_LINUX_NETDEVICE_H) && LINUX_VERSION_CODE < KERNEL_VERSION(2,6,18)
+#define netif_tx_lock_bh(dev) (spin_lock_bh(&(dev)->xmit_lock))
+#define netif_tx_unlock_bh(dev) (spin_unlock_bh(&(dev)->xmit_lock))
+#endif
+
+#if defined(__LINUX_SEQLOCK_H) && !defined(DEFINE_SEQLOCK)
+#define DEFINE_SEQLOCK(x) seqlock_t x = SEQLOCK_UNLOCKED
+#endif
+
 #endif
