e6f4576ed5f3e6a289f911d93e578c4b6ade3e2d
kfraser@localhost.localdomain <kfraser@localhost.localdomain>
2007-08-31 12:12:27 +0100

    Fix handling of auto-generated file xsm.py. Signed-off-by: Keir Fraser <keir@xensource.com>

--- tools/python/xen/util/xsm/xsm.py
+++
@@ -1,9 +0,0 @@
-XEN_SECURITY_MODULE = "flask"
-from xsm_core import *
-
-import xen.util.xsm.flask.flask as xsm_module
-
-xsm_init(xsm_module)
-from xen.util.xsm.flask.flask import *
-del xsm_module
-
