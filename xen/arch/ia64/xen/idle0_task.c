d5e665f1c606ce35ec41324d097576e02c2e4ac6
Alex Williamson <alex.williamson@hp.com>
2008-01-17 12:05:43 -0700

    [IA64] Remove init_task definition
    init_task in kernel area shouldn't be referenced.
    The corresponding address in the identity mapping area should be used.
    i.e. ia64_tpa(init_task) + PAGE_OFFSET, because ia64_switch_rr7() and
    vmx_switch_rr7() assume that stack is in indentity mapping area.
    To make it sure, remove init_task definition.
    
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>

--- xen/arch/ia64/xen/idle0_task.c
+++ xen/arch/ia64/xen/idle0_task.c
@@ -11,17 +11,19 @@
 /*
  * Initial task structure.
  *
- * We need to make sure that this is properly aligned due to the way process stacks are
- * handled. This is done by having a special ".data.init_task" section...
+ * We need to make sure that this is properly aligned due to the way process
+ * stacks are handled.
+ * This is done by having a special ".data.init_task" section...
+ *
+ * init_task_mem shouldn't be used directly. the corresponding address in
+ * the identity mapping area should be used.
+ * I.e. __va(ia64_tpa(init_task_mem)) should be used.
  */
 union {
 	struct {
 		struct vcpu task;
 	} s;
 	unsigned long stack[KERNEL_STACK_SIZE/sizeof (unsigned long)];
-} init_task_mem asm ("init_task") __attribute__((section(".data.init_task"))) = {{
+} init_task_mem __attribute__((section(".data.init_task"))) = {{
 	.task = IDLE_VCPU(init_task_mem.s.task)
 }};
-
-EXPORT_SYMBOL(init_task);
-
