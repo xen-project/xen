17e84f77fd29748bf92398489c6055114521ce7d
Keir Fraser <keir.fraser@citrix.com>
2008-03-25 09:48:41 +0000

    build: Add generic subdirs rules.
    Use them in the toplevel Makefile.
    
    Signed-off-by: Bastian Blank <waldi@debian.org>

--- tools/Rules.mk
+++ tools/Rules.mk
@@ -49,3 +49,13 @@ $(eval $(check-y))
 %.o: %.cc
 	$(CC) $(CPPFLAGS) $(CXXFLAGS) -c -o $@ $<
 
+.PHONY: subdirs-all subdirs-clean subdirs-install
+.NOTPARALLEL: subdirs-all subdirs-clean subdirs-install
+subdirs-all subdirs-clean subdirs-install:
+	@set -e; for subdir in $(SUBDIRS) $(SUBDIRS-y); do \
+		$(MAKE) subdir-$(patsubst subdirs-%,%,$@)-$$subdir; \
+	done
+
+subdir-all-% subdir-clean-% subdir-install-%:
+	$(MAKE) -C $* $(patsubst subdir-%-$*,%,$@)
+
