309459216a3331c5d001db829802385eba39c644
Keir Fraser <keir.fraser@citrix.com>
2008-08-29 16:06:41 +0100

    Run cross-compilation-aware install wrapper script. Signed-off-by: Keir Fraser <keir.fraser@citrix.com>

--- config/StdGNU.mk
+++ config/StdGNU.mk
@@ -16,7 +16,7 @@ MSGMERGE   = msgmerge
 # Allow git to be wrappered in the environment
 GIT        ?= git
 
-INSTALL      = install
+INSTALL      = $(XEN_ROOT)/tools/cross-install
 INSTALL_DIR  = $(INSTALL) -d -m0755 -p
 INSTALL_DATA = $(INSTALL) -m0644 -p
 INSTALL_PROG = $(INSTALL) -m0755 -p
