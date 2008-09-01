a06cb3d8d89d953b83bc9a4fcd015e41464fae2a
Keir Fraser <keir.fraser@citrix.com>
2008-09-01 10:48:09 +0100

    ioemu: fix multiple pci-attach/pci-detach by removing the meaningless irq_index
    For HVM guest, we can't do pci-attach/pci-detach for >64 times due to
    the meaningless checking in pci_register_device(). I made a test of
    300 times of attach/detach and everything now works well.
    
    Signed-off-by: Dexuan Cui <dexuan.cui@intel.com>

--- tools/ioemu/vl.h
+++ tools/ioemu/vl.h
@@ -812,8 +812,6 @@ struct PCIDevice {
     /* do not access the following fields */
     PCIConfigReadFunc *config_read;
     PCIConfigWriteFunc *config_write;
-    /* ??? This is a PC-specific hack, and should be removed.  */
-    int irq_index;
 
     /* Current IRQ levels.  Used internally by the generic PCI code.  */
     int irq_state[4];
