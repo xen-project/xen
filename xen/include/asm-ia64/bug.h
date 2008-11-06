710ff917a1321cf8382df77e2bd050558a10ff46
Isaku Yamahata <yamahata@valinux.co.jp>
2008-11-06 12:14:06 +0900

    [IA64] make ia64 dump_execution_state() print stack trace for debugging.
    make ia64 dump_execution_state() print stack trace for debugging.
    
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>

--- xen/include/asm-ia64/bug.h
+++ xen/include/asm-ia64/bug.h
@@ -4,7 +4,12 @@
 #define BUG() __bug(__FILE__, __LINE__)
 #define WARN() __warn(__FILE__, __LINE__)
 
-#define dump_execution_state() printk("FIXME: implement ia64 dump_execution_state()\n")
+#define dump_execution_state()                                      \
+    do {                                                            \
+        printk("FIXME: implement ia64 dump_execution_state()\n");	\
+        dump_stack();                                               \
+    } while (0)
+
 #define vcpu_show_execution_state(v) printk("FIXME: implement ia64 vcpu_show_execution_state()\n")
 
 #endif /* __IA64_BUG_H__ */
