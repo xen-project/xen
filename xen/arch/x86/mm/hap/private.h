49bd5ded4ed1d92696097e4c9d708049c33bddf6
Keir Fraser <keir.fraser@citrix.com>
2008-11-13 13:03:16 +0000

    x86: Use the shadow-code PT walker from the HAP functions
    Replace the guts of the HAP pagetable walker with a call to the
    newly-commonified walker from the shadow code.  This reduces
    duplication, and gives HAP guests proper access control and A/D-bit
    setting for memory accesses from the emulator
    
    Signed-off-by: Tim Deegan <Tim.Deegan@citrix.com>

--- xen/arch/x86/mm/hap/private.h
+++ xen/arch/x86/mm/hap/private.h
@@ -20,9 +20,6 @@
 #ifndef __HAP_PRIVATE_H__
 #define __HAP_PRIVATE_H__
 
-#include <asm/flushtlb.h>
-#include <asm/hvm/support.h>
-
 /********************************************/
 /*          GUEST TRANSLATION FUNCS         */
 /********************************************/
@@ -33,36 +30,5 @@ unsigned long hap_gva_to_gfn_3level(struct vcpu *v, unsigned long gva,
 unsigned long hap_gva_to_gfn_4level(struct vcpu *v, unsigned long gva,
                                     uint32_t *pfec);
 
-/********************************************/
-/*            MISC DEFINITIONS              */
-/********************************************/
-
-/* PT_SHIFT describes the amount by which a virtual address is shifted right 
- * to right justify the portion to be used for indexing into a page 
- * table, given the guest memory model (i.e. number of levels) and the level 
- * of the page table being accessed. The idea is from Virtual Iron's code.
- */
-static const int PT_SHIFT[][5] =
-  {   /*     ------  level ------           nr_levels  */
-    /*         1     2     3     4                   */
-    {    0,    0,    0,    0,    0},   /* 0 not used */
-    {    0,    0,    0,    0,    0},   /* 1 not used */
-    {    0,   12,   22,    0,    0},   /* 2  */
-    {    0,   12,   21,   30,    0},   /* 3  */
-    {    0,   12,   21,   30,   39}    /* 4  */
-  };
-
-/* PT_ENTRIES describes the number of entries in a page table, given the 
- * memory model (i.e. number of levels) and the level of the page table 
- * being considered. This idea from Virtual Iron's shadow code*/
-static const int PT_ENTRIES[][5] =
-  {   /*     ------  level ------           nr_levels  */
-    /*         1     2     3     4                   */
-    {    0,    0,    0,    0,    0},   /* 0 not used */
-    {    0,    0,    0,    0,    0},   /* 1 not used */
-    {    0, 1024, 1024,    0,    0},   /* 2  */
-    {    0,  512,  512,    4,    0},   /* 3  */
-    {    0,  512,  512,  512,  512}    /* 4  */
-  };
 
 #endif /* __SVM_NPT_H__ */
