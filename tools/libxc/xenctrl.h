446b916ca1380998ddad67325a4257f9a2ea37d9
Keir Fraser <keir.fraser@citrix.com>
2009-01-05 11:08:25 +0000

    Make xc_translate_foreign_address aware of compat-mode guests and (32-bit) HVM guests.  64-bit HVM guests are still not supported for now, pending a sensible way of getting at the guest's EFER.LMA.
    Signed-off-by: Tim Deegan <Tim.Deegan@citrix.com>

--- tools/libxc/xenctrl.h
+++ tools/libxc/xenctrl.h
@@ -716,8 +716,8 @@ void *xc_map_foreign_batch(int xc_handle, uint32_t dom, int prot,
 
 /**
  * Translates a virtual address in the context of a given domain and
- * vcpu returning the machine page frame number of the associated
- * page.
+ * vcpu returning the GFN containing the address (that is, an MFN for 
+ * PV guests, a PFN for HVM guests).  Returns 0 for failure.
  *
  * @parm xc_handle a handle on an open hypervisor interface
  * @parm dom the domain to perform the translation in
