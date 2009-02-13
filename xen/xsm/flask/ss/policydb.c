91d9dfaec8dce8b60c491a4c900807ab36333dde
Keir Fraser <keir.fraser@citrix.com>
2009-02-13 09:33:58 +0000

    [XSM][FLASK] basic documentation and a type cast error for x86_64
      - A simple txt howto based on previous list discussions and observations
      - A oneliner patch to address a compiler type cast error for x86_64
    
    Signed-off-by: George S. Coker, II <gscoker@alpha.ncsc.mil>

--- xen/xsm/flask/ss/policydb.c
+++ xen/xsm/flask/ss/policydb.c
@@ -1515,8 +1515,8 @@ int policydb_read(struct policydb *p, void *fp)
     if ( len != strlen(POLICYDB_STRING) )
     {
         printk(KERN_ERR "security:  policydb string length %d does not "
-               "match expected length %Zu\n",
-               len, (u32) strlen(POLICYDB_STRING));
+               "match expected length %lu\n",
+               len, strlen(POLICYDB_STRING));
         goto bad;
     }
     policydb_str = xmalloc_array(char, len + 1);
