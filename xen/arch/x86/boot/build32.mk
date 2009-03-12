670741eb9496339f88e0133ce8d96a85040faec1
Keir Fraser <keir.fraser@citrix.com>
2009-03-12 11:24:25 +0000

    Fix Makefile portability: head -n -1 isn't portable.
    Signed-off-by: John Levon <john.levon@sun.com>

--- xen/arch/x86/boot/build32.mk
+++ xen/arch/x86/boot/build32.mk
@@ -10,8 +10,9 @@ $(call cc-option-add,CFLAGS,CC,-fno-stack-protector-all)
 
 CFLAGS += -Werror -fno-builtin -msoft-float
 
+# NB. awk invocation is a portable alternative to 'head -n -1'
 %.S: %.bin
-	(od -v -t x $< | head -n -1 | \
+	(od -v -t x $< | awk 'NR > 1 {print s} {s=$$0}' | \
 	sed 's/ /,0x/g' | sed 's/^[0-9]*,/ .long /') >$@
 
 %.bin: %.lnk
