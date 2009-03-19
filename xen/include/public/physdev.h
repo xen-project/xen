7fe15ceac057e8c2d83ed1203c472592c604c6ed
Keir Fraser <keir.fraser@citrix.com>
2009-03-19 10:24:48 +0000

    Clean up physdev.h public header.
    Signed-off-by: Keir Fraser <keir.fraser@citrix.com>

--- xen/include/public/physdev.h
+++ xen/include/public/physdev.h
@@ -183,7 +183,16 @@ struct physdev_manage_pci {
 typedef struct physdev_manage_pci physdev_manage_pci_t;
 DEFINE_XEN_GUEST_HANDLE(physdev_manage_pci_t);
 
-#define PHYSDEVOP_manage_pci_add_ext    20
+#define PHYSDEVOP_restore_msi            19
+struct physdev_restore_msi {
+    /* IN */
+    uint8_t bus;
+    uint8_t devfn;
+};
+typedef struct physdev_restore_msi physdev_restore_msi_t;
+DEFINE_XEN_GUEST_HANDLE(physdev_restore_msi_t);
+
+#define PHYSDEVOP_manage_pci_add_ext     20
 struct physdev_manage_pci_ext {
     /* IN */
     uint8_t bus;
@@ -198,15 +207,6 @@ struct physdev_manage_pci_ext {
 
 typedef struct physdev_manage_pci_ext physdev_manage_pci_ext_t;
 DEFINE_XEN_GUEST_HANDLE(physdev_manage_pci_ext_t);
-
-#define PHYSDEVOP_restore_msi            19
-struct physdev_restore_msi {
-    /* IN */
-    uint8_t bus;
-    uint8_t devfn;
-};
-typedef struct physdev_restore_msi physdev_restore_msi_t;
-DEFINE_XEN_GUEST_HANDLE(physdev_restore_msi_t);
 
 /*
  * Argument to physdev_op_compat() hypercall. Superceded by new physdev_op()
