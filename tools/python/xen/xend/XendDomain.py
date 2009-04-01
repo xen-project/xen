db8811093e5b8bfacb90e3144fbf94c04ef3ace3
Keir Fraser <keir.fraser@citrix.com>
2009-04-01 13:17:34 +0100

    xend: Revert c/s 19436 -- fails with Python 2.3, and sock.close() should suffice to shutdown a connection.
    Signed-off-by: Keir Fraser <keir.fraser@citrix.com>

--- tools/python/xen/xend/XendDomain.py
+++ tools/python/xen/xend/XendDomain.py
@@ -1378,7 +1378,6 @@ class XendDomain:
                 XendCheckpoint.save(sock.fileno(), dominfo, True, live,
                                     dst, node=node)
             finally:
-                sock.shutdown(socket.SHUT_RDWR)
                 sock.close()
 
     def domain_save(self, domid, dst, checkpoint=False):
