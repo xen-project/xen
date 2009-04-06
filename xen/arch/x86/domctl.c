a5428a4b5f54fa660b37d8eec88a69700f6f8352
Keir Fraser <keir.fraser@citrix.com>
2009-04-06 13:56:35 +0100

    x86_64: explicitly zero CR[1] in getvcpucontext for guests with no user PT
    Signed-off-by: Ian Campbell <ian.campbell@citrix.com>

--- xen/arch/x86/domctl.c
+++ xen/arch/x86/domctl.c
@@ -1161,9 +1161,9 @@ void arch_get_info_guest(struct vcpu *v, vcpu_guest_context_u c)
             c.nat->ctrlreg[3] = xen_pfn_to_cr3(
                 pagetable_get_pfn(v->arch.guest_table));
 #ifdef __x86_64__
-            if ( !pagetable_is_null(v->arch.guest_table_user) )
-                c.nat->ctrlreg[1] = xen_pfn_to_cr3(
-                    pagetable_get_pfn(v->arch.guest_table_user));
+            c.nat->ctrlreg[1] =
+                pagetable_is_null(v->arch.guest_table_user) ? 0
+                : xen_pfn_to_cr3(pagetable_get_pfn(v->arch.guest_table_user));
 #endif
 
             /* Merge shadow DR7 bits into real DR7. */
