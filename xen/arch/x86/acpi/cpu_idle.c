3387588b84508d0ada672953d7819f7954b0291d
Keir Fraser <keir.fraser@citrix.com>
2009-04-10 06:56:23 +0100

    x86 acpi: Quieten boot messages regarding power modes Signed-off-by: Keir Fraser <keir.fraser@citrix.com>

--- xen/arch/x86/acpi/cpu_idle.c
+++ xen/arch/x86/acpi/cpu_idle.c
@@ -780,7 +780,7 @@ long set_cx_pminfo(uint32_t cpu, struct xen_processor_power *power)
 
     /* FIXME: C-state dependency is not supported by far */
 
-    print_acpi_power(cpu_id, acpi_power);
+    /*print_acpi_power(cpu_id, acpi_power);*/
 
     if ( cpu_id == 0 && pm_idle_save == NULL )
     {
