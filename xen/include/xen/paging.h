ee91a88190249e91f1621f85721d7ee07219207e
Keir Fraser <keir.fraser@citrix.com>
2009-07-20 10:07:28 +0100

    ia64: Define paging_mode_external(), now needed by grant_table.c
    Signed-off-by: Keir Fraser <keir.fraser@citrix.com>

--- xen/include/xen/paging.h
+++ xen/include/xen/paging.h
@@ -14,10 +14,12 @@
 #include <asm/shadow.h>
 
 #define paging_mode_translate(d)  shadow_mode_translate(d)
+#define paging_mode_external(d)   (0)
 
 #else
 
 #define paging_mode_translate(d)              (0)
+#define paging_mode_external(d)               (0)
 #define guest_physmap_add_page(d, p, m, o)    (0)
 #define guest_physmap_remove_page(d, p, m, o) ((void)0)
 
