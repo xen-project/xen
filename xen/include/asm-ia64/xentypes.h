2645f93a9a493789d7086e20a73286f6ad88ceb6
Keir Fraser <keir.fraser@citrix.com>
2009-08-19 14:21:56 +0100

    [IA64] define BYTES_PER_LONG to fix compilation error.
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>

--- xen/include/asm-ia64/xentypes.h
+++ xen/include/asm-ia64/xentypes.h
@@ -10,6 +10,8 @@ typedef char bool_t;
 #define test_and_set_bool(b)   xchg(&(b), 1)
 #define test_and_clear_bool(b) xchg(&(b), 0)
 
+#define BYTES_PER_LONG  8
+
 #endif /* !__ASSEMBLY__ */
 
 #endif /* _ASM_IA64_XENTYPES_H */
