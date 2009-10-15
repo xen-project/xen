89b6b19a224e7718500a00182e0099f45e877896
Keir Fraser <keir.fraser@citrix.com>
2009-10-15 08:30:31 +0100

    x86: Remove unused temporay variable 'old_gvec'.
    Signed-off-by: Xiantao Zhang <xiantao.zhang@intel.com>

--- xen/drivers/passthrough/io.c
+++ xen/drivers/passthrough/io.c
@@ -187,7 +187,6 @@ int pt_irq_create_bind_vtd(
         else
         {
             uint32_t mask = HVM_IRQ_DPCI_MACH_MSI | HVM_IRQ_DPCI_GUEST_MSI;
-            uint32_t old_gvec;
 
             if ( (hvm_irq_dpci->mirq[pirq].flags & mask) != mask)
             {
@@ -196,7 +195,6 @@ int pt_irq_create_bind_vtd(
             }
  
             /* if pirq is already mapped as vmsi, update the guest data/addr */
-            old_gvec = hvm_irq_dpci->mirq[pirq].gmsi.gvec;
             hvm_irq_dpci->mirq[pirq].gmsi.gvec = pt_irq_bind->u.msi.gvec;
             hvm_irq_dpci->mirq[pirq].gmsi.gflags = pt_irq_bind->u.msi.gflags;
         }
