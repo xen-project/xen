407b049aebf00b916e2b196c444a6a79ea064fd5
Keir Fraser <keir.fraser@citrix.com>
2010-02-08 10:18:14 +0000

    Don't scrub broken pages
    Don't touch the poison pages when scrub the pages. Consuming poison
    page will contaminate the CPU context and may cause system crash.
    
    Signed-off-by: Jiang, Yunhong <yunhong.jiang@intel.com>

--- xen/common/page_alloc.c
+++ xen/common/page_alloc.c
@@ -1256,6 +1256,9 @@ void scrub_one_page(struct page_info *pg)
 {
     void *p = __map_domain_page(pg);
 
+    if ( unlikely(pg->count_info & PGC_broken) )
+        return;
+
 #ifndef NDEBUG
     /* Avoid callers relying on allocations returning zeroed pages. */
     memset(p, 0xc2, PAGE_SIZE);
