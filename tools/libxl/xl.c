7da1c9269c679a7bc71b498c5bd0b1872445cd66
Keir Fraser <keir.fraser@citrix.com>
2010-04-13 12:48:17 +0100

    libxl: <sys/signal.h> -> <signal.h>
    Signed-off-by: Keir Fraser <keir.fraser@citrix.com>

--- tools/libxl/xl.c
+++ tools/libxl/xl.c
@@ -26,7 +26,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <sys/signal.h>
+#include <signal.h>
 #include <sys/socket.h>
 #include <sys/select.h>
 #include <arpa/inet.h>
