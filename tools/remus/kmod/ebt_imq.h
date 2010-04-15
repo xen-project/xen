04b41ebf20215c854aec09efa024c4e25c36e70a
Keir Fraser <keir.fraser@citrix.com>
2010-04-15 08:42:08 +0100

    Remus: make ebt_imq and sch_queue compatible with pvops
    Signed-off-by: Brendan Cully <brendan@cs.ubc.ca>

--- tools/remus/kmod/ebt_imq.h
+++ tools/remus/kmod/ebt_imq.h
@@ -1,10 +1,14 @@
 #ifndef __LINUX_BRIDGE_EBT_IMQ_H
 #define __LINUX_BRIDGE_EBT_IMQ_H
 
-#define IMQ_F_ENQUEUE 0x80
+#ifdef OLDKERNEL
+#  define IMQ_F_ENQUEUE 0x80
+#endif
 
 struct ebt_imq_info
 {
   unsigned int todev;
 };
+#define EBT_IMQ_TARGET "imq"
+
 #endif
