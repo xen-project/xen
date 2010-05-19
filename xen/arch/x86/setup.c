5dd6be7e601946afb7f12634e045e5b57e53532c
Keir Fraser <keir.fraser@citrix.com>
2010-05-19 21:14:57 +0100

    x86: During boot, initialise cpu-numa info for all present CPUs.
    Previously we would skip ones we didn't bring online.
    
    Signed-off-by: Keir Fraser <keir.fraser@citrix.com>

--- xen/arch/x86/setup.c
+++ xen/arch/x86/setup.c
@@ -1007,15 +1007,13 @@ void __init __start_xen(unsigned long mbi_p)
 
     for_each_present_cpu ( i )
     {
-        if ( num_online_cpus() >= max_cpus )
-            break;
-        if ( !cpu_online(i) )
-            cpu_up(i);
-
         /* Set up cpu_to_node[]. */
         srat_detect_node(i);
         /* Set up node_to_cpumask based on cpu_to_node[]. */
         numa_add_cpu(i);        
+
+        if ( (num_online_cpus() < max_cpus) && !cpu_online(i) )
+            cpu_up(i);
     }
 
     printk("Brought up %ld CPUs\n", (long)num_online_cpus());
