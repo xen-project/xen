d72b079a28ba88dfca30676a28800fbe78da2b57
Keir Fraser <keir.fraser@citrix.com>
2010-06-22 07:29:26 +0100

    Remus: include device name in vif objects
    Signed-off-by: Brendan Cully <brendan@cs.ubc.ca>
    xen-unstable changeset:   21242:e1482f91b235
    xen-unstable date:        Tue May 04 09:35:18 2010 +0100

--- tools/python/xen/remus/vif.py
+++ tools/python/xen/remus/vif.py
@@ -2,6 +2,7 @@ from xen.remus.util import canonifymac
 
 class VIF(object):
     def __init__(self, **props):
+        self.dev = 'unknown'
         self.__dict__.update(props)
         if 'mac' in props:
             self.mac = canonifymac(props['mac'])
@@ -9,6 +10,9 @@ class VIF(object):
     def __str__(self):
         return self.mac
 
-def parse(props):
+def parse(props, domid, index):
     "turn a vm device dictionary into a vif object"
-    return VIF(**props)
+    vif = VIF(**props)
+    vif.dev = 'vif%d.%d' % (domid, index)
+
+    return vif
