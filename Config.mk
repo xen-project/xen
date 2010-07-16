bef56bbb4b954d43b3736c6d83dd044227041dfc
Keir Fraser <keir.fraser@citrix.com>
2010-07-16 14:03:08 +0100

    Update Xen version to 4.0.1-rc4

--- Config.mk
+++ Config.mk
@@ -153,7 +153,7 @@ QEMU_REMOTE=http://xenbits.xensource.com/git-http/qemu-xen-4.0-testing.git
 # CONFIG_QEMU ?= ../qemu-xen.git
 CONFIG_QEMU ?= $(QEMU_REMOTE)
 
-QEMU_TAG := xen-4.0.1-rc3
+QEMU_TAG := xen-4.0.1-rc4
 #QEMU_TAG ?= f1d909f0f854194f5a40d850886d1413fb8b63c2
 # Wed Mar 24 17:16:51 2010 +0000
 # Make hweight32 function static
