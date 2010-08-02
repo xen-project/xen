cef33294d4bf6912138687780c7949fedc97140e
Keir Fraser <keir.fraser@citrix.com>
2010-08-02 17:11:33 +0100

    Walking the page lists needs the page_alloc lock
    There are a few places in Xen where we walk a domain's page lists
    without holding the page_alloc lock.  They race with updates to the
    page lists, which are normally rare but can be quite common under PoD
    when the domain is close to its memory limit and the PoD reclaimer is
    busy.  This patch protects those places by taking the page_alloc lock.
    
    I think this is OK for the two debug-key printouts - they don't run
    from irq context and look deadlock-free.  The tboot change seems safe
    too unless tboot shutdown functions are called from irq context or
    with the page_alloc lock held.  The p2m one is the scariest but there
    are already code paths in PoD that take the page_alloc lock with the
    p2m lock held so it's no worse than existing code.
    
    Signed-off-by: Tim Deegan <Tim.Deegan@citrix.com>
    xen-unstable changeset:   21881:57de3a3118bb
    xen-unstable date:        Wed Jul 28 07:54:12 2010 +0100

--- xen/arch/x86/tboot.c
+++ xen/arch/x86/tboot.c
@@ -211,12 +211,14 @@ static void tboot_gen_domain_integrity(const uint8_t key[TB_KEY_SIZE],
             continue;
         printk("MACing Domain %u\n", d->domain_id);
 
+        spin_lock(&d->page_alloc_lock);
         page_list_for_each(page, &d->page_list)
         {
             void *pg = __map_domain_page(page);
             vmac_update(pg, PAGE_SIZE, &ctx);
             unmap_domain_page(pg);
         }
+        spin_unlock(&d->page_alloc_lock);
 
         if ( !is_idle_domain(d) )
         {
