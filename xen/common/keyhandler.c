c6816ae91d95b51702a976db7373a63b47a72a92
Keir Fraser <keir@xen.org>
2010-12-24 10:29:14 +0000

    re-add calls accidentally deleted from run_all_nonirq_keyhandlers()
    c/s 22538:a3a29e67aa7e, having got applied in a form different from
    the one submitted, resulted in the calls to
    console_{start,end}_log_everything() getting removed without
    replacement. Add them back since, other than run_all_keyhandlers(),
    this doesn't run with log-everything already in effect.
    
    Signed-off-by: Jan Beulich <jbeulich@novell.com>
    xen-unstable changeset:   22631:dca1b7cf2e2c
    xen-unstable date:        Fri Dec 24 10:12:58 2010 +0000

--- xen/common/keyhandler.c
+++ xen/common/keyhandler.c
@@ -375,6 +375,8 @@ static void run_all_nonirq_keyhandlers(unsigned long unused)
     struct keyhandler *h;
     int k;
 
+    console_start_log_everything();
+
     for ( k = 0; k < ARRAY_SIZE(key_table); k++ )
     {
         process_pending_softirqs_nested();
@@ -384,6 +386,8 @@ static void run_all_nonirq_keyhandlers(unsigned long unused)
         printk("[%c: %s]\n", k, h->desc);
         (*h->u.fn)(k);
     }
+
+    console_end_log_everything();
 }
 
 static DECLARE_TASKLET(run_all_keyhandlers_tasklet,
