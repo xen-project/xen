f45b59ff53641c2ff19c86a1e23829e61d47dde0
Ian Campbell <ian.campbell@citrix.com>
2011-03-17 19:14:57 +0000

    tools/blktap2/libvhd: move uuid wrapper functions out of line.
    This isolates users of libvhd from the need to know about the
    different OS schemes for UUIDs.
    
    Signed-off-by: Ian Campbell <ian.campbell@citrix.com>
    Acked-by: Daniel Stodden <daniel.stodden@citrix.com>
    Committed-by: Ian Jackson <ian.jackson@eu.citrix.com>

--- tools/blktap2/vhd/lib/vhd-util-uuid.c
+++ tools/blktap2/vhd/lib/vhd-util-uuid.c
@@ -1,4 +1,6 @@
-/* Copyright (c) 2008, XenSource Inc.
+ /* Copyright (c) 2008, XenSource Inc.
+ * Copyright (c) 2011, Citrix
+ *
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -23,41 +25,104 @@
  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-#ifndef __BLKTAP2_VHD_UUID_H__
-#define __BLKTAP2_VHD_UUID_H__
+ */
 
 #if defined(__linux__)
 
 #include <uuid/uuid.h>
+
 typedef struct {
     uuid_t uuid;
 } vhd_uuid_t;
 
+int vhd_uuid_is_nil(vhd_uuid_t *uuid)
+{
+	return uuid_is_null(uuid->uuid);
+}
+
+void vhd_uuid_generate(vhd_uuid_t *uuid)
+{
+	uuid_generate(uuid->uuid);
+}
+
+void vhd_uuid_to_string(vhd_uuid_t *uuid, char *out, size_t size)
+{
+	uuid_unparse(uuid->uuid, out);
+}
+
+void vhd_uuid_from_string(vhd_uuid_t *uuid, const char *in)
+{
+	uuid_parse(in, uuid->uuid);
+}
+
+void vhd_uuid_copy(vhd_uuid_t *dst, vhd_uuid_t *src)
+{
+	uuid_copy(dst->uuid, src->uuid);
+}
+
+void vhd_uuid_clear(vhd_uuid_t *uuid)
+{
+	uuid_clear(uuid->uuid);
+}
+
+int vhd_uuid_compare(vhd_uuid_t *uuid1, vhd_uuid_t *uuid2)
+{
+	return uuid_compare(uuid1->uuid, uuid2->uuid);
+}
+
 #elif defined(__NetBSD__)
 
 #include <uuid.h>
+#include <string.h>
+#include <stdlib.h>
 
 typedef uuid_t vhd_uuid_t;
 
+int vhd_uuid_is_nil(vhd_uuid_t *uuid)
+{
+	uint32_t status;
+	return uuid_is_nil((uuid_t *)uuid, &status);
+}
+
+void vhd_uuid_generate(vhd_uuid_t *uuid)
+{
+	uint32_t status;
+	uuid_create((uuid_t *)uuid, &status);
+}
+
+void vhd_uuid_to_string(vhd_uuid_t *uuid, char *out, size_t size)
+{
+	uint32_t status;
+	char *_out = NULL;
+	uuid_to_string((uuid_t *)uuid, &_out, &status);
+	strlcpy(out, _out, size);
+	free(_out);
+}
+
+void vhd_uuid_from_string(vhd_uuid_t *uuid, const char *in)
+{
+	uint32_t status;
+	uuid_from_string(in, (uuid_t *)uuid, &status);
+}
+
+void vhd_uuid_copy(vhd_uuid_t *dst, vhd_uuid_t *src)
+{
+	memcpy((uuid_t *)dst, (uuid_t *)src, sizeof(uuid_t));
+}
+
+void vhd_uuid_clear(vhd_uuid_t *uuid)
+{
+	memset((uuid_t *)uuid, 0, sizeof(uuid_t));
+}
+
+int vhd_uuid_compare(vhd_uuid_t *uuid1, vhd_uuid_t *uuid2)
+{
+	uint32_t status;
+	return uuid_compare((uuid_t *)uuid1, (uuid_t *)uuid2, &status);
+}
+
 #else
 
-#error "Please update vhd-uuid.h for your OS"
+#error "Please update vhd-util-uuid.c for your OS"
 
 #endif
-
-int vhd_uuid_is_nil(vhd_uuid_t *uuid);
-
-void vhd_uuid_generate(vhd_uuid_t *uuid);
-
-void vhd_uuid_to_string(vhd_uuid_t *uuid, char *out, size_t size);
-
-void vhd_uuid_from_string(vhd_uuid_t *uuid, const char *in);
-
-void vhd_uuid_copy(vhd_uuid_t *dst, vhd_uuid_t *src);
-
-void vhd_uuid_clear(vhd_uuid_t *uuid);
-
-int vhd_uuid_compare(vhd_uuid_t *uuid1, vhd_uuid_t *uuid2);
-
-#endif /* __BLKTAP2_VHD_UUID_H__ */
