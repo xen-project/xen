8d247c677a90d44ee43cb9cc9f2987654ff00a28
Jan Beulich <jbeulich@novell.com>
2011-04-12 13:27:27 +0100

    passthrough: prevent non-HVM access to HVM-only data
    Spotted this oversight in c/s 23144:37c4f7d492a4.
    
    Signed-off-by: Jan Beulich <jbeulich@novell.com>

--- xen/drivers/passthrough/pci.c
+++ xen/drivers/passthrough/pci.c
@@ -246,7 +246,7 @@ static void pci_clean_dpci_irqs(struct domain *d)
     if ( !iommu_enabled )
         return;
 
-    if ( !need_iommu(d) )
+    if ( !is_hvm_domain(d) || !need_iommu(d) )
         return;
 
     spin_lock(&d->event_lock);
