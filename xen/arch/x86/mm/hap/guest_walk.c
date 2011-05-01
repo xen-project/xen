b96e7ce08e5df883f52ed42e9f9d3f9644f23d18
Jan Beulich <jbeulich@novell.com>
2011-05-01 10:16:54 +0100

    x86: clean up building in mm/hap/
    Building 4-level guest walks is unnecessary for x86-32, and with this
    no longer being built the fallback code used here isn't necessary
    anymore either.
    
    Additonally the mechanism to determine the value of
    GUEST_PAGING_LEVELS to be passed to the compiler can be much
    simplified given that we're using a pattern rule here.
    
    Signed-off-by: Jan Beulich <jbeulich@novell.com>

--- xen/arch/x86/mm/hap/guest_walk.c
+++ xen/arch/x86/mm/hap/guest_walk.c
@@ -32,7 +32,9 @@
 #define _hap_p2m_ga_to_gfn(levels) hap_p2m_ga_to_gfn_##levels##_levels
 #define hap_p2m_ga_to_gfn(levels) _hap_p2m_ga_to_gfn(levels)
 
-#if GUEST_PAGING_LEVELS <= CONFIG_PAGING_LEVELS
+#if GUEST_PAGING_LEVELS > CONFIG_PAGING_LEVELS
+#error GUEST_PAGING_LEVELS must not exceed CONFIG_PAGING_LEVELS
+#endif
 
 #include <asm/guest_pt.h>
 #include <asm/p2m.h>
@@ -118,28 +120,6 @@ unsigned long hap_p2m_ga_to_gfn(GUEST_PAGING_LEVELS)(
 
     return INVALID_GFN;
 }
-
-#else
-
-unsigned long hap_gva_to_gfn(GUEST_PAGING_LEVELS)(
-    struct vcpu *v, struct p2m_domain *p2m, unsigned long gva, uint32_t *pfec)
-{
-    gdprintk(XENLOG_ERR,
-             "Guest paging level is greater than host paging level!\n");
-    domain_crash(v->domain);
-    return INVALID_GFN;
-}
-
-unsigned long hap_p2m_ga_to_gfn(GUEST_PAGING_LEVELS)(
-    struct vcpu *v, struct p2m_domain *p2m, unsigned long cr3,
-    paddr_t ga, uint32_t *pfec)
-{
-    gdprintk(XENLOG_ERR,
-             "Guest paging level is greater than host paging level!\n");
-    domain_crash(v->domain);
-    return INVALID_GFN;
-}
-#endif
 
 
 /*
