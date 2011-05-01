4c7909939b059cd366426acaf9988cc7c5f4c00b
Keir Fraser <keir@xen.org>
2011-05-01 13:03:37 +0100

    public/arch-ia64/debug_op.h: Reinsert copyright that I accidentally deleted.
    Signed-off-by: Keir Fraser <keir@xen.org>

--- xen/include/public/arch-ia64/debug_op.h
+++ xen/include/public/arch-ia64/debug_op.h
@@ -1,6 +1,8 @@
 /******************************************************************************
  * debug_op.h
  *
+ * Copyright (c) 2007 Tristan Gingold <tgingold@free.fr>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
  * deal in the Software without restriction, including without limitation the
