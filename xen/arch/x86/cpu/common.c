6f1b7bcbb473e734eff8d105679a219c4c61081d
Yang, Wei <wei.y.yang@intel.com>
2011-05-28 08:57:12 +0100

    x86/intel: Fix CPUID leaf 7 detection
    Must set subleaf to 0 (input ECX==0).
    
    Signed-off-by: Yang, Wei <wei.y.yang@intel.com>
    Signed-off-by: Li, Xin <xin.li@intel.com>

--- xen/arch/x86/cpu/common.c
+++ xen/arch/x86/cpu/common.c
@@ -262,9 +262,11 @@ void __cpuinit generic_identify(struct cpuinfo_x86 * c)
 	}
 
 	/* Intel-defined flags: level 0x00000007 */
-	if ( c->cpuid_level >= 0x00000007 )
-		c->x86_capability[X86_FEATURE_FSGSBASE / 32]
-			= cpuid_ebx(0x00000007);
+	if ( c->cpuid_level >= 0x00000007 ) {
+		u32 dummy;
+		cpuid_count(0x00000007, 0, &dummy, &ebx, &dummy, &dummy);
+		c->x86_capability[X86_FEATURE_FSGSBASE / 32] = ebx;
+	}
 
 	early_intel_workaround(c);
 
