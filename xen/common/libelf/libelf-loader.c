a4b9cae427bd3e398c12847d1268488be71b73e7
Keir Fraser <keir@xen.org>
2011-05-28 09:17:15 +0100

    Clean up stdarg handling a little. Fix for NetBSD.
    Signed-off-by: Keir Fraser <keir@xen.org>
    xen-unstable changeset:   23432:14eb8e1fcd82
    xen-unstable date:        Fri May 27 15:49:24 2011 +0100

--- xen/common/libelf/libelf-loader.c
+++ xen/common/libelf/libelf-loader.c
@@ -16,8 +16,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#include <stdarg.h>
-
 #include "libelf-private.h"
 
 /* ------------------------------------------------------------------------ */
--- xen/include/xen/stdarg.h
+++ xen/include/xen/stdarg.h
@@ -1,5 +1,10 @@
 #if defined(__OpenBSD__)
 #  include "/usr/include/stdarg.h"
+#elif defined (__NetBSD__)
+   typedef __builtin_va_list va_list;
+#  define va_start(ap, last)    __builtin_stdarg_start((ap), (last))
+#  define va_end(ap)            __builtin_va_end(ap)
+#  define va_arg                __builtin_va_arg
 #else
 #  include <stdarg.h>
 #endif
