837e2846531a4b4e561f17e741f8a25c9e0b059c
Keir Fraser <keir@xen.org>
2011-06-23 12:01:11 +0100

    Protect xen/stdarg.h for multiple inclusion.
    Signed-off-by: Keir Fraser <keir@xen.org>
    xen-unstable changeset:   23549:a574bf2f5059
    xen-unstable date:        Thu Jun 16 16:14:51 2011 +0100

--- xen/include/xen/stdarg.h
+++ xen/include/xen/stdarg.h
@@ -1,3 +1,6 @@
+#ifndef __XEN_STDARG_H__
+#define __XEN_STDARG_H__
+
 #if defined(__OpenBSD__)
 #  include "/usr/include/stdarg.h"
 #elif defined (__NetBSD__)
@@ -8,3 +11,5 @@
 #else
 #  include <stdarg.h>
 #endif
+
+#endif /* __XEN_STDARG_H__ */
