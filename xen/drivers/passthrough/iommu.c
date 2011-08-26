eacb075f65ca88c0ef17d672527733257ba96589
Tim Deegan <tim@xen.org>
2011-08-26 13:24:48 +0100

    x86/mm: Add iommu=sharept parameter for compatibility with 4.2
    Signed-off-by: Tim Deegan <tim@xen.org>

--- xen/drivers/passthrough/iommu.c
+++ xen/drivers/passthrough/iommu.c
@@ -82,6 +82,8 @@ static void __init parse_iommu_param(char *s)
             iommu_passthrough = 1;
         else if ( !strcmp(s, "dom0-strict") )
             iommu_dom0_strict = 1;
+        else if ( !strcmp(s, "sharept") )
+            iommu_hap_pt_share = 1;
 
         s = ss + 1;
     } while ( ss );
