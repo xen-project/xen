6a688c6b6a26804a30d84d99f72a195b4efde006
Keir Fraser <keir@xen.org>
2011-09-30 21:17:47 +0100

    build: fix grep invocation in cc-options
    Currently the build produces lots of
            Usage: grep [OPTION]... PATTERN [FILE]...
            Try `grep --help' for more information.
    
    This is due to the "grep -- $(2)" in cc-options. It seems that the
    default of reading stdin is disabled when using "--". I don't know if
    this is a bug in grep or how it is supposed to be but we can work
    around it by explicitly passing in "-"
    
    Signed-off-by: Ian Campbell <ian.campbell@citrix.com>
    Committed-by: Keir Fraser <keir@xen.org>

--- Config.mk
+++ Config.mk
@@ -84,7 +84,7 @@ PYTHON_PREFIX_ARG ?= --prefix="$(PREFIX)"
 #
 # Usage: cflags-y += $(call cc-option,$(CC),-march=winchip-c6,-march=i586)
 cc-option = $(shell if test -z "`echo 'void*p=1;' | \
-              $(1) $(2) -S -o /dev/null -xc - 2>&1 | grep -- $(2)`"; \
+              $(1) $(2) -S -o /dev/null -xc - 2>&1 | grep -- $(2) -`"; \
               then echo "$(2)"; else echo "$(3)"; fi ;)
 
 # cc-option-add: Add an option to compilation flags, but only if supported.
