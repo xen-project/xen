6f8145eee37cdb6b4af2f33fbe80970608227dc3
Jan Beulich <jbeulich@suse.com>
2012-03-07 08:17:44 +0000

    x86/emulator: workaround for AMD erratum 573
    The only cases where we might end up emulating fsincos (as any other
    x87 operations without memory operands) are
    - when a HVM guest is in real mode (not applicable on AMD)
    - between two half page table updates in PAE mode (unlikely, and not
      doing the emulation here does affect only performance, not
      correctness)
    - when a guest maliciously (or erroneously) modifies an (MMIO or page
      table update) instruction under emulation (unspecified behavior)
    
    Hence, in order to avoid the erratum to cause harm to the entire host,
    don't emulate fsincos on the affected AMD CPU families.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Acked-by: Keir Fraser <keir@xen.org>
    xen-unstable changeset:   24417:1452fb248cd5
    xen-unstable date:        Fri Dec 16 15:45:40 2011 +0100

--- tools/tests/x86_emulator/x86_emulate.c
+++ tools/tests/x86_emulator/x86_emulate.c
@@ -3,5 +3,7 @@
 #include <string.h>
 #include <public/xen.h>
 
+#define cpu_has_amd_erratum(nr) 0
+
 #include "x86_emulate/x86_emulate.h"
 #include "x86_emulate/x86_emulate.c"
--- xen/arch/x86/x86_emulate.c
+++ xen/arch/x86/x86_emulate.c
@@ -10,8 +10,14 @@
  */
 
 #include <asm/x86_emulate.h>
+#include <asm/processor.h> /* current_cpu_info */
+#include <asm/amd.h> /* cpu_has_amd_erratum() */
 
 /* Avoid namespace pollution. */
 #undef cmpxchg
+#undef cpuid
+
+#define cpu_has_amd_erratum(nr) \
+        cpu_has_amd_erratum(&current_cpu_data, AMD_ERRATUM_##nr)
 
 #include "x86_emulate/x86_emulate.c"
