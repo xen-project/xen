4865cebd3f8defdf958c379244484078a5bf4c98
Zhenzhong Duan <zhenzhong.duan@oracle.com>
2012-03-29 10:20:58 +0100

    x86,hvm: enable VCPUOP_register_vcpu_info op in hvm hypercall
    pvhvm running with more than 32 vcpus and pv_irq/pv_time enabled
    need vcpu placement to work, or else it will softlockup.
    
    Signed-off-by: Zhenzhong Duan <zhenzhong.duan@oracle.com>
    Committed-by: Keir Fraser <keir@xen.org>
    xen-unstable changeset:   24013:c4ed56a102dc
    xen-unstable date:        Fri Oct 28 17:20:21 2011 +0100

--- xen/arch/x86/hvm/hvm.c
+++ xen/arch/x86/hvm/hvm.c
@@ -2586,6 +2586,7 @@ static long hvm_vcpu_op(
     case VCPUOP_stop_periodic_timer:
     case VCPUOP_set_singleshot_timer:
     case VCPUOP_stop_singleshot_timer:
+    case VCPUOP_register_vcpu_info:
         rc = do_vcpu_op(cmd, vcpuid, arg);
         break;
     default:
@@ -2651,6 +2652,7 @@ static long hvm_vcpu_op_compat32(
     case VCPUOP_stop_periodic_timer:
     case VCPUOP_set_singleshot_timer:
     case VCPUOP_stop_singleshot_timer:
+    case VCPUOP_register_vcpu_info:
         rc = compat_vcpu_op(cmd, vcpuid, arg);
         break;
     default:
