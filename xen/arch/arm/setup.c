d0a6bfdbf8a3f11e807624c6ff7b93f1f7b25a56
Ian Campbell <Ian.Campbell@citrix.com>
2012-09-03 11:22:00 +0100

    arm: correctly check for error on dom0 allocation
    Drop the redundant printk
    
    Signed-off-by: Ian Campbell <ian.campbell@citrix.com>
    Acked-by: Jan Beulich <JBeulich@suse.com>
    Committed-by: Ian Campbell <ian.campbell@citrix.com>

--- xen/arch/arm/setup.c
+++ xen/arch/arm/setup.c
@@ -238,9 +238,7 @@ void __init start_xen(unsigned long boot_phys_offset,
 
     /* Create initial domain 0. */
     dom0 = domain_create(0, 0, 0);
-    if ( IS_ERR(dom0) )
-            printk("domain_create failed\n");
-    if ( (dom0 == NULL) || (alloc_dom0_vcpu0() == NULL) )
+    if ( IS_ERR(dom0) || (alloc_dom0_vcpu0() == NULL) )
             panic("Error creating domain 0\n");
 
     dom0->is_privileged = 1;
