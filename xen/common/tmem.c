e75ecdea85ff23d916420ec299753e31c5610f47
Ian Campbell <ian.campbell@citrix.com>
2012-09-11 14:06:54 +0200

    tmem: check the pool_id is valid when destroying a tmem pool
    This is part of XSA-15 / CVE-2012-3497.
    
    Signed-off-by: Ian Campbell <ian.campbell@citrix.com>
    Committed-by: Jan Beulich <jbeulich@suse.com>

--- xen/common/tmem.c
+++ xen/common/tmem.c
@@ -1870,6 +1870,8 @@ static NOINLINE int do_tmem_destroy_pool(uint32_t pool_id)
 
     if ( client->pools == NULL )
         return 0;
+    if ( pool_id >= MAX_POOLS_PER_DOMAIN )
+        return 0;
     if ( (pool = client->pools[pool_id]) == NULL )
         return 0;
     client->pools[pool_id] = NULL;
