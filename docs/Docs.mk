48d3c84838c166de21d0ad60f069906f267b4b1b
Ian Campbell <ian.campbell@citrix.com>
2013-01-25 09:02:13 +0000

    docs: Remove xen-api docs
    This document is about an old unmaintained version of the XenAPI,
    which bears little to no relation to what is implemented in xapi and
    which is only partially implemented in xend (which is deprecated). The
    doc hasn't seen much in the way of updates since 2009.
    
    Anyone who is actually interested can continue to use the version
    which was in 4.2.
    
    Signed-off-by: Ian Campbell <ian.campbell@citrix.com>
    Acked-by: Roger Pau Monné <roger.pau@citrix.com>
    Committed-by: Ian Campbell <ian.campbell@citrix.com>

--- docs/Docs.mk
+++ docs/Docs.mk
@@ -1,12 +1,7 @@
-PS2PDF		:= ps2pdf
-DVIPS		:= dvips
-LATEX		:= latex
 FIG2DEV		:= fig2dev
 LATEX2HTML	:= latex2html
 DOXYGEN		:= doxygen
 POD2MAN		:= pod2man
 POD2HTML	:= pod2html
 POD2TEXT	:= pod2text
-DOT		:= dot
-NEATO		:= neato
 MARKDOWN	:= markdown
