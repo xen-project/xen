511278b4e239df00de7b33f7b42d8d5d7e52221b
Keir Fraser <keir@xen.org>
2013-02-13 17:03:31 +0000

    gcc4.8 build fix: Add -Wno-unused-local-typedefs to CFLAGS.
    Based on a patch by M A Young <m.a.young@durham.ac.uk>
    
    Signed-off-by: Keir Fraser <keir@xen.org>

--- Config.mk
+++ Config.mk
@@ -172,6 +172,7 @@ CFLAGS-$(clang) += -Wno-parentheses -Wno-format -Wno-unused-value
 $(call cc-option-add,HOSTCFLAGS,HOSTCC,-Wdeclaration-after-statement)
 $(call cc-option-add,CFLAGS,CC,-Wdeclaration-after-statement)
 $(call cc-option-add,CFLAGS,CC,-Wno-unused-but-set-variable)
+$(call cc-option-add,CFLAGS,CC,-Wno-unused-local-typedefs)
 
 LDFLAGS += $(foreach i, $(EXTRA_LIB), -L$(i)) 
 CFLAGS += $(foreach i, $(EXTRA_INCLUDES), -I$(i))
