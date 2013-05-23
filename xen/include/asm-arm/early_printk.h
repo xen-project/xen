54353cae096c19f176417f6d425de0a0f8a71228
Julien Grall <julien.grall@linaro.org>
2013-05-23 16:05:18 +0100

    xen/arm: check string format for early_printk
    Signed-off-by: Julien Grall <julien.grall@linaro.org>
    Acked-by: Ian Campbell <ian.campbell@citrix.com>

--- xen/include/asm-arm/early_printk.h
+++ xen/include/asm-arm/early_printk.h
@@ -14,13 +14,19 @@
 
 #ifdef EARLY_PRINTK
 
-void early_printk(const char *fmt, ...);
-void early_panic(const char *fmt, ...) __attribute__((noreturn));
+void early_printk(const char *fmt, ...)
+    __attribute__((format (printf, 1, 2)));
+void early_panic(const char *fmt, ...) __attribute__((noreturn))
+    __attribute__((format (printf, 1, 2)));
 
 #else
 
-static inline void early_printk(const char *fmt, ...) {}
-static inline void  __attribute__((noreturn)) early_panic(const char *fmt, ...) {while(1);}
+static inline void early_printk(const char *fmt, ...)
+    __attribute__((format (printf, 1, 2)))
+{}
+static inline void  __attribute__((noreturn)) early_panic(const char *fmt, ...)
+    __attribute__((format (printf, 1, 2)))
+{while(1);}
 
 #endif
 
