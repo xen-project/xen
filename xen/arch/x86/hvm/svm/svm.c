70ac97c6150f7a2e182c10654f191490cde90303
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2013-08-20 15:36:20 +0200

    x86/AMD: Inject #GP instead of #UD when unable to map vmcb
    According to AMD Programmer's Manual vol2, vmrun, vmsave and vmload
    should inject #GP instead of #UD when unable to access memory
    location for vmcb.  Also, the code should make sure that L1 guest
    EFER.SVME is not zero.  Otherwise, #UD should be injected.
    
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    Reviewed-by: Tim Deegan <tim@xen.org>
    master commit: 910daaf5aaa837624099c0fc5c373bea7202ff43
    master date: 2013-08-13 14:24:16 +0200

--- xen/arch/x86/hvm/svm/svm.c
+++ xen/arch/x86/hvm/svm/svm.c
@@ -1797,15 +1797,17 @@ static void
 svm_vmexit_do_vmrun(struct cpu_user_regs *regs,
                     struct vcpu *v, uint64_t vmcbaddr)
 {
-    if (!nestedhvm_enabled(v->domain)) {
+    if ( !nsvm_efer_svm_enabled(v) )
+    {
         gdprintk(XENLOG_ERR, "VMRUN: nestedhvm disabled, injecting #UD\n");
         hvm_inject_hw_exception(TRAP_invalid_op, HVM_DELIVER_NO_ERROR_CODE);
         return;
     }
 
-    if (!nestedsvm_vmcb_map(v, vmcbaddr)) {
-        gdprintk(XENLOG_ERR, "VMRUN: mapping vmcb failed, injecting #UD\n");
-        hvm_inject_hw_exception(TRAP_invalid_op, HVM_DELIVER_NO_ERROR_CODE);
+    if ( !nestedsvm_vmcb_map(v, vmcbaddr) )
+    {
+        gdprintk(XENLOG_ERR, "VMRUN: mapping vmcb failed, injecting #GP\n");
+        hvm_inject_hw_exception(TRAP_gp_fault, HVM_DELIVER_NO_ERROR_CODE);
         return;
     }
 
@@ -1851,7 +1853,8 @@ svm_vmexit_do_vmload(struct vmcb_struct *vmcb,
     if ( (inst_len = __get_instruction_length(v, INSTR_VMLOAD)) == 0 )
         return;
 
-    if (!nestedhvm_enabled(v->domain)) {
+    if ( !nsvm_efer_svm_enabled(v) )
+    {
         gdprintk(XENLOG_ERR, "VMLOAD: nestedhvm disabled, injecting #UD\n");
         ret = TRAP_invalid_op;
         goto inject;
@@ -1861,8 +1864,8 @@ svm_vmexit_do_vmload(struct vmcb_struct *vmcb,
     if ( !page )
     {
         gdprintk(XENLOG_ERR,
-            "VMLOAD: mapping failed, injecting #UD\n");
-        ret = TRAP_invalid_op;
+            "VMLOAD: mapping failed, injecting #GP\n");
+        ret = TRAP_gp_fault;
         goto inject;
     }
 
@@ -1892,7 +1895,8 @@ svm_vmexit_do_vmsave(struct vmcb_struct *vmcb,
     if ( (inst_len = __get_instruction_length(v, INSTR_VMSAVE)) == 0 )
         return;
 
-    if (!nestedhvm_enabled(v->domain)) {
+    if ( !nsvm_efer_svm_enabled(v) )
+    {
         gdprintk(XENLOG_ERR, "VMSAVE: nestedhvm disabled, injecting #UD\n");
         ret = TRAP_invalid_op;
         goto inject;
@@ -1902,8 +1906,8 @@ svm_vmexit_do_vmsave(struct vmcb_struct *vmcb,
     if ( !page )
     {
         gdprintk(XENLOG_ERR,
-            "VMSAVE: mapping vmcb failed, injecting #UD\n");
-        ret = TRAP_invalid_op;
+            "VMSAVE: mapping vmcb failed, injecting #GP\n");
+        ret = TRAP_gp_fault;
         goto inject;
     }
 
