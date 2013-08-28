887915680a701a2e258676a6c97ae0599ac9e3fe
Julien Grall <julien.grall@linaro.org>
2013-08-28 15:47:16 +0100

    xen: Introduce __initconst to store initial const data
    It's possible to have 2 type (const and non-const) of data in the same
    compilation unit. Using only __initdata will result to a compilation error:
    
        error: $variablename causes as section tupe conflict with $variablename2
    
    because a section containing const variables is marked read only and so cannot
    contain non-const variables.
    
    Signed-off-by: Julien Grall <julien.grall@linaro.org>
    Acked-by: Ian Cambell <ian.campbell@citrix.com>
    CC: Jan Beulich <JBeulich@suse.com>
    CC: Keir Fraser <keir@xen.org>

--- xen/include/xen/init.h
+++ xen/include/xen/init.h
@@ -10,6 +10,7 @@
 #define __init            __text_section(".init.text")
 #define __exit            __text_section(".exit.text")
 #define __initdata        __section(".init.data")
+#define __initconst       __section(".init.rodata")
 #define __exitdata        __used_section(".exit.data")
 #define __initsetup       __used_section(".init.setup")
 #define __init_call(lvl)  __used_section(".initcall" lvl ".init")
