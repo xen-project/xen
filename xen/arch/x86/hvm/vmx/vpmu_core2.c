334b5924569c6e391d78268b1481b96aa4293b0a
Jan Beulich <jbeulich@suse.com>
2013-09-06 14:47:37 +0200

    x86/Intel: add further support for Ivy Bridge CPU models
    And some initial Haswell ones at once.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Acked-by: "Nakajima, Jun" <jun.nakajima@intel.com>

--- xen/arch/x86/hvm/vmx/vpmu_core2.c
+++ xen/arch/x86/hvm/vmx/vpmu_core2.c
@@ -747,6 +747,7 @@ int vmx_vpmu_initialise(struct vcpu *v, unsigned int vpmu_flags)
         case 46:
         case 47:
         case 58:
+        case 62:
             ret = core2_vpmu_initialise(v, vpmu_flags);
             if ( !ret )
                 vpmu->arch_vpmu_ops = &core2_vpmu_ops;
