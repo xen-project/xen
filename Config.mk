50a7df0e4eaadccba39813fde3523c355474382a
Jan Beulich <jbeulich@suse.com>
2013-09-09 14:29:24 +0200

    update Xen version to 4.1.6

--- Config.mk
+++ Config.mk
@@ -180,7 +180,7 @@ endif
 # CONFIG_QEMU ?= `pwd`/$(XEN_ROOT)/../qemu-xen.git
 CONFIG_QEMU ?= $(QEMU_REMOTE)
 
-QEMU_TAG ?= xen-4.1.6-rc2
+QEMU_TAG ?= xen-4.1.6
 # Tue May 14 18:48:49 2013 +0100
 # piix4acpi, xen, hotplug: Fix race with ACPI AML code and hotplug.
 
