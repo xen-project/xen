633c8efb6285e77b13640d7e02178f0ec032262d
Jan Beulich <jbeulich@suse.com>
2013-09-11 08:23:29 +0200

    ACPI: fix acpi_os_map_memory()
    It using map_domain_page() was entirely wrong. Use __acpi_map_table()
    instead for the time being, with locking added as the mappings it
    produces get replaced with subsequent invocations. Using locking in
    this way is acceptable here since the only two runtime callers are
    acpi_os_{read,write}_memory(), which don't leave mappings pending upon
    returning to their callers.
    
    Also fix __acpi_map_table()'s first parameter's type - while benign for
    unstable, backports to pre-4.3 trees will need this.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    master commit: 2ee9cbf9d8eaeff6e21222905d22dbd58dc5fe29
    master date: 2013-08-21 08:38:40 +0200

--- xen/include/xen/acpi.h
+++ xen/include/xen/acpi.h
@@ -56,7 +56,7 @@ typedef int (*acpi_table_handler) (struct acpi_table_header *table);
 typedef int (*acpi_table_entry_handler) (struct acpi_subtable_header *header, const unsigned long end);
 
 unsigned int acpi_get_processor_id (unsigned int cpu);
-char * __acpi_map_table (unsigned long phys_addr, unsigned long size);
+char * __acpi_map_table (paddr_t phys_addr, unsigned long size);
 int acpi_boot_init (void);
 int acpi_boot_table_init (void);
 int acpi_numa_init (void);
