0e251a8371574b905d37d7650d1d625caf0f1181
Tim Deegan <tim@xen.org>
2014-02-13 15:13:07 +0000

    xen: Don't use __builtin_stdarg_start().
    Cset fca49a00 ("netbsd: build fix with gcc 4.5") changed the
    definition of va_start() to use __builtin_va_start() rather than
    __builtin_stdarg_start() for GCCs >= 4.5, but in fact GCC dropped
    __builtin_stdarg_start() before v3.3.
    
    Signed-off-by: Tim Deegan <tim@xen.org>
    Tested-by: Roger Pau Monné <roger.pau@citrix.com>

--- xen/include/xen/stdarg.h
+++ xen/include/xen/stdarg.h
@@ -1,18 +1,6 @@
 #ifndef __XEN_STDARG_H__
 #define __XEN_STDARG_H__
 
-#ifdef __GNUC__
-#  define __GNUC_PREREQ__(x, y)                                       \
-      ((__GNUC__ == (x) && __GNUC_MINOR__ >= (y)) ||                  \
-       (__GNUC__ > (x)))
-#else
-#  define __GNUC_PREREQ__(x, y)   0
-#endif
-
-#if !__GNUC_PREREQ__(4, 5)
-#  define __builtin_va_start(ap, last)    __builtin_stdarg_start((ap), (last))
-#endif
-
 typedef __builtin_va_list va_list;
 #define va_start(ap, last)    __builtin_va_start((ap), (last))
 #define va_end(ap)            __builtin_va_end(ap)
