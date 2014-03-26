3cdbc31bd77fa9e6a41eb8d285b903aa976ce96c
Ian Campbell <ian.campbell@citrix.com>
2014-03-26 13:38:43 +0000

    xen: arm32: add optimised strchr and strrchr routines
    Taken from Linux v3.14-rc7.
    
    These aren't widely used enough to be critical, but we may as well have them.
    
    Signed-off-by: Ian Campbell <ian.campbell@citrix.com>
    Acked-by: Julien Grall <julien.grall@linaro.org>
    Acked-by: Tim Deegan <tim@xen.org>

--- xen/include/asm-arm/string.h
+++ xen/include/asm-arm/string.h
@@ -4,6 +4,18 @@
 #include <xen/config.h>
 
 #if defined(CONFIG_ARM_32)
+
+/*
+ * We don't do inline string functions, since the
+ * optimised inline asm versions are not small.
+ */
+
+#define __HAVE_ARCH_STRRCHR
+extern char * strrchr(const char * s, int c);
+
+#define __HAVE_ARCH_STRCHR
+extern char * strchr(const char * s, int c);
+
 #define __HAVE_ARCH_MEMCPY
 extern void * memcpy(void *, const void *, __kernel_size_t);
 
