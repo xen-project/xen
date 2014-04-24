8fde66c8c0feddf6ce391ceaf061dca73d33787d
Andrew Cooper <andrew.cooper3@citrix.com>
2014-04-24 22:17:57 +0100

    tools/misc: Fix linkage of libxenstore
    * xen-mfndump doesn't use xenstore at all.  Don't link against it.
    
    * xen-hptool can include the correct header rather than externing itself a
      single function.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    CC: Ian Campbell <Ian.Campbell@citrix.com>
    CC: Ian Jackson <Ian.Jackson@eu.citrix.com>
    CC: Dario Faggioli <dario.faggioli@citrix.com>
    Reviewed-by: Dario Faggioli <dario.faggioli@citrix.com>
    Acked-by: Ian Campbell <ian.campbell@citrix.com>

--- tools/misc/xen-hptool.c
+++ tools/misc/xen-hptool.c
@@ -1,6 +1,7 @@
 #include <xenctrl.h>
 #include <xc_private.h>
 #include <xc_core.h>
+#include <xenstore.h>
 #include <unistd.h>
 
 #define ARRAY_SIZE(a) (sizeof (a) / sizeof ((a)[0]))
@@ -95,8 +96,6 @@ static int hp_mem_query_func(int argc, char *argv[])
 
     return ret;
 }
-
-extern int xs_suspend_evtchn_port(int domid);
 
 static int suspend_guest(xc_interface *xch, xc_evtchn *xce, int domid,
                          int *evtchn, int *lockfd)
