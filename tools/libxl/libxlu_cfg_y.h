dc92f1a781a160229e4935dc03040bc78712a828
Ian Jackson <ian.jackson@eu.citrix.com>
2014-05-02 17:47:55 +0100

    libxl: Rerun bison
    This updates libxlu_cfg_y.[ch] to code generated by bison from
    Debian wheezy (1:2.5.dfsg-2.1 i386).
    
    There should be no functional change since there is no change to the
    source file, but we will inherit bugfixes and behavioural changes from
    the new version of bison.  So this is more a matter of hope than
    knowledge.
    
    Signed-off-by: Ian Jackson <ian.jackson@eu.citrix.com>

--- tools/libxl/libxlu_cfg_y.h
+++ tools/libxl/libxlu_cfg_y.h
@@ -1,10 +1,8 @@
+/* A Bison parser, made by GNU Bison 2.5.  */
 
-/* A Bison parser, made by GNU Bison 2.4.1.  */
-
-/* Skeleton interface for Bison's Yacc-like parsers in C
+/* Bison interface for Yacc-like parsers in C
    
-      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
+      Copyright (C) 1984, 1989-1990, 2000-2011 Free Software Foundation, Inc.
    
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -52,7 +50,7 @@
 typedef union YYSTYPE
 {
 
-/* Line 1676 of yacc.c  */
+/* Line 2068 of yacc.c  */
 #line 25 "libxlu_cfg_y.y"
 
   char *string;
@@ -60,8 +58,8 @@ typedef union YYSTYPE
 
 
 
-/* Line 1676 of yacc.c  */
-#line 65 "libxlu_cfg_y.h"
+/* Line 2068 of yacc.c  */
+#line 63 "libxlu_cfg_y.h"
 } YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
