7fe67ca553320251398ccc56f8a9c95f13782a9b
Jan Beulich <jbeulich@suse.com>
2014-06-03 12:09:27 +0200

    ACPI/ERST: fix table mapping
    acpi_get_table(), when executed before reaching SYS_STATE_active, will
    return a mapping valid only until the next invocation of that funciton.
    Consequently storing the returned pointer for later use is incorrect.
    Copy the logic used in VT-d's DMAR handling.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    master commit: fca69b1fc606ece62430076ca4a157e4bed749a8
    master date: 2014-05-26 12:25:01 +0200

--- xen/drivers/acpi/apei/erst.c
+++ xen/drivers/acpi/apei/erst.c
@@ -760,21 +760,27 @@ int __init erst_init(void)
 {
 	int rc = 0;
 	acpi_status status;
+	acpi_physical_address erst_addr;
+	acpi_native_uint erst_len;
 	struct apei_exec_context ctx;
 
 	if (acpi_disabled)
 		return -ENODEV;
 
-	status = acpi_get_table(ACPI_SIG_ERST, 0,
-				(struct acpi_table_header **)&erst_tab);
+	status = acpi_get_table_phys(ACPI_SIG_ERST, 0, &erst_addr, &erst_len);
 	if (status == AE_NOT_FOUND) {
 		printk(KERN_INFO "ERST table was not found\n");
 		return -ENODEV;
-	} else if (ACPI_FAILURE(status)) {
+	}
+	if (ACPI_FAILURE(status)) {
 		const char *msg = acpi_format_exception(status);
 		printk(KERN_WARNING "Failed to get ERST table: %s\n", msg);
 		return -EINVAL;
 	}
+	map_pages_to_xen((unsigned long)__va(erst_addr), PFN_DOWN(erst_addr),
+			 PFN_UP(erst_addr + erst_len) - PFN_DOWN(erst_addr),
+			 PAGE_HYPERVISOR);
+	erst_tab = __va(erst_addr);
 
 	rc = erst_check_table(erst_tab);
 	if (rc) {
