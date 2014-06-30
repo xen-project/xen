eb2952b43a70ae485e297f826212b6b279ca0361
Jan Beulich <jbeulich@suse.com>
2014-06-30 16:01:03 +0200

    x86: move alternative.c data fully into .init.*
    This wasn't done upon the initial addition of the file since the
    section processing needs some adjustment for this: We can't mark the
    two arrays needing relocations as either __initdata or __initconst, as
    both have the potential of creating section conflicts (with -fPIC the
    compiler may want to mark the section writable due to the necessary
    relocations, colliding with other __initconst uses, and if the compiler
    chooses to use the provided section name, __initdata on a constant
    object might collide with other __initdata uses). However, local data
    known to be referenced only by init code/data can also safely be moved
    into .init.*. Hence the respective logic is being tweaked such that it
    won't complain on non-empty .*.local sections.
    
    While at it also drop the non-local section names from
    SPECIAL_DATA_SECTIONS - they can't be safely converted.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>

--- xen/arch/x86/alternative.c
+++ xen/arch/x86/alternative.c
@@ -39,7 +39,7 @@ static const unsigned char k8nops[] __initconst = {
     K8_NOP7,
     K8_NOP8
 };
-static const unsigned char * const k8_nops[ASM_NOP_MAX+1] /*__initconst*/ = {
+static const unsigned char * const k8_nops[ASM_NOP_MAX+1] = {
     NULL,
     k8nops,
     k8nops + 1,
@@ -63,7 +63,7 @@ static const unsigned char p6nops[] __initconst = {
     P6_NOP7,
     P6_NOP8
 };
-static const unsigned char * const p6_nops[ASM_NOP_MAX+1] /*__initconst*/ = {
+static const unsigned char * const p6_nops[ASM_NOP_MAX+1] = {
     NULL,
     p6nops,
     p6nops + 1,
