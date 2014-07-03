ee81dda7c3767bd9af7ae84a86d1be56d8fad343
Ian Jackson <ian.jackson@eu.citrix.com>
2014-07-03 13:58:23 +0100

    QEMU_TAG update - FIX!
    My qemu push and tag update script had transposed a revision number
    from qemu-xen-4.3-testing into xen-4.4-testing's Config.mk!
    
    The script is now fixed, but we also need to fix the tree.
    
    Signed-off-by: Ian Jackson <ian.jackson@eu.citrix.com>

--- Config.mk
+++ Config.mk
@@ -246,7 +246,7 @@ ETHERBOOT_NICS ?= rtl8139 8086100e
 # CONFIG_QEMU ?= `pwd`/$(XEN_ROOT)/../qemu-xen.git
 CONFIG_QEMU ?= $(QEMU_REMOTE)
 
-QEMU_TAG ?= 8a7abb1694f22cb18616ee7a7a544b026c914bf9
+QEMU_TAG ?= 6ae4e588081620b141071eb010ec40aca7e12876
 # Tue Apr 8 16:50:06 2014 +0000
 # qemu-xen-trad: free all the pirqs for msi/msix when driver unloads
 
