8ea3fb47af98efa84a26aaede772abf140102323
Andrew Cooper <andrew.cooper3@citrix.com>
2014-07-28 15:03:42 +0200

    properly reference count DOMCTL_{,un}pausedomain hypercalls
    For safety reasons, c/s 6ae2df93c27 "mem_access: Add helper API to setup
    ring and enable mem_access" has to pause the domain while it performs a set of
    operations.
    
    However without properly reference counted hypercalls, xc_mem_event_enable()
    now unconditionally unpauses a previously paused domain.
    
    To prevent toolstack software running wild, there is an arbitrary limit of 255
    on the toolstack pause count.  This is high enough for several components of
    the toolstack to safely use, but prevents over/underflow of d->pause_count.
    
    The previous domain_{,un}pause_by_systemcontroller() functions are updated to
    return an error code.  domain_pause_by_systemcontroller() is modified to have
    a common stub and take a pause_fn pointer, allowing for both sync and nosync
    domain pauses.  domain_pause_for_debugger() has a hand-rolled nosync pause
    replaced with the new domain_pause_by_systemcontroller_nosync(), and has its
    variables shuffled slightly to avoid rereading current multiple times.
    
    Suggested-by: Don Slutz <dslutz@verizon.com>
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    With a couple of formatting adjustments:
    Reviewed-by: Jan Beulich <jbeulich@suse.com>
    
    x86/gdbsx: invert preconditions for XEN_DOMCTL_gdbsx_{,un}pausevcpu hypercalls
    
    c/s 3eb1c708ab "properly reference count DOMCTL_{,un}pausedomain hypercalls"
    accidentally inverted the use of d->controller_pause_count.
    
    Revert back to how it was originally, i.e. the XEN_DOMCTL_gdbsx_{,un}pausevcpu
    hypercalls are only valid for a domain already paused by the system controller.
    
    Reported-by: Jan Beulich <jbeulich@suse.com>
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    master commit: 3eb1c708ab0fe1067a436498a684907afa14dacf
    master date: 2014-07-03 16:51:13 +0200
    master commit: 680d79f10bb70691a9ae3b4a6a8b669e0f2837f6
    master date: 2014-07-25 11:53:31 +0200

--- xen/arch/x86/domctl.c
+++ xen/arch/x86/domctl.c
@@ -1005,7 +1005,7 @@ long arch_do_domctl(
         struct vcpu *v;
 
         ret = -EBUSY;
-        if ( !d->is_paused_by_controller )
+        if ( !d->controller_pause_count )
             break;
         ret = -EINVAL;
         if ( domctl->u.gdbsx_pauseunp_vcpu.vcpu >= MAX_VIRT_CPUS ||
@@ -1021,7 +1021,7 @@ long arch_do_domctl(
         struct vcpu *v;
 
         ret = -EBUSY;
-        if ( !d->is_paused_by_controller )
+        if ( !d->controller_pause_count )
             break;
         ret = -EINVAL;
         if ( domctl->u.gdbsx_pauseunp_vcpu.vcpu >= MAX_VIRT_CPUS ||
@@ -1039,7 +1039,7 @@ long arch_do_domctl(
         struct vcpu *v;
 
         domctl->u.gdbsx_domstatus.vcpu_id = -1;
-        domctl->u.gdbsx_domstatus.paused = d->is_paused_by_controller;
+        domctl->u.gdbsx_domstatus.paused = d->controller_pause_count > 0;
         if ( domctl->u.gdbsx_domstatus.paused )
         {
             for_each_vcpu ( d, v )
