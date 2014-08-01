d91b0f9b58219724717db503f46ff2f731aa4c59
Andrew Cooper <andrew.cooper3@citrix.com>
2014-08-01 16:32:39 +0200

    x86/cpu: drop the num_siblings check against nr_cpu_ids
    The printk() is missing a newline which resulted in console corruption.
    
    However, nr_cpu_ids can be legitimately lower than valid num_sibling values
    given certain compile or boot time configuration.
    
    Suggested-by: Jan Beulich <jbeulich@suse.com>
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- xen/arch/x86/cpu/common.c
+++ xen/arch/x86/cpu/common.c
@@ -443,13 +443,6 @@ void __cpuinit detect_ht(struct cpuinfo_x86 *c)
 	if (c->x86_num_siblings == 1) {
 		printk(KERN_INFO  "CPU: Hyper-Threading is disabled\n");
 	} else if (c->x86_num_siblings > 1 ) {
-
-		if (c->x86_num_siblings > nr_cpu_ids) {
-			printk(KERN_WARNING "CPU: Unsupported number of the siblings %d", c->x86_num_siblings);
-			c->x86_num_siblings = 1;
-			return;
-		}
-
 		index_msb = get_count_order(c->x86_num_siblings);
 		c->phys_proc_id = phys_pkg_id((ebx >> 24) & 0xFF, index_msb);
 
