608aae36bba86cd2f3656910f3f3b079e53b9aa2
M A Young <m.a.young@durham.ac.uk>
2014-11-11 20:28:38 +0000

    fix commit xen/arm: Add support for GICv3 for domU
    The build of xen-4.5.0-rc2 fails if XSM_ENABLE=y due to an inconsistency
    in commit fda1614 "xen/arm: Add support for GICv3 for domU" which uses
    XEN_DOMCTL_configure_domain in xen/xsm/flask/hooks.c and
    xen/xsm/flask/policy/access_vectors but XEN_DOMCTL_arm_configure_domain
    elsewhere.
    
     	Michael Young
    In fda1614 ("xen/arm: Add support for GICv3 for domU")
    XEN_DOMCTL_configure_domain is used in xen/xsm/flask/hooks.c and
    xen/xsm/flask/policy/access_vectors but XEN_DOMCTL_arm_configure_domain
    is used elsewhere.
    
    Signed-off-by: Michael Young <m.a.young@durham.ac.uk>
    Acked-by: Ian Campbell <ian.campbell@citrix.com>

--- xen/xsm/flask/hooks.c
+++ xen/xsm/flask/hooks.c
@@ -727,7 +727,7 @@ static int flask_domctl(struct domain *d, int cmd)
     case XEN_DOMCTL_psr_cmt_op:
         return current_has_perm(d, SECCLASS_DOMAIN2, DOMAIN2__PSR_CMT_OP);
 
-    case XEN_DOMCTL_configure_domain:
+    case XEN_DOMCTL_arm_configure_domain:
         return current_has_perm(d, SECCLASS_DOMAIN2, DOMAIN2__CONFIGURE_DOMAIN);
 
     default:
