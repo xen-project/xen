3508bd4d7648501cbfb879585341e6a80f71581c
Julien Grall <julien.grall@linaro.org>
2015-03-03 16:36:16 +0000

    xen/arm: gic: Correctly check if an IRQ is valid in gic_interrupt
    The IRQ 1020 is reserved for special purpose, so the check should be
    "irq < 1020" rather than "irq < 1021".
    
    Signed-off-by: Julien Grall <julien.grall@linaro.org>
    Acked-by: Ian Campbell <ian.campbell@citrix.com>

--- xen/arch/arm/gic.c
+++ xen/arch/arm/gic.c
@@ -570,7 +570,7 @@ void gic_interrupt(struct cpu_user_regs *regs, int is_fiq)
         /* Reading IRQ will ACK it */
         irq = gic_hw_ops->read_irq();
 
-        if ( likely(irq >= 16 && irq < 1021) )
+        if ( likely(irq >= 16 && irq < 1020) )
         {
             local_irq_enable();
             do_IRQ(regs, irq, is_fiq);
