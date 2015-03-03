f919dbc0583797d1c5c09da815518084ce77eb81
Ian Campbell <ian.campbell@citrix.com>
2015-03-03 17:02:22 +0000

    libxl: Correct license header on libxl_flask.c to be LGPL
    libxl is intended to be an LGPL 2.1 licensed library, however this
    file inadvertently got given a GPL header.
    
    The following people have touched this file, although all but Machon's
    contributions are trivial and/or mechanical an Ack from each would be
    unambiguous:
    
    $ git log --format='%an <%aE>' tools/libxl/libxl_flask.c | sort -u
    Ian Campbell <ian.campbell@citrix.com>
    Ian Jackson <ian.jackson@eu.citrix.com>
    Machon Gregory <mbgrego@tycho.ncsc.mil>
    Wei Liu <liuw@liuw.name>
    $
    
    Signed-off-by: Ian Campbell <ian.campbell@citrix.com>
    Cc: Ian Jackson <ian.jackson@eu.citrix.com>
    Cc: Machon Gregory <mbgrego@tycho.ncsc.mil>
    Cc: Wei Liu <liuw@liuw.name>
    Cc: Daniel De Graaf <dgdegra@tycho.nsa.gov>
    Acked-by: Ian Jackson <ian.jackson@eu.citrix.com>
    Acked-by: Wei Liu <liuw@liuw.name>
    Acked-by: M. Gregory <mbgrego@tycho.ncsc.mil>

--- tools/libxl/libxl_flask.c
+++ tools/libxl/libxl_flask.c
@@ -1,10 +1,15 @@
 /*
- *
  *  Author: Machon Gregory, <mbgrego@tycho.ncsc.mil>
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 2,
- *  as published by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  */
 
 #include "libxl_osdeps.h" /* must come before any other headers */
