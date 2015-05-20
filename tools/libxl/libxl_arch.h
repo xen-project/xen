a74720dc2bcd65327bd0a7b3baf90539d9fcbcdb
Julien Grall <julien.grall@linaro.org>
2015-05-20 14:01:18 +0100

    tools/libx: Don't export libxl__arch_* to the library
    libxl__arch_* should only be used internally by libxl.
    
    Signed-off-by: Julien Grall <julien.grall@linaro.org>
    Acked-by: Ian Campbell <ian.campbell@citrix.com>

--- tools/libxl/libxl_arch.h
+++ tools/libxl/libxl_arch.h
@@ -16,31 +16,37 @@
 #define LIBXL_ARCH_H
 
 /* fill the arch specific configuration for the domain */
+_hidden
 int libxl__arch_domain_prepare_config(libxl__gc *gc,
                                       libxl_domain_config *d_config,
                                       xc_domain_configuration_t *xc_config);
 
 /* arch specific internal domain creation function */
+_hidden
 int libxl__arch_domain_create(libxl__gc *gc, libxl_domain_config *d_config,
                uint32_t domid);
 
 /* setup arch specific hardware description, i.e. DTB on ARM */
+_hidden
 int libxl__arch_domain_init_hw_description(libxl__gc *gc,
                                            libxl_domain_build_info *info,
                                            libxl__domain_build_state *state,
                                            struct xc_dom_image *dom);
 /* finalize arch specific hardware description. */
+_hidden
 int libxl__arch_domain_finalise_hw_description(libxl__gc *gc,
                                       libxl_domain_build_info *info,
                                       struct xc_dom_image *dom);
 
 /* build vNUMA vmemrange with arch specific information */
+_hidden
 int libxl__arch_vnuma_build_vmemrange(libxl__gc *gc,
                                       uint32_t domid,
                                       libxl_domain_build_info *b_info,
                                       libxl__domain_build_state *state);
 
 /* arch specific irq map function */
+_hidden
 int libxl__arch_domain_map_irq(libxl__gc *gc, uint32_t domid, int irq);
 
 #endif
