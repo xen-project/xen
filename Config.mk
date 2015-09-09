ff9758b54ffe27ce2961de5412c0dc5af9c6abef
Wei Liu <wei.liu2@citrix.com>
2015-09-09 16:14:16 +0200

    Config.mk: update in-tree OVMF changeset
    Signed-off-by: Wei Liu <wei.liu2@citrix.com>
    Acked-by: Ian Campbell <ian.campbell@citrix.com>
    master commit: 28e5d9a9ad8e7e1a50503ec97ce9d20cd451a5d1
    master date: 2015-06-30 16:16:47 +0100

--- Config.mk
+++ Config.mk
@@ -233,7 +233,7 @@ OVMF_UPSTREAM_URL ?= git://xenbits.xen.org/ovmf.git
 QEMU_UPSTREAM_URL ?= git://xenbits.xen.org/qemu-upstream-4.4-testing.git
 SEABIOS_UPSTREAM_URL ?= git://xenbits.xen.org/seabios.git
 endif
-OVMF_UPSTREAM_REVISION ?= 447d264115c476142f884af0be287622cd244423
+OVMF_UPSTREAM_REVISION ?= cb9a7ebabcd6b8a49dc0854b2f9592d732b5afbd
 QEMU_UPSTREAM_REVISION ?= qemu-xen-4.4.3
 SEABIOS_UPSTREAM_TAG ?= rel-1.7.3.1
 # Fri Aug 2 14:12:09 2013 -0400
