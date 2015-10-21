53c11b041793944e26165f294fb96242c459d197
Jan Beulich <jbeulich@suse.com>
2015-10-21 17:33:32 +0200

    x86/NUMA: fix SRAT table processor entry parsing and consumption
    - don't overrun apicid_to_node[] (possible in the x2APIC case)
    - don't limit number of processor related SRAT entries we can consume
    - make acpi_numa_{processor,x2apic}_affinity_init() as similar to one
      another as possible
    - print APIC IDs in hex (to ease matching with other log messages), at
      once making legacy and x2APIC ones distinguishable (by width)
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>
    master commit: 83281fc9b31396e94c0bfb6550b75c165037a0ad
    master date: 2015-10-14 12:46:27 +0200

--- xen/arch/x86/srat.c
+++ xen/arch/x86/srat.c
@@ -170,7 +170,6 @@ void __init
 acpi_numa_x2apic_affinity_init(struct acpi_srat_x2apic_cpu_affinity *pa)
 {
 	int pxm, node;
-	int apic_id;
 
 	if (srat_disabled())
 		return;
@@ -178,8 +177,13 @@ acpi_numa_x2apic_affinity_init(struct acpi_srat_x2apic_cpu_affinity *pa)
 		bad_srat();
 		return;
 	}
-	if ((pa->flags & ACPI_SRAT_CPU_ENABLED) == 0)
+	if (!(pa->flags & ACPI_SRAT_CPU_ENABLED))
 		return;
+	if (pa->apic_id >= MAX_LOCAL_APIC) {
+		printk(KERN_INFO "SRAT: APIC %08x ignored\n", pa->apic_id);
+		return;
+	}
+
 	pxm = pa->proximity_domain;
 	node = setup_node(pxm);
 	if (node < 0) {
@@ -187,11 +191,11 @@ acpi_numa_x2apic_affinity_init(struct acpi_srat_x2apic_cpu_affinity *pa)
 		return;
 	}
 
-	apic_id = pa->apic_id;
-	apicid_to_node[apic_id] = node;
+	apicid_to_node[pa->apic_id] = node;
+	node_set(node, processor_nodes_parsed);
 	acpi_numa = 1;
-	printk(KERN_INFO "SRAT: PXM %u -> APIC %u -> Node %u\n",
-	       pxm, apic_id, node);
+	printk(KERN_INFO "SRAT: PXM %u -> APIC %08x -> Node %u\n",
+	       pxm, pa->apic_id, node);
 }
 
 /* Callback for Proximity Domain -> LAPIC mapping */
@@ -221,7 +225,7 @@ acpi_numa_processor_affinity_init(struct acpi_srat_cpu_affinity *pa)
 	apicid_to_node[pa->apic_id] = node;
 	node_set(node, processor_nodes_parsed);
 	acpi_numa = 1;
-	printk(KERN_INFO "SRAT: PXM %u -> APIC %u -> Node %u\n",
+	printk(KERN_INFO "SRAT: PXM %u -> APIC %02x -> Node %u\n",
 	       pxm, pa->apic_id, node);
 }
 
