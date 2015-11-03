e134a69f5c649290accfe2d5e905c5810425b4c4
Jan Beulich <jbeulich@suse.com>
2015-11-03 18:11:15 +0100

    timer-op: demote a debugging message to really be debugging only
    The issue the message points out may have been of relevance during the
    early days, but shouldn't anymore.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Acked-by: Ian Campbell <ian.campbell@citrix.com>

--- xen/common/schedule.c
+++ xen/common/schedule.c
@@ -1118,8 +1118,8 @@ long do_set_timer_op(s_time_t timeout)
          * timeout in this case can burn a lot of CPU. We therefore go for a
          * reasonable middleground of triggering a timer event in 100ms.
          */
-        gprintk(XENLOG_INFO, "Warning: huge timeout set: %"PRIx64"\n",
-                (uint64_t)timeout);
+        gdprintk(XENLOG_INFO, "Warning: huge timeout set: %"PRIx64"\n",
+                 timeout);
         set_timer(&v->singleshot_timer, NOW() + MILLISECS(100));
     }
     else
