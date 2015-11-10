4d81504ff449010daf69d930ebcd2d23b19a1cda
Roger Pau Monné <roger.pau@citrix.com>
2015-11-10 12:07:32 +0100

    x86: allow disabling the emulated VGA
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Acked-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- xen/arch/x86/hvm/stdvga.c
+++ xen/arch/x86/hvm/stdvga.c
@@ -578,6 +578,9 @@ void stdvga_init(struct domain *d)
     struct page_info *pg;
     unsigned int i;
 
+    if ( !has_vvga(d) )
+        return;
+
     memset(s, 0, sizeof(*s));
     spin_lock_init(&s->lock);
     
@@ -614,6 +617,9 @@ void stdvga_deinit(struct domain *d)
 {
     struct hvm_hw_stdvga *s = &d->arch.hvm_domain.stdvga;
     int i;
+
+    if ( !has_vvga(d) )
+        return;
 
     for ( i = 0; i != ARRAY_SIZE(s->vram_page); i++ )
     {
