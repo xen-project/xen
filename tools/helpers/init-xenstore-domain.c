befd991a360da2b18226a96c2a1c3c09e0a6577f
Juergen Gross <jgross@suse.com>
2016-01-18 09:03:57 +0100

    xenstore: add error messages to init-xenstore-domain
    Additional add some diagnostic messages to the program to have an idea
    why it failed.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Acked-by: Ian Campbell <ian.campbell@citrix.com>
    Acked-by: Daniel De Graaf <dgdegra@tycho.nsa.gov>

--- tools/helpers/init-xenstore-domain.c
+++ tools/helpers/init-xenstore-domain.c
@@ -10,6 +10,7 @@
 #include <xc_dom.h>
 #include <xenstore.h>
 #include <xen/sys/xenbus_dev.h>
+#include <xen-xsm/flask/flask.h>
 
 static uint32_t domid = ~0;
 
@@ -25,69 +26,120 @@ static int build(xc_interface *xch, int argc, char** argv)
 
     xs_fd = open("/dev/xen/xenbus_backend", O_RDWR);
     if ( xs_fd == -1 )
+    {
+        fprintf(stderr, "Could not open /dev/xen/xenbus_backend\n");
         return -1;
+    }
 
     rv = xc_flask_context_to_sid(xch, argv[3], strlen(argv[3]), &ssid);
     if ( rv )
+    {
+        fprintf(stderr, "xc_flask_context_to_sid failed\n");
         goto err;
+    }
     rv = xc_domain_create(xch, ssid, handle, 0, &domid, NULL);
     if ( rv )
+    {
+        fprintf(stderr, "xc_domain_create failed\n");
         goto err;
+    }
     rv = xc_domain_max_vcpus(xch, domid, 1);
     if ( rv )
+    {
+        fprintf(stderr, "xc_domain_max_vcpus failed\n");
         goto err;
+    }
     rv = xc_domain_setmaxmem(xch, domid, limit_kb);
     if ( rv )
+    {
+        fprintf(stderr, "xc_domain_setmaxmem failed\n");
         goto err;
+    }
     rv = xc_domain_set_memmap_limit(xch, domid, limit_kb);
     if ( rv )
+    {
+        fprintf(stderr, "xc_domain_set_memmap_limit failed\n");
         goto err;
+    }
 
     rv = ioctl(xs_fd, IOCTL_XENBUS_BACKEND_SETUP, domid);
     if ( rv < 0 )
+    {
+        fprintf(stderr, "Xenbus setup ioctl failed\n");
         goto err;
+    }
     snprintf(cmdline, 512, "--event %d --internal-db", rv);
 
     dom = xc_dom_allocate(xch, cmdline, NULL);
     rv = xc_dom_kernel_file(dom, argv[1]);
     if ( rv )
+    {
+        fprintf(stderr, "xc_dom_kernel_file failed\n");
         goto err;
+    }
 
     if ( argc > 4 )
     {
         rv = xc_dom_ramdisk_file(dom, argv[4]);
         if ( rv )
+        {
+            fprintf(stderr, "xc_dom_ramdisk_file failed\n");
             goto err;
+        }
     }
 
     rv = xc_dom_boot_xen_init(dom, xch, domid);
     if ( rv )
+    {
+        fprintf(stderr, "xc_dom_boot_xen_init failed\n");
         goto err;
+    }
     rv = xc_dom_parse_image(dom);
     if ( rv )
+    {
+        fprintf(stderr, "xc_dom_parse_image failed\n");
         goto err;
+    }
     rv = xc_dom_mem_init(dom, maxmem);
     if ( rv )
+    {
+        fprintf(stderr, "xc_dom_mem_init failed\n");
         goto err;
+    }
     rv = xc_dom_boot_mem_init(dom);
     if ( rv )
+    {
+        fprintf(stderr, "xc_dom_boot_mem_init failed\n");
         goto err;
+    }
     rv = xc_dom_build_image(dom);
     if ( rv )
+    {
+        fprintf(stderr, "xc_dom_build_image failed\n");
         goto err;
+    }
     rv = xc_dom_boot_image(dom);
     if ( rv )
+    {
+        fprintf(stderr, "xc_dom_boot_image failed\n");
         goto err;
+    }
 
     xc_dom_release(dom);
     dom = NULL;
 
     rv = xc_domain_set_virq_handler(xch, domid, VIRQ_DOM_EXC);
     if ( rv )
+    {
+        fprintf(stderr, "xc_domain_set_virq_handler failed\n");
         goto err;
+    }
     rv = xc_domain_unpause(xch, domid);
     if ( rv )
+    {
+        fprintf(stderr, "xc_domain_unpause failed\n");
         goto err;
+    }
 
     return 0;
 
@@ -109,14 +161,18 @@ int main(int argc, char** argv)
 
     if ( argc < 4 || argc > 5 )
     {
-        printf("Use: %s <xenstore-kernel> <memory_mb> <flask-label> [<ramdisk-file>]\n",
-               argv[0]);
+        fprintf(stderr,
+                "Use: %s <xenstore-kernel> <memory_mb> <flask-label> [<ramdisk-file>]\n",
+            argv[0]);
         return 2;
     }
 
     xch = xc_interface_open(NULL, NULL, 0);
     if ( !xch )
+    {
+        fprintf(stderr, "xc_interface_open() failed\n");
         return 1;
+    }
 
     rv = build(xch, argc, argv);
 
@@ -132,12 +188,18 @@ int main(int argc, char** argv)
 
     fd = creat("/var/run/xenstored.pid", 0666);
     if ( fd < 0 )
+    {
+        fprintf(stderr, "Creating /var/run/xenstored.pid failed\n");
         return 3;
+    }
     rv = snprintf(buf, 16, "domid:%d\n", domid);
     rv = write(fd, buf, rv);
     close(fd);
     if ( rv < 0 )
+    {
+        fprintf(stderr, "Writing domid to /var/run/xenstored.pid failed\n");
         return 3;
+    }
 
     return 0;
 }
