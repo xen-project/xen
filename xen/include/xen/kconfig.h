601b5977008770e872827689eb5e868276eea7b9
Shannon Zhao <shannon.zhao@linaro.org>
2016-02-01 13:55:51 +0100

    Kconfig: import kconfig.h from Linux 4.3
    To support using CONFIG_ options in C/CPP expressions, import kconfig.h
    from the Linux v4.3 tag (commit id
    6a13feb9c82803e2b815eca72fa7a9f5561d7861).
    Only import IS_ENABLED for Xen since Xen doesn't support loadable
    modules.
    
    Signed-off-by: Shannon Zhao <shannon.zhao@linaro.org>
    Reviewed-by: Doug Goldstein <cardoe@cardoe.com>
    Acked-by: Jan Beulich <jbeulich@suse.com>

---
+++ xen/include/xen/kconfig.h
@@ -0,0 +1,31 @@
+#ifndef __XEN_KCONFIG_H
+#define __XEN_KCONFIG_H
+
+#include <generated/autoconf.h>
+
+/*
+ * Helper macros to use CONFIG_ options in C/CPP expressions. Note that
+ * these only work with boolean option.
+ */
+
+/*
+ * Getting something that works in C and CPP for an arg that may or may
+ * not be defined is tricky.  Here, if we have "#define CONFIG_BOOGER 1"
+ * we match on the placeholder define, insert the "0," for arg1 and generate
+ * the triplet (0, 1, 0).  Then the last step cherry picks the 2nd arg (a one).
+ * When CONFIG_BOOGER is not defined, we generate a (... 1, 0) pair, and when
+ * the last step cherry picks the 2nd arg, we get a zero.
+ */
+#define __ARG_PLACEHOLDER_1 0,
+#define config_enabled(cfg) _config_enabled(cfg)
+#define _config_enabled(value) __config_enabled(__ARG_PLACEHOLDER_##value)
+#define __config_enabled(arg1_or_junk) ___config_enabled(arg1_or_junk 1, 0)
+#define ___config_enabled(__ignored, val, ...) val
+
+/*
+ * IS_ENABLED(CONFIG_FOO) evaluates to 1 if CONFIG_FOO is set to 'y', 0
+ * otherwise.
+ */
+#define IS_ENABLED(option) config_enabled(option)
+
+#endif /* __XEN_KCONFIG_H */
