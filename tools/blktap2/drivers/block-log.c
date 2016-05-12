744fe0347d584f8b80b91ece93ef87e903c41bfa
Jan Beulich <jbeulich@suse.com>
2016-05-12 14:24:39 +0200

    Revert "blktap2: Use RING_COPY_REQUEST"
    This reverts commit 19f6c522a6a9599317ee1d8c4a155d1400d04c89. It
    did wrongly get associated with XSA-155, and was (rightfully) never
    backported to any of the stable trees. See also
    http://lists.xenproject.org/archives/html/xen-devel/2016-03/msg00571.html.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Acked-by: Wei Liu <wei.liu2@citrix.com>
    Acked-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Release-acked-by: Wei Liu <wei.liu2@citrix.com>

--- tools/blktap2/drivers/block-log.c
+++ tools/blktap2/drivers/block-log.c
@@ -494,12 +494,11 @@ static int ctl_kick(struct tdlog_state* s, int fd)
   reqstart = s->bring.req_cons;
   reqend = s->sring->req_prod;
 
-  xen_mb();
   BDPRINTF("ctl: ring kicked (start = %u, end = %u)", reqstart, reqend);
 
   while (reqstart != reqend) {
     /* XXX actually submit these! */
-    RING_COPY_REQUEST(&s->bring, reqstart, &req);
+    memcpy(&req, RING_GET_REQUEST(&s->bring, reqstart), sizeof(req));
     BDPRINTF("ctl: read request %"PRIu64":%u", req.sector, req.count);
     s->bring.req_cons = ++reqstart;
 
