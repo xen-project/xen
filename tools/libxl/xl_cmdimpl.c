f8972b4223ee76800afa13432403daf248b46805
Juergen Gross <jgross@suse.com>
2016-06-14 06:30:58 +0200

    xl: correct xl cpupool-numa-split with vcpu limited dom0
    When trying to use xl cpupool-numa-split and dom0 is limited to less
    vcpus than one numa node the operation will fail.
    
    Correct this by allowing this configuration.
    
    Reported-by: Glenn Enright <glenn@rimuhosting.com>
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Reviewed-by: Dario Faggioli <dario.faggioli@citrix.com>
    Acked-by: Wei Liu <wei.liu2@citrix.com>
    (cherry picked from commit c256d2afc1cad0cca912492e338d6ff97e477c4f)
    (cherry picked from commit 78a30103813a7929e922de7414cc56fa2ae52984)

--- tools/libxl/xl_cmdimpl.c
+++ tools/libxl/xl_cmdimpl.c
@@ -7766,7 +7766,11 @@ int main_cpupoolnumasplit(int argc, char **argv)
             n++;
         }
     }
-    if (libxl_set_vcpuonline(ctx, 0, &cpumap)) {
+    if (libxl_domain_info(ctx, &info, 0)) {
+        fprintf(stderr, "error on getting info for Domain-0\n");
+        goto out;
+    }
+    if (info.vcpu_online > n && libxl_set_vcpuonline(ctx, 0, &cpumap)) {
         fprintf(stderr, "error on removing vcpus for Domain-0\n");
         goto out;
     }
@@ -7781,7 +7785,7 @@ int main_cpupoolnumasplit(int argc, char **argv)
             fprintf(stderr, "error on getting info for Domain-0\n");
             goto out;
         }
-        if (info.vcpu_online == n) {
+        if (info.vcpu_online <= n) {
             break;
         }
         sleep(1);
