c36eba84a11f7cc4b40cd181d2f1215b105fc72b
Julien Grall <julien.grall@arm.com>
2016-06-22 12:15:22 +0100

    xen/arm: arm64: Reserve a brk immediate to fault on purpose
    It may not possible to return a proper error when encoding an
    instruction. Instead, a handcrafted instruction will be returned.
    
    Also, provide the encoding for the faulting instruction.
    
    Signed-off-by: Julien Grall <julien.grall@arm.com>
    Acked-by: Stefano Stabellini <sstabellini@kernel.org>

--- xen/include/asm-arm/arm64/brk.h
+++ xen/include/asm-arm/arm64/brk.h
@@ -12,8 +12,21 @@
 /*
  * #imm16 values used for BRK instruction generation
  * 0x001: xen-mode BUG() and WARN() traps
+ * 0x002: for triggering a fault on purpose (reserved)
  */
 #define BRK_BUG_FRAME_IMM   1
+#define BRK_FAULT_IMM       2
+
+/*
+ * BRK instruction encoding
+ * The #imm16 value should be placed at bits[20:5] within BRK ins
+ */
+#define AARCH64_BREAK_MON 0xd4200000
+
+/*
+ * BRK instruction for provoking a fault on purpose
+ */
+#define AARCH64_BREAK_FAULT (AARCH64_BREAK_MON | (BRK_FAULT_IMM << 5))
 
 #endif /* !__ASM_ARM_ARM64_BRK */
 /*
