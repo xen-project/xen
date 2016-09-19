44db782fd20edae12d9292e0e72450ae46f6a739
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2016-09-19 11:12:51 -0400

    arm/x86/common: Add HAS_[ALTERNATIVE|EX_TABLE]
    x86 implements all of them by default - and we just
    add two extra HAS_ variables to be declared in autoconf.h.
    
    ARM 64 only has alternative while ARM 32 has none of them.
    
    And while at it change the livepatch common code that
    would benefit from this.
    
    Reviewed-by: Julien Grall <julien.grall@arm.com>
    Reviewed-by: Ross Lagerwall <ross.lagerwall@citrix.com>
    Acked-by: Jan Beulich <jbeulich@suse.com> [relevant parts]
    Suggested-by: Julien Grall <julien.grall@arm.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- xen/common/livepatch.c
+++ xen/common/livepatch.c
@@ -641,7 +641,7 @@ static int prepare_payload(struct payload *payload,
                                   sizeof(*region->frame[i].bugs);
     }
 
-#ifndef CONFIG_ARM
+#ifdef CONFIG_HAS_ALTERNATIVE
     sec = livepatch_elf_sec_by_name(elf, ".altinstructions");
     if ( sec )
     {
@@ -672,7 +672,9 @@ static int prepare_payload(struct payload *payload,
         }
         apply_alternatives(start, end);
     }
+#endif
 
+#ifdef CONFIG_HAS_EX_TABLE
     sec = livepatch_elf_sec_by_name(elf, ".ex_table");
     if ( sec )
     {
