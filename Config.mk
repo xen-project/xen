86f912c86501b9a3c1abf908731e7d86778a594e
Jan Beulich <jbeulich@suse.com>
2016-11-04 12:30:52 +0100

    update Xen version to 4.7.1

--- Config.mk
+++ Config.mk
@@ -272,8 +272,8 @@ SEABIOS_UPSTREAM_URL ?= git://xenbits.xen.org/seabios.git
 MINIOS_UPSTREAM_URL ?= git://xenbits.xen.org/mini-os.git
 endif
 OVMF_UPSTREAM_REVISION ?= 52a99493cce88a9d4ec8a02d7f1bd1a1001ce60d
-QEMU_UPSTREAM_REVISION ?= qemu-xen-4.7.0
-MINIOS_UPSTREAM_REVISION ?= xen-RELEASE-4.7.0
+QEMU_UPSTREAM_REVISION ?= qemu-xen-4.7.1
+MINIOS_UPSTREAM_REVISION ?= xen-RELEASE-4.7.1
 
 SEABIOS_UPSTREAM_REVISION ?= rel-1.9.2
 # Tue, 1 Mar 2016 15:06:45 +0100 (16:06 +0200)
@@ -282,9 +282,7 @@ SEABIOS_UPSTREAM_REVISION ?= rel-1.9.2
 ETHERBOOT_NICS ?= rtl8139 8086100e
 
 
-QEMU_TRADITIONAL_REVISION ?= 81111451256fd2f77c361fe65fa591743dbf04db
-# Tue Jul 26 15:31:59 2016 +0100
-# virtio: error out if guest exceeds virtqueue size
+QEMU_TRADITIONAL_REVISION ?= xen-4.7.1
 
 # Specify which qemu-dm to use. This may be `ioemu' to use the old
 # Mercurial in-tree version, or a local directory, or a git URL.
