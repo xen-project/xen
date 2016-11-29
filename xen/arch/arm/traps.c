2cf7d2bafb9b68add1710b8c3f7ecad58e53a9db
Wei Chen <Wei.Chen@arm.com>
2016-11-29 15:57:52 +0100

    arm64: handle guest-generated EL1 asynchronous abort
    In current code, when the hypervisor receives an asynchronous abort
    from a guest, the hypervisor will do panic, the host will be down.
    We have to prevent such security issue, so, in this patch we crash
    the guest, when the hypervisor receives an asynchronous abort from
    the guest.
    
    This is part of XSA-201.
    
    Signed-off-by: Wei Chen <Wei.Chen@arm.com>
    Reviewed-by: Stefano Stabellini <sstabellini@kernel.org>
    Reviewed-by: Steve Capper <steve.capper@arm.com>
    Reviewed-by: Julien Grall <Julien.Grall@arm.com>

--- xen/arch/arm/traps.c
+++ xen/arch/arm/traps.c
@@ -2723,6 +2723,21 @@ asmlinkage void do_trap_hypervisor(struct cpu_user_regs *regs)
     }
 }
 
+asmlinkage void do_trap_guest_error(struct cpu_user_regs *regs)
+{
+    enter_hypervisor_head(regs);
+
+    /*
+     * Currently, to ensure hypervisor safety, when we received a
+     * guest-generated vSerror/vAbort, we just crash the guest to protect
+     * the hypervisor. In future we can better handle this by injecting
+     * a vSerror/vAbort to the guest.
+     */
+    gdprintk(XENLOG_WARNING, "Guest(Dom-%u) will be crashed by vSError\n",
+             current->domain->domain_id);
+    domain_crash_synchronous();
+}
+
 asmlinkage void do_trap_irq(struct cpu_user_regs *regs)
 {
     enter_hypervisor_head(regs);
