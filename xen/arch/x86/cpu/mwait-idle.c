c585f9f0da806d6256eab76c1dbaa7102062aced
Piotr Luc <piotr.luc@intel.com>
2017-01-04 14:29:30 +0100

    x86/mwait-idle: add Knights Mill CPUID
    Add Knights Mill (KNM) to the list of CPUIDs supported by mwait-idle.
    
    Signed-off-by: Piotr Luc <piotr.luc@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>
    [Linux commit: a2c1bc645e87346150516b3abf1933ed29d0f48b]
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Acked-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- xen/arch/x86/cpu/mwait-idle.c
+++ xen/arch/x86/cpu/mwait-idle.c
@@ -946,6 +946,7 @@ static const struct x86_cpu_id intel_idle_ids[] __initconstrel = {
 	ICPU(0x9e, skl),
 	ICPU(0x55, skx),
 	ICPU(0x57, knl),
+	ICPU(0x85, knl),
 	ICPU(0x5c, bxt),
 	ICPU(0x5f, dnv),
 	{}
