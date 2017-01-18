7524025b197a8ff38236f3e73a8667c5ba527e6c
Andrew Cooper <andrew.cooper3@citrix.com>
2017-01-18 10:06:42 +0100

    x86/emul: Don't deliver #UD with an error code
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>
    master commit: 9586cba3383021bb4bd57f3fa33e87cc64b4c74a
    master date: 2016-11-25 10:48:10 +0000

--- xen/arch/x86/hvm/emulate.c
+++ xen/arch/x86/hvm/emulate.c
@@ -1647,7 +1647,8 @@ static int hvmemul_vmfunc(
         return X86EMUL_UNHANDLEABLE;
     rc = hvm_funcs.altp2m_vcpu_emulate_vmfunc(ctxt->regs);
     if ( rc != X86EMUL_OKAY )
-        hvmemul_inject_hw_exception(TRAP_invalid_op, 0, ctxt);
+        hvmemul_inject_hw_exception(TRAP_invalid_op, HVM_DELIVER_NO_ERROR_CODE,
+                                    ctxt);
 
     return rc;
 }
