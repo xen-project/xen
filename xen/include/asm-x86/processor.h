f3358a8cd7e2debdfe87472f299235316a5dc062
Wei Liu <wei.liu2@citrix.com>
2017-01-30 12:51:08 +0000

    x86: extract vendor numeric id to x86-vendors.h
    They will be shared between xen and userspace programs.
    
    This is not strictly necessary, but it helps reduce overall code size.
    
    No functional change.
    
    Signed-off-by: Wei Liu <wei.liu2@citrix.com>
    Acked-by: Jan Beulich <jbeulich@suse.com>

--- xen/include/asm-x86/processor.h
+++ xen/include/asm-x86/processor.h
@@ -17,16 +17,7 @@
 #endif
 
 #include <asm/x86-defns.h>
-
-/*
- * CPU vendor IDs
- */
-#define X86_VENDOR_INTEL 0
-#define X86_VENDOR_AMD 1
-#define X86_VENDOR_CENTAUR 2
-#define X86_VENDOR_NUM 3
-#define X86_VENDOR_UNKNOWN 0xff
-
+#include <asm/x86-vendors.h>
 
 /*
  * Trap/fault mnemonics.
---
+++ xen/include/asm-x86/x86-vendors.h
@@ -0,0 +1,13 @@
+#ifndef __XEN_X86_VENDORS_H__
+#define __XEN_X86_VENDORS_H__
+
+/*
+ * CPU vendor IDs
+ */
+#define X86_VENDOR_INTEL 0
+#define X86_VENDOR_AMD 1
+#define X86_VENDOR_CENTAUR 2
+#define X86_VENDOR_NUM 3
+#define X86_VENDOR_UNKNOWN 0xff
+
+#endif	/* __XEN_X86_VENDORS_H__ */
