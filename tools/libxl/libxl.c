3fb71d79e971b6d7c51de635613da7c33efdce6e
Juergen Gross <jgross@suse.com>
2017-02-08 17:09:29 +0100

    libxl: adjust copyright comment of libxl.c
    The copyright of libxl.c is a little bit outdated.
    
    Adjust it to reality.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Reviewed-by: Wei Liu <wei.liu2@citrix.com>
    Acked-by: Ian Jackson <ian.jackson@eu.citrix.com>

--- tools/libxl/libxl.c
+++ tools/libxl/libxl.c
@@ -1,7 +1,5 @@
 /*
- * Copyright (C) 2009      Citrix Ltd.
- * Author Vincent Hanquez <vincent.hanquez@eu.citrix.com>
- * Author Stefano Stabellini <stefano.stabellini@eu.citrix.com>
+ * Copyright 2009-2017 Citrix Ltd and other contributors
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
