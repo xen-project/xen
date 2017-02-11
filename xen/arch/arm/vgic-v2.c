378384399ed661bed711221a5d8dbdac66b8e851
Stefano Stabellini <sstabellini@kernel.org>
2017-02-10 18:05:22 -0800

    arm: read/write rank->vcpu atomically
    We don't need a lock in vgic_get_target_vcpu anymore, solving the
    following lock inversion bug: the rank lock should be taken first, then
    the vgic lock. However, gic_update_one_lr is called with the vgic lock
    held, and it calls vgic_get_target_vcpu, which tries to obtain the rank
    lock.
    
    Coverity-ID: 1381855
    Coverity-ID: 1381853
    
    Signed-off-by: Stefano Stabellini <sstabellini@kernel.org>
    Reviewed-by: Julien Grall <julien.grall@arm.com>

--- xen/arch/arm/vgic-v2.c
+++ xen/arch/arm/vgic-v2.c
@@ -78,7 +78,7 @@ static uint32_t vgic_fetch_itargetsr(struct vgic_irq_rank *rank,
     offset &= ~(NR_TARGETS_PER_ITARGETSR - 1);
 
     for ( i = 0; i < NR_TARGETS_PER_ITARGETSR; i++, offset++ )
-        reg |= (1 << rank->vcpu[offset]) << (i * NR_BITS_PER_TARGET);
+        reg |= (1 << read_atomic(&rank->vcpu[offset])) << (i * NR_BITS_PER_TARGET);
 
     return reg;
 }
@@ -151,7 +151,7 @@ static void vgic_store_itargetsr(struct domain *d, struct vgic_irq_rank *rank,
         /* The vCPU ID always starts from 0 */
         new_target--;
 
-        old_target = rank->vcpu[offset];
+        old_target = read_atomic(&rank->vcpu[offset]);
 
         /* Only migrate the vIRQ if the target vCPU has changed */
         if ( new_target != old_target )
@@ -161,7 +161,7 @@ static void vgic_store_itargetsr(struct domain *d, struct vgic_irq_rank *rank,
                              virq);
         }
 
-        rank->vcpu[offset] = new_target;
+        write_atomic(&rank->vcpu[offset], new_target);
     }
 }
 
