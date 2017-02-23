5b8c41aeca9a4b557f2cdd74c7f5b677a57b7d88
Wei Liu <wei.liu2@citrix.com>
2017-02-23 18:38:13 +0000

    xl: update copyright information
    Signed-off-by: Wei Liu <wei.liu2@citrix.com>
    Acked-by: Ian Jackson <ian.jackson@eu.citrix.com>

--- tools/xl/xl.h
+++ tools/xl/xl.h
@@ -1,5 +1,5 @@
 /*
- * Author Yang Hongyang <yanghy@cn.fujitsu.com>
+ * Copyright 2009-2017 Citrix Ltd and other contributors
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
