19b1599ae3a7d391ea26699999ff4d56e5b61d00
Jonathan Davies <jonathan.davies@citrix.com>
2017-03-23 17:40:27 +0000

    oxenstored: refactor request processing
    Encapsulate the request in a record that is passed from do_input to
    process_packet and input_handle_error.
    
    This will be helpful when keeping track of the requests made as part of a
    transaction.
    
    Reported-by: Juergen Gross <jgross@suse.com>
    Signed-off-by: Jonathan Davies <jonathan.davies@citrix.com>
    Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Jon Ludlam <jonathan.ludlam@citrix.com>
    Reviewed-by: Euan Harris <euan.harris@citrix.com>
    Acked-by: David Scott <dave@recoil.org>

--- tools/ocaml/xenstored/packet.ml
+++ tools/ocaml/xenstored/packet.ml
@@ -1,3 +1,10 @@
+type request = {
+	tid: int;
+	rid: int;
+	ty: Xenbus.Xb.Op.operation;
+	data: string;
+}
+
 type response =
 	| Ack of (unit -> unit)  (* function is the action to execute after sending the ack *)
 	| Reply of string
