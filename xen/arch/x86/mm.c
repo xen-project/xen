0e7ffe9604ecd3bf534e0d224025e516f2cbd209
Wei Liu <wei.liu2@citrix.com>
2017-03-24 11:34:01 +0000

    x86/mm: fix the check in get_pg_owner
    PVH (both v1 and v2) guest is actually an translated guest. It should be
    able to manipulate page table for other domains when acting as Dom0.
    
    The removal of PVHv1 deleted the special case for PVH guest but didn't
    add a check for HVM guest.
    
    Signed-off-by: Wei Liu <wei.liu2@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>

--- xen/arch/x86/mm.c
+++ xen/arch/x86/mm.c
@@ -3041,7 +3041,7 @@ static struct domain *get_pg_owner(domid_t domid)
         goto out;
     }
 
-    if ( unlikely(paging_mode_translate(curr)) )
+    if ( !is_hvm_domain(curr) && unlikely(paging_mode_translate(curr)) )
     {
         MEM_LOG("Cannot mix foreign mappings with translated domains");
         goto out;
