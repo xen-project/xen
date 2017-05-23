f4a27a000d03e121eb1a36c485049a820c395539
Julien Grall <julien.grall@arm.com>
2017-05-23 18:03:36 +0100

    xen/arm: Remove unused helpers access_ok and array_access_ok
    Both helpers access_ok and array_access_ok are not used on ARM. Remove
    them.
    
    Signed-off-by: Julien Grall <julien.grall@arm.com>
    Reviewed-by: Stefano Stabellini <sstabellini@kernel.org>

--- xen/include/asm-arm/guest_access.h
+++ xen/include/asm-arm/guest_access.h
@@ -4,13 +4,6 @@
 #include <xen/guest_access.h>
 #include <xen/errno.h>
 
-/* Guests have their own comlete address space */
-#define access_ok(addr,size) (1)
-
-#define array_access_ok(addr,count,size) \
-    (likely((count) < (~0UL / (size))) && \
-     access_ok(addr, 0 + (count) * (size)))
-
 unsigned long raw_copy_to_guest(void *to, const void *from, unsigned len);
 unsigned long raw_copy_to_guest_flush_dcache(void *to, const void *from,
                                              unsigned len);
