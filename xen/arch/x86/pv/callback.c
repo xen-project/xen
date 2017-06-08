e55bf825b20e592d9751962cecf4b69a4d15f86c
Wei Liu <wei.liu2@citrix.com>
2017-06-08 17:19:48 +0100

    x86: move the compat callback ops next to the non-compat variant
    Take the chance to change v to curr.
    
    Signed-off-by: Wei Liu <wei.liu2@citrix.com>
    Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- xen/arch/x86/pv/callback.c
+++ xen/arch/x86/pv/callback.c
@@ -19,6 +19,7 @@
 #include <xen/guest_access.h>
 #include <xen/lib.h>
 #include <xen/sched.h>
+#include <compat/callback.h>
 
 #include <asm/current.h>
 #include <asm/nmi.h>
@@ -168,6 +169,147 @@ long do_set_callbacks(unsigned long event_address,
     register_guest_callback(&event);
     register_guest_callback(&failsafe);
     register_guest_callback(&syscall);
+
+    return 0;
+}
+
+static long compat_register_guest_callback(struct compat_callback_register *reg)
+{
+    long ret = 0;
+    struct vcpu *curr = current;
+
+    fixup_guest_code_selector(curr->domain, reg->address.cs);
+
+    switch ( reg->type )
+    {
+    case CALLBACKTYPE_event:
+        curr->arch.pv_vcpu.event_callback_cs     = reg->address.cs;
+        curr->arch.pv_vcpu.event_callback_eip    = reg->address.eip;
+        break;
+
+    case CALLBACKTYPE_failsafe:
+        curr->arch.pv_vcpu.failsafe_callback_cs  = reg->address.cs;
+        curr->arch.pv_vcpu.failsafe_callback_eip = reg->address.eip;
+        if ( reg->flags & CALLBACKF_mask_events )
+            set_bit(_VGCF_failsafe_disables_events,
+                    &curr->arch.vgc_flags);
+        else
+            clear_bit(_VGCF_failsafe_disables_events,
+                      &curr->arch.vgc_flags);
+        break;
+
+    case CALLBACKTYPE_syscall32:
+        curr->arch.pv_vcpu.syscall32_callback_cs     = reg->address.cs;
+        curr->arch.pv_vcpu.syscall32_callback_eip    = reg->address.eip;
+        curr->arch.pv_vcpu.syscall32_disables_events =
+            (reg->flags & CALLBACKF_mask_events) != 0;
+        break;
+
+    case CALLBACKTYPE_sysenter:
+        curr->arch.pv_vcpu.sysenter_callback_cs     = reg->address.cs;
+        curr->arch.pv_vcpu.sysenter_callback_eip    = reg->address.eip;
+        curr->arch.pv_vcpu.sysenter_disables_events =
+            (reg->flags & CALLBACKF_mask_events) != 0;
+        break;
+
+    case CALLBACKTYPE_nmi:
+        ret = register_guest_nmi_callback(reg->address.eip);
+        break;
+
+    default:
+        ret = -ENOSYS;
+        break;
+    }
+
+    return ret;
+}
+
+static long compat_unregister_guest_callback(
+    struct compat_callback_unregister *unreg)
+{
+    long ret;
+
+    switch ( unreg->type )
+    {
+    case CALLBACKTYPE_event:
+    case CALLBACKTYPE_failsafe:
+    case CALLBACKTYPE_syscall32:
+    case CALLBACKTYPE_sysenter:
+        ret = -EINVAL;
+        break;
+
+    case CALLBACKTYPE_nmi:
+        ret = unregister_guest_nmi_callback();
+        break;
+
+    default:
+        ret = -ENOSYS;
+        break;
+    }
+
+    return ret;
+}
+
+long compat_callback_op(int cmd, XEN_GUEST_HANDLE(void) arg)
+{
+    long ret;
+
+    switch ( cmd )
+    {
+    case CALLBACKOP_register:
+    {
+        struct compat_callback_register reg;
+
+        ret = -EFAULT;
+        if ( copy_from_guest(&reg, arg, 1) )
+            break;
+
+        ret = compat_register_guest_callback(&reg);
+    }
+    break;
+
+    case CALLBACKOP_unregister:
+    {
+        struct compat_callback_unregister unreg;
+
+        ret = -EFAULT;
+        if ( copy_from_guest(&unreg, arg, 1) )
+            break;
+
+        ret = compat_unregister_guest_callback(&unreg);
+    }
+    break;
+
+    default:
+        ret = -EINVAL;
+        break;
+    }
+
+    return ret;
+}
+
+long compat_set_callbacks(unsigned long event_selector,
+                          unsigned long event_address,
+                          unsigned long failsafe_selector,
+                          unsigned long failsafe_address)
+{
+    struct compat_callback_register event = {
+        .type = CALLBACKTYPE_event,
+        .address = {
+            .cs = event_selector,
+            .eip = event_address
+        }
+    };
+    struct compat_callback_register failsafe = {
+        .type = CALLBACKTYPE_failsafe,
+        .address = {
+            .cs = failsafe_selector,
+            .eip = failsafe_address
+        }
+    };
+
+    compat_register_guest_callback(&event);
+    compat_register_guest_callback(&failsafe);
 
     return 0;
 }
