f7ad92a74f0a3fd5a7b8bb77c346084ce3e8643c
Jan Beulich <jbeulich@suse.com>
2017-06-23 15:19:27 +0200

    memory: don't suppress P2M update in populate_physmap()
    Commit d18627583d ("memory: don't hand MFN info to translated guests")
    wrongly added a null-handle check there - just like stated in its
    description for memory_exchange(), the array is also an input for
    populate_physmap() (and hence can't reasonably be null). I have no idea
    how I've managed to overlook this.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Acked-by: Andrew Cooper <andrew.cooper3@citrix.com>
    master commit: b964e3106d2cdaa11cc4524181ff14607d110ae4
    master date: 2017-06-20 14:51:53 +0200

--- xen/common/memory.c
+++ xen/common/memory.c
@@ -216,8 +216,7 @@ static void populate_physmap(struct memop_args *a)
             mfn = page_to_mfn(page);
             guest_physmap_add_page(d, gpfn, mfn, a->extent_order);
 
-            if ( !paging_mode_translate(d) &&
-                 !guest_handle_is_null(a->extent_list) )
+            if ( !paging_mode_translate(d) )
             {
                 for ( j = 0; j < (1 << a->extent_order); j++ )
                     set_gpfn_from_mfn(mfn + j, gpfn + j);
