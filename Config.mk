f7395dbc5c0a69899717edfbed8bc5e22d912bc6
Ian Jackson <ian.jackson@eu.citrix.com>
2017-06-27 19:10:43 +0100

    Xen 4.9.0 release: Adjust Config.mk commits to refer to 4.9.0 tags
    Signed-off-by: Ian Jackson <Ian.Jackson@eu.citrix.com>

--- Config.mk
+++ Config.mk
@@ -268,10 +268,10 @@ SEABIOS_UPSTREAM_URL ?= git://xenbits.xen.org/seabios.git
 MINIOS_UPSTREAM_URL ?= git://xenbits.xen.org/mini-os.git
 endif
 OVMF_UPSTREAM_REVISION ?= 5920a9d16b1ab887c2858224316a98e961d71b05
-QEMU_UPSTREAM_REVISION ?= 414d069b38ab114b89085e44989bf57604ea86d7
-MINIOS_UPSTREAM_REVISION ?= xen-4.9.0-rc1.2
-# Tue Dec 13 15:02:02 2016 +0000
-# build: prepend OBJ_DIR to linker script
+QEMU_UPSTREAM_REVISION ?= qemu-xen-4.9.0
+MINIOS_UPSTREAM_REVISION ?= xen-RELEASE-4.9.0
+# Wed Feb 22 11:03:37 2017 +0000
+# Include libxendevicemodel with libxc
 
 SEABIOS_UPSTREAM_REVISION ?= rel-1.10.0
 # Wed Jun 22 14:53:24 2016 +0800
@@ -280,7 +280,7 @@ SEABIOS_UPSTREAM_REVISION ?= rel-1.10.0
 ETHERBOOT_NICS ?= rtl8139 8086100e
 
 
-QEMU_TRADITIONAL_REVISION ?= xen-4.9.0-rc1
+QEMU_TRADITIONAL_REVISION ?= xen-4.9.0
 # Thu Mar 9 11:14:55 2017 +0000
 # cirrus/vnc: zap drop bitblit support from console code.
 
