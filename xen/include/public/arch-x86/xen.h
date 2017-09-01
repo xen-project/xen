39a2a62e5626327f141596ed3e78a55899437e11
Roger Pau Monné <roger.pau@citrix.com>
2017-09-01 11:06:44 +0200

    x86/pvh: remove stale PVHv1 comment from public headers
    From the vcpu_guest_context structure. PVHv2 uses it in the same exact
    way as HVM guests, and from the hypervisor point of view PVHv2 is not
    even a different guest type, so only mention HVM in the public
    headers.
    
    Reported-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Acked-by: Jan Beulich <jbeulich@suse.com>

--- xen/include/public/arch-x86/xen.h
+++ xen/include/public/arch-x86/xen.h
@@ -162,14 +162,10 @@ typedef uint64_t tsc_timestamp_t; /* RDTSC timestamp */
  * The following is all CPU context. Note that the fpu_ctxt block is filled
  * in by FXSAVE if the CPU has feature FXSR; otherwise FSAVE is used.
  *
- * Also note that when calling DOMCTL_setvcpucontext and VCPUOP_initialise
- * for HVM and PVH guests, not all information in this structure is updated:
- *
- * - For HVM guests, the structures read include: fpu_ctxt (if
- * VGCT_I387_VALID is set), flags, user_regs, debugreg[*]
- *
- * - PVH guests are the same as HVM guests, but additionally use ctrlreg[3] to
- * set cr3. All other fields not used should be set to 0.
+ * Also note that when calling DOMCTL_setvcpucontext and VCPUOP_initialise for
+ * HVM guests, not all information in this structure is updated, the fields
+ * read include: fpu_ctxt (if VGCT_I387_VALID is set), flags, user_regs and
+ * debugreg[*].
  */
 struct vcpu_guest_context {
     /* FPU registers come first so they can be aligned for FXSAVE/FXRSTOR. */
