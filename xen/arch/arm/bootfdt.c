ee582140af6eb29cad83878542ccf4fac544ccfe
Wei Liu <wei.liu2@citrix.com>
2017-09-15 10:12:33 +0100

    arm/bootfdt.c: switch to plain bool
    Signed-off-by: Wei Liu <wei.liu2@citrix.com>
    Reviewed-by: Julien Grall <julien.grall@arm.com>

--- xen/arch/arm/bootfdt.c
+++ xen/arch/arm/bootfdt.c
@@ -16,8 +16,8 @@
 #include <xsm/xsm.h>
 #include <asm/setup.h>
 
-static bool_t __init device_tree_node_matches(const void *fdt, int node,
-                                              const char *match)
+static bool __init device_tree_node_matches(const void *fdt, int node,
+                                            const char *match)
 {
     const char *name;
     size_t match_len;
@@ -31,8 +31,8 @@ static bool_t __init device_tree_node_matches(const void *fdt, int node,
         && (name[match_len] == '@' || name[match_len] == '\0');
 }
 
-static bool_t __init device_tree_node_compatible(const void *fdt, int node,
-                                                 const char *match)
+static bool __init device_tree_node_compatible(const void *fdt, int node,
+                                               const char *match)
 {
     int len, l;
     int mlen;
@@ -42,17 +42,17 @@ static bool_t __init device_tree_node_compatible(const void *fdt, int node,
 
     prop = fdt_getprop(fdt, node, "compatible", &len);
     if ( prop == NULL )
-        return 0;
+        return false;
 
     while ( len > 0 ) {
         if ( !dt_compat_cmp(prop, match) )
-            return 1;
+            return true;
         l = strlen(prop) + 1;
         prop += l;
         len -= l;
     }
 
-    return 0;
+    return false;
 }
 
 static void __init device_tree_get_reg(const __be32 **cell, u32 address_cells,
