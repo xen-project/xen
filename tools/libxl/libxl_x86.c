1a42ffa3476ab433da9dc27c6d36f051b70592ed
Juergen Gross <jgross@suse.com>
2017-11-21 12:06:06 +0100

    tools/libxl: mark special pages as reserved in e820 map for PVH
    The "special pages" for PVH guests include the frames for console and
    Xenstore ring buffers. Those have to be marked as "Reserved" in the
    guest's E820 map, as otherwise conflicts might arise later e.g. when
    hotplugging memory into the guest.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Reviewed-by: Roger Pau Monné <roger.pau@citrix.com>
    Acked-by: Wei Liu <wei.liu2@citrix.com>

--- tools/libxl/libxl_x86.c
+++ tools/libxl/libxl_x86.c
@@ -530,6 +530,9 @@ int libxl__arch_domain_construct_memmap(libxl__gc *gc,
         if (d_config->rdms[i].policy != LIBXL_RDM_RESERVE_POLICY_INVALID)
             e820_entries++;
 
+    /* Add mmio entry for PVH. */
+    if (dom->mmio_size && d_config->b_info.type == LIBXL_DOMAIN_TYPE_PVH)
+        e820_entries++;
 
     /* If we should have a highmem range. */
     if (highmem_size)
@@ -560,6 +563,14 @@ int libxl__arch_domain_construct_memmap(libxl__gc *gc,
 
         e820[nr].addr = d_config->rdms[i].start;
         e820[nr].size = d_config->rdms[i].size;
+        e820[nr].type = E820_RESERVED;
+        nr++;
+    }
+
+    /* mmio area */
+    if (dom->mmio_size && d_config->b_info.type == LIBXL_DOMAIN_TYPE_PVH) {
+        e820[nr].addr = dom->mmio_start;
+        e820[nr].size = dom->mmio_size;
         e820[nr].type = E820_RESERVED;
         nr++;
     }
