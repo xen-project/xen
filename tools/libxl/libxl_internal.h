357bf02e49a019b1bb9f508a23bd4841d1daf614
Wei Liu <wei.liu2@citrix.com>
2018-01-18 12:32:35 +0000

    x86/shim: use credit scheduler
    Remove sched=null from shim cmdline and doc
    
    We use the default scheduler (credit1 as of writing). The NULL
    scheduler still has bugs to fix.
    
    Update shim.config.
    
    Signed-off-by: Wei Liu <wei.liu2@citrix.com>
    Reviewed-by: Roger Pau Monné <roger.pau@citrix.com>
    Acked-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Acked-by: Ian Jackson <ian.jackson@eu.citrix.com>
    (cherry picked from commit 831cbe98f6d13e24a3e6521ddd35102729e1d505)

--- tools/libxl/libxl_internal.h
+++ tools/libxl/libxl_internal.h
@@ -119,7 +119,7 @@
 #define DOMID_XS_PATH "domid"
 #define INVALID_DOMID ~0
 #define PVSHIM_BASENAME "xen-shim"
-#define PVSHIM_CMDLINE "pv-shim console=xen,pv sched=null"
+#define PVSHIM_CMDLINE "pv-shim console=xen,pv"
 
 /* Size macros. */
 #define __AC(X,Y)   (X##Y)
