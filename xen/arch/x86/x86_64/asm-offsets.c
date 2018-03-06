31bf55cb5fe3796cf6a4efbcfc0a9418bb1c783f
Jan Beulich <jbeulich@suse.com>
2018-03-06 16:49:36 +0100

    x86: remove CR reads from exit-to-guest path
    CR3 is - during normal operation - only ever loaded from v->arch.cr3,
    so there's no need to read the actual control register. For CR4 we can
    generally use the cached value on all synchronous entry end exit paths.
    Drop the write_cr3 macro, as the two use sites are probably easier to
    follow without its use.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Tested-by: Juergen Gross <jgross@suse.com>
    Reviewed-by: Juergen Gross <jgross@suse.com>
    Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- xen/arch/x86/x86_64/asm-offsets.c
+++ xen/arch/x86/x86_64/asm-offsets.c
@@ -87,6 +87,7 @@ void __dummy__(void)
     OFFSET(VCPU_kernel_ss, struct vcpu, arch.pv_vcpu.kernel_ss);
     OFFSET(VCPU_iopl, struct vcpu, arch.pv_vcpu.iopl);
     OFFSET(VCPU_guest_context_flags, struct vcpu, arch.vgc_flags);
+    OFFSET(VCPU_cr3, struct vcpu, arch.cr3);
     OFFSET(VCPU_arch_msr, struct vcpu, arch.msr);
     OFFSET(VCPU_nmi_pending, struct vcpu, nmi_pending);
     OFFSET(VCPU_mce_pending, struct vcpu, mce_pending);
