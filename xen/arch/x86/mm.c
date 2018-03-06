ab62fc3171b560a64fc3156d42776672943228f0
Jan Beulich <jbeulich@suse.com>
2018-03-06 15:48:31 +0100

    x86: reduce Meltdown band-aid IPI overhead
    In case we can detect single-threaded guest processes (by checking
    whether we can account for all root page table uses locally on the vCPU
    that's running), there's no point in issuing a sync IPI upon an L4 entry
    update, as no other vCPU of the guest will have that page table loaded.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Acked-by: George Dunlap <george.dunlap@citrix.com>
    Acked-by: Andrew Cooper <andrew.cooper3@citrix.com>
    master commit: a22320e32dca0918ed23799583f470afe4c24330
    master date: 2018-02-07 16:31:41 +0100

--- xen/arch/x86/mm.c
+++ xen/arch/x86/mm.c
@@ -3664,8 +3664,18 @@ long do_mmu_update(
                 case PGT_l4_page_table:
                     rc = mod_l4_entry(va, l4e_from_intpte(req.val), mfn,
                                       cmd == MMU_PT_UPDATE_PRESERVE_AD, v);
-                    if ( !rc )
-                        sync_guest = this_cpu(root_pgt);
+                    /*
+                     * No need to sync if all uses of the page can be accounted
+                     * to the page lock we hold, its pinned status, and uses on
+                     * this (v)CPU.
+                     */
+                    if ( !rc && this_cpu(root_pgt) &&
+                         ((page->u.inuse.type_info & PGT_count_mask) >
+                          (1 + !!(page->u.inuse.type_info & PGT_pinned) +
+                           (pagetable_get_pfn(curr->arch.guest_table) == mfn) +
+                           (pagetable_get_pfn(curr->arch.guest_table_user) ==
+                            mfn))) )
+                        sync_guest = true;
                     break;
                 case PGT_writable_page:
                     perfc_incr(writable_mmu_updates);
