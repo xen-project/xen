860f470ba1d6a5e018bf147890988843ba781a5e
Jan Beulich <jbeulich@suse.com>
2018-03-20 14:19:36 +0100

    x86/xpti: don't map stack guard pages
    Other than for the main mappings, don't even do this in release builds,
    as there are no huge page shattering concerns here.
    
    Note that since we don't run on the restructed page tables while HVM
    guests execute, the non-present mappings won't trigger the triple fault
    issue AMD SVM is susceptible to with our current placement of STGI vs
    TR loading.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Acked-by: Andrew Cooper <andrew.cooper3@citrix.com>
    master commit: d303784b68237ff3050daa184f560179dda21b8c
    master date: 2018-03-06 16:46:57 +0100

--- xen/include/asm-x86/mm.h
+++ xen/include/asm-x86/mm.h
@@ -519,6 +519,7 @@ void memguard_unguard_range(void *p, unsigned long l);
 
 void memguard_guard_stack(void *p);
 void memguard_unguard_stack(void *p);
+bool __attribute_const__ memguard_is_stack_guard_page(unsigned long addr);
 
 struct mmio_ro_emulate_ctxt {
         unsigned long cr2;
