5464d5f0c9cf030186ba88c246a3eb96f67277fc
Andrew Cooper <andrew.cooper3@citrix.com>
2018-03-28 15:21:39 +0100

    x86/spec-ctrl: CPUID/MSR definitions for L1D_FLUSH
    This is part of XSA-273 / CVE-2018-3646.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>
    (cherry picked from commit 3563fc2b2731a63fd7e8372ab0f5cef205bf8477)

--- xen/include/asm-x86/msr-index.h
+++ xen/include/asm-x86/msr-index.h
@@ -47,7 +47,11 @@
 #define ARCH_CAPABILITIES_RDCL_NO	(_AC(1, ULL) << 0)
 #define ARCH_CAPABILITIES_IBRS_ALL	(_AC(1, ULL) << 1)
 #define ARCH_CAPS_RSBA			(_AC(1, ULL) << 2)
+#define ARCH_CAPS_SKIP_L1DFL		(_AC(1, ULL) << 3)
 #define ARCH_CAPS_SSB_NO		(_AC(1, ULL) << 4)
+
+#define MSR_FLUSH_CMD			0x0000010b
+#define FLUSH_CMD_L1D			(_AC(1, ULL) << 0)
 
 /* Intel MSRs. Some also available on other CPUs */
 #define MSR_IA32_PERFCTR0		0x000000c1
