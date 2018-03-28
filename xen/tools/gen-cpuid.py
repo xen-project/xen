3f85ebbea00cddb7d3714806e02d906591ee0776
Andrew Cooper <andrew.cooper3@citrix.com>
2018-03-28 15:21:39 +0100

    x86/Intel: Mitigations for GPZ SP4 - Speculative Store Bypass
    To combat GPZ SP4 "Speculative Store Bypass", Intel have extended their
    speculative sidechannel mitigations specification as follows:
    
     * A feature bit to indicate that Speculative Store Bypass Disable is
       supported.
     * A new bit in MSR_SPEC_CTRL which, when set, disables memory disambiguation
       in the pipeline.
     * A new bit in MSR_ARCH_CAPABILITIES, which will be set in future hardware,
       indicating that the hardware is not susceptible to Speculative Store Bypass
       sidechannels.
    
    For contemporary processors, this interface will be implemented via a
    microcode update.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>

--- xen/tools/gen-cpuid.py
+++ xen/tools/gen-cpuid.py
@@ -257,10 +257,19 @@ def crunch_numbers(state):
                   AVX512BW, AVX512VL, AVX512VBMI, AVX512_4VNNIW,
                   AVX512_4FMAPS, AVX512_VPOPCNTDQ],
 
-        # Single Thread Indirect Branch Predictors enumerates a new bit in the
-        # MSR enumerated by Indirect Branch Restricted Speculation/Indirect
-        # Branch Prediction Barrier enumeration.
-        IBRSB: [STIBP],
+        # The features:
+        #   * Single Thread Indirect Branch Predictors
+        #   * Speculative Store Bypass Disable
+        #
+        # enumerate new bits in MSR_SPEC_CTRL, which is enumerated by Indirect
+        # Branch Restricted Speculation/Indirect Branch Prediction Barrier.
+        #
+        # In practice, these features also enumerate the presense of
+        # MSR_SPEC_CTRL.  However, no real hardware will exist with SSBD but
+        # not IBRSB, and we pass this MSR directly to guests.  Treating them
+        # as dependent features simplifies Xen's logic, and prevents the guest
+        # from seeing implausible configurations.
+        IBRSB: [STIBP, SSBD],
     }
 
     deep_features = tuple(sorted(deps.keys()))
