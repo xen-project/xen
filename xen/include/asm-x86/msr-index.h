4f713cf37dc16d777be19a17058e0fdbee4f7578
Andrew Cooper <andrew.cooper3@citrix.com>
2018-05-18 13:21:53 +0200

    x86/spec_ctrl: Updates to retpoline-safety decision making
    All of this is as recommended by the Intel whitepaper:
    
    https://software.intel.com/sites/default/files/managed/1d/46/Retpoline-A-Branch-Target-Injection-Mitigation.pdf
    
    The 'RSB Alternative' bit in MSR_ARCH_CAPABILITIES may be set by a hypervisor
    to indicate that the virtual machine may migrate to a processor which isn't
    retpoline-safe.  Introduce a shortened name (to reduce code volume), treat it
    as authorative in retpoline_safe(), and print its value along with the other
    ARCH_CAPS bits.
    
    The exact processor models which do have RSB semantics which fall back to BTB
    predictions are enumerated, and include Kabylake and Coffeelake.  Leave a
    printk() in the default case to help identify cases which aren't covered.
    
    The exact microcode versions from Broadwell RSB-safety are taken from the
    referenced microcode update file (adjusting for the known-bad microcode
    versions).  Despite the exact wording of the text, it is only Broadwell
    processors which need a microcode check.
    
    In practice, this means that all Broadwell hardware with up-to-date microcode
    will use retpoline in preference to IBRS, which will be a performance
    improvement for desktop and server systems which would previously always opt
    for IBRS over retpoline.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>
    
    x86/spec_ctrl: Fix typo in ARCH_CAPS decode
    
    Reported-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Acked-by: Jan Beulich <jbeulich@suse.com>
    master commit: 1232378bd2fef45f613db049b33852fdf84d7ddf
    master date: 2018-04-19 17:28:23 +0100
    master commit: 27170adb54a558e11defcd51989326a9beb95afe
    master date: 2018-04-24 13:34:12 +0100

--- xen/include/asm-x86/msr-index.h
+++ xen/include/asm-x86/msr-index.h
@@ -42,6 +42,7 @@
 #define MSR_ARCH_CAPABILITIES		0x0000010a
 #define ARCH_CAPABILITIES_RDCL_NO	(_AC(1, ULL) << 0)
 #define ARCH_CAPABILITIES_IBRS_ALL	(_AC(1, ULL) << 1)
+#define ARCH_CAPS_RSBA			(_AC(1, ULL) << 2)
 
 /* Intel MSRs. Some also available on other CPUs */
 #define MSR_IA32_PERFCTR0		0x000000c1
