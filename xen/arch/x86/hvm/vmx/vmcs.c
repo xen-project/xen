2a47c7550910f5d591ca0de369234f8c18daa2d2
Andrew Cooper <andrew.cooper3@citrix.com>
2018-05-29 18:44:16 +0100

    x86/spec-ctrl: Introduce an option to control L1D_FLUSH for HVM HAP guests
    This mitigation requires up-to-date microcode, and is enabled by default on
    affected hardware if available, and is used for HVM guests
    
    The default for SMT/Hyperthreading is far more complicated to reason about,
    not least because we don't know if the user is going to want to run any HVM
    guests to begin with.  If a explicit default isn't given, nag the user to
    perform a risk assessment and choose an explicit default, and leave other
    configuration to the toolstack.
    
    This is part of XSA-273 / CVE-2018-3620.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>
    (cherry picked from commit 3bd36952dab60290f33d6791070b57920e10754b)

--- xen/arch/x86/hvm/vmx/vmcs.c
+++ xen/arch/x86/hvm/vmx/vmcs.c
@@ -38,6 +38,7 @@
 #include <asm/flushtlb.h>
 #include <asm/monitor.h>
 #include <asm/shadow.h>
+#include <asm/spec_ctrl.h>
 #include <asm/tboot.h>
 #include <asm/apic.h>
 
@@ -1273,6 +1274,10 @@ static int construct_vmcs(struct vcpu *v)
     paging_update_paging_modes(v);
 
     vmx_vlapic_msr_changed(v);
+
+    if ( opt_l1d_flush && paging_mode_hap(d) )
+        rc = vmx_add_msr(v, MSR_FLUSH_CMD, FLUSH_CMD_L1D,
+                         VMX_MSR_GUEST_LOADONLY);
 
  out:
     vmx_vmcs_exit(v);
