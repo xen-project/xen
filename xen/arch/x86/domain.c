a645331a9f4190e92ccf41a950bc4692f8904239
Jan Beulich <jbeulich@suse.com>
2018-07-04 12:30:30 +0200

    x86/EFI: further correct FPU state handling around runtime calls
    We must not leave a vCPU with CR0.TS clear when it is not in fully eager
    mode and has not touched non-lazy state. Instead of adding a 3rd
    invocation of stts() to vcpu_restore_fpu_eager(), consolidate all of
    them into a single one done at the end of the function.
    
    Rename the function at the same time to better reflect its purpose, as
    the patches touches all of its occurences anyway.
    
    The new function parameter is not really well named, but
    "need_stts_if_not_fully_eager" seemed excessive to me.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Paul Durrant <paul.durrant@citrix.com>
    master commit: 23839a0fa0bbe78c174cd2bb49083e153f0f99df
    master date: 2018-06-26 15:23:08 +0200

--- xen/arch/x86/domain.c
+++ xen/arch/x86/domain.c
@@ -1635,7 +1635,7 @@ static void __context_switch(void)
             if ( cpu_has_xsaves && is_hvm_vcpu(n) )
                 set_msr_xss(n->arch.hvm_vcpu.msr_xss);
         }
-        vcpu_restore_fpu_eager(n);
+        vcpu_restore_fpu_nonlazy(n, false);
         nd->arch.ctxt_switch->to(n);
     }
 
