8797d20a6ec2dd75195585a107ce345c51c0a59a
Jan Beulich <JBeulich@suse.com>
2018-07-19 04:32:06 -0600

    x86: possibly bring up all CPUs even if not all are supposed to be used
    Reportedly Intel CPUs which can't broadcast #MC to all targeted
    cores/threads because some have CR4.MCE clear will shut down. Therefore
    we want to keep CR4.MCE enabled when offlining a CPU, and we need to
    bring up all CPUs in order to be able to set CR4.MCE in the first place.
    
    The use of clear_in_cr4() in cpu_mcheck_disable() was ill advised
    anyway, and to avoid future similar mistakes I'm removing clear_in_cr4()
    altogether right here.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Roger Pau Monné <roger.pau@citrix.com>
    Reviewed-by: Wei Liu <wei.liu2@citrix.com>

--- xen/arch/x86/cpu/common.c
+++ xen/arch/x86/cpu/common.c
@@ -13,6 +13,7 @@
 #include <public/sysctl.h> /* for XEN_INVALID_{SOCKET,CORE}_ID */
 
 #include "cpu.h"
+#include "mcheck/x86_mca.h"
 
 bool_t opt_arat = 1;
 boolean_param("arat", opt_arat);
@@ -342,6 +343,9 @@ static void __init early_cpu_detect(void)
 		if (hap_paddr_bits > PADDR_BITS)
 			hap_paddr_bits = PADDR_BITS;
 	}
+
+	if (c->x86_vendor != X86_VENDOR_AMD)
+		park_offline_cpus = opt_mce;
 
 	initialize_cpu_data(0);
 }
