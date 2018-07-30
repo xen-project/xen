dc111e9f0d99761c62109055ae30f66aebcb556a
Andrew Cooper <andrew.cooper3@citrix.com>
2018-07-30 14:08:08 +0200

    xen: Port the array_index_nospec() infrastructure from Linux
    This is as the infrastructure appeared in Linux 4.17, adapted slightly for
    Xen.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Signed-off-by: Julien Grall <julien.grall@arm.com>
    Acked-by: Jan Beulich <jbeulich@suse.com>
    master commit: 2ddfae51d8b1d7b8cd33a4f6ad4d16d27cb869ae
    master date: 2018-07-06 16:49:57 +0100

--- xen/include/asm-arm/arm64/system.h
+++ xen/include/asm-arm/arm64/system.h
@@ -58,6 +58,28 @@ static inline int local_fiq_is_enabled(void)
     return !(flags & PSR_FIQ_MASK);
 }
 
+#define csdb()  asm volatile ( "hint #20" : : : "memory" )
+
+/*
+ * Generate a mask for array_index__nospec() that is ~0UL when 0 <= idx < sz
+ * and 0 otherwise.
+ */
+static inline unsigned long array_index_mask_nospec(unsigned long idx,
+                                                    unsigned long sz)
+{
+    unsigned long mask;
+
+    asm volatile ( "cmp     %1, %2\n"
+                   "sbc     %0, xzr, xzr\n"
+                   : "=r" (mask)
+                   : "r" (idx), "Ir" (sz)
+                   : "cc" );
+    csdb();
+
+    return mask;
+}
+#define array_index_mask_nospec array_index_mask_nospec
+
 #endif
 /*
  * Local variables:
