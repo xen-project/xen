9a5c16a3e75778c8a094ca87784d93b74676f46c
Stefano Stabellini <sstabellini@kernel.org>
2018-08-13 17:25:51 +0100

    ARM: disable grant table v2
    It was never expected to work, the implementation is incomplete.
    
    As a side effect, it also prevents guests from triggering a
    "BUG_ON(page_get_owner(pg) != d)" in gnttab_unpopulate_status_frames().
    
    This is XSA-268.
    
    Signed-off-by: Stefano Stabellini <sstabellini@kernel.org>
    Acked-by: Jan Beulich <jbeulich@suse.com>

--- xen/common/grant_table.c
+++ xen/common/grant_table.c
@@ -88,7 +88,11 @@ integer_runtime_param("gnttab_max_frames", opt_max_grant_frames);
 unsigned int __read_mostly opt_max_maptrack_frames = 1024;
 integer_runtime_param("gnttab_max_maptrack_frames", opt_max_maptrack_frames);
 
-static unsigned int __read_mostly opt_gnttab_max_version = 2;
+#ifndef GNTTAB_MAX_VERSION
+#define GNTTAB_MAX_VERSION 2
+#endif
+
+static unsigned int __read_mostly opt_gnttab_max_version = GNTTAB_MAX_VERSION;
 static bool __read_mostly opt_transitive_grants = true;
 
 static int __init parse_gnttab(const char *s)
