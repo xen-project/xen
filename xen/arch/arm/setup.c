76bfbcec423e83c4ed975645ebf5a9a4ad2494f7
Wei Liu <wei.liu2@citrix.com>
2018-10-22 14:40:21 +0100

    arm: fix Dom0 creation after ef72c93df9
    ARM Dom0 creation was broken by the said commit because ARM neither
    provided XEN_DOMCTL_CDF_hvm_guest nor had CONFIG_PV set.
    
    Set XEN_DOMCTL_CDF_hvm_guest flag for ARM Dom0 to fix the issue. Also
    set XEN_DOMCTL_CDF_hap while at it.
    
    Signed-off-by: Wei Liu <wei.liu2@citrix.com>
    Acked-by: Julien Grall <julien.grall@arm.com>

--- xen/arch/arm/setup.c
+++ xen/arch/arm/setup.c
@@ -693,6 +693,7 @@ void __init start_xen(unsigned long boot_phys_offset,
     struct bootmodule *xen_bootmodule;
     struct domain *dom0;
     struct xen_domctl_createdomain dom0_cfg = {
+        .flags = XEN_DOMCTL_CDF_hvm_guest | XEN_DOMCTL_CDF_hap,
         .max_evtchn_port = -1,
         .max_grant_frames = gnttab_dom0_frames(),
         .max_maptrack_frames = opt_max_maptrack_frames,
