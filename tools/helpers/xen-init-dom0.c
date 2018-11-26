c3acf04dbd703403759025877cca3a1e27e8e59e
Wei Liu <wei.liu2@citrix.com>
2018-11-26 10:40:44 +0000

    tools: set Dom0 UUID if requested
    Introduce XEN_DOM0_UUID in Xen's global configuration file.  Make
    xen-init-dom0 accept an extra argument for UUID.
    
    Also switch xs_open error message in xen-init-dom0 to use perror.
    
    Signed-off-by: Wei Liu <wei.liu2@citrix.com>
    Reviewed-by: Juergen Gross <jgross@suse.com>
    Reviewed-by: Sergey Dyasli <sergey.dyasli@citrix.com>
    Reviewed-by: Anthony PERARD <anthony.perard@citrix.com>
    Acked-by: Ian Jackson <ian.jackson@eu.citrix.com>

--- tools/helpers/xen-init-dom0.c
+++ tools/helpers/xen-init-dom0.c
@@ -3,7 +3,9 @@
 #include <string.h>
 #include <stdio.h>
 
+#include <xenctrl.h>
 #include <xenstore.h>
+#include <libxl.h>
 
 #include "init-dom-json.h"
 
@@ -13,13 +15,30 @@
 int main(int argc, char **argv)
 {
     int rc;
-    struct xs_handle *xsh;
+    struct xs_handle *xsh = NULL;
+    xc_interface *xch = NULL;
     char *domname_string = NULL, *domid_string = NULL;
+    libxl_uuid uuid;
+
+    /* Accept 0 or 1 argument */
+    if (argc > 2) {
+        fprintf(stderr, "too many arguments\n");
+        rc = 1;
+        goto out;
+    }
 
     xsh = xs_open(0);
     if (!xsh) {
-        fprintf(stderr, "cannot open xenstore connection\n");
-        exit(1);
+        perror("cannot open xenstore connection");
+        rc = 1;
+        goto out;
+    }
+
+    xch = xc_interface_open(NULL, NULL, 0);
+    if (!xch) {
+        perror("xc_interface_open() failed");
+        rc = 1;
+        goto out;
     }
 
     /* Sanity check: this program can only be run once. */
@@ -31,7 +50,23 @@ int main(int argc, char **argv)
         goto out;
     }
 
-    rc = gen_stub_json_config(0, NULL);
+    libxl_uuid_clear(&uuid);
+
+    /* If UUID is supplied, parse it. */
+    if (argc == 2 && libxl_uuid_from_string(&uuid, argv[1])) {
+        fprintf(stderr, "failed to parse UUID %s\n", argv[1]);
+        rc = 1;
+        goto out;
+    }
+
+    if (!libxl_uuid_is_nil(&uuid) &&
+        xc_domain_sethandle(xch, 0, libxl_uuid_bytearray(&uuid))) {
+        perror("failed to set Dom0 UUID");
+        rc = 1;
+        goto out;
+    }
+
+    rc = gen_stub_json_config(0, &uuid);
     if (rc)
         goto out;
 
@@ -55,6 +90,7 @@ out:
     free(domid_string);
     free(domname_string);
     xs_close(xsh);
+    xc_interface_close(xch);
     return rc;
 }
 
