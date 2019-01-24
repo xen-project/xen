755eb6403ec722db37f1b8f8b51e0b0ab661c003
Wei Liu <wei.liu2@citrix.com>
2019-01-24 14:03:48 +0000

    automation: introduce a QEMU smoke test for PVH Dom0
    Make qemu-smoke-x86-64.sh take a variant argument. Make two new tests
    in test.yaml.
    
    Signed-off-by: Wei Liu <wei.liu2@citrix.com>
    Acked-by: Doug Goldstein <cardoe@cardoe.com>
    Release-acked-by: Juergen Gross <jgross@suse.com>

--- automation/scripts/qemu-smoke-x86-64.sh
+++ automation/scripts/qemu-smoke-x86-64.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+# variant should be either pv or pvh
+variant=$1
+
 # Install QEMU
 export DEBIAN_FRONTENT=noninteractive
 apt-get -qy update
@@ -11,12 +14,17 @@ apt-get -qy install qemu-system-x86
 git clone https://xenbits.xen.org/git-http/xtf.git
 cd xtf && make -j$(nproc) && cd -
 
+case $variant in
+    pvh) k=test-hvm32pae-example extra="dom0-iommu=none dom0=pvh" ;;
+    *)   k=test-pv32pae-example  extra= ;;
+esac
+
 rm -f smoke.serial
 set +e
 timeout -k 1 10 \
 qemu-system-x86_64 -nographic -kernel binaries/xen \
-        -initrd xtf/tests/example/test-pv32pae-example \
-        -append 'loglvl=all com1=115200,,8n1 console=com1 noreboot' \
+        -initrd xtf/tests/example/$k \
+        -append "loglvl=all com1=115200,,8n1 console=com1 noreboot $extra" \
         -m 512 -monitor none -serial file:smoke.serial
 set -e
 grep -q 'Test result: SUCCESS' smoke.serial || exit 1
