1ba432fb73f1437f27aae53486091a2b9d73df9a
Wei Liu <wei.liu2@citrix.com>
2019-02-28 12:50:02 +0000

    automation: add a script to build newly pushed commits in Gitlab CI
    Signed-off-by: Wei Liu <wei.liu2@citrix.com>
    Acked-by: Doug Goldstein <cardoe@cardoe.com>

---
+++ automation/gitlab-ci/build-each-commit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# For a newly pushed branch the BEFORE_SHA will be all 0s
+if [[ ${CI_COMMIT_BEFORE_SHA} == 0000000000000000000000000000000000000000 ]]; then
+    echo "Newly pushed branch, skipped"
+    exit 0
+fi
+
+git merge-base --is-ancestor ${CI_COMMIT_BEFORE_SHA} ${CI_COMMIT_SHA}
+if [[ $? -ne 0 ]]; then
+    echo "${CI_COMMIT_SHA} is not a descendent of ${CI_COMMIT_BEFORE_SHA}, skipped"
+    exit 0
+fi
+
+NON_SYMBOLIC_REF=1 ./automation/scripts/build-test.sh ${CI_COMMIT_BEFORE_SHA} ${CI_COMMIT_SHA} \
+    bash -c "make -j4 distclean && ./automation/scripts/build"
