1a09cb35deaf961776f8fb4acdda95c0e7acfc86
Andrew Cooper <andrew.cooper3@citrix.com>
2019-04-03 19:55:55 +0100

    x86/pv: Drop redundant CONFIG_PV ifdefary
    These were made redundant by c/s 23058e7b3 "x86/shadow: put PV L1TF functions
    under CONFIG_PV" but makes the surrounding code read as if is outside of the
    ifdef.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Wei Liu <wei.liu2@citrix.com>
    Acked-by: Jan Beulich <jbeulich@suse.com>

--- xen/include/asm-x86/shadow.h
+++ xen/include/asm-x86/shadow.h
@@ -237,7 +237,7 @@ static inline void pv_l1tf_domain_init(struct domain *d)
     d->arch.pv.check_l1tf = is_hardware_domain(d) ? opt_pv_l1tf_hwdom
                                                   : opt_pv_l1tf_domu;
 
-#if defined(CONFIG_SHADOW_PAGING) && defined(CONFIG_PV)
+#ifdef CONFIG_SHADOW_PAGING
     tasklet_init(&d->arch.paging.shadow.pv_l1tf_tasklet,
                  pv_l1tf_tasklet, (unsigned long)d);
 #endif
@@ -245,7 +245,7 @@ static inline void pv_l1tf_domain_init(struct domain *d)
 
 static inline void pv_l1tf_domain_destroy(struct domain *d)
 {
-#if defined(CONFIG_SHADOW_PAGING) && defined(CONFIG_PV)
+#ifdef CONFIG_SHADOW_PAGING
     tasklet_kill(&d->arch.paging.shadow.pv_l1tf_tasklet);
 #endif
 }
