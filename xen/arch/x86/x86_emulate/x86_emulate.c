6cb7e52edf823fd89fe14da94f9bf3e5cf99d1ff
Jan Beulich <jbeulich@suse.com>
2019-04-05 17:27:13 +0200

    x86emul: don't read mask register on AVX512F-incapable platforms
    Nor when register state isn't sufficiently enabled.
    
    Reported-by: George Dunlap <george.dunlap@citrix.com>
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- xen/arch/x86/x86_emulate/x86_emulate.c
+++ xen/arch/x86/x86_emulate/x86_emulate.c
@@ -3557,7 +3557,8 @@ x86_emulate(
     }
 
     /* With a memory operand, fetch the mask register in use (if any). */
-    if ( ea.type == OP_MEM && evex.opmsk )
+    if ( ea.type == OP_MEM && evex.opmsk &&
+         _get_fpu(fpu_type = X86EMUL_FPU_opmask, ctxt, ops) == X86EMUL_OKAY )
     {
         uint8_t *stb = get_stub(stub);
 
@@ -3576,6 +3577,14 @@ x86_emulate(
         put_stub(stub);
 
         fault_suppression = true;
+    }
+
+    if ( fpu_type == X86EMUL_FPU_opmask )
+    {
+        /* Squash (side) effects of the _get_fpu() above. */
+        x86_emul_reset_event(ctxt);
+        put_fpu(X86EMUL_FPU_opmask, false, state, ctxt, ops);
+        fpu_type = X86EMUL_FPU_none;
     }
 
     /* Decode (but don't fetch) the destination operand: register or memory. */
