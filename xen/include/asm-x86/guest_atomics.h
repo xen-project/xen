28d636da7fffee4ba0165a00de054c158443d9a2
Julien Grall <julien.grall@arm.com>
2019-04-29 15:05:26 +0100

    xen/bitops: Provide helpers to safely modify guest memory atomically
    On Arm, exclusive load-store atomics should only be used between trusted
    thread. As not all the guests are trusted, it may be possible to DoS Xen
    when updating shared memory with guest atomically.
    
    This patch adds a new set of helper that will update the guest memory
    safely. For x86, it is already possible to use the current helpers
    safely. So just wrap them.
    
    For Arm, we will first attempt to update the guest memory with the loop
    bounded by a maximum number of iterations. If it fails, we will pause the
    domain and try again.
    
    Note that this heuristics assumes that a page can only be shared between
    Xen and one domain. Not Xen and multiple domain.
    
    The maximum number of iterations is based on how many times a simple
    load-store atomic operation can be executed in 1uS. The maximum value is
    per-CPU to cater big.LITTLE and calculated when the CPU is booting. The
    heuristic was randomly chosen and can be modified if impact too much
    good-behaving guest.
    
    Note, while test_bit does not requires to use atomic operation, a
    wrapper for test_bit was added for completeness. In this case, the
    domain stays constified to avoid major rework in the caller for the
    time-being.
    
    This is part of XSA-295.
    
    Signed-of-by: Julien Grall <julien.grall@arm.com>
    Acked-by: Jan Beulich <jbeulich@suse.com>
    Reviewed-by: Stefano Stabellini <sstabellini@kernel.org>

---
+++ xen/include/asm-x86/guest_atomics.h
@@ -0,0 +1,30 @@
+#ifndef _X86_GUEST_ATOMICS_H
+#define _X86_GUEST_ATOMICS_H
+
+#include <xen/bitops.h>
+
+/*
+ * It is safe to use the atomics helpers on x86 on memory shared with
+ * the guests.
+ */
+#define guest_set_bit(d, nr, p)     ((void)(d), set_bit(nr, p))
+#define guest_clear_bit(d, nr, p)   ((void)(d), clear_bit(nr, p))
+#define guest_change_bit(d, nr, p)  ((void)(d), change_bit(nr, p))
+#define guest_test_bit(d, nr, p)    ((void)(d), test_bit(nr, p))
+
+#define guest_test_and_set_bit(d, nr, p)    \
+    ((void)(d), test_and_set_bit(nr, p))
+#define guest_test_and_clear_bit(d, nr, p)  \
+    ((void)(d), test_and_clear_bit(nr, p))
+#define guest_test_and_change_bit(d, nr, p) \
+    ((void)(d), test_and_change_bit(nr, p))
+
+#endif /* _X86_GUEST_ATOMICS_H */
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
