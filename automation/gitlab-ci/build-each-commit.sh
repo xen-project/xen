1d1f1611535f5d8f91f550b04935bf38a6ecf002
Wei Liu <wei.liu2@citrix.com>
2019-05-15 11:00:38 +0100

    gitlab-ci: allow specifying base and tip in build test
    We will soon provide this new capability to humans and automated
    systems.
    
    The default behaviour is retained: tip and base are passed by Gitlab
    CI.
    
    Signed-off-by: Wei Liu <wei.liu2@citrix.com>
    Acked-by: Doug Goldstein <cardoe@cardoe.com>

--- automation/gitlab-ci/build-each-commit.sh
+++ automation/gitlab-ci/build-each-commit.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # For a newly pushed branch the BEFORE_SHA will be all 0s
-if [[ ${CI_COMMIT_BEFORE_SHA} == 0000000000000000000000000000000000000000 ]]; then
+if [[ ${BASE} == 0000000000000000000000000000000000000000 ]]; then
     echo "Newly pushed branch, skipped"
     exit 0
 fi
 
-git merge-base --is-ancestor ${CI_COMMIT_BEFORE_SHA} ${CI_COMMIT_SHA}
+git merge-base --is-ancestor ${BASE} ${TIP}
 if [[ $? -ne 0 ]]; then
-    echo "${CI_COMMIT_SHA} is not a descendent of ${CI_COMMIT_BEFORE_SHA}, skipped"
+    echo "${TIP} is not a descendent of ${BASE}, skipped"
     exit 0
 fi
 
-echo "Building ${CI_COMMIT_BEFORE_SHA}..${CI_COMMIT_SHA}"
+echo "Building ${BASE}..${TIP}"
 
-NON_SYMBOLIC_REF=1 ./automation/scripts/build-test.sh ${CI_COMMIT_BEFORE_SHA} ${CI_COMMIT_SHA} \
+NON_SYMBOLIC_REF=1 ./automation/scripts/build-test.sh ${BASE} ${TIP} \
     bash -c "git clean -ffdx && ./automation/scripts/build"
