22ec7474348fea2c4a32b0872dd3385bf3785a26
Stefano Stabellini <sstabellini@kernel.org>
2019-06-21 13:20:25 -0700

    Re-instate "xen/arm: fix mask calculation in pdx_init_mask"
    The commit 11911563610786615c2b3a01cdcaaf09a6f9e38d "xen/arm: fix mask
    calculation in pdx_init_mask" was correct, but exposed a bug in
    maddr_to_virt(). The bug in maddr_to_virt() was fixed by
    612d476e74a314be514ee6a9744eea8db09d32e5 "xen/arm64: Correctly compute
    the virtual address in maddr_to_virt()", so we can re-instate the
    first commit now.
    
    Signed-off-by: Stefano Stabellini <stefanos@xilinx.com>

--- xen/common/pdx.c
+++ xen/common/pdx.c
@@ -50,9 +50,11 @@ static u64 __init fill_mask(u64 mask)
     return mask;
 }
 
+/* We don't want to compress the low MAX_ORDER bits of the addresses. */
 uint64_t __init pdx_init_mask(uint64_t base_addr)
 {
-    return fill_mask(base_addr - 1);
+    return fill_mask(max(base_addr,
+                         (uint64_t)1 << (MAX_ORDER + PAGE_SHIFT)) - 1);
 }
 
 u64 __init pdx_region_mask(u64 base, u64 len)
@@ -80,6 +82,9 @@ void __init pfn_pdx_hole_setup(unsigned long mask)
      * This guarantees that page-pointer arithmetic remains valid within
      * contiguous aligned ranges of 2^MAX_ORDER pages. Among others, our
      * buddy allocator relies on this assumption.
+     *
+     * If the logic changes here, we might have to update the ARM specific
+     * init_pdx too.
      */
     for ( j = MAX_ORDER-1; ; )
     {
