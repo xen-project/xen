xen-project/xen

commit c588c002cc19d74f7aea0792608d57e4aa22604a

tools: remove tmem code and commands
Remove all tmem related code in libxc.

Leave some stubs in libxl in case anyone has linked to those functions
before the removal.

Remove all tmem related commands in xl, all tmem related code in other
utilities we ship.

Signed-off-by: Wei Liu <wei.liu2@citrix.com>
Acked-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
Acked-by: Ian Jackson <ian.jackson@eu.citrix.com>

tools/xenstat/xentop/xentop.c
@@ -209,7 +209,6 @@ unsigned int iterations = 0;
 int show_vcpus = 0;
 int show_networks = 0;
 int show_vbds = 0;
-int show_tmem = 0;
 int repeat_header = 0;
 int show_full_name = 0;
 #define PROMPT_VAL_LEN 80
@@ -361,9 +360,6 @@ static int handle_key(int ch)
 			break;
 		case 'b': case 'B':
 			show_vbds ^= 1;
-			break;
-		case 't': case 'T':
-			show_tmem ^= 1;
 			break;
 		case 'r': case 'R':
 			repeat_header ^= 1;
@@ -973,8 +969,8 @@ void do_summary(void)
 	      "%u crashed, %u dying, %u shutdown \n",
 	      num_domains, run, block, pause, crash, dying, shutdown);
 
-	used = xenstat_node_tot_mem(cur_node)-xenstat_node_free_mem(cur_node);
-	freeable_mb = xenstat_node_freeable_mb(cur_node);
+	used = xenstat_node_tot_mem(cur_node);
+	freeable_mb = 0;
 
 	/* Dump node memory and cpu information */
 	if ( freeable_mb <= 0 )
@@ -1031,12 +1027,6 @@ void do_bottom_line(void)
 		addch(A_REVERSE | 'B');
 		attr_addstr(show_vbds ? COLOR_PAIR(1) : 0, "ds");
 		addstr("  ");
-
-		/* tmem */
-		addch(A_REVERSE | 'T');
-		attr_addstr(show_tmem ? COLOR_PAIR(1) : 0, "mem");
-		addstr("  ");
-
 
 		/* vcpus */
 		addch(A_REVERSE | 'V');
@@ -1175,23 +1165,6 @@ void do_vbd(xenstat_domain *domain)
 	}
 }
 
-/* Output all tmem information */
-void do_tmem(xenstat_domain *domain)
-{
-	xenstat_tmem *tmem = xenstat_domain_tmem(domain);
-	unsigned long long curr_eph_pages = xenstat_tmem_curr_eph_pages(tmem);
-	unsigned long long succ_eph_gets = xenstat_tmem_succ_eph_gets(tmem);
-	unsigned long long succ_pers_puts = xenstat_tmem_succ_pers_puts(tmem);
-	unsigned long long succ_pers_gets = xenstat_tmem_succ_pers_gets(tmem);
-
-	if (curr_eph_pages | succ_eph_gets | succ_pers_puts | succ_pers_gets)
-		print("Tmem:  Curr eph pages: %8llu   Succ eph gets: %8llu   "
-	              "Succ pers puts: %8llu   Succ pers gets: %8llu\n",
-			curr_eph_pages, succ_eph_gets,
-			succ_pers_puts, succ_pers_gets);
-
-}
-
 static void top(void)
 {
 	xenstat_domain **domains;
@@ -1244,8 +1217,6 @@ static void top(void)
 			do_network(domains[i]);
 		if (show_vbds)
 			do_vbd(domains[i]);
-		if (show_tmem)
-			do_tmem(domains[i]);
 	}
 
 	if (!batch)
@@ -1320,9 +1291,6 @@ int main(int argc, char **argv)
 			break;
 		case 'f':
 			show_full_name = 1;
-			break;
-		case 't':
-			show_tmem = 1;
 			break;
 		}
 	}

tools/libxc/Makefile
@@ -30,7 +30,6 @@ CTRL_SRCS-y       += xc_tbuf.c
 CTRL_SRCS-y       += xc_pm.c
 CTRL_SRCS-y       += xc_cpu_hotplug.c
 CTRL_SRCS-y       += xc_resume.c
-CTRL_SRCS-y       += xc_tmem.c
 CTRL_SRCS-y       += xc_vm_event.c
 CTRL_SRCS-y       += xc_monitor.c
 CTRL_SRCS-y       += xc_mem_paging.c

tools/libxc/include/xenctrl.h
@@ -44,7 +44,6 @@
 #include <xen/hvm/dm_op.h>
 #include <xen/hvm/params.h>
 #include <xen/xsm/flask_op.h>
-#include <xen/tmem.h>
 #include <xen/kexec.h>
 #include <xen/platform.h>
 
@@ -1909,22 +1908,6 @@ int xc_set_cpuidle_max_cstate(xc_interface *xch, uint32_t value);
 
 int xc_enable_turbo(xc_interface *xch, int cpuid);
 int xc_disable_turbo(xc_interface *xch, int cpuid);
-/**
- * tmem operations
- */
-
-int xc_tmem_control_oid(xc_interface *xch, int32_t pool_id, uint32_t subop,
-                        uint32_t cli_id, uint32_t len, uint32_t arg,
-                        struct xen_tmem_oid oid, void *buf);
-int xc_tmem_control(xc_interface *xch,
-                    int32_t pool_id, uint32_t subop, uint32_t cli_id,
-                    uint32_t len, uint32_t arg, void *buf);
-int xc_tmem_auth(xc_interface *xch, int cli_id, char *uuid_str, int enable);
-int xc_tmem_save(xc_interface *xch, uint32_t domid, int live, int fd, int field_marker);
-int xc_tmem_save_extra(xc_interface *xch, uint32_t domid, int fd, int field_marker);
-void xc_tmem_save_done(xc_interface *xch, uint32_t domid);
-int xc_tmem_restore(xc_interface *xch, uint32_t domid, int fd);
-int xc_tmem_restore_extra(xc_interface *xch, uint32_t domid, int fd);
 
 /**
  * altp2m operations

tools/libxc/xc_tmem.c
@@ -1,507 +1,0 @@
-/******************************************************************************
- * xc_tmem.c
- *
- * Copyright (C) 2008 Oracle Corp.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation;
- * version 2.1 of the License.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; If not, see <http://www.gnu.org/licenses/>.
- */
-
-#include "xc_private.h"
-#include <inttypes.h>
-#include <assert.h>
-#include <xen/tmem.h>
-
-int xc_tmem_control(xc_interface *xch,
-                    int32_t pool_id,
-                    uint32_t cmd,
-                    uint32_t cli_id,
-                    uint32_t len,
-                    uint32_t arg,
-                    void *buf)
-{
-    DECLARE_SYSCTL;
-    DECLARE_HYPERCALL_BOUNCE(buf, len, XC_HYPERCALL_BUFFER_BOUNCE_OUT);
-    int rc;
-
-    sysctl.cmd = XEN_SYSCTL_tmem_op;
-    sysctl.u.tmem_op.pool_id = pool_id;
-    sysctl.u.tmem_op.cmd = cmd;
-    sysctl.u.tmem_op.cli_id = cli_id;
-    sysctl.u.tmem_op.len = len;
-    sysctl.u.tmem_op.arg = arg;
-    sysctl.u.tmem_op.pad = 0;
-    sysctl.u.tmem_op.oid.oid[0] = 0;
-    sysctl.u.tmem_op.oid.oid[1] = 0;
-    sysctl.u.tmem_op.oid.oid[2] = 0;
-
-    if ( cmd == XEN_SYSCTL_TMEM_OP_SET_CLIENT_INFO ||
-         cmd == XEN_SYSCTL_TMEM_OP_SET_AUTH )
-        HYPERCALL_BOUNCE_SET_DIR(buf, XC_HYPERCALL_BUFFER_BOUNCE_IN);
-    if ( len )
-    {
-        if ( buf == NULL )
-        {
-            errno = EINVAL;
-            return -1;
-        }
-        if ( xc_hypercall_bounce_pre(xch, buf) )
-        {
-            PERROR("Could not bounce buffer for tmem control hypercall");
-            return -1;
-        }
-    }
-
-    set_xen_guest_handle(sysctl.u.tmem_op.u.buf, buf);
-
-    rc = do_sysctl(xch, &sysctl);
-
-    if ( len )
-        xc_hypercall_bounce_post(xch, buf);
-
-    return rc;
-}
-
-int xc_tmem_control_oid(xc_interface *xch,
-                        int32_t pool_id,
-                        uint32_t cmd,
-                        uint32_t cli_id,
-                        uint32_t len,
-                        uint32_t arg,
-                        struct xen_tmem_oid oid,
-                        void *buf)
-{
-    DECLARE_SYSCTL;
-    DECLARE_HYPERCALL_BOUNCE(buf, len, XC_HYPERCALL_BUFFER_BOUNCE_OUT);
-    int rc;
-
-    sysctl.cmd = XEN_SYSCTL_tmem_op;
-    sysctl.u.tmem_op.pool_id = pool_id;
-    sysctl.u.tmem_op.cmd = cmd;
-    sysctl.u.tmem_op.cli_id = cli_id;
-    sysctl.u.tmem_op.len = len;
-    sysctl.u.tmem_op.arg = arg;
-    sysctl.u.tmem_op.pad = 0;
-    sysctl.u.tmem_op.oid = oid;
-
-    if ( len  )
-    {
-        if ( buf == NULL )
-        {
-            errno = EINVAL;
-            return -1;
-        }
-        if ( xc_hypercall_bounce_pre(xch, buf) )
-        {
-            PERROR("Could not bounce buffer for tmem control (OID) hypercall");
-            return -1;
-        }
-    }
-
-    set_xen_guest_handle(sysctl.u.tmem_op.u.buf, buf);
-
-    rc = do_sysctl(xch, &sysctl);
-
-    if ( len )
-        xc_hypercall_bounce_post(xch, buf);
-
-    return rc;
-}
-
-static int xc_tmem_uuid_parse(char *uuid_str, uint64_t *uuid_lo, uint64_t *uuid_hi)
-{
-    char *p = uuid_str;
-    uint64_t *x = uuid_hi;
-    int i = 0, digit;
-
-    *uuid_lo = 0; *uuid_hi = 0;
-    for ( p = uuid_str, i = 0; i != 36 && *p != '\0'; p++, i++ )
-    {
-        if ( (i == 8 || i == 13 || i == 18 || i == 23) )
-        {
-            if ( *p != '-' )
-                return -1;
-            if ( i == 18 )
-                x = uuid_lo;
-            continue;
-        }
-        else if ( *p >= '0' && *p <= '9' )
-            digit = *p - '0';
-        else if ( *p >= 'A' && *p <= 'F' )
-            digit = *p - 'A' + 10;
-        else if ( *p >= 'a' && *p <= 'f' )
-            digit = *p - 'a' + 10;
-        else
-            return -1;
-        *x = (*x << 4) | digit;
-    }
-    if ( (i != 1 && i != 36) || *p != '\0' )
-        return -1;
-    return 0;
-}
-
-int xc_tmem_auth(xc_interface *xch,
-                 int cli_id,
-                 char *uuid_str,
-                 int enable)
-{
-    xen_tmem_pool_info_t pool = {
-        .flags.u.auth = enable,
-        .id = 0,
-        .n_pages = 0,
-        .uuid[0] = 0,
-        .uuid[1] = 0,
-    };
-    if ( xc_tmem_uuid_parse(uuid_str, &pool.uuid[0],
-                                      &pool.uuid[1]) < 0 )
-    {
-        PERROR("Can't parse uuid, use xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx");
-        return -1;
-    }
-    return xc_tmem_control(xch, 0 /* pool_id */,
-                           XEN_SYSCTL_TMEM_OP_SET_AUTH,
-                           cli_id, sizeof(pool),
-                           0 /* arg */, &pool);
-}
-
-/* Save/restore/live migrate */
-
-/*
-   Note that live migration complicates the save/restore format in
-   multiple ways: Though saving/migration can only occur when all
-   tmem pools belonging to the domain-being-saved are frozen and
-   this ensures that new pools can't be created or existing pools
-   grown (in number of pages), it is possible during a live migration
-   that pools may be destroyed and pages invalidated while the migration
-   is in process.  As a result, (1) it is not safe to pre-specify counts
-   for these values precisely, but only as a "max", and (2) a "invalidation"
-   list (of pools, objects, pages) must be appended when the domain is truly
-   suspended.
- */
-
-/* returns 0 if nothing to save, -1 if error saving, 1 if saved successfully */
-int xc_tmem_save(xc_interface *xch,
-                 uint32_t domid, int io_fd, int live, int field_marker)
-{
-    int marker = field_marker;
-    int i, j, rc;
-    uint32_t minusone = -1;
-    struct tmem_handle *h;
-    xen_tmem_client_t info;
-    xen_tmem_pool_info_t *pools;
-    char *buf = NULL;
-
-    rc = xc_tmem_control(xch, 0, XEN_SYSCTL_TMEM_OP_SAVE_BEGIN,
-                         domid, 0 /* len*/ , live, NULL);
-    if ( rc )
-    {
-        /* Nothing to save - no tmem enabled. */
-        if ( errno == ENOENT )
-            return 0;
-
-        return rc;
-    }
-
-    if ( xc_tmem_control(xch, 0 /* pool_id */,
-                         XEN_SYSCTL_TMEM_OP_GET_CLIENT_INFO,
-                         domid /* cli_id */, sizeof(info), 0 /* arg */,
-                         &info) < 0 )
-        return -1;
-
-    /* Nothing to do. */
-    if ( !info.nr_pools )
-        return 0;
-
-    pools = calloc(info.nr_pools, sizeof(*pools));
-    if ( !pools )
-        return -1;
-
-    rc = xc_tmem_control(xch, 0 /* pool_id is ignored. */,
-                         XEN_SYSCTL_TMEM_OP_GET_POOLS,
-                         domid /* cli_id */, sizeof(*pools) * info.nr_pools,
-                         0 /* arg */, pools);
-
-    if ( rc < 0 || (uint32_t)rc > info.nr_pools )
-        goto out_memory;
-
-    /* Update it - as we have less pools between the two hypercalls. */
-    info.nr_pools = (uint32_t)rc;
-
-    if ( write_exact(io_fd, &marker, sizeof(marker)) )
-        goto out_memory;
-
-    if ( write_exact(io_fd, &info, sizeof(info)) )
-        goto out_memory;
-
-    if ( write_exact(io_fd, &minusone, sizeof(minusone)) )
-        goto out_memory;
-
-    for ( i = 0; i < info.nr_pools; i++ )
-    {
-        uint32_t pagesize;
-        int bufsize = 0;
-        int checksum = 0;
-        xen_tmem_pool_info_t *pool = &pools[i];
-
-        if ( pool->flags.raw != -1 )
-        {
-            if ( !pool->flags.u.persist )
-                pool->n_pages = 0;
-
-            if ( write_exact(io_fd, pool, sizeof(*pool)) )
-                goto out_memory;
-
-            if ( !pool->flags.u.persist )
-                continue;
-
-            pagesize = 1 << (pool->flags.u.pagebits + 12);
-            if ( pagesize > bufsize )
-            {
-                bufsize = pagesize + sizeof(struct tmem_handle);
-                if ( (buf = realloc(buf,bufsize)) == NULL )
-                    goto out_memory;
-            }
-            for ( j = pool->n_pages; j > 0; j-- )
-            {
-                int ret;
-                if ( (ret = xc_tmem_control(
-                          xch, pool->id, XEN_SYSCTL_TMEM_OP_SAVE_GET_NEXT_PAGE,
-                          domid, bufsize, 0, buf)) > 0 )
-                {
-                    h = (struct tmem_handle *)buf;
-                    if ( write_exact(io_fd, &h->oid, sizeof(h->oid)) )
-                        goto out_memory;
-
-                    if ( write_exact(io_fd, &h->index, sizeof(h->index)) )
-                        goto out_memory;
-                    h++;
-                    checksum += *(char *)h;
-                    if ( write_exact(io_fd, h, pagesize) )
-                        goto out_memory;
-                } else if ( ret == 0 ) {
-                    continue;
-                } else {
-                    /* page list terminator */
-                    h = (struct tmem_handle *)buf;
-                    h->oid.oid[0] = h->oid.oid[1] = h->oid.oid[2] = -1L;
-                    if ( write_exact(io_fd, &h->oid, sizeof(h->oid)) )
-                    {
- out_memory:
-                        free(pools);
-                        free(buf);
-                        return -1;
-                    }
-                    break;
-                }
-            }
-            DPRINTF("saved %"PRId64" tmem pages for dom=%d pool=%d, checksum=%x\n",
-                    pool->n_pages - j, domid, pool->id, checksum);
-        }
-    }
-    free(pools);
-    free(buf);
-
-    /* pool list terminator */
-    minusone = -1;
-    if ( write_exact(io_fd, &minusone, sizeof(minusone)) )
-        return -1;
-
-    return 1;
-}
-
-/* only called for live migration */
-int xc_tmem_save_extra(xc_interface *xch, uint32_t domid, int io_fd, int field_marker)
-{
-    struct tmem_handle handle;
-    int marker = field_marker;
-    uint32_t minusone;
-    int count = 0, checksum = 0;
-
-    if ( write_exact(io_fd, &marker, sizeof(marker)) )
-        return -1;
-    while ( xc_tmem_control(xch, 0, XEN_SYSCTL_TMEM_OP_SAVE_GET_NEXT_INV, domid,
-                            sizeof(handle),0,&handle) > 0 ) {
-        if ( write_exact(io_fd, &handle.pool_id, sizeof(handle.pool_id)) )
-            return -1;
-        if ( write_exact(io_fd, &handle.oid, sizeof(handle.oid)) )
-            return -1;
-        if ( write_exact(io_fd, &handle.index, sizeof(handle.index)) )
-            return -1;
-        count++;
-        checksum += handle.pool_id + handle.oid.oid[0] + handle.oid.oid[1] +
-                    handle.oid.oid[2] + handle.index;
-    }
-    if ( count )
-            DPRINTF("needed %d tmem invalidates, check=%d\n",count,checksum);
-    minusone = -1;
-    if ( write_exact(io_fd, &minusone, sizeof(minusone)) )
-        return -1;
-    return 0;
-}
-
-/* only called for live migration */
-void xc_tmem_save_done(xc_interface *xch, uint32_t domid)
-{
-    xc_tmem_control(xch, 0, XEN_SYSCTL_TMEM_OP_SAVE_END, domid, 0, 0, NULL);
-}
-
-/* restore routines */
-
-static int xc_tmem_restore_new_pool(
-                    xc_interface *xch,
-                    int cli_id,
-                    uint32_t pool_id,
-                    uint32_t flags,
-                    uint64_t uuid_lo,
-                    uint64_t uuid_hi)
-{
-    xen_tmem_pool_info_t pool = {
-        .flags.raw = flags,
-        .id = pool_id,
-        .n_pages = 0,
-        .uuid[0] = uuid_lo,
-        .uuid[1] = uuid_hi,
-    };
-
-    return xc_tmem_control(xch, pool_id,
-                           XEN_SYSCTL_TMEM_OP_SET_POOLS,
-                           cli_id, sizeof(pool),
-                           0 /* arg */, &pool);
-}
-
-int xc_tmem_restore(xc_interface *xch, uint32_t domid, int io_fd)
-{
-    uint32_t minusone;
-    xen_tmem_client_t info;
-    int checksum = 0;
-    unsigned int i;
-    char *buf = NULL;
-
-    if ( read_exact(io_fd, &info, sizeof(info)) )
-        return -1;
-
-    /* We would never save if there weren't any pools! */
-    if ( !info.nr_pools )
-        return -1;
-
-    if ( xc_tmem_control(xch, 0, XEN_SYSCTL_TMEM_OP_RESTORE_BEGIN, domid, 0, 0, NULL) < 0 )
-        return -1;
-
-    if ( xc_tmem_control(xch, 0 /* pool_id */,
-                         XEN_SYSCTL_TMEM_OP_SET_CLIENT_INFO,
-                         domid /* cli_id */, sizeof(info), 0 /* arg */,
-                         &info) < 0 )
-        return -1;
-
-    if ( read_exact(io_fd, &minusone, sizeof(minusone)) )
-        return -1;
-
-    for ( i = 0; i < info.nr_pools; i++ )
-    {
-        int bufsize = 0, pagesize;
-        int j;
-        xen_tmem_pool_info_t pool;
-
-        if ( read_exact(io_fd, &pool, sizeof(pool)) )
-            goto out_memory;
-
-        if ( xc_tmem_restore_new_pool(xch, domid, pool.id, pool.flags.raw,
-                                      pool.uuid[0], pool.uuid[1]) < 0 )
-            goto out_memory;
-
-        if ( pool.n_pages <= 0 )
-            continue;
-
-        pagesize = 1 << (pool.flags.u.pagebits + 12);
-        if ( pagesize > bufsize )
-        {
-            bufsize = pagesize;
-            if ( (buf = realloc(buf,bufsize)) == NULL )
-                goto out_memory;
-        }
-        for ( j = pool.n_pages; j > 0; j-- )
-        {
-            struct xen_tmem_oid oid;
-            uint32_t index;
-            int rc;
-
-            if ( read_exact(io_fd, &oid, sizeof(oid)) )
-                goto out_memory;
-
-            if ( oid.oid[0] == -1L && oid.oid[1] == -1L && oid.oid[2] == -1L )
-                break;
-            if ( read_exact(io_fd, &index, sizeof(index)) )
-                goto out_memory;
-
-            if ( read_exact(io_fd, buf, pagesize) )
-                goto out_memory;
-
-            checksum += *buf;
-            if ( (rc = xc_tmem_control_oid(
-                      xch, pool.id, XEN_SYSCTL_TMEM_OP_RESTORE_PUT_PAGE,
-                      domid, bufsize, index, oid, buf)) <= 0 )
-            {
-                DPRINTF("xc_tmem_restore: putting page failed, rc=%d\n",rc);
- out_memory:
-                free(buf);
-                return -1;
-            }
-        }
-        if ( pool.n_pages )
-            DPRINTF("restored %"PRId64" tmem pages for dom=%d pool=%d, check=%x\n",
-                    pool.n_pages - j, domid, pool.id, checksum);
-    }
-    free(buf);
-
-    return 0;
-}
-
-/* only called for live migration, must be called after suspend */
-int xc_tmem_restore_extra(xc_interface *xch, uint32_t domid, int io_fd)
-{
-    uint32_t pool_id;
-    struct xen_tmem_oid oid;
-    uint32_t index;
-    int count = 0;
-    int checksum = 0;
-
-    while ( read_exact(io_fd, &pool_id, sizeof(pool_id)) == 0 && pool_id != -1 )
-    {
-        if ( read_exact(io_fd, &oid, sizeof(oid)) )
-            return -1;
-        if ( read_exact(io_fd, &index, sizeof(index)) )
-            return -1;
-        if ( xc_tmem_control_oid(
-                 xch, pool_id, XEN_SYSCTL_TMEM_OP_RESTORE_FLUSH_PAGE,
-                 domid, 0, index, oid, NULL) <= 0 )
-            return -1;
-        count++;
-        checksum += pool_id + oid.oid[0] + oid.oid[1] + oid.oid[2] + index;
-    }
-    if ( pool_id != -1 )
-        return -1;
-    if ( count )
-            DPRINTF("invalidated %d tmem pages, check=%d\n",count,checksum);
-
-    return 0;
-}
-
-/*
- * Local variables:
- * mode: C
- * c-file-style: "BSD"
- * c-basic-offset: 4
- * tab-width: 4
- * indent-tabs-mode: nil
- * End:
- */

tools/libxl/libxl_tmem.c
@@ -16,146 +16,55 @@
 
 #include "libxl_internal.h"
 
+/* TMEM is gone. Leave some stubs here. */
+
 char *libxl_tmem_list(libxl_ctx *ctx, uint32_t domid, int use_long)
 {
-    int r;
-    char _buf[32768];
     GC_INIT(ctx);
-
-    r = xc_tmem_control(ctx->xch, -1, XEN_SYSCTL_TMEM_OP_LIST, domid, 32768,
-                        use_long, _buf);
-    if (r < 0) {
-        LOGED(ERROR, domid, "Can not get tmem list");
-        GC_FREE;
-        return NULL;
-    }
-
+    LOGED(ERROR, domid, "Can not get tmem list");
     GC_FREE;
-    return strdup(_buf);
+    return NULL;
 }
 
 int libxl_tmem_freeze(libxl_ctx *ctx, uint32_t domid)
 {
-    int r, rc;
     GC_INIT(ctx);
-
-    r = xc_tmem_control(ctx->xch, -1, XEN_SYSCTL_TMEM_OP_FREEZE, domid, 0, 0,
-                        NULL);
-    if (r < 0) {
-        LOGED(ERROR, domid, "Can not freeze tmem pools");
-        rc = ERROR_FAIL;
-        goto out;
-    }
-
-    rc = 0;
-out:
+    LOGED(ERROR, domid, "Can not freeze tmem pools");
     GC_FREE;
-    return rc;
+    return ERROR_FAIL;
 }
 
 int libxl_tmem_thaw(libxl_ctx *ctx, uint32_t domid)
 {
-    int r, rc;
     GC_INIT(ctx);
-
-    r = xc_tmem_control(ctx->xch, -1, XEN_SYSCTL_TMEM_OP_THAW, domid, 0, 0,
-                        NULL);
-    if (r < 0) {
-        LOGED(ERROR, domid, "Can not thaw tmem pools");
-        rc = ERROR_FAIL;
-        goto out;
-    }
-
-    rc = 0;
-out:
+    LOGED(ERROR, domid, "Can not thaw tmem pools");
     GC_FREE;
-    return rc;
-}
-
-static int32_t tmem_setop_from_string(char *set_name, uint32_t val,
-                                      xen_tmem_client_t *info)
-{
-    if (!strcmp(set_name, "weight"))
-        info->weight = val;
-    else if (!strcmp(set_name, "compress"))
-        info->flags.u.compress = val;
-    else
-        return -1;
-
-    return 0;
+    return ERROR_FAIL;
 }
 
 int libxl_tmem_set(libxl_ctx *ctx, uint32_t domid, char* name, uint32_t set)
 {
-    int r, rc;
-    xen_tmem_client_t info;
     GC_INIT(ctx);
-
-    r = xc_tmem_control(ctx->xch, -1 /* pool_id */,
-                        XEN_SYSCTL_TMEM_OP_GET_CLIENT_INFO,
-                        domid, sizeof(info), 0 /* arg */, &info);
-    if (r < 0) {
-        LOGED(ERROR, domid, "Can not get tmem data!");
-        rc = ERROR_FAIL;
-        goto out;
-    }
-    rc = tmem_setop_from_string(name, set, &info);
-    if (rc == -1) {
-        LOGEVD(ERROR, -1, domid, "Invalid set, valid sets are <weight|compress>");
-        rc = ERROR_INVAL;
-        goto out;
-    }
-    r = xc_tmem_control(ctx->xch, -1 /* pool_id */,
-                        XEN_SYSCTL_TMEM_OP_SET_CLIENT_INFO,
-                        domid, sizeof(info), 0 /* arg */, &info);
-    if (r < 0) {
-        LOGED(ERROR, domid, "Can not set tmem %s", name);
-        rc = ERROR_FAIL;
-        goto out;
-    }
-
-    rc = 0;
-out:
+    LOGED(ERROR, domid, "Can not set tmem %s", name);
     GC_FREE;
-    return rc;
+    return ERROR_FAIL;
 }
 
 int libxl_tmem_shared_auth(libxl_ctx *ctx, uint32_t domid,
                            char* uuid, int auth)
 {
-    int r, rc;
     GC_INIT(ctx);
-
-    r = xc_tmem_auth(ctx->xch, domid, uuid, auth);
-    if (r < 0) {
-        LOGED(ERROR, domid, "Can not set tmem shared auth");
-        rc = ERROR_FAIL;
-        goto out;
-    }
-
-    rc = 0;
-out:
+    LOGED(ERROR, domid, "Can not set tmem shared auth");
     GC_FREE;
-    return rc;
+    return ERROR_FAIL;
 }
 
 int libxl_tmem_freeable(libxl_ctx *ctx)
 {
-    int r, rc;
     GC_INIT(ctx);
-
-    r = xc_tmem_control(ctx->xch, -1, XEN_SYSCTL_TMEM_OP_QUERY_FREEABLE_MB,
-                        -1, 0, 0, 0);
-    if (r < 0) {
-        LOGE(ERROR, "Can not get tmem freeable memory");
-        rc = ERROR_FAIL;
-        goto out;
-    }
-
-    rc = 0;
-out:
+    LOGE(ERROR, "Can not get tmem freeable memory");
     GC_FREE;
-    return rc;
+    return ERROR_FAIL;
 }
 
 /*

tools/misc/Makefile
@@ -22,7 +22,6 @@ INSTALL_SBIN-$(CONFIG_X86)     += xen-hvmcrash
 INSTALL_SBIN-$(CONFIG_X86)     += xen-hvmctx
 INSTALL_SBIN-$(CONFIG_X86)     += xen-lowmemd
 INSTALL_SBIN-$(CONFIG_X86)     += xen-mfndump
-INSTALL_SBIN                   += xen-tmem-list-parse
 INSTALL_SBIN                   += xencov
 INSTALL_SBIN                   += xenlockprof
 INSTALL_SBIN                   += xenperf

commit 4b5b431edd984b26f43b3efc7de465f3560a949e

tools/xentop: Fix calculation of used memory
Used memory should be calculated by subtracting free memory from total
memory.

Fixes: c588c002cc1 ("tools: remove tmem code and commands")
Signed-off-by: Sander Eikelenboom <linux@eikelenboom.it>
Acked-by: Wei Liu <wl@xen.org>

tools/xenstat/xentop/xentop.c
@@ -969,7 +969,7 @@ void do_summary(void)
 	      "%u crashed, %u dying, %u shutdown \n",
 	      num_domains, run, block, pause, crash, dying, shutdown);
 
-	used = xenstat_node_tot_mem(cur_node);
+	used = xenstat_node_tot_mem(cur_node) - xenstat_node_free_mem(cur_node);
 	freeable_mb = 0;
 
 	/* Dump node memory and cpu information */