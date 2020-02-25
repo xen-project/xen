26cada81c43719508783e72c244fcf57e495ccc7
Julien Grall <julien@xen.org>
2020-02-25 12:32:49 +0000

    xen/grant-table: Remove outdated warning in gnttab_grow_table()
    One of the warning message in gnttab_grow_table() refers to a function
    was removed in commit 6425f91c72 "xen/gnttab: Fold grant_table_{create,
    set_limits}() into grant_table_init()".
    
    Since the commit, gt->active will be allocated while initializing the
    grant table at domain creation. Therefore gt-active will always be
    valid.
    
    Rather than replacing the warning by another one, drop the check
    completely as we will likely not come back to a semi-initialized world.
    
    Signed-off-by: Julien Grall <jgrall@amazon.com>
    Acked-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- xen/common/grant_table.c
+++ xen/common/grant_table.c
@@ -1793,12 +1793,6 @@ gnttab_grow_table(struct domain *d, unsigned int req_nr_frames)
     struct grant_table *gt = d->grant_table;
     unsigned int i, j;
 
-    if ( unlikely(!gt->active) )
-    {
-        gprintk(XENLOG_WARNING, "grant_table_set_limits() call missing\n");
-        return -ENODEV;
-    }
-
     if ( req_nr_frames < INITIAL_NR_GRANT_FRAMES )
         req_nr_frames = INITIAL_NR_GRANT_FRAMES;
     ASSERT(req_nr_frames <= gt->max_grant_frames);
