dacdbf7088d6a3705a9831e73991c2b14c519a65
Anthony PERARD <anthony.perard@citrix.com>
2020-05-20 17:39:42 +0100

    tools/xenstore: mark variable in header as extern
    This patch fix "multiple definition of `xprintf'" (or xgt_handle)
    build error with GCC 10.1.0.
    
    These are the error reported:
        gcc xs_tdb_dump.o utils.o tdb.o talloc.o      -o xs_tdb_dump
        /usr/bin/ld: utils.o:./utils.h:27: multiple definition of `xprintf'; xs_tdb_dump.o:./utils.h:27: first defined here
        [...]
        gcc xenstored_core.o xenstored_watch.o xenstored_domain.o xenstored_transaction.o xenstored_control.o xs_lib.o talloc.o utils.o tdb.o hashtable.o xenstored_posix.o      -lsystemd   -Wl,-rpath-link=... ../libxc/libxenctrl.so -lrt  -o xenstored
        /usr/bin/ld: xenstored_watch.o:./xenstored_core.h:207: multiple definition of `xgt_handle'; xenstored_core.o:./xenstored_core.h:207: first defined here
        /usr/bin/ld: xenstored_domain.o:./xenstored_core.h:207: multiple definition of `xgt_handle'; xenstored_core.o:./xenstored_core.h:207: first defined here
        /usr/bin/ld: xenstored_transaction.o:./xenstored_core.h:207: multiple definition of `xgt_handle'; xenstored_core.o:./xenstored_core.h:207: first defined here
        /usr/bin/ld: xenstored_control.o:./xenstored_core.h:207: multiple definition of `xgt_handle'; xenstored_core.o:./xenstored_core.h:207: first defined here
        /usr/bin/ld: xenstored_posix.o:./xenstored_core.h:207: multiple definition of `xgt_handle'; xenstored_core.o:./xenstored_core.h:207: first defined here
    
    A difference that I noticed with earlier version of the build chain is
    that before, I had:
        $ nm xs_tdb_dump.o | grep xprintf
        0000000000000008 C xprintf
    And now, it's:
        0000000000000000 B xprintf
    With the patch apply, the symbol isn't in xs_tdb_dump.o anymore.
    
    Signed-off-by: Anthony PERARD <anthony.perard@citrix.com>
    Acked-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Ian Jackson <ian.jackson@eu.citrix.com>

--- tools/xenstore/xenstored_core.h
+++ tools/xenstore/xenstored_core.h
@@ -204,7 +204,7 @@ void finish_daemonize(void);
 /* Open a pipe for signal handling */
 void init_pipe(int reopen_log_pipe[2]);
 
-xengnttab_handle **xgt_handle;
+extern xengnttab_handle **xgt_handle;
 
 int remember_string(struct hashtable *hash, const char *str);
 
