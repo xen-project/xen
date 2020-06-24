ed44947e18ebdc7f345fbc36918febd77f7d5775
Roger Pau Monné <roger.pau@citrix.com>
2020-06-24 17:02:23 +0200

    x86/idle: prevent entering C6 with in service interrupts on Intel
    Apply a workaround for Intel errata BDX99, CLX30, SKX100, CFW125,
    BDF104, BDH85, BDM135, KWB131: "A Pending Fixed Interrupt May Be
    Dispatched Before an Interrupt of The Same Priority Completes".
    
    Apply the errata to all server and client models (big cores) from
    Broadwell to Cascade Lake. The workaround is grouped together with the
    existing fix for errata AAJ72, and the eoi from the function name is
    removed.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>
    master commit: fc44a7014cafe28b8c53eeaf6ac2a71f5bc8b815
    master date: 2020-05-22 16:07:38 +0200

--- xen/include/asm-x86/cpuidle.h
+++ xen/include/asm-x86/cpuidle.h
@@ -26,7 +26,7 @@ void update_idle_stats(struct acpi_processor_power *,
 void update_last_cx_stat(struct acpi_processor_power *,
                          struct acpi_processor_cx *, uint64_t);
 
-bool errata_c6_eoi_workaround(void);
+bool errata_c6_workaround(void);
 
 /*
  * vcpu is urgent if vcpu is polling event channel
