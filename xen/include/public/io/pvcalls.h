02d69864b51a4302a148c28d6d391238a6778b4b
Julien Grall <jgrall@amazon.com>
2020-06-27 10:55:33 +0100

    pvcalls: Document correctly and explicitely the padding for all arches
    The specification of pvcalls suggests there is padding for 32-bit x86 at
    the end of most the structure. However, they are not described in in the
    public header.
    
    Because of that all the structures would have a different size between
    32-bit x86 and 64-bit x86.
    
    For all the other architectures supported (Arm and 64-bit x86), the
    structure have the sames sizes because they contain implicit padding
    thanks to the 64-bit alignment of the field uint64_t field.
    
    Given the specification is authoritative, the padding will now be the
    same for all architectures. The potential breakage of compatibility is
    ought to be fine as pvcalls is still a tech preview.
    
    Signed-off-by: Julien Grall <jgrall@amazon.com>
    Reviewed-by: Juergen Gross <jgross@suse.com>
    Reviewed-by: Stefano Stabellini <sstabellini@kernel.org>
    Release-acked-by: Paul Durrant <paul@xen.org>
    Signed-off-by: Stefano Stabellini <stefano.stabellini@xilinx.com>

--- xen/include/public/io/pvcalls.h
+++ xen/include/public/io/pvcalls.h
@@ -68,6 +68,7 @@ struct xen_pvcalls_request {
             uint32_t domain;
             uint32_t type;
             uint32_t protocol;
+            uint8_t pad[4];
         } socket;
         struct xen_pvcalls_connect {
             uint64_t id;
@@ -76,10 +77,12 @@ struct xen_pvcalls_request {
             uint32_t flags;
             grant_ref_t ref;
             uint32_t evtchn;
+            uint8_t pad[4];
         } connect;
         struct xen_pvcalls_release {
             uint64_t id;
             uint8_t reuse;
+            uint8_t pad[7];
         } release;
         struct xen_pvcalls_bind {
             uint64_t id;
@@ -89,6 +92,7 @@ struct xen_pvcalls_request {
         struct xen_pvcalls_listen {
             uint64_t id;
             uint32_t backlog;
+            uint8_t pad[4];
         } listen;
         struct xen_pvcalls_accept {
             uint64_t id;
