d9751646697b1e2d337990264068ead3c10b993a
Jan Beulich <jbeulich@suse.com>
2021-01-14 13:00:26 +0100

    memory: avoid pointless continuation in xenmem_add_to_physmap()
    Adjust so we uniformly avoid needlessly arranging for a continuation on
    the last iteration.
    
    Fixes: 5777a3742d88 ("IOMMU: hold page ref until after deferred TLB flush")
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Reviewed-by: Julien Grall <jgrall@amazon.com>

--- xen/common/memory.c
+++ xen/common/memory.c
@@ -858,8 +858,9 @@ int xenmem_add_to_physmap(struct domain *d, struct xen_add_to_physmap *xatp,
             ++extra.ppage;
 
         /* Check for continuation if it's not the last iteration. */
-        if ( (++done >= ARRAY_SIZE(pages) && extra.ppage) ||
-             (xatp->size > done && hypercall_preempt_check()) )
+        if ( xatp->size > ++done &&
+             ((done >= ARRAY_SIZE(pages) && extra.ppage) ||
+              hypercall_preempt_check()) )
         {
             rc = start + done;
             break;
