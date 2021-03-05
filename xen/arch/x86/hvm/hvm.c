be2f8932e27872b79d11778b4fce22ddb81bae70
Jan Beulich <jbeulich@suse.com>
2021-03-05 15:42:59 +0100

    x86/HVM: re-order error path of hvm_domain_initialise()
    hvm_destroy_all_ioreq_servers(), called from
    hvm_domain_relinquish_resources(), invokes relocate_portio_handler(),
    which uses d->arch.hvm.io_handler. Defer freeing of this array
    accordingly on the error path of hvm_domain_initialise().
    
    Similarly rtc_deinit() requires d->arch.hvm.pl_time to still be around,
    or else an armed timer structure would get freed, and that timer never
    get killed.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Acked-by: Andrew Cooper <andrew.cooper3@citrix.com>
    master commit: fbb3bf002b42803ef289ea2a649ebd5f25d22236
    master date: 2021-01-29 11:36:54 +0100

--- xen/arch/x86/hvm/hvm.c
+++ xen/arch/x86/hvm/hvm.c
@@ -702,15 +702,15 @@ int hvm_domain_initialise(struct domain *d)
  fail1:
     if ( is_hardware_domain(d) )
         xfree(d->arch.hvm.io_bitmap);
-    XFREE(d->arch.hvm.io_handler);
     XFREE(d->arch.hvm.params);
-    XFREE(d->arch.hvm.pl_time);
     XFREE(d->arch.hvm.irq);
  fail0:
     hvm_destroy_cacheattr_region_list(d);
     destroy_perdomain_mapping(d, PERDOMAIN_VIRT_START, 0);
  fail:
     hvm_domain_relinquish_resources(d);
+    XFREE(d->arch.hvm.io_handler);
+    XFREE(d->arch.hvm.pl_time);
     return rc;
 }
 
