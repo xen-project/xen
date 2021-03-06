21657ad4f01a634beac570c64c0691e51b9cf366
Julien Grall <jgrall@amazon.com>
2021-03-06 21:41:48 +0000

    xen: Bump the minimum version of GCC supported to 4.9 for arm32 and 5.1 on arm64
    Compilers older than 4.8 have known codegen issues which can lead to
    silent miscompilation:
    
    https://gcc.gnu.org/bugzilla/show_bug.cgi?id=58145
    
    Furthermore, pre-4.9 GCC have known bugs (including things like
    internal compiler errors on Arm) which would require workaround (I
    haven't checked if we have any in Xen).
    
    The minimum version of GCC to build the hypervisor on arm is now
    raised to 4.9.
    
    In addition to that, on arm64, GCC version >= 4.9 and < 5.1 have been
    shown to emit memory references beyond the stack pointer, resulting in
    memory corruption if an interrupt is taken after the stack pointer has
    been adjusted but before the reference has been executed.
    
    Therefore, the minimum for arm64 is raised to 5.1.
    
    Signed-off-by: Julien Grall <jgrall@amazon.com>
    Reviewed-by: Bertrand Marquis <bertrand.marquis@arm.com>
    Reviewed-by: Stefano Stabellini <sstabellini@kernel.org>
    Release-Acked-by: Ian Jackson <iwj@xenproject.org>

--- xen/include/xen/compiler.h
+++ xen/include/xen/compiler.h
@@ -3,6 +3,16 @@
 
 #if !defined(__GNUC__) || (__GNUC__ < 4)
 #error Sorry, your compiler is too old/not recognized.
+#elif CONFIG_CC_IS_GCC
+# if defined(CONFIG_ARM_32) && CONFIG_GCC_VERSION < 40900
+#  error Sorry, your version of GCC is too old - please use 4.9 or newer.
+# elif defined(CONFIG_ARM_64) && CONFIG_GCC_VERSION < 50100
+/*
+ * https://gcc.gnu.org/bugzilla/show_bug.cgi?id=63293
+ * https://lore.kernel.org/r/20210107111841.GN1551@shell.armlinux.org.uk
+ */
+#  error Sorry, your version of GCC is too old - please use 5.1 or newer.
+# endif
 #endif
 
 #define barrier()     __asm__ __volatile__("": : :"memory")
