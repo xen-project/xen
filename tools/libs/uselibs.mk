6ca6d8c8050f368d97279f342aac7ecc67e0dc05
Andrew Cooper <andrew.cooper3@citrix.com>
2021-03-08 23:31:11 +0000

    tools/libs: Make uselibs.mk more legible
    A few blank lines go a very long way.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Acked-by: Anthony PERARD <anthony.perard@vates.tech>

--- tools/libs/uselibs.mk
+++ tools/libs/uselibs.mk
@@ -2,34 +2,49 @@
 
 LIBS_LIBS += toolcore
 USELIBS_toolcore :=
+
 LIBS_LIBS += toollog
 USELIBS_toollog :=
+
 LIBS_LIBS += evtchn
 USELIBS_evtchn := toollog toolcore
+
 LIBS_LIBS += gnttab
 USELIBS_gnttab := toollog toolcore
+
 LIBS_LIBS += call
 USELIBS_call := toollog toolcore
+
 LIBS_LIBS += foreignmemory
 USELIBS_foreignmemory := toollog toolcore
+
 LIBS_LIBS += devicemodel
 USELIBS_devicemodel := toollog toolcore call
+
 LIBS_LIBS += hypfs
 USELIBS_hypfs := toollog toolcore call
+
 LIBS_LIBS += manage
 USELIBS_manage := toollog toolcore call
+
 LIBS_LIBS += ctrl
 USELIBS_ctrl := toollog call evtchn gnttab foreignmemory devicemodel
+
 LIBS_LIBS += guest
 USELIBS_guest := evtchn ctrl
+
 LIBS_LIBS += store
 USELIBS_store := toolcore
+
 LIBS_LIBS += vchan
 USELIBS_vchan := toollog store gnttab evtchn
+
 LIBS_LIBS += stat
 USELIBS_stat := ctrl store
+
 LIBS_LIBS += light
 USELIBS_light := toollog evtchn toolcore ctrl store hypfs guest
+
 LIBS_LIBS += util
 USELIBS_util := light
 FILENAME_util := xlutil
