b14d8345e530a54710700ec5fcb0879c2e056d31
Jan Beulich <jbeulich@suse.com>
2021-04-20 12:05:20 +0200

    x86/hpet: Don't enable legacy replacement mode unconditionally
    Commit e1de4c196a2e ("x86/timer: Fix boot on Intel systems using ITSSPRC
    static PIT clock gating") was reported to cause boot failures on certain
    AMD Ryzen systems.
    
    Refine the fix to do nothing in the default case, and only attempt to
    configure legacy replacement mode if IRQ0 is found to not be working.  If
    legacy replacement mode doesn't help, undo it before falling back to other IRQ
    routing configurations.
    
    In addition, introduce a "hpet" command line option so this heuristic
    can be overridden.  Since it makes little sense to introduce just
    "hpet=legacy-replacement", also allow for a boolean argument as well as
    "broadcast" to replace the separate "hpetbroadcast" option.
    
    Reported-by: Frédéric Pierret frederic.pierret@qubes-os.org
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Roger Pau Monné <roger.pau@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>
    Tested-by: Frédéric Pierret <frederic.pierret@qubes-os.org>
    master commit: b53173e7cdafb7a318a239d557478fd73734a86a
    master date: 2021-04-15 18:25:40 +0100

--- xen/arch/x86/io_apic.c
+++ xen/arch/x86/io_apic.c
@@ -29,6 +29,8 @@
 #include <xen/acpi.h>
 #include <xen/keyhandler.h>
 #include <xen/softirq.h>
+
+#include <asm/hpet.h>
 #include <asm/mc146818rtc.h>
 #include <asm/smp.h>
 #include <asm/desc.h>
@@ -1930,6 +1932,35 @@ static void __init check_timer(void)
             local_irq_restore(flags);
             return;
         }
+
+        /*
+         * Intel chipsets from Skylake/ApolloLake onwards can statically clock
+         * gate the 8254 PIT.  This option is enabled by default in slightly
+         * later systems, as turning the PIT off is a prerequisite to entering
+         * the C11 power saving state.
+         *
+         * Xen currently depends on the legacy timer interrupt being active
+         * while IRQ routing is configured.
+         *
+         * If the user hasn't made an explicit choice, attempt to reconfigure
+         * the HPET into legacy mode to re-establish the timer interrupt.
+         */
+        if ( opt_hpet_legacy_replacement < 0 &&
+             hpet_enable_legacy_replacement_mode() )
+        {
+            printk(XENLOG_ERR "..no 8254 timer found - trying HPET Legacy Replacement Mode\n");
+
+            if ( timer_irq_works() )
+            {
+                local_irq_restore(flags);
+                return;
+            }
+
+            /* Legacy Replacement mode hasn't helped.  Undo it. */
+            printk(XENLOG_ERR "..no HPET timer found - reverting Legacy Replacement Mode\n");
+            hpet_disable_legacy_replacement_mode();
+        }
+
         clear_IO_APIC_pin(apic1, pin1);
         printk(KERN_ERR "..MP-BIOS bug: 8254 timer not connected to "
                "IO-APIC\n");
