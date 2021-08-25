27bc41d712fe546be7bcccfa324005b67f167137
Jan Beulich <jbeulich@suse.com>
2021-08-25 14:43:09 +0200

    AMD/IOMMU: re-arrange exclusion range and unity map recording
    The spec makes no provisions for OS behavior here to depend on the
    amount of RAM found on the system. While the spec may not sufficiently
    clearly distinguish both kinds of regions, they are surely meant to be
    separate things: Only regions with ACPI_IVMD_EXCLUSION_RANGE set should
    be candidates for putting in the exclusion range registers. (As there's
    only a single such pair of registers per IOMMU, secondary non-adjacent
    regions with the flag set already get converted to unity mapped
    regions.)
    
    First of all, drop the dependency on max_page. With commit b4f042236ae0
    ("AMD/IOMMU: Cease using a dynamic height for the IOMMU pagetables") the
    use of it here was stale anyway; it was bogus already before, as it
    didn't account for max_page getting increased later on. Simply try an
    exclusion range registration first, and if it fails (for being
    unsuitable or non-mergeable), register a unity mapping range.
    
    With this various local variables become unnecessary and hence get
    dropped at the same time.
    
    With the max_page boundary dropped for using unity maps, the minimum
    page table tree height now needs both recording and enforcing in
    amd_iommu_domain_init(). Since we can't predict which devices may get
    assigned to a domain, our only option is to uniformly force at least
    that height for all domains, now that the height isn't dynamic anymore.
    
    Further don't make use of the exclusion range unless ACPI data says so.
    
    Note that exclusion range registration in
    register_range_for_all_devices() is on a best effort basis. Hence unity
    map entries also registered are redundant when the former succeeded, but
    they also do no harm. Improvements in this area can be done later imo.
    
    Also adjust types where suitable without touching extra lines.
    
    This is part of XSA-378.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Reviewed-by: Paul Durrant <paul@xen.org>
    master commit: 8ea80530cd0dbb8ffa7ac92606a3ee29663fdc93
    master date: 2021-08-25 14:16:46 +0200

--- xen/drivers/passthrough/amd/pci_amd_iommu.c
+++ xen/drivers/passthrough/amd/pci_amd_iommu.c
@@ -234,6 +234,8 @@ static int __must_check allocate_domain_resources(struct domain *d)
     return rc;
 }
 
+int __read_mostly amd_iommu_min_paging_mode = 1;
+
 static int amd_iommu_domain_init(struct domain *d)
 {
     struct domain_iommu *hd = dom_iommu(d);
@@ -245,11 +247,13 @@ static int amd_iommu_domain_init(struct domain *d)
      * - HVM could in principle use 3 or 4 depending on how much guest
      *   physical address space we give it, but this isn't known yet so use 4
      *   unilaterally.
+     * - Unity maps may require an even higher number.
      */
-    hd->arch.amd.paging_mode = amd_iommu_get_paging_mode(
-        is_hvm_domain(d)
-        ? 1ul << (DEFAULT_DOMAIN_ADDRESS_WIDTH - PAGE_SHIFT)
-        : get_upper_mfn_bound() + 1);
+    hd->arch.amd.paging_mode = max(amd_iommu_get_paging_mode(
+            is_hvm_domain(d)
+            ? 1ul << (DEFAULT_DOMAIN_ADDRESS_WIDTH - PAGE_SHIFT)
+            : get_upper_mfn_bound() + 1),
+        amd_iommu_min_paging_mode);
 
     return 0;
 }
