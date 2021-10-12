aa44f3c0a697cc387f79958d73e21594a9059cf6
Anthony PERARD <anthony.perard@citrix.com>
2021-10-12 11:50:47 +0200

    build,arm: move LDFLAGS change to arch.mk
    Changes to XEN_LDFLAGS may or may not apply to targets in for example
    "common/" depending on whether one runs `make` or `make common/`.
    
    But arch.mk is loaded before doing any build, so changes to LDFLAGS
    there mean that the value of XEN_LDFLAGS won't depends on the initial
    target.
    
    Signed-off-by: Anthony PERARD <anthony.perard@citrix.com>
    Acked-by: Julien Grall <jgrall@amazon.com>

--- xen/arch/arm/arch.mk
+++ xen/arch/arm/arch.mk
@@ -17,3 +17,11 @@ $(call cc-option-add,CFLAGS-$(CONFIG_ARM_64),CC,-mno-outline-atomics)
 ifneq ($(filter command line environment,$(origin CONFIG_EARLY_PRINTK)),)
     $(error You must use 'make menuconfig' to enable/disable early printk now)
 endif
+
+ifeq ($(CONFIG_ARM64_ERRATUM_843419),y)
+    ifeq ($(call ld-option, --fix-cortex-a53-843419),n)
+        $(warning ld does not support --fix-cortex-a53-843419; xen may be susceptible to erratum)
+    else
+        LDFLAGS += --fix-cortex-a53-843419
+    endif
+endif
