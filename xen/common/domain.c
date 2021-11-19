751efc5882f54f45d9c895d6745a1d95f1359dec
Dongli Zhang <dongli.zhang@oracle.com>
2021-11-19 09:30:14 +0100

    update system time immediately when VCPUOP_register_vcpu_info
    The guest may access the pv vcpu_time_info immediately after
    VCPUOP_register_vcpu_info. This is to borrow the idea of
    VCPUOP_register_vcpu_time_memory_area, where the
    force_update_vcpu_system_time() is called immediately when the new memory
    area is registered.
    
    Otherwise, we may observe clock drift at the VM side if the VM accesses
    the clocksource immediately after VCPUOP_register_vcpu_info().
    
    Reference: https://lists.xenproject.org/archives/html/xen-devel/2021-10/msg00571.html
    Signed-off-by: Dongli Zhang <dongli.zhang@oracle.com>
    Acked-by: Stefano Stabellini <sstabellini@kernel.org>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>
    master commit: b67f09721f136cc3a9afcb6a82466d1bd27aa6c0
    master date: 2021-11-03 10:19:06 +0100

--- xen/common/domain.c
+++ xen/common/domain.c
@@ -1694,6 +1694,8 @@ long do_vcpu_op(int cmd, unsigned int vcpuid, XEN_GUEST_HANDLE_PARAM(void) arg)
         rc = map_vcpu_info(v, info.mfn, info.offset);
         domain_unlock(d);
 
+        force_update_vcpu_system_time(v);
+
         break;
     }
 
