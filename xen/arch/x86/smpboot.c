c61cd82e01d58f04384afb78069afe71d994ef14
Andrew Cooper <andrew.cooper3@citrix.com>
2021-11-19 09:30:42 +0100

    x86/shstk: Fix use of shadow stacks with XPTI active
    The call to setup_cpu_root_pgt(0) in smp_prepare_cpus() is too early.  It
    clones the BSP's stack while the .data mapping is still in use, causing all
    mappings to be fully read read/write (and with no guard pages either).  This
    ultimately causes #DF when trying to enter the dom0 kernel for the first time.
    
    Defer setting up BSPs XPTI pagetable until reinit_bsp_stack() after we've set
    up proper shadow stack permissions.
    
    Fixes: 60016604739b ("x86/shstk: Rework the stack layout to support shadow stacks")
    Fixes: b60ab42db2f0 ("x86/shstk: Activate Supervisor Shadow Stacks")
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>
    master commit: b2851580b1f2ff121737a37cb25a370d7692ae3b
    master date: 2021-11-03 13:08:42 +0000

--- xen/arch/x86/smpboot.c
+++ xen/arch/x86/smpboot.c
@@ -805,7 +805,7 @@ static root_pgentry_t common_pgt;
 
 extern const char _stextentry[], _etextentry[];
 
-static int setup_cpu_root_pgt(unsigned int cpu)
+int setup_cpu_root_pgt(unsigned int cpu)
 {
     root_pgentry_t *rpt;
     unsigned int off;
@@ -1122,8 +1122,6 @@ static struct notifier_block cpu_smpboot_nfb = {
 
 void __init smp_prepare_cpus(void)
 {
-    int rc;
-
     register_cpu_notifier(&cpu_smpboot_nfb);
 
     mtrr_aps_sync_begin();
@@ -1137,10 +1135,7 @@ void __init smp_prepare_cpus(void)
 
     stack_base[0] = (void *)((unsigned long)stack_start & ~(STACK_SIZE - 1));
 
-    rc = setup_cpu_root_pgt(0);
-    if ( rc )
-        panic("Error %d setting up PV root page table\n", rc);
-    if ( per_cpu(root_pgt, 0) )
+    if ( opt_xpti_hwdom || opt_xpti_domu )
     {
         get_cpu_info()->pv_cr3 = 0;
 
