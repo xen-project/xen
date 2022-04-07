d7b22226b5172c86c0c19b23edc16a4432d04fb8
Roger Pau Monné <roger.pau@citrix.com>
2022-04-07 09:11:53 +0200

    tools/firmware: do not add a .note.gnu.property section
    Prevent the assembler from creating a .note.gnu.property section on
    the output objects, as it's not useful for firmware related binaries,
    and breaks the resulting rombios image.
    
    This requires modifying the cc-option Makefile macro so it can test
    assembler options (by replacing the usage of the -S flag with -c) and
    also stripping the -Wa, prefix if present when checking for the test
    output.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Acked-by: Anthony PERARD <anthony.perard@citrix.com>
    Acked-by: Andrew Cooper <andrew.cooper3@citrix.com>
    master commit: e270af94280e6a9610705ebc1fdd1d7a9b1f8a98
    master date: 2022-04-04 12:30:07 +0100

--- tools/firmware/Rules.mk
+++ tools/firmware/Rules.mk
@@ -17,5 +17,9 @@ $(call cc-options-add,CFLAGS,CC,$(EMBEDDED_EXTRA_CFLAGS))
 
 $(call cc-option-add,CFLAGS,CC,-fcf-protection=none)
 
+# Do not add the .note.gnu.property section to any of the firmware objects: it
+# breaks the rombios binary and is not useful for firmware anyway.
+$(call cc-option-add,CFLAGS,CC,-Wa$$(comma)-mx86-used-note=no)
+
 # Extra CFLAGS suitable for an embedded type of environment.
 CFLAGS += -fno-builtin -msoft-float
