87faac2c5ea07ccf306f4d94ac0b41bca76f504b
Roger Pau Monné <roger.pau@citrix.com>
2022-04-07 09:11:35 +0200

    tools/firmware: force -fcf-protection=none
    Do so right in firmware/Rules.mk, like it's done for other compiler
    flags.
    
    Fixes: 3667f7f8f7 ('x86: Introduce support for CET-IBT')
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Reviewed-by: Anthony PERARD <anthony.perard@citrix.com>
    Acked-by: Andrew Cooper <andrew.cooper3@citrix.com>
    master commit: 7225f6e0cd3afd48b4d61c43dd8fead0f4c92193
    master date: 2022-04-04 12:30:00 +0100

--- tools/firmware/Rules.mk
+++ tools/firmware/Rules.mk
@@ -15,5 +15,7 @@ CFLAGS += -Werror
 
 $(call cc-options-add,CFLAGS,CC,$(EMBEDDED_EXTRA_CFLAGS))
 
+$(call cc-option-add,CFLAGS,CC,-fcf-protection=none)
+
 # Extra CFLAGS suitable for an embedded type of environment.
 CFLAGS += -fno-builtin -msoft-float
