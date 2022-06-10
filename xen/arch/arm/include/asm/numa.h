e8f1c8118f7693be20eab2abcb86d16d12729ba0
Wei Chen <wei.chen@arm.com>
2022-06-10 13:53:13 +0800

    xen/arm: use !CONFIG_NUMA to keep fake NUMA API
    We have introduced CONFIG_NUMA in a previous patch. And this
    option is enabled only on x86 at the current stage. In a follow
    up patch, we will enable this option for Arm. But we still
    want users to be able to disable the CONFIG_NUMA via Kconfig. In
    this case, keep the fake NUMA API, will make Arm code still
    able to work with NUMA aware memory allocation and scheduler.
    
    Signed-off-by: Wei Chen <wei.chen@arm.com>
    Tested-by: Jiamei Xie <jiamei.xie@arm.com>
    Reviewed-by: Stefano Stabellini <sstabellini@kernel.org>

--- xen/arch/arm/include/asm/numa.h
+++ xen/arch/arm/include/asm/numa.h
@@ -5,6 +5,8 @@
 
 typedef u8 nodeid_t;
 
+#ifndef CONFIG_NUMA
+
 /* Fake one node for now. See also node_online_map. */
 #define cpu_to_node(cpu) 0
 #define node_to_cpumask(node)   (cpu_online_map)
@@ -24,6 +26,9 @@ extern mfn_t first_valid_mfn;
 #define node_spanned_pages(nid) (max_page - mfn_x(first_valid_mfn))
 #define node_start_pfn(nid) (mfn_x(first_valid_mfn))
 #define __node_distance(a, b) (20)
+
+#endif
+
 #define arch_want_default_dmazone() (false)
 
 #endif /* __ARCH_ARM_NUMA_H */
