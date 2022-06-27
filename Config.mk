cc3329fbfbb5af1c4fdb8a7a4e3a87c12264661c
Jan Beulich <jbeulich@suse.com>
2022-06-27 15:16:56 +0200

    update Xen version to 4.15.3

--- Config.mk
+++ Config.mk
@@ -244,15 +244,15 @@ SEABIOS_UPSTREAM_URL ?= git://xenbits.xen.org/seabios.git
 MINIOS_UPSTREAM_URL ?= git://xenbits.xen.org/mini-os.git
 endif
 OVMF_UPSTREAM_REVISION ?= a3741780fe3535e19e02efa869a7cac481891129
-QEMU_UPSTREAM_REVISION ?= qemu-xen-4.15.2
-MINIOS_UPSTREAM_REVISION ?= xen-RELEASE-4.15.2
+QEMU_UPSTREAM_REVISION ?= qemu-xen-4.15.3
+MINIOS_UPSTREAM_REVISION ?= xen-RELEASE-4.15.3
 
 SEABIOS_UPSTREAM_REVISION ?= rel-1.14.0
 
 ETHERBOOT_NICS ?= rtl8139 8086100e
 
 
-QEMU_TRADITIONAL_REVISION ?= xen-4.15.2
+QEMU_TRADITIONAL_REVISION ?= xen-4.15.3
 
 # Specify which qemu-dm to use. This may be `ioemu' to use the old
 # Mercurial in-tree version, or a local directory, or a git URL.
