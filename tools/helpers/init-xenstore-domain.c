d174fefa90487ddd25ebc618028f67b2e8a1f795
Juergen Gross <jgross@suse.com>
2022-09-13 07:35:13 +0200

    tools/xenstore: make the internal memory data base the default
    Having a file backed data base has the only advantage of being capable
    to dump the contents of it while Xenstore is running, and potentially
    using less swap space in case the data base can't be kept in memory.
    
    It has the major disadvantage of a huge performance overhead: switching
    to keep the data base in memory only speeds up live update of xenstored
    with 120000 nodes from 20 minutes to 11 seconds. A complete tree walk
    of this configuration will be reduced from 7 seconds to 280 msecs
    (measured by "xenstore-control check").
    
    So make the internal memory data base the default and enhance the
    "--internal-db" command line parameter to take an optional parameter
    allowing to switch the internal data base back to the file based one.
    
    This is part of XSA-419.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>
    Reviewed-by: Julien Grall <jgrall@amazon.com>

--- tools/helpers/init-xenstore-domain.c
+++ tools/helpers/init-xenstore-domain.c
@@ -222,9 +222,9 @@ static int build(xc_interface *xch)
     }
 
     if ( param )
-        snprintf(cmdline, 512, "--event %d --internal-db %s", rv, param);
+        snprintf(cmdline, 512, "--event %d %s", rv, param);
     else
-        snprintf(cmdline, 512, "--event %d --internal-db", rv);
+        snprintf(cmdline, 512, "--event %d", rv);
 
     dom->guest_domid = domid;
     dom->cmdline = xc_dom_strdup(dom, cmdline);
