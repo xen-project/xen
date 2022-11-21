345135942bf9632eba1409ba432cfcae3b7649c7
Andrew Cooper <andrew.cooper3@citrix.com>
2022-11-21 12:46:39 +0000

    xen/flask: Wire up XEN_DOMCTL_{get,set}_paging_mempool_size
    These were overlooked in the original patch, and noticed by OSSTest which does
    run some Flask tests.
    
    Fixes: 22b20bd98c02 ("xen: Introduce non-broken hypercalls for the paging mempool size")
    Suggested-by: Daniel Smith <dpsmith@apertussolutions.com>
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Jason Andryuk <jandryuk@gmail.com>
    Acked-by: Daniel P. Smith <dpsmith@apertussolutions.com>
    Release-acked-by: Henry Wang <Henry.Wang@arm.com>

--- xen/xsm/flask/hooks.c
+++ xen/xsm/flask/hooks.c
@@ -822,6 +822,12 @@ static int cf_check flask_domctl(struct domain *d, int cmd)
     case XEN_DOMCTL_get_cpu_policy:
         return current_has_perm(d, SECCLASS_DOMAIN2, DOMAIN2__GET_CPU_POLICY);
 
+    case XEN_DOMCTL_get_paging_mempool_size:
+        return current_has_perm(d, SECCLASS_DOMAIN, DOMAIN__GETPAGINGMEMPOOL);
+
+    case XEN_DOMCTL_set_paging_mempool_size:
+        return current_has_perm(d, SECCLASS_DOMAIN, DOMAIN__SETPAGINGMEMPOOL);
+
     default:
         return avc_unknown_permission("domctl", cmd);
     }
