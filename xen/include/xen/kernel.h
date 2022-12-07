bc19af0bd4b8d0bb817defa4470873c80f67b6c2
Luca Fancellu <luca.fancellu@arm.com>
2022-12-07 13:07:04 +0000

    xen: Justify linker script defined symbols in include/xen/kernel.h
    Eclair and Coverity found violation of the MISRA rule 8.6 for the
    symbols _start, _end, start, _stext, _etext, _srodata, _erodata,
    _sinittext, _einittext which are declared in
    xen/include/xen/kernel.h.
    All those symbols are defined by the liker script so we can deviate
    from the rule 8.6 for these cases.
    
    Signed-off-by: Luca Fancellu <luca.fancellu@arm.com>
    Reviewed-by: Stefano Stabellini <sstabellini@kernel.org>
    Acked-by: Jan Beulich <jbeulich@suse.com>
    Tested-by: Stefano Stabellini <sstabellini@kernel.org>

--- xen/include/xen/kernel.h
+++ xen/include/xen/kernel.h
@@ -65,24 +65,28 @@
 	1;                                      \
 })
 
+/* SAF-0-safe */
 extern char _start[], _end[], start[];
 #define is_kernel(p) ({                         \
     char *__p = (char *)(unsigned long)(p);     \
     (__p >= _start) && (__p < _end);            \
 })
 
+/* SAF-0-safe */
 extern char _stext[], _etext[];
 #define is_kernel_text(p) ({                    \
     char *__p = (char *)(unsigned long)(p);     \
     (__p >= _stext) && (__p < _etext);          \
 })
 
+/* SAF-0-safe */
 extern const char _srodata[], _erodata[];
 #define is_kernel_rodata(p) ({                  \
     const char *__p = (const char *)(unsigned long)(p);     \
     (__p >= _srodata) && (__p < _erodata);      \
 })
 
+/* SAF-0-safe */
 extern char _sinittext[], _einittext[];
 #define is_kernel_inittext(p) ({                \
     char *__p = (char *)(unsigned long)(p);     \
