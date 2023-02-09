e51d31f79edc10f8d622e7f5bf4b935af6d4618f
Andrew Cooper <andrew.cooper3@citrix.com>
2023-02-09 15:25:10 +0000

    tools/ocaml: Drop libxl and xentoollog bindings
    There are significant issues with these bindings (they segfault for starters)
    and their companion half in Xapi was deleted in 2018
    
      https://github.com/xapi-project/xen-api/commit/203292ebe0c487d7ae4adb961a6d080f4fbe933d
    
    owing to there having been no development of these bindings since 2014.
    
    In the unlikely event that we'd want to reinstate them, they'd need reworking
    basically from scratch anyway.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Acked-by: Christian Lindig <christian.lindig@citrix.com>

--- tools/ocaml/test/raise_exception.ml
+++
@@ -1,9 +0,0 @@
-open Printf
-
-let _ =
-  try
-    Xenlight.test_raise_exception ()
-  with Xenlight.Error(err, fn) -> begin
-      printf "Caught Exception: %s: %s\n" (Xenlight.string_of_error err) fn;
-    end
-
