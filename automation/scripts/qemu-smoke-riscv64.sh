7174971f5f108455c219c999b900ff2056c239c8
Oleksii Kurochko <oleksii.kurochko@gmail.com>
2023-02-09 11:14:40 +0100

    automation: add RISC-V smoke test
    Add check if there is a message 'Hello from C env' presents
    in log file to be sure that stack is set and C part of early printk
    is working.
    
    Signed-off-by: Oleksii Kurochko <oleksii.kurochko@gmail.com>
    Acked-by: Stefano Stabellini <sstabellini@kernel.org>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>

---
+++ automation/scripts/qemu-smoke-riscv64.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ex
+
+# Run the test
+rm -f smoke.serial
+set +e
+
+timeout -k 1 2 \
+qemu-system-riscv64 \
+    -M virt \
+    -smp 1 \
+    -nographic \
+    -m 2g \
+    -kernel binaries/xen \
+    |& tee smoke.serial
+
+set -e
+(grep -q "Hello from C env" smoke.serial) || exit 1
+exit 0
