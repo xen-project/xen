60b18e55c2ad0e0c840422b25b155a1544542907
Ayan Kumar Halder <ayan.kumar.halder@amd.com>
2023-05-02 12:04:14 +0200

    xen/riscv: update two license headers
    Updated the license header in a separate comment of its own.
    
    Signed-off-by: Ayan Kumar Halder <ayan.kumar.halder@amd.com>
    Reviewed-by: Oleksii Kurochko <oleksii.kurochko@gmail.com>
    Acked-by: Jan Beulich <jbeulich@suse.com>

--- xen/arch/riscv/include/asm/csr.h
+++ xen/arch/riscv/include/asm/csr.h
@@ -1,6 +1,5 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * SPDX-License-Identifier: GPL-2.0-only
- *
  * Copyright (C) 2015 Regents of the University of California
  */
 
--- xen/arch/riscv/include/asm/riscv_encoding.h
+++ xen/arch/riscv/include/asm/riscv_encoding.h
@@ -1,6 +1,5 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * SPDX-License-Identifier: BSD-2-Clause
- *
  * Copyright (c) 2019 Western Digital Corporation or its affiliates.
  *
  * Authors:
