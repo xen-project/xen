886c22f00c575199f7bb3dc5b7662dd09bf05146
Jan Beulich <jbeulich@suse.com>
2023-07-06 09:06:48 +0200

    ppc: make also linking work with big-endian tool chain
    Telling just the compiler to produce little-endian objects isn't enough.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Acked-by: Shawn Anastasio <sanastasio@raptorengineering.com>

--- xen/arch/ppc/arch.mk
+++ xen/arch/ppc/arch.mk
@@ -7,6 +7,8 @@ ppc-march-$(CONFIG_POWER_ISA_3_00) := power9
 CFLAGS += -m64 -mlittle-endian -mcpu=$(ppc-march-y)
 CFLAGS += -mstrict-align -mcmodel=large -mabi=elfv2 -mno-altivec -mno-vsx
 
+LDFLAGS += -m elf64lppc
+
 # TODO: Drop override when more of the build is working
 override ALL_OBJS-y = arch/$(SRCARCH)/built_in.o
 override ALL_LIBS-y =
