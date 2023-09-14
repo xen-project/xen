ea36ac0de27c2a7c847a2a52c3e0f97a45864d81
Shawn Anastasio <sanastasio@raptorengineering.com>
2023-09-14 14:03:34 -0500

    xen/ppc: Enable full Xen build
    Bring ppc's Makefile and arch.mk in line with arm and x86 to disable the
    build overrides and enable the full Xen build.
    
    Signed-off-by: Shawn Anastasio <sanastasio@raptorengineering.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>

--- xen/arch/ppc/arch.mk
+++ xen/arch/ppc/arch.mk
@@ -7,6 +7,3 @@ CFLAGS += -m64 -mlittle-endian -mcpu=$(ppc-march-y)
 CFLAGS += -mstrict-align -mcmodel=medium -mabi=elfv2 -fPIC -mno-altivec -mno-vsx -msoft-float
 
 LDFLAGS += -m elf64lppc
-
-# TODO: Drop override when more of the build is working
-override ALL_OBJS-y = arch/$(SRCARCH)/built_in.o common/libfdt/built_in.o lib/built_in.o
