9c27a730b85e69abca5eaf24111b1340a79a6dd7
Federico Serafini <federico.serafini@bugseng.com>
2023-11-16 09:12:19 +0100

    x86/vmce: address violations of MISRA C:2012 Rule 8.2
    Add missing parameter names. No functional change.
    
    Signed-off-by: Federico Serafini <federico.serafini@bugseng.com>
    Acked-by: Jan Beulich <jbeulich@suse.com>

--- xen/arch/x86/cpu/mcheck/vmce.h
+++ xen/arch/x86/cpu/mcheck/vmce.h
@@ -11,10 +11,10 @@ int vmce_init(struct cpuinfo_x86 *c);
 
 int unmmap_broken_page(struct domain *d, mfn_t mfn, unsigned long gfn);
 
-int vmce_intel_rdmsr(const struct vcpu *, uint32_t msr, uint64_t *val);
-int vmce_intel_wrmsr(struct vcpu *, uint32_t msr, uint64_t val);
-int vmce_amd_rdmsr(const struct vcpu *, uint32_t msr, uint64_t *val);
-int vmce_amd_wrmsr(struct vcpu *, uint32_t msr, uint64_t val);
+int vmce_intel_rdmsr(const struct vcpu *v, uint32_t msr, uint64_t *val);
+int vmce_intel_wrmsr(struct vcpu *v, uint32_t msr, uint64_t val);
+int vmce_amd_rdmsr(const struct vcpu *v, uint32_t msr, uint64_t *val);
+int vmce_amd_wrmsr(struct vcpu *v, uint32_t msr, uint64_t val);
 
 int fill_vmsr_data(struct mcinfo_bank *mc_bank, struct domain *d,
                    uint64_t gstatus, int vmce_vcpuid);
