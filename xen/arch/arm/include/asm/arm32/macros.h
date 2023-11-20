fa2da5bce90b3777aa7a323e1cf201c97b56d278
Ayan Kumar Halder <ayan.kumar.halder@amd.com>
2023-11-20 14:48:59 +0000

    xen/arm32: head Split and move MMU-specific head.S to mmu/head.S
    The MMU specific code in head.S will not be used on MPU systems.
    Instead of introducing more #ifdefs which will bring complexity
    to the code, move MMU related code to mmu/head.S and keep common
    code in head.S. Few notes while moving:
     - As "fail" in original head.S is very simple and this name is too
       easy to be conflicted, duplicate it in mmu/head.S instead of
       exporting it.
     - Realigned ".macro ret" so that the alignment matches to the other
       macros.
     - Rename puts to asm_puts, putn to asm_putn (this denotes that the
       macros are used within the context of assembly only).
     - Use ENTRY() for enable_secondary_cpu_mm, enable_boot_cpu_mm,
       setup_fixmap, asm_puts, asm_putn  as they will be used externally.
     - Removed early_puts() function definition when EARLY_PRINTK is not
       enabled.
    
    Also move the assembly macros shared by head.S and mmu/head.S to
    macros.h.
    
    This is based on 6734327d76be ("xen/arm64: Split and move MMU-specific head.S to mmu/head.S").
    
    Signed-off-by: Ayan Kumar Halder <ayan.kumar.halder@amd.com>
    Reviewed-by: Michal Orzel <michal.orzel@amd.com>

--- xen/arch/arm/include/asm/arm32/macros.h
+++ xen/arch/arm/include/asm/arm32/macros.h
@@ -1,8 +1,63 @@
 #ifndef __ASM_ARM_ARM32_MACROS_H
 #define __ASM_ARM_ARM32_MACROS_H
 
-    .macro ret
+.macro ret
         mov     pc, lr
-    .endm
+.endm
+
+/*
+ * Move an immediate constant into a 32-bit register using movw/movt
+ * instructions.
+ */
+.macro mov_w reg, word
+        movw  \reg, #:lower16:\word
+        movt  \reg, #:upper16:\word
+.endm
+
+/*
+ * Pseudo-op for PC relative adr <reg>, <symbol> where <symbol> is
+ * within the range +/- 4GB of the PC.
+ *
+ * @dst: destination register
+ * @sym: name of the symbol
+ */
+.macro adr_l, dst, sym
+        mov_w \dst, \sym - .Lpc\@
+        .set  .Lpc\@, .+ 8          /* PC bias */
+        add   \dst, \dst, pc
+.endm
+
+#ifdef CONFIG_EARLY_PRINTK
+/*
+ * Macro to print a string to the UART, if there is one.
+ *
+ * Clobbers r0 - r3
+ */
+#define PRINT(_s)           \
+        mov   r3, lr       ;\
+        adr_l r0, 98f      ;\
+        bl    asm_puts     ;\
+        mov   lr, r3       ;\
+        RODATA_STR(98, _s)
+
+/*
+ * Macro to print the value of register \rb
+ *
+ * Clobbers r0 - r4
+ */
+.macro print_reg rb
+        mov   r0, \rb
+        mov   r4, lr
+        bl    asm_putn
+        mov   lr, r4
+.endm
+
+#else /* CONFIG_EARLY_PRINTK */
+#define PRINT(s)
+
+.macro print_reg rb
+.endm
+
+#endif /* !CONFIG_EARLY_PRINTK */
 
 #endif /* __ASM_ARM_ARM32_MACROS_H */
