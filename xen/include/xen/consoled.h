e7ad219bb223113fc14df69655a07385fc2b2f86
Nicola Vetrini <nicola.vetrini@bugseng.com>
2023-11-21 10:08:46 +0100

    xen/console: remove stub definition in consoled.h
    The stub definition of 'consoled_guest_tx' can be removed, since its
    its single caller uses the implementation built with PV_SHIM enabled.
    
    Fixes: 5ef49f185c2d ("x86/pv-shim: shadow PV console's page for L2 DomU")
    Signed-off-by: Nicola Vetrini <nicola.vetrini@bugseng.com>
    Reviewed-by: Stefano Stabellini <sstabellini@kernel.org>

--- xen/include/xen/consoled.h
+++ xen/include/xen/consoled.h
@@ -3,18 +3,11 @@
 
 #include <public/io/console.h>
 
-#ifdef CONFIG_PV_SHIM
-
 void consoled_set_ring_addr(struct xencons_interface *ring);
 struct xencons_interface *consoled_get_ring_addr(void);
 size_t consoled_guest_rx(void);
 size_t consoled_guest_tx(char c);
 
-#else
-
-size_t consoled_guest_tx(char c) { return 0; }
-
-#endif /* !CONFIG_PV_SHIM */
 #endif /* __XEN_CONSOLED_H__ */
 /*
  * Local variables:
