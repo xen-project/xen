xen-project/xen

commit 00a70f44a68c762a6a369e5400064ecf31b0bbed

[HVM] Update VPIC device model for new interrupt delivery code. Move BSP VLAPIC initialisation to hvmloader. Remove callback_irq update hack from Linux unmodified drivers. Signed-off-by: Keir Fraser <keir@xensource.com>

xen/arch/x86/hvm/vpic.c
@@ -1,0 +1,463 @@
+/*
+ * i8259 interrupt controller emulation
+ * 
+ * Copyright (c) 2003-2004 Fabrice Bellard
+ * Copyright (c) 2005 Intel Corperation
+ * Copyright (c) 2006 Keir Fraser, XenSource Inc.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <xen/config.h>
+#include <xen/types.h>
+#include <xen/event.h>
+#include <xen/lib.h>
+#include <xen/errno.h>
+#include <xen/sched.h>
+#include <asm/hvm/hvm.h>
+#include <asm/hvm/io.h>
+#include <asm/hvm/support.h>
+
+#define vpic_domain(v) (container_of((v), struct domain, \
+                        arch.hvm_domain.irq.vpic[!vpic->is_master]))
+#define __vpic_lock(v) &container_of((v), struct hvm_irq, \
+                                     vpic[!(v)->is_master])->lock
+#define vpic_lock(v)   spin_lock(__vpic_lock(v))
+#define vpic_unlock(v) spin_unlock(__vpic_lock(v))
+#define vpic_is_locked(v) spin_is_locked(__vpic_lock(v))
+#define vpic_elcr_mask(v) (vpic->is_master ? (uint8_t)0xd8 : (uint8_t)0xfe);
+
+/* Return the highest priority found in mask. Return 8 if none. */
+#define VPIC_PRIO_NONE 8
+static int vpic_get_priority(struct vpic *vpic, uint8_t mask)
+{
+    int prio;
+
+    ASSERT(vpic_is_locked(vpic));
+
+    if ( mask == 0 )
+        return VPIC_PRIO_NONE;
+
+    /* prio = ffs(mask ROL vpic->priority_add); */
+    asm ( "rol %%cl,%b1 ; bsf %1,%0"
+          : "=r" (prio) : "r" ((uint32_t)mask), "c" (vpic->priority_add) );
+    return prio;
+}
+
+/* Return the PIC's highest priority pending interrupt. Return -1 if none. */
+static int vpic_get_highest_priority_irq(struct vpic *vpic)
+{
+    int cur_priority, priority, irq;
+    uint8_t mask;
+
+    ASSERT(vpic_is_locked(vpic));
+
+    mask = vpic->irr & ~vpic->imr;
+    priority = vpic_get_priority(vpic, mask);
+    if ( priority == VPIC_PRIO_NONE )
+        return -1;
+
+    irq = (priority + vpic->priority_add) & 7;
+
+    /*
+     * Compute current priority. If special fully nested mode on the master,
+     * the IRQ coming from the slave is not taken into account for the
+     * priority computation. In special mask mode, masked interrupts do not
+     * block lower-priority interrupts even if their IS bit is set.
+     */
+    mask = vpic->isr;
+    if ( vpic->special_fully_nested_mode && vpic->is_master && (irq == 2) )
+        mask &= ~(1 << 2);
+    if ( vpic->special_mask_mode )
+        mask &= ~vpic->imr;
+    cur_priority = vpic_get_priority(vpic, mask);
+
+    /* If a higher priority is found then an irq should be generated. */
+    return (priority < cur_priority) ? irq : -1;
+}
+
+static void vpic_update_int_output(struct vpic *vpic)
+{
+    int irq;
+
+    ASSERT(vpic_is_locked(vpic));
+
+    irq = vpic_get_highest_priority_irq(vpic);
+    if ( vpic->int_output == (irq >= 0) )
+        return;
+
+    /* INT line transition L->H or H->L. */
+    vpic->int_output = !vpic->int_output;
+
+    if ( vpic->int_output )
+    {
+        if ( vpic->is_master )
+        {
+            /* Master INT line is connected to VCPU0's VLAPIC LVT0. */
+            struct vcpu *v = vpic_domain(vpic)->vcpu[0];
+            if ( (v != NULL) && vlapic_accept_pic_intr(v) )
+                vcpu_kick(v);
+        }
+        else
+        {
+            /* Assert slave line in master PIC. */
+            (--vpic)->irr |= 1 << 2;
+            vpic_update_int_output(vpic);
+        }
+    }
+    else if ( !vpic->is_master )
+    {
+        /* Clear slave line in master PIC. */
+        (--vpic)->irr &= ~(1 << 2);
+        vpic_update_int_output(vpic);
+    }
+}
+
+static void __vpic_intack(struct vpic *vpic, int irq)
+{
+    uint8_t mask = 1 << irq;
+
+    ASSERT(vpic_is_locked(vpic));
+
+    /* Edge-triggered: clear the IRR (forget the edge). */
+    if ( !(vpic->elcr & mask) )
+        vpic->irr &= ~mask;
+
+    if ( !vpic->auto_eoi )
+        vpic->isr |= mask;
+    else if ( vpic->rotate_on_auto_eoi )
+        vpic->priority_add = (irq + 1) & 7;
+
+    vpic_update_int_output(vpic);
+}
+
+static int vpic_intack(struct vpic *vpic)
+{
+    int irq = -1;
+
+    vpic_lock(vpic);
+
+    if ( !vpic->int_output )
+        goto out;
+
+    irq = vpic_get_highest_priority_irq(vpic);
+    BUG_ON(irq < 0);
+    __vpic_intack(vpic, irq);
+
+    if ( (irq == 2) && vpic->is_master )
+    {
+        vpic++; /* Slave PIC */
+        irq = vpic_get_highest_priority_irq(vpic);
+        BUG_ON(irq < 0);
+        __vpic_intack(vpic, irq);
+        irq += 8;
+    }
+
+ out:
+    vpic_unlock(vpic);
+    return irq;
+}
+
+static void vpic_ioport_write(struct vpic *vpic, uint32_t addr, uint32_t val)
+{
+    int priority, cmd, irq;
+    uint8_t mask;
+
+    vpic_lock(vpic);
+
+    addr &= 1;
+    if ( addr == 0 )
+    {
+        if ( val & 0x10 )
+        {
+            /* ICW1 */
+            /* Clear edge-sensing logic. */
+            vpic->irr &= vpic->elcr;
+
+            /* No interrupts masked or in service. */
+            vpic->imr = vpic->isr = 0;
+
+            /* IR7 is lowest priority. */
+            vpic->priority_add = 0;
+            vpic->rotate_on_auto_eoi = 0;
+
+            vpic->special_mask_mode = 0;
+            vpic->readsel_isr = 0;
+            vpic->poll = 0;
+
+            if ( !(val & 1) )
+            {
+                /* NO ICW4: ICW4 features are cleared. */
+                vpic->auto_eoi = 0;
+                vpic->special_fully_nested_mode = 0;
+            }
+
+            vpic->init_state = ((val & 3) << 2) | 1;
+        }
+        else if ( val & 0x08 )
+        {
+            /* OCW3 */
+            if ( val & 0x04 )
+                vpic->poll = 1;
+            if ( val & 0x02 )
+                vpic->readsel_isr = val & 1;
+            if ( val & 0x40 )
+                vpic->special_mask_mode = (val >> 5) & 1;
+        }
+        else
+        {
+            /* OCW2 */
+            cmd = val >> 5;
+            switch ( cmd )
+            {
+            case 0: /* Rotate in AEOI Mode (Clear) */
+            case 4: /* Rotate in AEOI Mode (Set)   */
+                vpic->rotate_on_auto_eoi = cmd >> 2;
+                break;
+            case 1: /* Non-Specific EOI            */
+            case 5: /* Non-Specific EOI & Rotate   */
+                mask = vpic->isr;
+                if ( vpic->special_mask_mode )
+                    mask &= ~vpic->imr; /* SMM: ignore masked IRs. */
+                priority = vpic_get_priority(vpic, mask);
+                if ( priority == VPIC_PRIO_NONE )
+                    break;
+                irq = (priority + vpic->priority_add) & 7;
+                vpic->isr &= ~(1 << irq);
+                if ( cmd == 5 )
+                    vpic->priority_add = (irq + 1) & 7;
+                break;
+            case 3: /* Specific EOI                */
+            case 7: /* Specific EOI & Rotate       */
+                irq = val & 7;
+                vpic->isr &= ~(1 << irq);
+                if ( cmd == 7 )
+                    vpic->priority_add = (irq + 1) & 7;
+                break;
+            case 6: /* Set Priority                */
+                vpic->priority_add = (val + 1) & 7;
+                break;
+            }
+        }
+    }
+    else
+    {
+        switch ( vpic->init_state & 3 )
+        {
+        case 0:
+            /* OCW1 */
+            vpic->imr = val;
+            break;
+        case 1:
+            /* ICW2 */
+            vpic->irq_base = val & 0xf8;
+            vpic->init_state++;
+            if ( !(vpic->init_state & 8) )
+                break; /* CASCADE mode: wait for write to ICW3. */
+            /* SNGL mode: fall through (no ICW3). */
+        case 2:
+            /* ICW3 */
+            vpic->init_state++;
+            if ( !(vpic->init_state & 4) )
+                vpic->init_state = 0; /* No ICW4: init done */
+            break;
+        case 3:
+            /* ICW4 */
+            vpic->special_fully_nested_mode = (val >> 4) & 1;
+            vpic->auto_eoi = (val >> 1) & 1;
+            vpic->init_state = 0;
+            break;
+        }
+    }
+
+    vpic_update_int_output(vpic);
+
+    vpic_unlock(vpic);
+}
+
+static uint32_t vpic_ioport_read(struct vpic *vpic, uint32_t addr)
+{
+    if ( vpic->poll )
+    {
+        vpic->poll = 0;
+        return vpic_intack(vpic);
+    }
+
+    if ( (addr & 1) == 0 )
+        return (vpic->readsel_isr ? vpic->isr : vpic->irr);
+
+    return vpic->imr;
+}
+
+static int vpic_intercept_pic_io(ioreq_t *p)
+{
+    struct vpic *vpic;
+    uint32_t data;
+
+    if ( (p->size != 1) || (p->count != 1) )
+    {
+        gdprintk(XENLOG_WARNING, "PIC_IO bad access size %d\n", (int)p->size);
+        return 1;
+    }
+
+    vpic = &current->domain->arch.hvm_domain.irq.vpic[p->addr >> 7];
+
+    if ( p->dir == IOREQ_WRITE )
+    {
+        if ( p->data_is_ptr )
+            (void)hvm_copy_from_guest_phys(&data, p->data, p->size);
+        else
+            data = p->data;
+        vpic_ioport_write(vpic, (uint32_t)p->addr, (uint8_t)data);
+    }
+    else
+    {
+        data = vpic_ioport_read(vpic, (uint32_t)p->addr);
+        if ( p->data_is_ptr )
+            (void)hvm_copy_to_guest_phys(p->data, &data, p->size);
+        else
+            p->data = (u64)data;
+    }
+
+    return 1;
+}
+
+static int vpic_intercept_elcr_io(ioreq_t *p)
+{
+    struct vpic *vpic;
+    uint32_t data;
+
+    if ( (p->size != 1) || (p->count != 1) )
+    {
+        gdprintk(XENLOG_WARNING, "PIC_IO bad access size %d\n", (int)p->size);
+        return 1;
+    }
+
+    vpic = &current->domain->arch.hvm_domain.irq.vpic[p->addr & 1];
+
+    if ( p->dir == IOREQ_WRITE )
+    {
+        if ( p->data_is_ptr )
+            (void)hvm_copy_from_guest_phys(&data, p->data, p->size);
+        else
+            data = p->data;
+
+        /* Some IRs are always edge trig. Slave IR is always level trig. */
+        data &= vpic_elcr_mask(vpic);
+        if ( vpic->is_master )
+            data |= 1 << 2;
+        vpic->elcr = data;
+    }
+    else
+    {
+        /* Reader should not see hardcoded level-triggered slave IR. */
+        data = vpic->elcr & vpic_elcr_mask(vpic);
+
+        if ( p->data_is_ptr )
+            (void)hvm_copy_to_guest_phys(p->data, &data, p->size);
+        else
+            p->data = data;
+    }
+
+    return 1;
+}
+
+void vpic_init(struct domain *d)
+{
+    struct vpic *vpic;
+
+    /* Master PIC. */
+    vpic = &d->arch.hvm_domain.irq.vpic[0];
+    memset(vpic, 0, sizeof(*vpic));
+    vpic->is_master = 1;
+    vpic->elcr      = 1 << 2;
+    register_portio_handler(d, 0x20, 2, vpic_intercept_pic_io);
+    register_portio_handler(d, 0x4d0, 1, vpic_intercept_elcr_io);
+
+    /* Slave PIC. */
+    vpic++;
+    memset(vpic, 0, sizeof(*vpic));
+    register_portio_handler(d, 0xa0, 2, vpic_intercept_pic_io);
+    register_portio_handler(d, 0x4d1, 1, vpic_intercept_elcr_io);
+}
+
+void vpic_irq_positive_edge(struct domain *d, int irq)
+{
+    struct vpic *vpic = &d->arch.hvm_domain.irq.vpic[irq >> 3];
+    uint8_t mask = 1 << (irq & 7);
+
+    ASSERT(irq <= 15);
+    ASSERT(vpic_is_locked(vpic));
+
+    if ( irq == 2 )
+        return;
+
+    vpic->irr |= mask;
+    if ( !(vpic->imr & mask) )
+        vpic_update_int_output(vpic);
+}
+
+void vpic_irq_negative_edge(struct domain *d, int irq)
+{
+    struct vpic *vpic = &d->arch.hvm_domain.irq.vpic[irq >> 3];
+    uint8_t mask = 1 << (irq & 7);
+
+    ASSERT(irq <= 15);
+    ASSERT(vpic_is_locked(vpic));
+
+    if ( irq == 2 )
+        return;
+
+    vpic->irr &= ~mask;
+    if ( !(vpic->imr & mask) )
+        vpic_update_int_output(vpic);
+}
+
+int cpu_get_pic_interrupt(struct vcpu *v, int *type)
+{
+    int irq, vector;
+    struct vpic *vpic = &v->domain->arch.hvm_domain.irq.vpic[0];
+
+    if ( !vlapic_accept_pic_intr(v) || !vpic->int_output )
+        return -1;
+
+    irq = vpic_intack(vpic);
+    if ( irq == -1 )
+        return -1;
+
+    vector = vpic[irq >> 3].irq_base + (irq & 7);
+    *type = APIC_DM_EXTINT;
+    return vector;
+}
+
+int is_periodic_irq(struct vcpu *v, int irq, int type)
+{
+    int vec;
+    struct periodic_time *pt = &v->domain->arch.hvm_domain.pl_time.periodic_tm;
+
+    if ( pt->irq != 0 )
+        return 0;
+
+    if ( type == APIC_DM_EXTINT )
+        vec = v->domain->arch.hvm_domain.irq.vpic[0].irq_base;
+    else
+        vec = domain_vioapic(v->domain)->redirtbl[0].fields.vector;
+
+    return (irq == vec);
+}

tools/firmware/hvmloader/apic_regs.h
@@ -1,0 +1,108 @@
+#ifndef __ASM_APICDEF_H
+#define __ASM_APICDEF_H
+
+#define APIC_DEFAULT_PHYS_BASE 0xfee00000
+ 
+#define APIC_ID       0x20
+#define     APIC_ID_MASK             (0xFFu<<24)
+#define     GET_APIC_ID(x)           (((x)>>24)&0xFFu)
+#define     SET_APIC_ID(x)           (((x)<<24))
+#define APIC_LVR      0x30
+#define     APIC_LVR_MASK            0xFF00FF
+#define     GET_APIC_VERSION(x)      ((x)&0xFF)
+#define     GET_APIC_MAXLVT(x)       (((x)>>16)&0xFF)
+#define     APIC_INTEGRATED(x)       ((x)&0xF0)
+#define     APIC_XAPIC(x)            ((x) >= 0x14)
+#define APIC_TASKPRI  0x80
+#define     APIC_TPRI_MASK           0xFF
+#define APIC_ARBPRI   0x90
+#define     APIC_ARBPRI_MASK         0xFF
+#define APIC_PROCPRI  0xA0
+#define APIC_EOI      0xB0
+#define     APIC_EIO_ACK             0x0
+#define APIC_RRR      0xC0
+#define APIC_LDR      0xD0
+#define     APIC_LDR_MASK            (0xFF<<24)
+#define     GET_APIC_LOGICAL_ID(x)   (((x)>>24)&0xFF)
+#define     SET_APIC_LOGICAL_ID(x)   (((x)<<24))
+#define     APIC_ALL_CPUS            0xFF
+#define APIC_DFR      0xE0
+#define     APIC_DFR_CLUSTER         0x0FFFFFFFul
+#define     APIC_DFR_FLAT            0xFFFFFFFFul
+#define APIC_SPIV     0xF0
+#define     APIC_SPIV_FOCUS_DISABLED (1<<9)
+#define     APIC_SPIV_APIC_ENABLED   (1<<8)
+#define APIC_ISR      0x100
+#define APIC_TMR      0x180
+#define     APIC_IRR                 0x200
+#define     APIC_ESR                 0x280
+#define     APIC_ESR_SEND_CS         0x00001
+#define     APIC_ESR_RECV_CS         0x00002
+#define     APIC_ESR_SEND_ACC        0x00004
+#define     APIC_ESR_RECV_ACC        0x00008
+#define     APIC_ESR_SENDILL         0x00020
+#define     APIC_ESR_RECVILL         0x00040
+#define     APIC_ESR_ILLREGA         0x00080
+#define APIC_ICR      0x300
+#define     APIC_DEST_SELF           0x40000
+#define     APIC_DEST_ALLINC         0x80000
+#define     APIC_DEST_ALLBUT         0xC0000
+#define     APIC_ICR_RR_MASK         0x30000
+#define     APIC_ICR_RR_INVALID      0x00000
+#define     APIC_ICR_RR_INPROG       0x10000
+#define     APIC_ICR_RR_VALID        0x20000
+#define     APIC_INT_LEVELTRIG       0x08000
+#define     APIC_INT_ASSERT          0x04000
+#define     APIC_ICR_BUSY            0x01000
+#define     APIC_DEST_LOGICAL        0x00800
+#define     APIC_DEST_PHYSICAL       0x00000
+#define     APIC_DM_FIXED            0x00000
+#define     APIC_DM_LOWEST           0x00100
+#define     APIC_DM_SMI              0x00200
+#define     APIC_DM_REMRD            0x00300
+#define     APIC_DM_NMI              0x00400
+#define     APIC_DM_INIT             0x00500
+#define     APIC_DM_STARTUP          0x00600
+#define     APIC_DM_EXTINT           0x00700
+#define     APIC_VECTOR_MASK         0x000FF
+#define APIC_ICR2     0x310
+#define     GET_APIC_DEST_FIELD(x)   (((x)>>24)&0xFF)
+#define     SET_APIC_DEST_FIELD(x)   ((x)<<24)
+#define APIC_LVTT     0x320
+#define APIC_LVTTHMR  0x330
+#define APIC_LVTPC    0x340
+#define APIC_LVT0     0x350
+#define     APIC_LVT_TIMER_BASE_MASK (0x3<<18)
+#define     GET_APIC_TIMER_BASE(x)   (((x)>>18)&0x3)
+#define     SET_APIC_TIMER_BASE(x)   (((x)<<18))
+#define     APIC_TIMER_BASE_CLKIN    0x0
+#define     APIC_TIMER_BASE_TMBASE   0x1
+#define     APIC_TIMER_BASE_DIV      0x2
+#define     APIC_LVT_TIMER_PERIODIC  (1<<17)
+#define     APIC_LVT_MASKED          (1<<16)
+#define     APIC_LVT_LEVEL_TRIGGER   (1<<15)
+#define     APIC_LVT_REMOTE_IRR      (1<<14)
+#define     APIC_INPUT_POLARITY      (1<<13)
+#define     APIC_SEND_PENDING        (1<<12)
+#define     APIC_MODE_MASK           0x700
+#define     GET_APIC_DELIVERY_MODE(x) (((x)>>8)&0x7)
+#define     SET_APIC_DELIVERY_MODE(x,y) (((x)&~0x700)|((y)<<8))
+#define         APIC_MODE_FIXED          0x0
+#define         APIC_MODE_NMI            0x4
+#define         APIC_MODE_EXTINT         0x7
+#define APIC_LVT1     0x360
+#define APIC_LVTERR   0x370
+#define APIC_TMICT    0x380
+#define APIC_TMCCT    0x390
+#define APIC_TDCR     0x3E0
+#define     APIC_TDR_DIV_TMBASE      (1<<2)
+#define     APIC_TDR_DIV_1           0xB
+#define     APIC_TDR_DIV_2           0x0
+#define     APIC_TDR_DIV_4           0x1
+#define     APIC_TDR_DIV_8           0x2
+#define     APIC_TDR_DIV_16          0x3
+#define     APIC_TDR_DIV_32          0x8
+#define     APIC_TDR_DIV_64          0x9
+#define     APIC_TDR_DIV_128         0xA
+
+#endif

tools/firmware/hvmloader/hvmloader.c
@@ -26,6 +26,7 @@
 #include "acpi_utils.h"
 #include "smbios.h"
 #include "config.h"
+#include "apic_regs.h"
 #include "pci_regs.h"
 #include <xen/version.h>
 #include <xen/hvm/params.h>
@@ -154,16 +155,13 @@ init_hypercalls(void)
 
 static void apic_setup(void)
 {
-    volatile uint32_t *ioregsel;
-    volatile uint32_t *iowin;
-
-    /* IOAPIC memory-mapped access window registers. */
-    ioregsel = (volatile uint32_t *)(IOAPIC_BASE_ADDRESS + 0x00);
-    iowin    = (volatile uint32_t *)(IOAPIC_BASE_ADDRESS + 0x10);
-
     /* Set the IOAPIC ID to tha static value used in the MP/ACPI tables. */
-    *ioregsel = 0;
-    *iowin    = IOAPIC_ID;
+    ioapic_write(0x00, IOAPIC_ID);
+
+    /* Set up Virtual Wire mode. */
+    lapic_write(APIC_SPIV, APIC_SPIV_APIC_ENABLED | 0xFF);
+    lapic_write(APIC_LVT0, APIC_MODE_EXTINT << 8);
+    lapic_write(APIC_LVT1, APIC_MODE_NMI    << 8);
 }
 
 static void pci_setup(void)

tools/firmware/hvmloader/util.c
@@ -309,22 +309,24 @@ uint64_t e820_malloc(uint64_t size, uint32_t type, uint64_t mask)
 
 uint32_t ioapic_read(uint32_t reg)
 {
-    uint32_t *ioregsel = (uint32_t *)(IOAPIC_BASE_ADDRESS + 0x00);
-    uint32_t *iowin    = (uint32_t *)(IOAPIC_BASE_ADDRESS + 0x10);
-
-    *ioregsel = reg;
-    mb();
-    return *iowin;
+    *(volatile uint32_t *)(IOAPIC_BASE_ADDRESS + 0x00) = reg;
+    return *(volatile uint32_t *)(IOAPIC_BASE_ADDRESS + 0x10);
 }
 
 void ioapic_write(uint32_t reg, uint32_t val)
 {
-    uint32_t *ioregsel = (uint32_t *)(IOAPIC_BASE_ADDRESS + 0x00);
-    uint32_t *iowin    = (uint32_t *)(IOAPIC_BASE_ADDRESS + 0x10);
+    *(volatile uint32_t *)(IOAPIC_BASE_ADDRESS + 0x00) = reg;
+    *(volatile uint32_t *)(IOAPIC_BASE_ADDRESS + 0x10) = val;
+}
 
-    *ioregsel = reg;
-    wmb();
-    *iowin = val;
+uint32_t lapic_read(uint32_t reg)
+{
+    return *(volatile uint32_t *)(LAPIC_BASE_ADDRESS + reg);
+}
+
+void lapic_write(uint32_t reg, uint32_t val)
+{
+    *(volatile uint32_t *)(LAPIC_BASE_ADDRESS + reg) = val;
 }
 
 #define PCI_CONF1_ADDRESS(bus, devfn, reg) \

tools/firmware/hvmloader/util.h
@@ -20,6 +20,12 @@ uint8_t  inb(uint16_t addr);
 uint16_t inw(uint16_t addr);
 uint32_t inl(uint16_t addr);
 
+/* APIC access */
+uint32_t ioapic_read(uint32_t reg);
+void ioapic_write(uint32_t reg, uint32_t val);
+uint32_t lapic_read(uint32_t reg);
+void lapic_write(uint32_t reg, uint32_t val);
+
 /* PCI access */
 uint32_t pci_read(uint32_t devfn, uint32_t reg, uint32_t len);
 #define pci_readb(devfn, reg) ((uint8_t) pci_read(devfn, reg, 1))

unmodified_drivers/linux-2.6/platform-pci/evtchn.c
@@ -132,7 +132,7 @@ EXPORT_SYMBOL(notify_remote_via_irq);
 
 irqreturn_t evtchn_interrupt(int irq, void *dev_id, struct pt_regs *regs)
 {
-	unsigned int l1i, l2i, port;
+	unsigned int l1i, port;
 	int cpu = smp_processor_id();
 	irqreturn_t(*handler) (int, void *, struct pt_regs *);
 	shared_info_t *s = shared_info_area;
@@ -140,44 +140,28 @@ irqreturn_t evtchn_interrupt(int irq, void *dev_id, struct pt_regs *regs)
 	unsigned long l1, l2;
 
 	v->evtchn_upcall_pending = 0;
-	/* NB. No need for a barrier here -- XCHG is a barrier
-	 * on x86. */
+	/* NB. No need for a barrier here -- XCHG is a barrier on x86. */
 	l1 = xchg(&v->evtchn_pending_sel, 0);
-	while (l1 != 0)
-	{
+	while (l1 != 0) {
 		l1i = __ffs(l1);
 		l1 &= ~(1 << l1i);
-
-		l2 = s->evtchn_pending[l1i] & ~s->evtchn_mask[l1i];
-		while (l2 != 0)
-		{
-			l2i = __ffs(l2);
-
-			port = (l1i * BITS_PER_LONG) + l2i;
+		while ((l2 = s->evtchn_pending[l1i] & ~s->evtchn_mask[l1i])) {
+			port = (l1i * BITS_PER_LONG) + __ffs(l2);
 			synch_clear_bit(port, &s->evtchn_pending[0]);
 			if ((handler = evtchns[port].handler) != NULL)
-			{
 				handler(port, evtchns[port].dev_id,
 					regs);
-			}
 			else
-			{
-				printk(KERN_WARNING "unexpected event channel upcall on port %d!\n", port);
-			}
-			l2 = s->evtchn_pending[l1i] & ~s->evtchn_mask[l1i];
+				printk(KERN_WARNING "unexpected event channel "
+				       "upcall on port %d!\n", port);
 		}
 	}
-
-	/* Make sure the hypervisor has a chance to notice that the
-	   upcall_pending condition has been cleared, so that we don't
-	   try and reinject the interrupt again. */
-        (void)HYPERVISOR_xen_version(0, NULL);
 
 	return IRQ_HANDLED;
 }
 
 void force_evtchn_callback(void)
 {
-        (void)HYPERVISOR_xen_version(0, NULL);
+	(void)HYPERVISOR_xen_version(0, NULL);
 }
 EXPORT_SYMBOL(force_evtchn_callback);

xen/arch/x86/hvm/Makefile
@@ -3,7 +3,6 @@ subdir-y += vmx
 
 obj-y += hvm.o
 obj-y += i8254.o
-obj-y += i8259.o
 obj-y += instrlen.o
 obj-y += intercept.o
 obj-y += io.o
@@ -13,3 +12,4 @@ obj-y += pmtimer.o
 obj-y += rtc.o
 obj-y += vioapic.o
 obj-y += vlapic.o
+obj-y += vpic.o

xen/arch/x86/hvm/hvm.c
@@ -168,14 +168,13 @@ int hvm_domain_initialise(struct domain *d)
 
     spin_lock_init(&d->arch.hvm_domain.pbuf_lock);
     spin_lock_init(&d->arch.hvm_domain.buffered_io_lock);
+    spin_lock_init(&d->arch.hvm_domain.irq.lock);
 
     rc = shadow_enable(d, SHM2_refcounts|SHM2_translate|SHM2_external);
     if ( rc != 0 )
         return rc;
 
-    pic_init(domain_vpic(d));
-    register_pic_io_hook(d);
-
+    vpic_init(d);
     vioapic_init(d);
 
     return 0;
@@ -244,13 +243,14 @@ void hvm_vcpu_destroy(struct vcpu *v)
 
 int cpu_get_interrupt(struct vcpu *v, int *type)
 {
-    int irq;
+    int vector;
 
-    if ( (irq = cpu_get_apic_interrupt(v, type)) != -1 )
-        return irq;
+    if ( (vector = cpu_get_apic_interrupt(v, type)) != -1 )
+        return vector;
 
-    if ( (v->vcpu_id == 0) && ((irq = cpu_get_pic_interrupt(v, type)) != -1) )
-        return irq;
+    if ( (v->vcpu_id == 0) &&
+         ((vector = cpu_get_pic_interrupt(v, type)) != -1) )
+        return vector;
 
     return -1;
 }

commit a66b02a6b6bb73232a4fe2cd6f9ed970f34fd157

x86/vPIC: correct vpic_domain()
Make it use its parameter in both places.

Fixes: 00a70f44a68c ("[HVM] Update VPIC device model for new interrupt delivery code")
Signed-off-by: Jan Beulich <jbeulich@suse.com>
Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>
Acked-by: Roger Pau Monné <roger.pau@citrix.com>

xen/arch/x86/hvm/vpic.c
@@ -35,7 +35,7 @@
 #include <asm/hvm/save.h>
 
 #define vpic_domain(v) (container_of((v), struct domain, \
-                        arch.hvm.vpic[!vpic->is_master]))
+                                     arch.hvm.vpic[!(v)->is_master]))
 #define __vpic_lock(v) &container_of((v), struct hvm_domain, \
                                         vpic[!(v)->is_master])->irq_lock
 #define vpic_lock(v)   spin_lock(__vpic_lock(v))