b7559a0488ac286d92a09fc00614207b32abb72d
Roger Pau Monné <roger.pau@citrix.com>
2024-01-30 10:13:59 +0100

    x86/spec-ctrl: Expose RRSBA_CTRL to guests
    The CPUID feature bit signals the presence of the RRSBA_DIS_{U,S} controls in
    SPEC_CTRL MSR, first available in Intel AlderLake and Sapphire Rapids CPUs.
    
    Xen already knows how to context switch MSR_SPEC_CTRL properly between guest
    and hypervisor context.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>
    Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>
    (cherry picked from commit 478e4787fa64b621061177a7843c452e9a19916d)

--- xen/arch/x86/msr.c
+++ xen/arch/x86/msr.c
@@ -333,6 +333,8 @@ uint64_t msr_spec_ctrl_valid_bits(const struct cpu_policy *cp)
             (psfd       ? SPEC_CTRL_PSFD       : 0) |
             (cp->feat.ipred_ctrl
              ? (SPEC_CTRL_IPRED_DIS_U | SPEC_CTRL_IPRED_DIS_S) : 0) |
+            (cp->feat.rrsba_ctrl
+             ? (SPEC_CTRL_RRSBA_DIS_U | SPEC_CTRL_RRSBA_DIS_S) : 0) |
             0);
 }
 
