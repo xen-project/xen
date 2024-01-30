363745e52dc758bdfb2fd42d32f12276c80ed447
Roger Pau Monné <roger.pau@citrix.com>
2024-01-30 10:14:00 +0100

    x86/spec-ctrl: Expose BHI_CTRL to guests
    The CPUID feature bit signals the presence of the BHI_DIS_S control in
    SPEC_CTRL MSR, first available in Intel AlderLake and Sapphire Rapids CPUs
    
    Xen already knows how to context switch MSR_SPEC_CTRL properly between guest
    and hypervisor context.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>
    Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>
    (cherry picked from commit 583f1d0950529f3517b1741c2b21a028a82ba831)

--- xen/arch/x86/msr.c
+++ xen/arch/x86/msr.c
@@ -335,6 +335,7 @@ uint64_t msr_spec_ctrl_valid_bits(const struct cpu_policy *cp)
              ? (SPEC_CTRL_IPRED_DIS_U | SPEC_CTRL_IPRED_DIS_S) : 0) |
             (cp->feat.rrsba_ctrl
              ? (SPEC_CTRL_RRSBA_DIS_U | SPEC_CTRL_RRSBA_DIS_S) : 0) |
+            (cp->feat.bhi_ctrl   ? SPEC_CTRL_BHI_DIS_S : 0) |
             0);
 }
 
