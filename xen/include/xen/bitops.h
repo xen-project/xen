ba0d1a7cedc8b282b5b6e780e0846f3189e9cc90
Oleksii Kurochko <oleksii.kurochko@gmail.com>
2024-02-09 18:58:30 +0100

    xen/lib: introduce generic find next bit operations
    find-next-bit.c is common for Arm64, PPC and RISCV64,
    so it is moved to xen/lib.
    
    PPC has been transitioned to generic functions from find-next-bit.c
    since it now shares the same implementation as the PPC-specific code.
    
    The MISRA exclude list has been updated to verify
    lib/find-next-bit.c instead of Arm's find_next_bit.c,
    as Arm's find_next_bit.c has been relocated to xen/lib/.
    
    Despite CONFIG_GENERIC_FIND_FIRST_BIT not currently being used in
    Xen and being removed from the Linux kernel [1], it could theoretically
    prove useful for Xen. This is because the Linux kernel transitioned
    Arm to the generic version of find_first_bit() and
    find_first_zero_bit() due to improvements [1] in both performance
    and .text size.
    It would be prudent to investigate whether [1] is applicable to Xen
    as well and, if so, implement the necessary changes in a separate patch.
    
    [1] https://lore.kernel.org/linux-arch/20211005054059.475634-5-yury.norov@gmail.com/
    
    Suggested-by: Jan Beulich <jbeulich@suse.com>
    Signed-off-by: Oleksii Kurochko <oleksii.kurochko@gmail.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>
    Acked-by: Shawn Anastasio <sanastasio@raptorengineering.com>
    Reviewed-by: Julien Grall <jgrall@amazon.com>

--- xen/include/xen/bitops.h
+++ xen/include/xen/bitops.h
@@ -105,6 +105,54 @@ static inline int generic_flsl(unsigned long x)
  */
 #include <asm/bitops.h>
 
+#ifndef find_next_bit
+/**
+ * find_next_bit - find the next set bit in a memory region
+ * @addr: The address to base the search on
+ * @offset: The bitnumber to start searching at
+ * @size: The bitmap size in bits
+ */
+extern unsigned long find_next_bit(const unsigned long *addr,
+                                   unsigned long size,
+                                   unsigned long offset);
+#endif
+
+#ifndef find_next_zero_bit
+/**
+ * find_next_zero_bit - find the next cleared bit in a memory region
+ * @addr: The address to base the search on
+ * @offset: The bitnumber to start searching at
+ * @size: The bitmap size in bits
+ */
+extern unsigned long find_next_zero_bit(const unsigned long *addr,
+                                        unsigned long size,
+                                        unsigned long offset);
+#endif
+
+#ifndef find_first_bit
+/**
+ * find_first_bit - find the first set bit in a memory region
+ * @addr: The address to start the search at
+ * @size: The maximum size to search
+ *
+ * Returns the bit number of the first set bit.
+ */
+extern unsigned long find_first_bit(const unsigned long *addr,
+                                    unsigned long size);
+#endif
+
+#ifndef find_first_zero_bit
+/**
+ * find_first_zero_bit - find the first cleared bit in a memory region
+ * @addr: The address to start the search at
+ * @size: The maximum size to search
+ *
+ * Returns the bit number of the first cleared bit.
+ */
+extern unsigned long find_first_zero_bit(const unsigned long *addr,
+                                         unsigned long size);
+#endif
+
 #if BITS_PER_LONG == 64
 # define fls64 flsl
 # define ffs64 ffsl
