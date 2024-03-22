94896de1a98c4289fe6fef9e16ef99fc6ef2efc4
Andrew Cooper <andrew.cooper3@citrix.com>
2024-03-22 11:41:41 +0000

    x86/spec-ctrl: Rework conditional safety for SPEC_CTRL_ENTRY_*
    Right now, we have a mix of safety strategies in different blocks, making the
    logic fragile and hard to follow.
    
    Start addressing this by having a safety LFENCE at the end of the blocks,
    which can be patched out if other safety criteria are met.  This will allow us
    to simplify the sub-blocks.  For SPEC_CTRL_ENTRY_FROM_IST, simply leave an
    LFENCE unconditionally at the end; the IST path is not a fast-path by any
    stretch of the imagination.
    
    For SPEC_CTRL_ENTRY_FROM_INTR, the existing description was incorrect.  The
    IRET #GP path is non-fatal but can occur with the guest's choice of
    MSR_SPEC_CTRL.  It is safe to skip the flush/barrier-like protections when
    interrupting Xen, but we must run DO_SPEC_CTRL_ENTRY irrespective.
    
    This will skip RSB stuffing which was previously unconditional even when
    interrupting Xen.
    
    AFAICT, this is a missing cleanup from commit 3fffaf9c13e9 ("x86/entry: Avoid
    using alternatives in NMI/#MC paths") where we split the IST entry path out of
    the main INTR entry path.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Acked-by: Roger Pau Monné <roger.pau@citrix.com>

--- xen/arch/x86/include/asm/cpufeatures.h
+++ xen/arch/x86/include/asm/cpufeatures.h
@@ -52,5 +52,9 @@ XEN_CPUFEATURE(IBPB_ENTRY_HVM,    X86_SYNTH(29)) /* MSR_PRED_CMD used by Xen for
 #define X86_BUG_CLFLUSH_MFENCE    X86_BUG( 2) /* MFENCE needed to serialise CLFLUSH */
 #define X86_BUG_IBPB_NO_RET       X86_BUG( 3) /* IBPB doesn't flush the RSB/RAS */
 
+#define X86_SPEC_NO_LFENCE_ENTRY_PV X86_BUG(16) /* (No) safety LFENCE for SPEC_CTRL_ENTRY_PV. */
+#define X86_SPEC_NO_LFENCE_ENTRY_INTR X86_BUG(17) /* (No) safety LFENCE for SPEC_CTRL_ENTRY_INTR. */
+#define X86_SPEC_NO_LFENCE_ENTRY_VMX X86_BUG(18) /* (No) safety LFENCE for SPEC_CTRL_ENTRY_VMX. */
+
 /* Total number of capability words, inc synth and bug words. */
 #define NCAPINTS (FSCAPINTS + X86_NR_SYNTH + X86_NR_BUG) /* N 32-bit words worth of info */
