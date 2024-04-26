be5b08dd6ea6ef0f01caf537bdae125fa66a2230
Roger Pau Monné <roger.pau@citrix.com>
2024-04-26 12:43:44 +0200

    xen-livepatch: fix --force option comparison
    The check for --force option shouldn't be against 0.
    
    Reported-by: Jan Beulich <jbeulich@suse.com>
    Fixes: 62a72092a517 ('livepatch: introduce --force option')
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Acked-by: Anthony PERARD <anthony.perard@citrix.com>
    Reviewed-by: Ross Lagerwall <ross.lagerwall@citrix.com>

--- tools/misc/xen-livepatch.c
+++ tools/misc/xen-livepatch.c
@@ -576,7 +576,7 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    if ( strcmp("--force", argv[1]) )
+    if ( !strcmp("--force", argv[1]) )
     {
         if ( argc <= 2 )
         {
