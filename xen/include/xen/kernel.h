1672df9ebd80edb0832eb4671f946a4d98bfdd42
Luca Fancellu <luca.fancellu@arm.com>
2024-04-30 12:09:21 +0100

    xen/kernel.h: Import __struct_group from Linux
    Import __struct_group from Linux, commit 50d7bd38c3aa
    ("stddef: Introduce struct_group() helper macro"), in order to
    allow the access through the anonymous structure to the members
    without having to write also the name, e.g:
    
    struct foo {
        int one;
        struct {
            int two;
            int three, four;
        } thing;
        int five;
    };
    
    would become:
    
    struct foo {
        int one;
        __struct_group(/* None */, thing, /* None */,
            int two;
            int three, four;
        );
        int five;
    };
    
    Allowing the users of this structure to access the .thing members by
    using .two/.three/.four on the struct foo.
    This construct will become useful in order to have some generalized
    interfaces that shares some common members.
    
    Origin: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git 50d7bd38c3aa
    Reported-by: Nicola Vetrini <nicola.vetrini@bugseng.com>
    Signed-off-by: Luca Fancellu <luca.fancellu@arm.com>
    Reviewed-by: Stefano Stabellini <sstabellini@kernel.org>

--- xen/include/xen/kernel.h
+++ xen/include/xen/kernel.h
@@ -54,6 +54,27 @@
         typeof_field(type, member) *__mptr = (ptr);             \
         (type *)( (char *)__mptr - offsetof(type,member) );})
 
+/**
+ * __struct_group() - Create a mirrored named and anonyomous struct
+ *
+ * @TAG: The tag name for the named sub-struct (usually empty)
+ * @NAME: The identifier name of the mirrored sub-struct
+ * @ATTRS: Any struct attributes (usually empty)
+ * @MEMBERS: The member declarations for the mirrored structs
+ *
+ * Used to create an anonymous union of two structs with identical layout
+ * and size: one anonymous and one named. The former's members can be used
+ * normally without sub-struct naming, and the latter can be used to
+ * reason about the start, end, and size of the group of struct members.
+ * The named struct can also be explicitly tagged for layer reuse, as well
+ * as both having struct attributes appended.
+ */
+#define __struct_group(TAG, NAME, ATTRS, MEMBERS...) \
+    union { \
+        struct { MEMBERS } ATTRS; \
+        struct TAG { MEMBERS } ATTRS NAME; \
+    } ATTRS
+
 /*
  * Check at compile time that something is of a particular type.
  * Always evaluates to 1 so you may use it easily in comparisons.
