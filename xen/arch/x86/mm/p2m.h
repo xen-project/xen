ae7584f63678cd9adc1c2f3a1e813b67a6b24544
Sergiy Kibrik <Sergiy_Kibrik@epam.com>
2024-05-16 13:36:22 +0200

    x86/p2m: move altp2m-related code to separate file
    Move altp2m code from generic p2m.c file to altp2m.c, so it is kept separately
    and can possibly be disabled in the build. We may want to disable it when
    building for specific platform only, that doesn't support alternate p2m.
    
    No functional change intended.
    
    Signed-off-by: Sergiy Kibrik <Sergiy_Kibrik@epam.com>
    Acked-by: Jan Beulich <jbeulich@suse.com>

--- xen/arch/x86/mm/p2m.h
+++ xen/arch/x86/mm/p2m.h
@@ -22,6 +22,9 @@ static inline void p2m_free_logdirty(struct p2m_domain *p2m) {}
 int p2m_init_altp2m(struct domain *d);
 void p2m_teardown_altp2m(struct domain *d);
 
+void p2m_flush_table_locked(struct p2m_domain *p2m);
+int __must_check p2m_remove_entry(struct p2m_domain *p2m, gfn_t gfn, mfn_t mfn,
+                                  unsigned int page_order);
 void p2m_nestedp2m_init(struct p2m_domain *p2m);
 int p2m_init_nestedp2m(struct domain *d);
 void p2m_teardown_nestedp2m(struct domain *d);
