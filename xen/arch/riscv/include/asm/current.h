e6ed98863bc79728e5aa759d51f21c90187e2519
Oleksii Kurochko <oleksii.kurochko@gmail.com>
2024-05-17 15:54:58 +0200

    xen/riscv: add required things to current.h
    Add minimal requied things to be able to build full Xen.
    
    Signed-off-by: Oleksii Kurochko <oleksii.kurochko@gmail.com>
    Acked-by: Jan Beulich <jbeulich@suse.com>

--- xen/arch/riscv/include/asm/current.h
+++ xen/arch/riscv/include/asm/current.h
@@ -3,11 +3,30 @@
 #ifndef __ASM_CURRENT_H
 #define __ASM_CURRENT_H
 
+#include <xen/lib.h>
+#include <xen/percpu.h>
+#include <asm/processor.h>
+
+#ifndef __ASSEMBLY__
+
+/* Which VCPU is "current" on this PCPU. */
+DECLARE_PER_CPU(struct vcpu *, curr_vcpu);
+
+#define current            this_cpu(curr_vcpu)
+#define set_current(vcpu)  do { current = (vcpu); } while (0)
+#define get_cpu_current(cpu)  per_cpu(curr_vcpu, cpu)
+
+#define guest_cpu_user_regs() ({ BUG_ON("unimplemented"); NULL; })
+
 #define switch_stack_and_jump(stack, fn) do {               \
     asm volatile (                                          \
             "mv sp, %0\n"                                   \
             "j " #fn :: "r" (stack), "X" (fn) : "memory" ); \
     unreachable();                                          \
 } while ( false )
+
+#define get_per_cpu_offset() __per_cpu_offset[smp_processor_id()]
+
+#endif /* __ASSEMBLY__ */
 
 #endif /* __ASM_CURRENT_H */
