ad15a02e261d0fcb0d6466b2c8a21afa76256832
Tamas K Lengyel <tamas@tklengyel.com>
2024-07-23 13:58:54 +0200

    Add tools/fuzz/oss-fuzz/build.sh
    The build integration script for oss-fuzz targets. Future fuzzing targets can
    be added to this script and those targets will be automatically picked up by
    oss-fuzz without having to open separate PRs on the oss-fuzz repo.
    
    Signed-off-by: Tamas K Lengyel <tamas@tklengyel.com>
    Acked-by: Jan Beulich <jbeulich@suse.com>

---
+++ tools/fuzz/oss-fuzz/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -eu
+# Copyright 2024 Google LLC
+# SPDX-License-Identifier: Apache-2.0
+
+# This script is intended to be run only from the oss-fuzz docker framework
+# See https://google.github.io/oss-fuzz/getting-started/new-project-guide/#buildsh
+cd xen
+./configure --disable-stubdom --disable-pvshim --disable-docs --disable-xen --with-system-qemu
+make clang=y -C tools/include
+make clang=y -C tools/fuzz/x86_instruction_emulator libfuzzer-harness
+cp tools/fuzz/x86_instruction_emulator/libfuzzer-harness $OUT/x86_instruction_emulator
