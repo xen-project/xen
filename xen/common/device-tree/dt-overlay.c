1e81d2df4f48f37d2086bc75199c75ca93cace92
Michal Orzel <michal.orzel@amd.com>
2024-10-10 12:57:46 +0200

    device-tree: Move dt-overlay.c to common/device-tree/
    The code is DT specific and as such should be placed under common
    directory for DT related files. Update MAINTAINERS file accordingly
    and drop the line with a path from a top-level comment in dt-overlay.c.
    It serves no purpose and requires being updated on every code movement.
    
    Signed-off-by: Michal Orzel <michal.orzel@amd.com>
    Reviewed-by: Stefano Stabellini <sstabellini@kernel.org>

--- xen/common/device-tree/dt-overlay.c
+++ xen/common/device-tree/dt-overlay.c
@@ -1,7 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * xen/common/dt-overlay.c
- *
  * Device tree overlay support in Xen.
  *
  * Copyright (C) 2023, Advanced Micro Devices, Inc. All Rights Reserved.
