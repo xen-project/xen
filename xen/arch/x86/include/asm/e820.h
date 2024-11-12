380b32a476e714275c53e51f6482b3b650aff6f8
Andrew Cooper <andrew.cooper3@citrix.com>
2024-11-12 21:27:52 +0000

    x86/e820: Fix parameter names of reserve_e820_ram()/e820_change_range_type()
    The work to address Rule 5.3 introduced violations of Rule 8.3 by failing to
    rename the parameters in the declaration.
    
    Fixes: b5fd405aa381 ("x86/e820 address violations of MISRA C:2012 Rule 5.3")
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Stefano Stabellini <sstabellini@kernel.org>

--- xen/arch/x86/include/asm/e820.h
+++ xen/arch/x86/include/asm/e820.h
@@ -25,9 +25,9 @@ struct e820map {
 
 extern int sanitize_e820_map(struct e820entry *biosmap, unsigned int *pnr_map);
 extern int e820_all_mapped(u64 start, u64 end, unsigned type);
-extern int reserve_e820_ram(struct e820map *e820, uint64_t s, uint64_t e);
+extern int reserve_e820_ram(struct e820map *map, uint64_t s, uint64_t e);
 extern int e820_change_range_type(
-    struct e820map *e820, uint64_t s, uint64_t e,
+    struct e820map *map, uint64_t s, uint64_t e,
     uint32_t orig_type, uint32_t new_type);
 extern int e820_add_range(uint64_t s, uint64_t e, uint32_t type);
 extern unsigned long init_e820(const char *str, struct e820map *raw);
