ccf400846780289ae779c62ef0c94757ff43bb60
Jan Beulich <jbeulich@suse.com>
2024-12-04 08:52:37 +0100

    update Xen version to 4.19.1

--- Config.mk
+++ Config.mk
@@ -221,10 +221,10 @@ OVMF_UPSTREAM_URL ?= https://xenbits.xen.org/git-http/ovmf.git
 OVMF_UPSTREAM_REVISION ?= ba91d0292e593df8528b66f99c1b0b14fadc8e16
 
 QEMU_UPSTREAM_URL ?= https://xenbits.xen.org/git-http/qemu-xen.git
-QEMU_UPSTREAM_REVISION ?= qemu-xen-4.19.0
+QEMU_UPSTREAM_REVISION ?= qemu-xen-4.19.1
 
 MINIOS_UPSTREAM_URL ?= https://xenbits.xen.org/git-http/mini-os.git
-MINIOS_UPSTREAM_REVISION ?= a400dd51706867565ed1382b23d3475bb30668c2
+MINIOS_UPSTREAM_REVISION ?= xen-RELEASE-4.19.1
 
 SEABIOS_UPSTREAM_URL ?= https://xenbits.xen.org/git-http/seabios.git
 SEABIOS_UPSTREAM_REVISION ?= rel-1.16.3
