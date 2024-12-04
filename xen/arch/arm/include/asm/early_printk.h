214f9cc67b8cd179df0f5ab460413e9be46ff148
Ayan Kumar Halder <ayan.kumar.halder@amd.com>
2024-12-04 17:22:42 +0000

    xen/mpu: Map early uart when earlyprintk on
    CONFIG_EARLY_UART_SIZE is introduced to let user provide physical size of
    early UART. Unlike MMU where we map a page in the virtual address space,
    here we need to know the exact physical size to be mapped.
    As VA == PA in case of MPU, the memory layout follows exactly the hardware
    configuration. As a consequence, we set  EARLY_UART_VIRTUAL_ADDRESS as physical
    address.
    
    EARLY_UART_BASE_ADDRESS and EARLY_UART_SIZE should be aligned to the minimum
    size of MPU region (ie 64 bits) as per the hardware restrictions. Refer ARM
    DDI 0600A.d ID120821 A1.3 "A minimum protection region size of 64 bytes.".
    
    UART is mapped as nGnRE region (as specified by ATTR=100 , refer G1.3.13,
    MAIR_EL2, "---0100 Device memory nGnRE"). Also, it is mapped as outer shareable,
    RW at EL2 only and execution of instructions from the region is not permitted.
    
    Signed-off-by: Ayan Kumar Halder <ayan.kumar.halder@amd.com>
    Reviewed-by: Luca Fancellu <luca.fancellu@arm.com>
    Acked-by: Julien Grall <jgrall@amazon.com>

--- xen/arch/arm/include/asm/early_printk.h
+++ xen/arch/arm/include/asm/early_printk.h
@@ -11,9 +11,32 @@
 #define __ARM_EARLY_PRINTK_H__
 
 #include <xen/page-size.h>
+#include <asm/arm64/mpu.h>
 #include <asm/fixmap.h>
 
 #ifdef CONFIG_EARLY_PRINTK
+
+#if defined(CONFIG_MPU)
+
+/*
+ * For MPU systems, there is no VMSA support in EL2, so we use VA == PA
+ * for EARLY_UART_VIRTUAL_ADDRESS.
+ */
+#define EARLY_UART_VIRTUAL_ADDRESS CONFIG_EARLY_UART_BASE_ADDRESS
+
+/*
+ * User-defined EARLY_UART_BASE_ADDRESS and EARLY_UART_SIZE must be aligned to
+ * minimum size of MPU region.
+ */
+#if (CONFIG_EARLY_UART_BASE_ADDRESS % MPU_REGION_ALIGN) != 0
+#error "EARLY_UART_BASE_ADDRESS must be aligned to minimum MPU region size"
+#endif
+
+#if (CONFIG_EARLY_UART_SIZE % MPU_REGION_ALIGN) != 0
+#error "EARLY_UART_SIZE must be aligned to minimum MPU region size"
+#endif
+
+#elif defined(CONFIG_MMU)
 
 /* need to add the uart address offset in page to the fixmap address */
 #define EARLY_UART_VIRTUAL_ADDRESS \
@@ -22,6 +45,9 @@
 #define TEMPORARY_EARLY_UART_VIRTUAL_ADDRESS \
     (TEMPORARY_FIXMAP_ADDR(FIX_CONSOLE) + (CONFIG_EARLY_UART_BASE_ADDRESS & ~PAGE_MASK))
 
-#endif /* !CONFIG_EARLY_PRINTK */
+#else
+#error "Unknown Memory management system"
+#endif
 
+#endif /* !CONFIG_EARLY_PRINTK */
 #endif
