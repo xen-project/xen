7fa1411676150634b1d6ca030e53b94c26a949dd
Michal Orzel <michal.orzel@amd.com>
2025-01-07 10:27:18 +0100

    xen/flask: Wire up XEN_DOMCTL_dt_overlay
    Addition of FLASK permission for this hypercall was overlooked in the
    original patch. Fix it. The only dt overlay operation is attaching that can
    happen only after the domain is created. Dom0 can attach overlay to itself
    as well.
    
    Fixes: 4c733873b5c2 ("xen/arm: Add XEN_DOMCTL_dt_overlay and device attachment to domains")
    Signed-off-by: Michal Orzel <michal.orzel@amd.com>
    Release-Acked-By: Oleksii Kurochko <oleksii.kurochko@gmail.com>
    Acked-by: Daniel P. Smith <dpsmith@apertussolutions.com>

--- xen/xsm/flask/hooks.c
+++ xen/xsm/flask/hooks.c
@@ -844,6 +844,9 @@ static int cf_check flask_domctl(struct domain *d, unsigned int cmd,
     case XEN_DOMCTL_set_paging_mempool_size:
         return current_has_perm(d, SECCLASS_DOMAIN, DOMAIN__SETPAGINGMEMPOOL);
 
+    case XEN_DOMCTL_dt_overlay:
+        return current_has_perm(d, SECCLASS_DOMAIN2, DOMAIN2__DT_OVERLAY);
+
     default:
         return avc_unknown_permission("domctl", cmd);
     }
