f79af67982ef26e0395d81baff39d10705dedcda
Yann Dirson <yann.dirson@vates.tech>
2025-01-15 12:27:56 +0000

    docs/sphinx: import sys for error reporting
    Signed-off-by: Yann Dirson <yann.dirson@vates.tech>
    Acked-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,8 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import sys
+
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -13,7 +15,6 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 # import os
-# import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
 
