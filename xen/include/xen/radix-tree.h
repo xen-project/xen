xen-project/xen

commit b004883e29bbedb6c9f01a5e926e90f3effe28fd

Simplify and build-fix (for some gcc versions) radix_tree_int_to_ptr().
Signed-off-by: Keir Fraser <keir@xen.org>

xen/include/xen/radix-tree.h
@@ -181,8 +181,9 @@ static inline void radix_tree_replace_slot(void **pslot, void *item)
  */
 static inline void *radix_tree_int_to_ptr(int val)
 {
-    ASSERT((val <= (LONG_MAX >> 2)) && (val >= (LONG_MIN >> 2)));
-    return (void *)(((long)val << 2) | 0x2ul);
+    long _ptr = ((long)val << 2) | 0x2l;
+    ASSERT((_ptr >> 2) == val);
+    return (void *)_ptr;
 }
 
 static inline int radix_tree_ptr_to_int(void *ptr)

commit 61ee1a8151fc55113d50064e0cc825a55b83160c

radix-tree: don't left-shift negative values
Any (signed) integer is okay to pass into radix_tree_int_to_ptr(), yet
left shifting negative values is UB. Use an unsigned intermediate type,
reducing the impact to implementation defined behavior (for the
unsigned->signed conversion).

Also please Misra C:2012 rule 7.3 by dropping the lower case numeric 'l'
tag.

No difference in generated code, at least on x86.

Fixes: b004883e29bb ("Simplify and build-fix (for some gcc versions) radix_tree_int_to_ptr()")
Reported-by: Teddy Astie <teddy.astie@vates.tech>
Signed-off-by: Jan Beulich <jbeulich@suse.com>
Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>

xen/include/xen/radix-tree.h
@@ -172,7 +172,7 @@ static inline void radix_tree_replace_slot(void **pslot, void *item)
  */
 static inline void *radix_tree_int_to_ptr(int val)
 {
-    long _ptr = ((long)val << 2) | 0x2l;
+    long _ptr = ((unsigned long)val << 2) | 2;
     ASSERT((_ptr >> 2) == val);
     return (void *)_ptr;
 }