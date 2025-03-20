408935872ae7a5348e954b2e6b713b32e652ca08
Roger Pau Monné <roger.pau@citrix.com>
2025-03-20 12:54:16 +0100

    x86/ioremap: prevent additions against the NULL pointer
    This was reported by clang UBSAN as:
    
    UBSAN: Undefined behaviour in arch/x86/mm.c:6297:40
    applying zero offset to null pointer
    [...]
    Xen call trace:
        [<ffff82d040303662>] R common/ubsan/ubsan.c#ubsan_epilogue+0xa/0xc0
        [<ffff82d040304aa3>] F __ubsan_handle_pointer_overflow+0xcb/0x100
        [<ffff82d0406ebbc0>] F ioremap_wc+0xc8/0xe0
        [<ffff82d0406c3728>] F video_init+0xd0/0x180
        [<ffff82d0406ab6f5>] F console_init_preirq+0x3d/0x220
        [<ffff82d0406f1876>] F __start_xen+0x68e/0x5530
        [<ffff82d04020482e>] F __high_start+0x8e/0x90
    
    Fix bt_ioremap() and ioremap{,_wc}() to not add the offset if the returned
    pointer from __vmap() is NULL.
    
    Fixes: d0d4635d034f ('implement vmap()')
    Fixes: f390941a92f1 ('x86/DMI: fix table mapping when one lives above 1Mb')
    Fixes: 81d195c6c0e2 ('x86: introduce ioremap_wc()')
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>
    master commit: 9a6f2c52f75781acda39fab5cc96d1bcc54bf534
    master date: 2025-03-17 13:33:29 +0100

--- xen/arch/x86/mm.c
+++ xen/arch/x86/mm.c
@@ -6274,7 +6274,9 @@ void __iomem *ioremap(paddr_t pa, size_t len)
         unsigned int offs = pa & (PAGE_SIZE - 1);
         unsigned int nr = PFN_UP(offs + len);
 
-        va = __vmap(&mfn, nr, 1, 1, PAGE_HYPERVISOR_UCMINUS, VMAP_DEFAULT) + offs;
+        va = __vmap(&mfn, nr, 1, 1, PAGE_HYPERVISOR_UCMINUS, VMAP_DEFAULT);
+        if ( va )
+            va += offs;
     }
 
     return (void __force __iomem *)va;
@@ -6291,7 +6293,7 @@ void __iomem *__init ioremap_wc(paddr_t pa, size_t len)
 
     va = __vmap(&mfn, nr, 1, 1, PAGE_HYPERVISOR_WC, VMAP_DEFAULT);
 
-    return (void __force __iomem *)(va + offs);
+    return (void __force __iomem *)(va ? va + offs : NULL);
 }
 
 int create_perdomain_mapping(struct domain *d, unsigned long va,
