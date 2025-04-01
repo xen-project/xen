61e10fc28ccddff7c72c14acec56dc7ef2b155d1
Andrew Cooper <andrew.cooper3@citrix.com>
2025-04-01 14:59:01 +0100

    x86/idle: Move monitor()/mwait() wrappers into cpu-idle.c
    They're not used by any other translation unit, so shouldn't live in
    asm/processor.h, which is included almost everywhere.
    
    Our new toolchain baseline knows the MONITOR/MWAIT instructions, so use them
    directly rather than using raw hex.
    
    Change the hint/extention parameters from long to int.  They're specified to
    remain 32bit operands even 64-bit mode.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>

--- xen/arch/x86/include/asm/processor.h
+++ xen/arch/x86/include/asm/processor.h
@@ -315,23 +315,6 @@ static always_inline void set_in_cr4 (unsigned long mask)
         cr4_pv32_mask |= (mask & XEN_CR4_PV32_BITS);
 }
 
-static always_inline void __monitor(const void *eax, unsigned long ecx,
-                                    unsigned long edx)
-{
-    /* "monitor %eax,%ecx,%edx;" */
-    asm volatile (
-        ".byte 0x0f,0x01,0xc8;"
-        : : "a" (eax), "c" (ecx), "d"(edx) );
-}
-
-static always_inline void __mwait(unsigned long eax, unsigned long ecx)
-{
-    /* "mwait %eax,%ecx;" */
-    asm volatile (
-        ".byte 0x0f,0x01,0xc9;"
-        : : "a" (eax), "c" (ecx) );
-}
-
 #define IOBMP_BYTES             8192
 #define IOBMP_INVALID_OFFSET    0x8000
 
