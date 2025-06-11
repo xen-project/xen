xen-project/xen

commit c06a7db0c547b6c5280327f0856019aeb52a7c70

X86 and IA64: Update cpufreq statistic logic for supporting both x86 and ia64
Signed-off-by: Yu, Ke <ke.yu@intel.com>
Signed-off-by: Liu, Jinsong <jinsong.liu@intel.com>

xen/drivers/acpi/pmstat.c
@@ -126,6 +126,7 @@ int do_get_pm_info(struct xen_sysctl_get_pmstat *op)
         break;
     }
 
+#ifdef CONFIG_X86
     case PMSTAT_get_max_cx:
     {
         op->u.getcx.nr = pmstat_get_cx_nr(op->cpuid);
@@ -144,6 +145,7 @@ int do_get_pm_info(struct xen_sysctl_get_pmstat *op)
         ret = pmstat_reset_cx_stat(op->cpuid);
         break;
     }
+#endif
 
     default:
         printk("not defined sub-hypercall @ do_get_pm_info\n");

tools/libxc/xc_pm.c
@@ -66,6 +66,7 @@ int xc_pm_get_pxstat(int xc_handle, int cpuid, struct xc_px_stat *pxpt)
     sysctl.cmd = XEN_SYSCTL_get_pmstat;
     sysctl.u.get_pmstat.type = PMSTAT_get_pxstat;
     sysctl.u.get_pmstat.cpuid = cpuid;
+    sysctl.u.get_pmstat.u.getpx.total = max_px;
     set_xen_guest_handle(sysctl.u.get_pmstat.u.getpx.trans_pt, pxpt->trans_pt);
     set_xen_guest_handle(sysctl.u.get_pmstat.u.getpx.pt, 
                         (pm_px_val_t *)pxpt->pt);

xen/arch/ia64/xen/dom0_ops.c
@@ -459,16 +459,6 @@ long arch_do_sysctl(xen_sysctl_t *op, XEN_GUEST_HANDLE(xen_sysctl_t) u_sysctl)
     return ret;
 }
 
-int
-do_get_pm_info(struct xen_sysctl_get_pmstat *op)
-{
-    /*
-     * For now just place holder to compile.
-     * xen/common/sysctl.c refers this function.
-     */
-    return -ENOSYS;
-}
-
 static unsigned long
 dom0vp_ioremap(struct domain *d, unsigned long mpaddr, unsigned long size)
 {

xen/arch/x86/acpi/cpufreq/cpufreq.c
@@ -307,6 +307,7 @@ static int acpi_cpufreq_target(struct cpufreq_policy *policy,
     struct drv_cmd cmd;
     unsigned int next_state = 0; /* Index into freq_table */
     unsigned int next_perf_state = 0; /* Index into perf table */
+    unsigned int j;
     int result = 0;
 
     if (unlikely(data == NULL ||
@@ -369,7 +370,8 @@ static int acpi_cpufreq_target(struct cpufreq_policy *policy,
     if (!check_freqs(cmd.mask, freqs.new, data))
         return -EAGAIN;
 
-    cpufreq_statistic_update(cmd.mask, perf->state, next_perf_state);
+    for_each_cpu_mask(j, cmd.mask)
+        cpufreq_statistic_update(j, perf->state, next_perf_state);
 
     perf->state = next_perf_state;
     policy->cur = freqs.new;

xen/drivers/cpufreq/utility.c
@@ -39,36 +39,31 @@ struct cpufreq_policy   *__read_mostly cpufreq_cpu_policy[NR_CPUS];
  *                    Px STATISTIC INFO                              *
  *********************************************************************/
 
-void cpufreq_statistic_update(cpumask_t cpumask, uint8_t from, uint8_t to)
+void cpufreq_statistic_update(unsigned int cpu, uint8_t from, uint8_t to)
 {
-    uint32_t i;
     uint64_t now;
+    struct pm_px *pxpt = cpufreq_statistic_data[cpu];
+    struct processor_pminfo *pmpt = processor_pminfo[cpu];
+    uint64_t total_idle_ns;
+    uint64_t tmp_idle_ns;
+
+    if ( !pxpt || !pmpt )
+        return;
 
     now = NOW();
+    total_idle_ns = get_cpu_idle_time(cpu);
+    tmp_idle_ns = total_idle_ns - pxpt->prev_idle_wall;
 
-    for_each_cpu_mask(i, cpumask) {
-        struct pm_px *pxpt = cpufreq_statistic_data[i];
-        struct processor_pminfo *pmpt = processor_pminfo[i];
-        uint64_t total_idle_ns;
-        uint64_t tmp_idle_ns;
+    pxpt->u.last = from;
+    pxpt->u.cur = to;
+    pxpt->u.pt[to].count++;
+    pxpt->u.pt[from].residency += now - pxpt->prev_state_wall;
+    pxpt->u.pt[from].residency -= tmp_idle_ns;
 
-        if ( !pxpt || !pmpt )
-            continue;
+    (*(pxpt->u.trans_pt + from * pmpt->perf.state_count + to))++;
 
-        total_idle_ns = get_cpu_idle_time(i);
-        tmp_idle_ns = total_idle_ns - pxpt->prev_idle_wall;
-
-        pxpt->u.last = from;
-        pxpt->u.cur = to;
-        pxpt->u.pt[to].count++;
-        pxpt->u.pt[from].residency += now - pxpt->prev_state_wall;
-        pxpt->u.pt[from].residency -= tmp_idle_ns;
-
-        (*(pxpt->u.trans_pt + from * pmpt->perf.state_count + to))++;
-
-        pxpt->prev_state_wall = now;
-        pxpt->prev_idle_wall = total_idle_ns;
-    }
+    pxpt->prev_state_wall = now;
+    pxpt->prev_idle_wall = total_idle_ns;
 }
 
 int cpufreq_statistic_init(unsigned int cpuid)

xen/include/acpi/cpufreq/processor_perf.h
@@ -9,7 +9,7 @@
 int get_cpu_id(u8);
 int powernow_cpufreq_init(void);
 
-void cpufreq_statistic_update(cpumask_t, uint8_t, uint8_t);
+void cpufreq_statistic_update(unsigned int, uint8_t, uint8_t);
 int  cpufreq_statistic_init(unsigned int);
 void cpufreq_statistic_exit(unsigned int);
 void cpufreq_statistic_reset(unsigned int);

commit aa70996a6896dfc9de60f60540098b7d3ac3fb4f

x86/pmstat: Check size of PMSTAT_get_pxstat buffers
Check that the total number of states passed in and hence the size of
buffers is sufficient to avoid writing more than the caller has
allocated.

The interface is not explicit about whether getpx.total is expected to
be set by the caller in this case but since it is always set in
libxenctrl it seems reasonable to check it and make it explicit.

Fixes: c06a7db0c547 ("X86 and IA64: Update cpufreq statistic logic for supporting both x86 and ia64")
Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>
Reviewed-by: Jan Beulich <jbeulich@suse.com>

xen/drivers/acpi/pmstat.c
@@ -103,8 +103,11 @@ int do_get_pm_info(struct xen_sysctl_get_pmstat *op)
 
         cpufreq_residency_update(op->cpuid, pxpt->u.cur);
 
-        ct = pmpt->perf.state_count;
-        if ( copy_to_guest(op->u.getpx.trans_pt, pxpt->u.trans_pt, ct*ct) )
+        ct = min(pmpt->perf.state_count, op->u.getpx.total + 0U);
+
+        /* Avoid partial copying of 2-D array */
+        if ( ct == op->u.getpx.total &&
+             copy_to_guest(op->u.getpx.trans_pt, pxpt->u.trans_pt, ct * ct) )
         {
             spin_unlock(cpufreq_statistic_lock);
             ret = -EFAULT;

xen/include/public/sysctl.h
@@ -215,11 +215,22 @@ typedef struct pm_px_val pm_px_val_t;
 DEFINE_XEN_GUEST_HANDLE(pm_px_val_t);
 
 struct pm_px_stat {
-    uint8_t total;        /* total Px states */
+    /*
+     * IN: Number of elements in pt, number of rows/columns in trans_pt
+     *     (PMSTAT_get_pxstat)
+     * OUT: total Px states (PMSTAT_get_max_px, PMSTAT_get_pxstat)
+     */
+    uint8_t total;
     uint8_t usable;       /* usable Px states */
     uint8_t last;         /* last Px state */
     uint8_t cur;          /* current Px state */
-    XEN_GUEST_HANDLE_64(uint64) trans_pt;   /* Px transition table */
+    /*
+     * OUT: Px transition table. This should have total * total elements.
+     *      As it is a 2-D array, this will not be copied if input total is
+     *      less than output total. (PMSTAT_get_pxstat)
+     */
+    XEN_GUEST_HANDLE_64(uint64) trans_pt;
+    /* OUT: This should have total elements (PMSTAT_get_pxstat) */
     XEN_GUEST_HANDLE_64(pm_px_val_t) pt;
 };
 