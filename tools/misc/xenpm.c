xen-project/xen

commit 81ce87fc5e36ddaf5e788c517771f2d708f3601c

xen/cpufreq: rename cppc preset name to "XEN_SYSCTL_CPPC_SET_PRESET_ONDEMAND"
"ondemand" is more suitable to describe a preset in which epp value is set
with medium(CPPC_ENERGY_PERF_BALANCE), showing no preference over performance or
powersave, minimum with lowest and maximum with highest.

Signed-off-by: Penny Zheng <Penny.Zheng@amd.com>
Acked-by: Jan Beulich <jbeulich@suse.com>

tools/misc/xenpm.c
@@ -1447,9 +1447,9 @@ static int parse_cppc_opts(xc_set_cppc_para_t *set_cppc, int *cpuid,
         set_cppc->set_params = XEN_SYSCTL_CPPC_SET_PRESET_PERFORMANCE;
         i++;
     }
-    else if ( strcasecmp(argv[i], "balance") == 0 )
+    else if ( strcasecmp(argv[i], "ondemand") == 0 )
     {
-        set_cppc->set_params = XEN_SYSCTL_CPPC_SET_PRESET_BALANCE;
+        set_cppc->set_params = XEN_SYSCTL_CPPC_SET_PRESET_ONDEMAND;
         i++;
     }
 

xen/arch/x86/acpi/cpufreq/hwp.c
@@ -610,7 +610,7 @@ int set_hwp_para(struct cpufreq_policy *policy,
         data->desired = 0;
         break;
 
-    case XEN_SYSCTL_CPPC_SET_PRESET_BALANCE:
+    case XEN_SYSCTL_CPPC_SET_PRESET_ONDEMAND:
         data->minimum = data->hw.lowest;
         data->maximum = data->hw.highest;
         data->activity_window = 0;

xen/include/public/sysctl.h
@@ -430,7 +430,7 @@ struct xen_set_cppc_para {
 #define XEN_SYSCTL_CPPC_SET_ACT_WINDOW           (1U << 4)
 #define XEN_SYSCTL_CPPC_SET_PRESET_MASK          0xf0000000U
 #define XEN_SYSCTL_CPPC_SET_PRESET_NONE          0x00000000U
-#define XEN_SYSCTL_CPPC_SET_PRESET_BALANCE       0x10000000U
+#define XEN_SYSCTL_CPPC_SET_PRESET_ONDEMAND      0x10000000U
 #define XEN_SYSCTL_CPPC_SET_PRESET_POWERSAVE     0x20000000U
 #define XEN_SYSCTL_CPPC_SET_PRESET_PERFORMANCE   0x30000000U
 #define XEN_SYSCTL_CPPC_SET_PARAM_MASK \

commit a6acdc8f661846063d7ff1e962fcb686c5db6b5a

tools: fix help info for "xenpm set-cpufreq-cppc"
Change "balance" to "ondemand" in help info for "xenpm set-cpufreq-cppc"

Fixes: 81ce87fc5e36 (xen/cpufreq: rename cppc preset name to "XEN_SYSCTL_CPPC_SET_PRESET_ONDEMAND")
Signed-off-by: Penny Zheng <Penny.Zheng@amd.com>
Acked-by: Jan Beulich <jbeulich@suse.com>

tools/misc/xenpm.c
@@ -69,7 +69,7 @@ void show_help(void)
             " set-max-cstate        <num>|'unlimited' [<num2>|'unlimited']\n"
             "                                     set the C-State limitation (<num> >= 0) and\n"
             "                                     optionally the C-sub-state limitation (<num2> >= 0)\n"
-            " set-cpufreq-cppc      [cpuid] [balance|performance|powersave] <param:val>*\n"
+            " set-cpufreq-cppc      [cpuid] [ondemand|performance|powersave] <param:val>*\n"
             "                                     set Hardware P-State (HWP) parameters\n"
             "                                     on CPU <cpuid> or all if omitted.\n"
             "                                     optionally a preset of one of:\n"