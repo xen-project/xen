16fae1561354f35dd524eb8953385d31eac3ce37
Victor Lira <victorm.lira@amd.com>
2025-09-11 12:32:47 +0200

    coverage: add missing include for macro
    The missing include prevents it from compiling when CONFIG_COVERAGE is not set
    and the header is included in a file that has not already included errno.h,
    causing EOPNOTSUPP to be undeclared.
    
    Add the missing include and a few blank lines.
    
    Signed-off-by: Victor Lira <victorm.lira@amd.com>
    Acked-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- xen/include/xen/coverage.h
+++ xen/include/xen/coverage.h
@@ -2,13 +2,18 @@
 #define _XEN_COV_H
 
 #ifdef CONFIG_COVERAGE
+
 #include <public/sysctl.h>
 int sysctl_cov_op(struct xen_sysctl_coverage_op *op);
+
 #else
+
+#include <xen/errno.h>
 static inline int sysctl_cov_op(void *unused)
 {
     return -EOPNOTSUPP;
 }
+
 #endif
 
 #endif	/* _XEN_GCOV_H */
