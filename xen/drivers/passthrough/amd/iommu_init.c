xen-project/xen

commit 487a1cffd71a34d729c05c9a379ff05ea39b9fe2

x86: Implement per-cpu vector for xen hypervisor
Since Xen and Linux has big differece in code base, it
is very hard to port Linux's patch and apply it to Xen
directly, so this patch only adopts core logic of Linux,
and make it work for Xen.

Key changes:
1. vector allocation algorithm
2. all IRQ chips' set_affinity logic
3. IRQ migration when cpu hot remove.
4. Break assumptions which depend on global vector policy.

Signed-off-by: Xiantao Zhang <xiantao.zhang@intel.com>

xen/drivers/passthrough/amd/iommu_init.c
@@ -26,6 +26,7 @@
 #include <asm/msi.h>
 #include <asm/hvm/svm/amd-iommu-proto.h>
 #include <asm-x86/fixmap.h>
+#include <mach_apic.h>
 
 static struct amd_iommu **irq_to_iommu;
 static int nr_amd_iommus;
@@ -303,40 +304,46 @@ static int amd_iommu_read_event_log(struct amd_iommu *iommu, u32 event[])
     return -EFAULT;
 }
 
-static void amd_iommu_msi_data_init(struct amd_iommu *iommu)
+static void iommu_msi_set_affinity(unsigned int irq, cpumask_t mask)
 {
-    u32 msi_data;
+    struct msi_msg msg;
+    unsigned int dest;
+    struct amd_iommu *iommu = irq_to_iommu[irq];
+    struct irq_desc *desc = irq_to_desc(irq);
+    struct irq_cfg *cfg = desc->chip_data;
     u8 bus = (iommu->bdf >> 8) & 0xff;
     u8 dev = PCI_SLOT(iommu->bdf & 0xff);
     u8 func = PCI_FUNC(iommu->bdf & 0xff);
-    int vector = irq_to_vector(iommu->irq);
 
-    msi_data = MSI_DATA_TRIGGER_EDGE |
-        MSI_DATA_LEVEL_ASSERT |
-        MSI_DATA_DELIVERY_FIXED |
-        MSI_DATA_VECTOR(vector);
+    dest = set_desc_affinity(desc, mask);
+    if (dest == BAD_APICID){
+        gdprintk(XENLOG_ERR, "Set iommu interrupt affinity error!\n");
+        return;
+    }
+
+    memset(&msg, 0, sizeof(msg)); 
+    msg.data = MSI_DATA_VECTOR(cfg->vector) & 0xff;
+    msg.data |= 1 << 14;
+    msg.data |= (INT_DELIVERY_MODE != dest_LowestPrio) ?
+        MSI_DATA_DELIVERY_FIXED:
+        MSI_DATA_DELIVERY_LOWPRI;
+
+    msg.address_hi =0;
+    msg.address_lo = (MSI_ADDRESS_HEADER << (MSI_ADDRESS_HEADER_SHIFT + 8)); 
+    msg.address_lo |= INT_DEST_MODE ? MSI_ADDR_DESTMODE_LOGIC:
+                    MSI_ADDR_DESTMODE_PHYS;
+    msg.address_lo |= (INT_DELIVERY_MODE != dest_LowestPrio) ?
+                    MSI_ADDR_REDIRECTION_CPU:
+                    MSI_ADDR_REDIRECTION_LOWPRI;
+    msg.address_lo |= MSI_ADDR_DEST_ID(dest & 0xff);
 
     pci_conf_write32(bus, dev, func,
-        iommu->msi_cap + PCI_MSI_DATA_64, msi_data);
-}
-
-static void amd_iommu_msi_addr_init(struct amd_iommu *iommu, int phy_cpu)
-{
-
-    int bus = (iommu->bdf >> 8) & 0xff;
-    int dev = PCI_SLOT(iommu->bdf & 0xff);
-    int func = PCI_FUNC(iommu->bdf & 0xff);
-
-    u32 address_hi = 0;
-    u32 address_lo = MSI_ADDR_HEADER |
-            MSI_ADDR_DESTMODE_PHYS |
-            MSI_ADDR_REDIRECTION_CPU |
-            MSI_ADDR_DEST_ID(phy_cpu);
-
+        iommu->msi_cap + PCI_MSI_DATA_64, msg.data);
     pci_conf_write32(bus, dev, func,
-        iommu->msi_cap + PCI_MSI_ADDRESS_LO, address_lo);
+        iommu->msi_cap + PCI_MSI_ADDRESS_LO, msg.address_lo);
     pci_conf_write32(bus, dev, func,
-        iommu->msi_cap + PCI_MSI_ADDRESS_HI, address_hi);
+        iommu->msi_cap + PCI_MSI_ADDRESS_HI, msg.address_hi);
+    
 }
 
 static void amd_iommu_msi_enable(struct amd_iommu *iommu, int flag)
@@ -373,6 +380,9 @@ static void iommu_msi_mask(unsigned int irq)
 {
     unsigned long flags;
     struct amd_iommu *iommu = irq_to_iommu[irq];
+    struct irq_desc *desc = irq_to_desc(irq);
+
+    irq_complete_move(&desc);
 
     /* FIXME: do not support mask bits at the moment */
     if ( iommu->maskbit )
@@ -395,11 +405,6 @@ static void iommu_msi_end(unsigned int irq)
     ack_APIC_irq();
 }
 
-static void iommu_msi_set_affinity(unsigned int irq, cpumask_t dest)
-{
-    struct amd_iommu *iommu = irq_to_iommu[irq];
-    amd_iommu_msi_addr_init(iommu, cpu_physical_id(first_cpu(dest)));
-}
 
 static struct hw_interrupt_type iommu_msi_type = {
     .typename = "AMD_IOV_MSI",
@@ -485,7 +490,7 @@ static int set_iommu_interrupt_handler(struct amd_iommu *iommu)
         gdprintk(XENLOG_ERR VTDPREFIX, "IOMMU: no irqs\n");
         return 0;
     }
-
+    
     irq_desc[irq].handler = &iommu_msi_type;
     irq_to_iommu[irq] = iommu;
     ret = request_irq(irq, amd_iommu_page_fault, 0,
@@ -524,8 +529,7 @@ void enable_iommu(struct amd_iommu *iommu)
     register_iommu_event_log_in_mmio_space(iommu);
     register_iommu_exclusion_range(iommu);
 
-    amd_iommu_msi_data_init (iommu);
-    amd_iommu_msi_addr_init(iommu, cpu_physical_id(first_cpu(cpu_online_map)));
+    iommu_msi_set_affinity(iommu->irq, cpu_online_map);
     amd_iommu_msi_enable(iommu, IOMMU_CONTROL_ENABLED);
 
     set_iommu_command_buffer_control(iommu, IOMMU_CONTROL_ENABLED);

xen/arch/x86/apic.c
@@ -70,7 +70,7 @@ int modern_apic(void)
  */
 void ack_bad_irq(unsigned int irq)
 {
-    printk("unexpected IRQ trap at vector %02x\n", irq);
+    printk("unexpected IRQ trap at irq %02x\n", irq);
     /*
      * Currently unexpected vectors happen only on SMP and APIC.
      * We _must_ ack these because every local APIC has only N
@@ -1197,9 +1197,11 @@ int reprogram_timer(s_time_t timeout)
 
 fastcall void smp_apic_timer_interrupt(struct cpu_user_regs * regs)
 {
+    struct cpu_user_regs *old_regs = set_irq_regs(regs);
     ack_APIC_irq();
     perfc_incr(apic_timer);
     raise_softirq(TIMER_SOFTIRQ);
+    set_irq_regs(old_regs);
 }
 
 /*
@@ -1208,6 +1210,7 @@ fastcall void smp_apic_timer_interrupt(struct cpu_user_regs * regs)
 fastcall void smp_spurious_interrupt(struct cpu_user_regs *regs)
 {
     unsigned long v;
+    struct cpu_user_regs *old_regs = set_irq_regs(regs);
 
     irq_enter();
     /*
@@ -1223,6 +1226,7 @@ fastcall void smp_spurious_interrupt(struct cpu_user_regs *regs)
     printk(KERN_INFO "spurious APIC interrupt on CPU#%d, should never happen.\n",
            smp_processor_id());
     irq_exit();
+    set_irq_regs(old_regs);
 }
 
 /*
@@ -1232,6 +1236,7 @@ fastcall void smp_spurious_interrupt(struct cpu_user_regs *regs)
 fastcall void smp_error_interrupt(struct cpu_user_regs *regs)
 {
     unsigned long v, v1;
+    struct cpu_user_regs *old_regs = set_irq_regs(regs);
 
     irq_enter();
     /* First tickle the hardware, only then report what went on. -- REW */
@@ -1254,6 +1259,7 @@ fastcall void smp_error_interrupt(struct cpu_user_regs *regs)
     printk (KERN_DEBUG "APIC error on CPU%d: %02lx(%02lx)\n",
             smp_processor_id(), v , v1);
     irq_exit();
+    set_irq_regs(old_regs);
 }
 
 /*
@@ -1262,8 +1268,10 @@ fastcall void smp_error_interrupt(struct cpu_user_regs *regs)
 
 fastcall void smp_pmu_apic_interrupt(struct cpu_user_regs *regs)
 {
+    struct cpu_user_regs *old_regs = set_irq_regs(regs);
     ack_APIC_irq();
     hvm_do_pmu_interrupt(regs);
+    set_irq_regs(old_regs);
 }
 
 /*

xen/arch/x86/cpu/mcheck/mce_intel.c
@@ -84,9 +84,11 @@ static void (*vendor_thermal_interrupt)(struct cpu_user_regs *regs)
 
 fastcall void smp_thermal_interrupt(struct cpu_user_regs *regs)
 {
+    struct cpu_user_regs *old_regs = set_irq_regs(regs);
     irq_enter();
     vendor_thermal_interrupt(regs);
     irq_exit();
+    set_irq_regs(old_regs);
 }
 
 /* P4/Xeon Thermal regulation detect and init */
@@ -964,6 +966,7 @@ fastcall void smp_cmci_interrupt(struct cpu_user_regs *regs)
 {
     mctelem_cookie_t mctc;
     struct mca_summary bs;
+    struct cpu_user_regs *old_regs = set_irq_regs(regs);
 
     ack_APIC_irq();
     irq_enter();
@@ -984,6 +987,7 @@ fastcall void smp_cmci_interrupt(struct cpu_user_regs *regs)
         mctelem_dismiss(mctc);
 
     irq_exit();
+    set_irq_regs(old_regs);
 }
 
 void mce_intel_feature_init(struct cpuinfo_x86 *c)

xen/arch/x86/genapic/delivery.c
@@ -29,13 +29,17 @@ void clustered_apic_check_flat(void)
 cpumask_t target_cpus_flat(void)
 {
 	return cpu_online_map;
+}
+
+cpumask_t vector_allocation_domain_flat(int cpu)
+{
+	return cpu_online_map;
 } 
 
 unsigned int cpu_mask_to_apicid_flat(cpumask_t cpumask)
 {
-	return cpus_addr(cpumask)[0];
+	return cpus_addr(cpumask)[0]&0xFF;
 }
-
 
 /*
  * PHYSICAL DELIVERY MODE (unicast to physical APIC IDs).
@@ -57,8 +61,12 @@ void clustered_apic_check_phys(void)
 
 cpumask_t target_cpus_phys(void)
 {
-	/* IRQs will get bound more accurately later. */
-	return cpumask_of_cpu(0);
+	return cpu_online_map;
+}
+
+cpumask_t vector_allocation_domain_phys(int cpu)
+{
+	return cpumask_of_cpu(cpu);
 }
 
 unsigned int cpu_mask_to_apicid_phys(cpumask_t cpumask)

xen/arch/x86/genapic/x2apic.c
@@ -47,8 +47,12 @@ void clustered_apic_check_x2apic(void)
 
 cpumask_t target_cpus_x2apic(void)
 {
-    /* Deliver interrupts only to CPU0 for now */
-    return cpumask_of_cpu(0);
+    return cpu_online_map;
+}
+
+cpumask_t vector_allocation_domain_x2apic(int cpu)
+{
+	return cpumask_of_cpu(cpu);
 }
 
 unsigned int cpu_mask_to_apicid_x2apic(cpumask_t cpumask)

xen/arch/x86/hpet.c
@@ -287,6 +287,9 @@ static void hpet_msi_shutdown(unsigned int irq)
 
 static void hpet_msi_ack(unsigned int irq)
 {
+    struct irq_desc *desc = irq_to_desc(irq);
+
+    irq_complete_move(&desc);
     ack_APIC_irq();
 }
 
@@ -298,24 +301,19 @@ static void hpet_msi_set_affinity(unsigned int irq, cpumask_t mask)
 {
     struct msi_msg msg;
     unsigned int dest;
-    cpumask_t tmp;
-    int vector = irq_to_vector(irq);
+    struct irq_desc * desc = irq_to_desc(irq);
+    struct irq_cfg *cfg= desc->chip_data;
 
-    cpus_and(tmp, mask, cpu_online_map);
-    if ( cpus_empty(tmp) )
-        mask = TARGET_CPUS;
+    dest = set_desc_affinity(desc, mask);
+    if (dest == BAD_APICID)
+        return;
 
-    dest = cpu_mask_to_apicid(mask);
-
-    hpet_msi_read(vector, &msg);
-
+    hpet_msi_read(irq, &msg);
     msg.data &= ~MSI_DATA_VECTOR_MASK;
-    msg.data |= MSI_DATA_VECTOR(vector);
+    msg.data |= MSI_DATA_VECTOR(cfg->vector);
     msg.address_lo &= ~MSI_ADDR_DEST_ID_MASK;
     msg.address_lo |= MSI_ADDR_DEST_ID(dest);
-
-    hpet_msi_write(vector, &msg);
-    irq_desc[irq].affinity = mask;
+    hpet_msi_write(irq, &msg);
 }
 
 /*

xen/arch/x86/hvm/vmx/vmx.c
@@ -2061,13 +2061,14 @@ static void vmx_do_extint(struct cpu_user_regs *regs)
 
     asmlinkage void do_IRQ(struct cpu_user_regs *);
     fastcall void smp_apic_timer_interrupt(struct cpu_user_regs *);
-    fastcall void smp_event_check_interrupt(void);
+    fastcall void smp_event_check_interrupt(struct cpu_user_regs *regs);
     fastcall void smp_invalidate_interrupt(void);
-    fastcall void smp_call_function_interrupt(void);
+    fastcall void smp_call_function_interrupt(struct cpu_user_regs *regs);
     fastcall void smp_spurious_interrupt(struct cpu_user_regs *regs);
     fastcall void smp_error_interrupt(struct cpu_user_regs *regs);
     fastcall void smp_pmu_apic_interrupt(struct cpu_user_regs *regs);
     fastcall void smp_cmci_interrupt(struct cpu_user_regs *regs);
+    fastcall void smp_irq_move_cleanup_interrupt(struct cpu_user_regs *regs);
 #ifdef CONFIG_X86_MCE_THERMAL
     fastcall void smp_thermal_interrupt(struct cpu_user_regs *regs);
 #endif
@@ -2080,17 +2081,20 @@ static void vmx_do_extint(struct cpu_user_regs *regs)
 
     switch ( vector )
     {
+    case IRQ_MOVE_CLEANUP_VECTOR:
+        smp_irq_move_cleanup_interrupt(regs);
+        break;
     case LOCAL_TIMER_VECTOR:
         smp_apic_timer_interrupt(regs);
         break;
     case EVENT_CHECK_VECTOR:
-        smp_event_check_interrupt();
+        smp_event_check_interrupt(regs);
         break;
     case INVALIDATE_TLB_VECTOR:
         smp_invalidate_interrupt();
         break;
     case CALL_FUNCTION_VECTOR:
-        smp_call_function_interrupt();
+        smp_call_function_interrupt(regs);
         break;
     case SPURIOUS_APIC_VECTOR:
         smp_spurious_interrupt(regs);

xen/arch/x86/i8259.c
@@ -58,6 +58,7 @@ BUILD_16_IRQS(0xc) BUILD_16_IRQS(0xd) BUILD_16_IRQS(0xe) BUILD_16_IRQS(0xf)
  * is no hardware IRQ pin equivalent for them, they are triggered
  * through the ICC by us (IPIs)
  */
+BUILD_SMP_INTERRUPT(irq_move_cleanup_interrupt,IRQ_MOVE_CLEANUP_VECTOR)
 BUILD_SMP_INTERRUPT(event_check_interrupt,EVENT_CHECK_VECTOR)
 BUILD_SMP_INTERRUPT(invalidate_interrupt,INVALIDATE_TLB_VECTOR)
 BUILD_SMP_INTERRUPT(call_function_interrupt,CALL_FUNCTION_VECTOR)
@@ -374,7 +375,7 @@ static struct irqaction cascade = { no_action, "cascade", NULL};
 
 void __init init_IRQ(void)
 {
-    int i, vector;
+    int vector, irq, cpu = smp_processor_id();
 
     init_bsp_APIC();
 
@@ -389,15 +390,17 @@ void __init init_IRQ(void)
         set_intr_gate(vector, interrupt[vector]);
     }
 
-    for ( i = 0; i < 16; i++ )
-    {
-        vector_irq[LEGACY_VECTOR(i)] = i;
-        irq_desc[i].handler = &i8259A_irq_type;
+    for (irq = 0; irq < 16; irq++) {
+        struct irq_desc *desc = irq_to_desc(irq);
+        struct irq_cfg *cfg = desc->chip_data;
+        
+        desc->handler = &i8259A_irq_type;
+        per_cpu(vector_irq, cpu)[FIRST_LEGACY_VECTOR + irq] = irq;
+        cfg->domain = cpumask_of_cpu(cpu);
+        cfg->vector = FIRST_LEGACY_VECTOR + irq;
     }
-
-    /* Never allocate the hypercall vector or Linux/BSD fast-trap vector. */
-    vector_irq[HYPERCALL_VECTOR] = NEVER_ASSIGN_IRQ;
-    vector_irq[0x80] = NEVER_ASSIGN_IRQ;
+    
+    per_cpu(vector_irq, cpu)[FIRST_HIPRIORITY_VECTOR] = 0;
 
     apic_intr_init();
 

commit aed6278b2a7faac1bacce2d22aad29bc463fa0c0

AMD/IOMMU: unshare IRQ .ack and .disable handlers
A .disable handler can't typically be re-used for .ack: The latter needs
to deal with IRQ migration, while the former shouldn't. Furthermore
invoking just irq_complete_move() isn't enough; one of
move_{native,masked}_irq() also needs invoking.

Fixes: 487a1cffd71a ("x86: Implement per-cpu vector for xen hypervisor")
Fixes: f821102450a1 ("x86: IRQ Migration logic enhancement")
Signed-off-by: Jan Beulich <jbeulich@suse.com>
Acked-by: Andrew Cooper <andrew.cooper3@citrix.com>
Reviewed-by: Roger Pau Monné <roger.pau@citrix.com>
Release-Acked-by: Oleksii Kurochko<oleksii.kurochko@gmail.com>

xen/drivers/passthrough/amd/iommu_init.c
@@ -428,8 +428,6 @@ static void cf_check iommu_msi_mask(struct irq_desc *desc)
     unsigned long flags;
     struct amd_iommu *iommu = desc->action->dev_id;
 
-    irq_complete_move(desc);
-
     spin_lock_irqsave(&iommu->lock, flags);
     amd_iommu_msi_enable(iommu, IOMMU_CONTROL_DISABLED);
     spin_unlock_irqrestore(&iommu->lock, flags);
@@ -440,6 +438,13 @@ static unsigned int cf_check iommu_msi_startup(struct irq_desc *desc)
 {
     iommu_msi_unmask(desc);
     return 0;
+}
+
+static void cf_check iommu_msi_ack(struct irq_desc *desc)
+{
+    irq_complete_move(desc);
+    iommu_msi_mask(desc);
+    move_masked_irq(desc);
 }
 
 static void cf_check iommu_msi_end(struct irq_desc *desc, u8 vector)
@@ -455,7 +460,7 @@ static hw_irq_controller iommu_msi_type = {
     .shutdown = iommu_msi_mask,
     .enable = iommu_msi_unmask,
     .disable = iommu_msi_mask,
-    .ack = iommu_msi_mask,
+    .ack = iommu_msi_ack,
     .end = iommu_msi_end,
     .set_affinity = set_msi_affinity,
 };