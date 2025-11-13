0ca7deb02da41e06422f258461509d4d26da42bd
Jan Beulich <jbeulich@suse.com>
2025-11-13 09:08:53 +0100

    x86: there's no use for -mpreferred-stack-boundary= when assembling
    This option is only relevant for C ones.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Acked-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- xen/arch/x86/Rules.mk
+++ xen/arch/x86/Rules.mk
@@ -7,4 +7,4 @@ else
 object_label_flags = '-D__OBJECT_LABEL__=$(subst /,$$,$(subst -,_,$@))'
 endif
 c_flags += $(object_label_flags) $(CFLAGS_stack_boundary)
-a_flags += $(object_label_flags) $(CFLAGS_stack_boundary)
+a_flags += $(object_label_flags)
