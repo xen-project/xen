a13ce6cee8eadb5620d2d2ffa620e95bb0dddcff
Roger Pau Monné <roger.pau@citrix.com>
2026-01-13 15:44:43 +0100

    x86/pod: fix decrease_reservation() clearing of M2P entries
    Add the missing index increase in the M2P clearing loop, otherwise the loop
    keeps pointlessly setting the same MFN entry repeatedly.  This seems to be
    an oversight from the change that introduced support to process high order
    pages in one go.
    
    Fixes: 3c352011c0d3 ("x86/PoD: shorten certain operations on higher order ranges")
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>
    master commit: 8e4d495cb8f70280482edae12b48f422b5715099
    master date: 2025-12-11 09:50:10 +0100

--- xen/arch/x86/mm/p2m-pod.c
+++ xen/arch/x86/mm/p2m-pod.c
@@ -655,7 +655,7 @@ decrease_reservation(struct domain *d, gfn_t gfn, unsigned int order)
             }
             p2m_tlb_flush_sync(p2m);
             for ( j = 0; j < n; ++j )
-                set_gpfn_from_mfn(mfn_x(mfn), INVALID_M2P_ENTRY);
+                set_gpfn_from_mfn(mfn_x(mfn) + j, INVALID_M2P_ENTRY);
             p2m_pod_cache_add(p2m, page, cur_order);
 
             ioreq_request_mapcache_invalidate(d);
