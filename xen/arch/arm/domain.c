b9f6a102f535b8444c3538fa99ccb0b12c3ab6b4
Michal Orzel <michal.orzel@amd.com>
2026-04-01 16:58:16 +0200

    xen/arm: vpci: Move content of xen/arch/arm/vpci.h to arch header
    Move domain_vpci_init() and domain_vpci_get_num_mmio_handlers()
    declarations from the private xen/arch/arm/vpci.h into the public
    xen/arch/arm/include/asm/vpci.h, and remove the now redundant
    private header.
    
    Update domain.c to include <asm/vpci.h> instead of the removed private
    header.
    
    This ensures the declarations are visible where the functions are
    defined, resolving a MISRA C Rule 8.4 violation.
    
    Signed-off-by: Michal Orzel <michal.orzel@amd.com>
    Reviewed-by: Luca Fancellu <luca.fancellu@arm.com>
    Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- xen/arch/arm/domain.c
+++ xen/arch/arm/domain.c
@@ -31,10 +31,10 @@
 #include <asm/tee/tee.h>
 #include <asm/vfp.h>
 #include <asm/vgic.h>
+#include <asm/vpci.h>
 #include <asm/vpsci.h>
 #include <asm/vtimer.h>
 
-#include "vpci.h"
 #include "vuart.h"
 
 DEFINE_PER_CPU(struct vcpu *, curr_vcpu);
--- xen/arch/arm/include/asm/vpci.h
+++ xen/arch/arm/include/asm/vpci.h
@@ -2,6 +2,9 @@
 #ifndef ARM_VPCI_H
 #define ARM_VPCI_H
 
+struct domain;
+
+#ifdef CONFIG_HAS_VPCI
 /* Arch-specific MSI data for vPCI. */
 struct vpci_arch_msi {
 };
@@ -9,5 +12,20 @@ struct vpci_arch_msi {
 /* Arch-specific MSI-X entry data for vPCI. */
 struct vpci_arch_msix_entry {
 };
+
+
+int domain_vpci_init(struct domain *d);
+unsigned int domain_vpci_get_num_mmio_handlers(struct domain *d);
+#else
+static inline int domain_vpci_init(struct domain *d)
+{
+    return 0;
+}
+
+static inline unsigned int domain_vpci_get_num_mmio_handlers(struct domain *d)
+{
+    return 0;
+}
+#endif /* CONFIG_HAS_VPCI */
 
 #endif /* ARM_VPCI_H */
