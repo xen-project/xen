cba8a584de171c8c4510709c2edc9f1cf86b21ab
Jan Beulich <jbeulich@suse.com>
2026-04-07 13:52:44 +0200

    llc-coloring: improve checking while parsing
    We can easily avoid the risk of wrapping UINT_MAX <-> 0 by applying a
    check against the compile-time-constant maximum number of colors.
    
    Additionally the overflow checks suffered from an off-by-1, as the parsed
    ranges are inclusive (e.g. end == start being possible, requiring 1 array
    slot, while availability of 0 slots was checked in that case).
    
    Fixes: 6cdea3444eaf ("xen/arm: add Dom0 cache coloring support")
    Reported-by: Mykola Kvach <xakep.amatop@gmail.com>
    Reported-by: Kamil Frankowicz <kamil.frankowicz@cert.pl>
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Acked-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- xen/common/llc-coloring.c
+++ xen/common/llc-coloring.c
@@ -76,8 +76,9 @@ static int __init parse_color_config(const char *buf, unsigned int colors[],
         else                /* Single value */
             end = start;
 
-        if ( start > end || (end - start) > (UINT_MAX - *num_colors) ||
-             (*num_colors + (end - start)) >= max_num_colors )
+        if ( end >= NR_LLC_COLORS || start > end ||
+             (end - start) >= (UINT_MAX - *num_colors) ||
+             (*num_colors + (end - start + 1)) >= max_num_colors )
             return -EINVAL;
 
         /* Colors are range checked in check_colors() */
