e701842f7dbfa404dfc22732c69604d08161e19b
Jan Beulich <jbeulich@suse.com>
2026-06-04 21:39:31 +0100

    domctl: handle XEN_DOMCTL_memory_mapping without acquiring domctl lock
    With dedicated locking added, the domctl lock isn't required here anymore.
    Move the re-purposed dedicated XSM check as early as possible.
    
    Minimal "modernization": Switch "add" to bool and use %pd in log messages.
    
    This is part of XSA-492.
    
    Fixes: fda49f9b3fbb ("Add build option to allow more hypercalls from stubdoms")
    Reported-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Acked-by: Daniel P. Smith <dpsmith@apertussolutions.com>
    Reviewed-by: Roger Pau Monné <roger.pau@citrix.com>
    (cherry picked from commit 5a81fb873cb0c7913995372d22660d6a2db0ec48)

--- xen/xsm/flask/hooks.c
+++ xen/xsm/flask/hooks.c
@@ -682,6 +682,7 @@ static int cf_check flask_domctl(struct domain *d, unsigned int cmd,
 
     /* These have individual XSM hooks and don't make it here. */
     case XEN_DOMCTL_getdomaininfo:
+    case XEN_DOMCTL_memory_mapping:
         ASSERT_UNREACHABLE();
         return -EILSEQ;
 
@@ -689,7 +690,6 @@ static int cf_check flask_domctl(struct domain *d, unsigned int cmd,
     case XEN_DOMCTL_scheduler_op:
     case XEN_DOMCTL_irq_permission:
     case XEN_DOMCTL_iomem_permission:
-    case XEN_DOMCTL_memory_mapping:
     case XEN_DOMCTL_set_target:
     case XEN_DOMCTL_vm_event_op:
 
