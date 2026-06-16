6ba5e9c7b887d85c7e7f385892d7edbf2573a279
Andrew Cooper <andrew.cooper3@citrix.com>
2026-06-16 11:37:44 +0100

    x86/ucode: Exclude Zen6 from entrysign digest checking
    There is a 3rd path which should have gained an is_zen6_uarch() check to
    exclude Zen6 from entrysign mitigations.
    
    Fixes: bd15fdedafb3 ("x86/ucode: Exclude Zen6 from entrysign mitigations")
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Reviewed-by: Teddy Astie <teddy.astie@vates.tech>
    Acked-by: Jan Beulich <jbeulich@suse.com>
    Release-Acked-by: Oleksii Kurochko <oleksii.kurochko@gmail.com>

--- xen/arch/x86/cpu/microcode/amd.c
+++ xen/arch/x86/cpu/microcode/amd.c
@@ -128,7 +128,8 @@ static bool check_digest(const struct container_microcode *mc)
      * the digest of the patch against a list of known provenance.
      */
     if ( boot_cpu_data.family < 0x17 || boot_cpu_data.family > 0x1a ||
-         entrysign_mitigated_in_firmware || !opt_digest_check )
+         is_zen6_uarch() || entrysign_mitigated_in_firmware ||
+         !opt_digest_check )
         return true;
 
     pd = bsearch(&patch->patch_id, patch_digests, ARRAY_SIZE(patch_digests),
