6e278ffe6207ed6da25eb11a0bf7c6707513e545
Jan Beulich <jbeulich@suse.com>
2026-07-20 16:42:22 +0100

    platform-op/XSM: move resource-{,un}plug-core checks
    Integrate the checking with flask_platform_op(); there never really was a
    need to defer these checks, as the sub-op has always been known to the
    function. As a positive side effect, permissions are then checked at the
    same early point with and without Flask.
    
    This is CVE-2026-62427 / part of XSA-499.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Reviewed-by: Roger Pau Monné <roger.pau@citrix.com>
    Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Acked-By: Daniel P. Smith <dpsmith@apertussolutions.com>
    (cherry picked from commit 4ef45cbe31427bb3cedb11be45c90b1706205ee9)

--- xen/arch/x86/platform_hypercall.c
+++ xen/arch/x86/platform_hypercall.c
@@ -669,10 +669,6 @@ ret_t do_platform_op(
     {
         int cpu = op->u.cpu_ol.cpuid;
 
-        ret = xsm_resource_plug_core(XSM_HOOK);
-        if ( ret )
-            break;
-
         if ( cpu >= nr_cpu_ids || !cpu_present(cpu) ||
              clocksource_is_tsc() )
         {
@@ -694,10 +690,6 @@ ret_t do_platform_op(
     case XENPF_cpu_offline:
     {
         int cpu = op->u.cpu_ol.cpuid;
-
-        ret = xsm_resource_unplug_core(XSM_HOOK);
-        if ( ret )
-            break;
 
         if ( cpu == 0 )
         {
@@ -723,20 +715,12 @@ ret_t do_platform_op(
     }
 
     case XENPF_cpu_hotadd:
-        ret = xsm_resource_plug_core(XSM_HOOK);
-        if ( ret )
-            break;
-
         ret = cpu_add(op->u.cpu_add.apic_id,
                       op->u.cpu_add.acpi_id,
                       op->u.cpu_add.pxm);
         break;
 
     case XENPF_mem_hotadd:
-        ret = xsm_resource_plug_core(XSM_HOOK);
-        if ( ret )
-            break;
-
         ret = memory_add(op->u.mem_add.spfn,
                       op->u.mem_add.epfn,
                       op->u.mem_add.pxm);
