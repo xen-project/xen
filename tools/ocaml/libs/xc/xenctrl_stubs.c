xen-project/xen

commit 9d683b5e375d360403c65ebb414f5edb63b0ed38

tools/ocaml: Expose arch_config in domaininfo
Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>

tools/ocaml/libs/xc/xenctrl_stubs.c
@@ -273,10 +273,10 @@ CAMLprim value stub_xc_domain_shutdown(value xch, value domid, value reason)
 static value alloc_domaininfo(xc_domaininfo_t * info)
 {
 	CAMLparam0();
-	CAMLlocal2(result, tmp);
+	CAMLlocal5(result, tmp, arch_config, x86_arch_config, emul_list);
 	int i;
 
-	result = caml_alloc_tuple(16);
+	result = caml_alloc_tuple(17);
 
 	Store_field(result,  0, Val_int(info->domain));
 	Store_field(result,  1, Val_bool(info->flags & XEN_DOMINF_dying));
@@ -301,6 +301,28 @@ static value alloc_domaininfo(xc_domaininfo_t * info)
 	}
 
 	Store_field(result, 15, tmp);
+
+	/* emulation_flags: x86_arch_emulation_flags list; */
+	tmp = emul_list = Val_emptylist;
+	for (i = 0; i < 10; i++) {
+		if ((info->arch_config.emulation_flags >> i) & 1) {
+			tmp = caml_alloc_small(2, Tag_cons);
+			Field(tmp, 0) = Val_int(i);
+			Field(tmp, 1) = emul_list;
+			emul_list = tmp;
+		}
+	}
+
+	/* xen_x86_arch_domainconfig */
+	x86_arch_config = caml_alloc_tuple(1);
+	Store_field(x86_arch_config, 0, emul_list);
+
+	/* arch_config: arch_domainconfig */
+	arch_config = caml_alloc_small(1, 1);
+
+	Store_field(arch_config, 0, x86_arch_config);
+
+	Store_field(result, 16, arch_config);
 
 	CAMLreturn(result);
 }

tools/ocaml/libs/xc/xenctrl.ml
@@ -28,6 +28,34 @@ type vcpuinfo =
 	cpumap: int32;
 }
 
+type xen_arm_arch_domainconfig =
+{
+	gic_version: int;
+	nr_spis: int;
+	clock_frequency: int32;
+}
+
+type x86_arch_emulation_flags =
+	| X86_EMU_LAPIC
+	| X86_EMU_HPET
+	| X86_EMU_PM
+	| X86_EMU_RTC
+	| X86_EMU_IOAPIC
+	| X86_EMU_PIC
+	| X86_EMU_VGA
+	| X86_EMU_IOMMU
+	| X86_EMU_PIT
+	| X86_EMU_USE_PIRQ
+
+type xen_x86_arch_domainconfig =
+{
+	emulation_flags: x86_arch_emulation_flags list;
+}
+
+type arch_domainconfig =
+	| ARM of xen_arm_arch_domainconfig
+	| X86 of xen_x86_arch_domainconfig
+
 type domaininfo =
 {
 	domid             : domid;
@@ -46,6 +74,7 @@ type domaininfo =
 	max_vcpu_id       : int;
 	ssidref           : int32;
 	handle            : int array;
+	arch_config       : arch_domainconfig;
 }
 
 type sched_control =

tools/ocaml/libs/xc/xenctrl.mli
@@ -22,6 +22,33 @@ type vcpuinfo = {
   cputime : int64;
   cpumap : int32;
 }
+
+type xen_arm_arch_domainconfig = {
+  gic_version: int;
+  nr_spis: int;
+  clock_frequency: int32;
+}
+
+type x86_arch_emulation_flags =
+  | X86_EMU_LAPIC
+  | X86_EMU_HPET
+  | X86_EMU_PM
+  | X86_EMU_RTC
+  | X86_EMU_IOAPIC
+  | X86_EMU_PIC
+  | X86_EMU_VGA
+  | X86_EMU_IOMMU
+  | X86_EMU_PIT
+  | X86_EMU_USE_PIRQ
+
+type xen_x86_arch_domainconfig = {
+  emulation_flags: x86_arch_emulation_flags list;
+}
+
+type arch_domainconfig =
+  | ARM of xen_arm_arch_domainconfig
+  | X86 of xen_x86_arch_domainconfig
+
 type domaininfo = {
   domid : domid;
   dying : bool;
@@ -39,6 +66,7 @@ type domaininfo = {
   max_vcpu_id : int;
   ssidref : int32;
   handle : int array;
+  arch_config : arch_domainconfig;
 }
 type sched_control = { weight : int; cap : int; }
 type physinfo_cap_flag = CAP_HVM | CAP_DirectIO

commit 01b4b39e2eaf2991024190adcc0e015b3d77ec41

tools/ocaml: Fill arch_config for ARM in domain_getinfo{,list}()
Add the missing ARM logic, populating xen_arm_arch_domainconfig
from the raw xc_domaininfo_t the same way the x86 branch does for
xen_x86_arch_domainconfig.

Fixes: 9d683b5e375d ("tools/ocaml: Expose arch_config in domaininfo")
Fixes: 81838c9067ab ("ocaml: fix arm build")
Signed-off-by: Julian Vetter <julian.vetter@vates.tech>
Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
Reviewed-by: Teddy Astie <teddy.astie@vates.tech>
Release-Acked-by: Oleksii Kurochko <oleksii.kurochko@gmail.com>

tools/ocaml/libs/xc/xenctrl_stubs.c
@@ -444,7 +444,19 @@ static value alloc_domaininfo(xc_domaininfo_t * info)
 
 	Store_field(result, 15, tmp);
 
-#if defined(__i386__) || defined(__x86_64__)
+#if defined(__arm__) || defined(__aarch64__)
+
+	tag = 0; /* tag ARM */
+
+	/* xen_arm_arch_domainconfig */
+	arch_config = caml_alloc_tuple(3);
+	Field(arch_config, 0) = Val_int(info->arch_config.gic_version);
+	Field(arch_config, 1) = Val_int(info->arch_config.nr_spis);
+
+	tmp = caml_copy_int32(info->arch_config.clock_frequency);
+	Field(arch_config, 2) = tmp;
+
+#elif defined(__i386__) || defined(__x86_64__)
 
 	tag = 1; /* tag x86 */
 