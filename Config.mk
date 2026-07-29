3e23992b765b27d5644e76f3c560ce02f84f2007
Andrew Cooper <andrew.cooper3@citrix.com>
2026-07-29 19:29:21 +0100

    Config.mk: Bump tags to final
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- Config.mk
+++ Config.mk
@@ -214,10 +214,10 @@ OVMF_UPSTREAM_URL ?= https://xenbits.xen.org/git-http/ovmf.git
 OVMF_UPSTREAM_REVISION ?= ba91d0292e593df8528b66f99c1b0b14fadc8e16
 
 QEMU_UPSTREAM_URL ?= https://xenbits.xen.org/git-http/qemu-xen.git
-QEMU_UPSTREAM_REVISION ?= 0edeb44c093bea39f0fe4d936ee363b99113ffe1
+QEMU_UPSTREAM_REVISION ?= qemu-xen-4.22.0
 
 MINIOS_UPSTREAM_URL ?= https://xenbits.xen.org/git-http/mini-os.git
-MINIOS_UPSTREAM_REVISION ?= b6f79f5f44cf69044079c042b88fe9d75367642e
+MINIOS_UPSTREAM_REVISION ?= xen-RELEASE-4.22.0
 
 SEABIOS_UPSTREAM_URL ?= https://xenbits.xen.org/git-http/seabios.git
 SEABIOS_UPSTREAM_REVISION ?= rel-1.17.0
