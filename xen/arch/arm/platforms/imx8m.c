420193f87cc70751aec0712cded82b44c66328d1
Wig Cheng <onlywig@gmail.com>
2026-08-19 23:28:21 +0800

    MAINTAINERS: add myself as reviewer of i.MX8M related patches
    I wrote the i.MX8M platform and UART support and can help review
    patches touching these areas.
    
    Signed-off-by: Wig Cheng <onlywig@gmail.com>
    Reviewed-by: Michal Orzel <michal.orzel@amd.com>

--- xen/arch/arm/platforms/imx8m.c
+++ xen/arch/arm/platforms/imx8m.c
@@ -111,14 +111,8 @@ static bool imx8m_smc(struct cpu_user_regs *regs)
          * NoC QoS priority setup.  Only i.MX8MQ issues this at boot;
          * i.MX8MP issues no NoC call, but the platform covers both.
          */
-        switch ( subfunction_id )
-        {
-        case IMX_SIP_NOC_SF_PRIORITY:
-            break;
-
-        default:
+        if ( subfunction_id != IMX_SIP_NOC_SF_PRIORITY )
             return false;
-        }
         break;
 
     default:
