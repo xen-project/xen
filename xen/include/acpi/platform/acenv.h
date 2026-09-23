a229e8a50f9d7b7840667a8a9e2b7ae038ca53ee
Jan Beulich <jbeulich@suse.com>
2026-09-23 09:04:41 +0200

    ACPI: prune undue ia64 leftover
    This undoes the part of 3b0246ea0347 ("Fix ia64 tools build") which was
    missed by e567964a54b8 ("tools: drop ia64 support").
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Acked-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- xen/include/acpi/platform/acenv.h
+++ xen/include/acpi/platform/acenv.h
@@ -136,9 +136,7 @@
 
 /*! [Begin] no source code translation */
 
-#if defined(__XEN_TOOLS__) && defined(__ia64__)
-#include "ac_ia64_tools.h"
-#elif 1 /*defined(_LINUX) || defined(__linux__)*/
+#if 1 /*defined(_LINUX) || defined(__linux__)*/
 #include "aclinux.h"
 
 #elif defined(_AED_EFI)
